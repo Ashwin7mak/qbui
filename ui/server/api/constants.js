e152a5ee8640095d406525cb825bdf94235d6ead
Mark Roper <mark_roper@intuit.com>
2015-04-14 12:05:07 -0400

    Implements code review feedback from @dhatch

--- ui/server/api/constants.js
+++ ui/server/api/constants.js
@@ -2,12 +2,12 @@
  * This module contains global shared constant values for the server
  */
 (function(){
-    module.exports = {
+    module.exports = Object.freeze({
         POST: 'POST',
         GET: 'GET',
         DELETE: 'DELETE',
         CONTENT_TYPE: 'Content-Type',
         APPLICATION_JSON: 'application/json',
         APPLICATION_XML: 'application/xml'
-    }
+    });
 }());
