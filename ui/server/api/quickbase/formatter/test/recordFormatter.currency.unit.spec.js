73b21f23130fab8b2cc4f8edfd93bbce0bed9e7f
Mark Roper <mark_roper@intuit.com>
2015-04-23 08:29:31 -0400

    Adds unit test cases for currency

--- ui/server/api/quickbase/formatter/test/recordFormatter.currency.unit.spec.js
+++ ui/server/api/quickbase/formatter/test/recordFormatter.currency.unit.spec.js
@@ -33,6 +33,7 @@ describe('Currency record formatter unit test', function () {
             }
         }];
 
+        //Default behavior
         var expectedDecimal_NoFlags = JSON.parse(JSON.stringify(defaultRecordExp));
         expectedDecimal_NoFlags[0][0].value = numberDecimalOnly;
         expectedDecimal_NoFlags[0][0].display = "$0.75";
@@ -45,10 +46,38 @@ describe('Currency record formatter unit test', function () {
         var rightOfSignExpected = JSON.parse(JSON.stringify(rightOfSignInput));
         rightOfSignExpected[0][0].display = '-$2.88';
 
+        //Right of the number
+        var rightFieldInfo = JSON.parse(JSON.stringify(noFlagsFieldInfo));
+        rightFieldInfo[0].clientSideAttributes.position = 'RIGHT';
+        var rightInput = JSON.parse(JSON.stringify(defaultRecordInput));
+        rightInput[0][0].value = -2.883;
+        var rightExpected = JSON.parse(JSON.stringify(rightOfSignInput));
+        rightExpected[0][0].display = '-2.88 $';
+
+        //Test for right of sign
+        var rightOfSignEuroFieldInfo = JSON.parse(JSON.stringify(noFlagsFieldInfo));
+        rightOfSignEuroFieldInfo[0].clientSideAttributes.position = 'RIGHT_OF_SIGN';
+        rightOfSignEuroFieldInfo[0].clientSideAttributes.symbol = '€';
+        var rightOfSignEuroInput = JSON.parse(JSON.stringify(defaultRecordInput));
+        rightOfSignEuroInput[0][0].value = -2.883;
+        var rightOfSignEuroExpected = JSON.parse(JSON.stringify(rightOfSignInput));
+        rightOfSignEuroExpected[0][0].display = '-€2.88';
+
+        //Right of the number
+        var rightEuroFieldInfo = JSON.parse(JSON.stringify(noFlagsFieldInfo));
+        rightEuroFieldInfo[0].clientSideAttributes.position = 'RIGHT';
+        rightEuroFieldInfo[0].clientSideAttributes.symbol = '€';
+        var rightEuroInput = JSON.parse(JSON.stringify(defaultRecordInput));
+        rightEuroInput[0][0].value = -2.883;
+        var rightEuroExpected = JSON.parse(JSON.stringify(rightOfSignInput));
+        rightEuroExpected[0][0].display = '-2.88 €';
 
         var cases =[
-            { message: "Currency - decimal with no format", records: recordInputDecimalOnly, fieldInfo: noFlagsFieldInfo, expectedRecords: expectedDecimal_NoFlags },
-            { message: "Currency - right of sign positioning of symbol", records: rightOfSignInput, fieldInfo: rightOfSignFieldInfo, expectedRecords: rightOfSignExpected }
+            { message: "Currency - decimal with no format -> symbol left", records: recordInputDecimalOnly, fieldInfo: noFlagsFieldInfo, expectedRecords: expectedDecimal_NoFlags },
+            { message: "Currency - right of sign positioning of symbol", records: rightOfSignInput, fieldInfo: rightOfSignFieldInfo, expectedRecords: rightOfSignExpected },
+            { message: "Currency - symbol to the right of the number", records: rightInput, fieldInfo: rightFieldInfo, expectedRecords: rightExpected },
+            { message: "Currency - euro symbol right of sign positioning", records: rightOfSignEuroInput, fieldInfo: rightOfSignEuroFieldInfo, expectedRecords: rightOfSignEuroExpected },
+            { message: "Currency - euro symbol to the right of the number", records: rightEuroInput, fieldInfo: rightEuroFieldInfo, expectedRecords: rightEuroExpected }
         ];
 
         return cases;
