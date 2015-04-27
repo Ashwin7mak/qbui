c67b8cff1e1b968d64c0c99b0739949175131924
disaman <david_isaman@intuit.com>
2015-04-27 14:42:01 -0400

    Initial UI unit and integration formatting test cases for email, text, and URL fields.

---
+++ ui/server/api/quickbase/formatter/test/recordFormatter.email.unit.spec.disabled.js
@@ -0,0 +1,166 @@
+'use strict';
+
+var should = require('should');
+var recordFormatter = require('./../recordFormatter')();
+var assert = require('assert');
+
+/**
+ * Unit tests for EmailAddress field formatting
+ */
+describe('Email address record formatter unit test', function () {
+
+    /**
+     * Helper method to generate random strings
+     */
+    function generateRandomString(size) {
+        var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+        var text = '';
+        for (var i = 0; i < size; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    }
+
+    /**
+     * DataProvider containing Records, FieldProperties and record display expectations EmailAddress fields
+     */
+    function emailDataProvider() {
+        var fullEmail = "firstName_lastName@quickbase.com";
+        var linkText = "some link text";
+
+        // Generate a maximum email address
+        // Maximum valid email length is 256 which equals -> firstName(100) + "_"(1) + lastName (100) + domain(55)
+        var domain = "@" + generateRandomString(50) + ".com";
+        var firstName = generateRandomString(100);
+        var lastName = generateRandomString(100);
+        var fullName = firstName + "_" + lastName;
+        var maxEmail =  fullName + domain;
+
+        /**
+         * FieldInfo and expectations for flag: no flags
+         */
+        var fieldInfo_NoFlags = [{
+            "id": 7,
+            "name": "email",
+            "type": "EMAIL_ADDRESS",
+            "clientSideAttributes": {
+            }
+        }];
+
+        // Various email address inputs
+        var inputEmailRecord =  [[{
+            "id": 7,
+            "value": fullEmail}]];
+        var expectedEmail_NoFlags = [[{
+            "id": 7,
+            "value": fullEmail,
+            "display": fullEmail}]];
+
+        var maxEmailRecord =  [[{
+            "id": 7,
+            "value": maxEmail}]];
+        var expectedMaxEmail_NoFlags = [[{
+            "id": 7,
+            "value": maxEmail,
+            "display": maxEmail}]];
+
+        /**
+         * FieldInfo and expectations for flag: entire email flag
+         */
+        var fieldInfo_EntireEmail = JSON.parse(JSON.stringify(fieldInfo_NoFlags));
+        fieldInfo_EntireEmail[0].clientSideAttributes.display = "entire";
+        var expectedEmail_EntireEmailFlag = JSON.parse(JSON.stringify(expectedEmail_NoFlags));
+        var expectedMaxEmail_EntireEmailFlag = JSON.parse(JSON.stringify(expectedMaxEmail_NoFlags));
+
+        /**
+         * FieldInfo and expectations for flag: before @ flag
+         */
+        var fieldInfo_BeforeAtSign = JSON.parse(JSON.stringify(fieldInfo_NoFlags));
+        fieldInfo_BeforeAtSign[0].clientSideAttributes.display = "before_atSign";
+        var expectedEmail_BeforeAtSignFlag = JSON.parse(JSON.stringify(expectedEmail_NoFlags));
+        expectedEmail_BeforeAtSignFlag[0].display = "firstName_lastName";
+        var expectedMaxEmail_BeforeAtSignFlag = JSON.parse(JSON.stringify(expectedMaxEmail_NoFlags));
+        expectedMaxEmail_BeforeAtSignFlag[0].display = fullName;
+
+        /**
+         * FieldInfo and expectations for flag: before first "_" flag
+         */
+        var fieldInfo_BeforeUnderscore = JSON.parse(JSON.stringify(fieldInfo_NoFlags));
+        fieldInfo_BeforeUnderscore[0].clientSideAttributes.display = "before_underscore";
+        var expectedEmail_BeforeUnderscoreFlag = JSON.parse(JSON.stringify(expectedEmail_NoFlags));
+        expectedEmail_BeforeUnderscoreFlag[0].display = "firstName";
+        var expectedMaxEmail_BeforeUnderscoreFlag = JSON.parse(JSON.stringify(expectedMaxEmail_NoFlags));
+        expectedMaxEmail_BeforeUnderscoreFlag[0].display = firstName;
+
+        /**
+         * FieldInfo and expectations for flag: link text flag
+         */
+        var fieldInfo_LinkText = JSON.parse(JSON.stringify(fieldInfo_NoFlags));
+        fieldInfo_LinkText[0].clientSideAttributes.linkText = linkText;
+        var expectedEmail_LinkTextFlag = JSON.parse(JSON.stringify(expectedEmail_NoFlags));
+        expectedEmail_LinkTextFlag[0].display = linkText;
+        var expectedMaxEmail_LinkTextFlag = JSON.parse(JSON.stringify(expectedMaxEmail_NoFlags));
+        expectedMaxEmail_LinkTextFlag[0].display = linkText;
+
+        /**
+         * FieldInfo and expectations for flag: all flags
+         */
+        var fieldInfo_AllFlags = JSON.parse(JSON.stringify(fieldInfo_NoFlags));
+        fieldInfo_AllFlags[0].clientSideAttributes.display = "entire";
+        fieldInfo_AllFlags[0].clientSideAttributes.linkText = linkText;
+        var expectedEmail_AllFlags = JSON.parse(JSON.stringify(expectedEmail_NoFlags));
+        expectedEmail_AllFlags[0].display = linkText;
+        var expectedMaxEmail_AllFlags = JSON.parse(JSON.stringify(expectedMaxEmail_NoFlags));
+        expectedMaxEmail_AllFlags[0].display = linkText;
+
+        var recordsNull = JSON.parse(JSON.stringify(inputEmailRecord));
+        recordsNull[0][0].value = null;
+        var expectedNull = JSON.parse(JSON.stringify(expectedEmail_NoFlags));
+        expectedNull[0][0].display = "";
+        expectedNull[0][0].value = null;
+
+        var recordsEmpty = JSON.parse(JSON.stringify(inputEmailRecord));
+        recordsEmpty[0][0].value = "";
+        var expectedEmpty = JSON.parse(JSON.stringify(expectedEmail_NoFlags));
+        expectedEmpty[0][0].display = "";
+        expectedEmpty[0][0].value = "";
+
+        var cases =[
+            // Standard email
+            { message: "Email - standard email with no flags", records: inputEmailRecord, fieldInfo: fieldInfo_NoFlags, expectedRecords: expectedEmail_NoFlags },
+            { message: "Email - standard email with 'entire email' flag", records: inputEmailRecord, fieldInfo: fieldInfo_EntireEmail, expectedRecords: expectedEmail_EntireEmailFlag },
+            { message: "Email - standard email with 'before @' flag", records: inputEmailRecord, fieldInfo: fieldInfo_BeforeAtSign, expectedRecords: expectedEmail_BeforeAtSignFlag },
+            { message: "Email - standard email with 'before _' flag", records: inputEmailRecord, fieldInfo: fieldInfo_BeforeUnderscore, expectedRecords: expectedEmail_BeforeUnderscoreFlag },
+            { message: "Email - standard email with link text flag", records: inputEmailRecord, fieldInfo: fieldInfo_LinkText, expectedRecords: expectedEmail_LinkTextFlag },
+            { message: "Email - standard email with all flags", records: inputEmailRecord, fieldInfo: fieldInfo_AllFlags, expectedRecords: expectedEmail_AllFlags },
+
+            // Maximum valid email
+            { message: "Email - maximum email with no flags", records: maxEmailRecord, fieldInfo: fieldInfo_NoFlags, expectedRecords: expectedMaxEmail_NoFlags },
+            { message: "Email - maximum email with 'entire email' flag", records: maxEmailRecord, fieldInfo: fieldInfo_EntireEmail, expectedRecords: expectedMaxEmail_EntireEmailFlag },
+            { message: "Email - maximum email with 'before @' flag", records: maxEmailRecord, fieldInfo: fieldInfo_BeforeAtSign, expectedRecords: expectedMaxEmail_BeforeAtSignFlag },
+            { message: "Email - maximum email with 'before _' flag", records: maxEmailRecord, fieldInfo: fieldInfo_BeforeUnderscore, expectedRecords: expectedMaxEmail_BeforeUnderscoreFlag },
+            { message: "Email - maximum email with link text flag", records: maxEmailRecord, fieldInfo: fieldInfo_LinkText, expectedRecords: expectedMaxEmail_LinkTextFlag },
+            { message: "Email - maximum email with all flags", records: maxEmailRecord, fieldInfo: fieldInfo_AllFlags, expectedRecords: expectedMaxEmail_AllFlags },
+
+
+            { message: "Email - null -> empty string", records: recordsNull, fieldInfo: fieldInfo_NoFlags, expectedRecords: expectedNull },
+            { message: "Email - empty string -> empty string", records: recordsEmpty, fieldInfo: fieldInfo_NoFlags, expectedRecords: expectedEmpty },
+        ];
+
+        return cases;
+    }
+
+    /**
+     * Unit test that validates EmailAddress records formatting with various field property flags set
+     */
+    describe('should format an email address record with various properties for display',function(){
+        emailDataProvider().forEach(function(entry){
+            it('Test case: ' + entry.message, function (done) {
+                var formattedRecords = recordFormatter.formatRecords(entry.records, entry.fieldInfo);
+                //console.log('entry: ' + JSON.stringify(entry));
+                assert.deepEqual(formattedRecords, entry.expectedRecords, entry.message);
+                done();
+            });
+        });
+    });
+});
---
+++ ui/server/api/test/recordApi.text.integration.spec.disabled.js
@@ -0,0 +1,126 @@
+'use strict';
+var should = require('should');
+var assert = require('assert');
+var app = require('../../app');
+var config = require('../../config/environment');
+var recordBase = require('./recordApi.base')(config);
+var Promise = require('bluebird');
+var _ = require('lodash');
+
+/**
+ * Integration test for Text field formatting
+ */
+describe('API - Text record test cases', function () {
+
+    // Application containing a Text field
+    var exampleApp = {
+        "name": "Text App",
+        "tables": [
+            {
+                "name": "table1", "fields": [
+                {"name": "text", "type": "TEXT"}
+            ]
+            }
+        ]
+    };
+
+    /**
+     * Generates and returns a random string of specified length
+     */
+    function generateRandomString(size) {
+        var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+        var text = '';
+        for (var i = 0; i < size; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    }
+
+    /**
+     * DataProvider containing Records and record display expectations for Text field with various record values
+     */
+    function textRecordsDataProvider(fid) {
+        var smallText = generateRandomString(10);
+        var maxText = generateRandomString(4000);
+
+        // Small text value
+        var smallInput = [{"id": fid, "value": smallText}];
+        var expectedSmallRecord = {"id": fid, "value": smallText, "display": smallText};
+
+        // Text value of 4000 characters
+        var largeInput = [{"id": fid, "value": maxText}];
+        var expectedLargeRecord = {"id": fid, "value": maxText, "display": maxText};
+
+        //Empty records
+        var emptyInput = [{"id": fid, "value": ""}];
+        var expectedEmptyRecord = {"id": fid, "value": null, "display": ""};
+
+        //null record value
+        var nullInput = [{"id": fid, "value": null}];
+        var expectedNullRecord = {"id": fid, "value": null, "display": ""};
+
+        return [
+            { message: "display small text", record: smallInput, format: "display", expectedFieldValue: expectedSmallRecord },
+            { message: "raw small text", record: smallInput, format: "raw", expectedFieldValue: smallInput },
+            { message: "display maximum supported text", record: largeInput, format: "display", expectedFieldValue: expectedLargeRecord },
+            { message: "raw maximum supported text", record: largeInput, format: "raw", expectedFieldValue: largeInput },
+            { message: "display empty text", record: emptyInput, format: "display", expectedFieldValue: expectedEmptyRecord},
+            { message: "raw empty text", record: emptyInput, format: "raw", expectedFieldValue: emptyInput},
+            { message: "display null text", record: nullInput, format: "display", expectedFieldValue: expectedNullRecord },
+            { message: "raw null text", record: nullInput, format: "raw", expectedFieldValue: nullInput }]
+    }
+
+    /**
+     * Integration test that validates Text records formatting
+     */
+    it('Should create and retrieve display formatted text records', function (done) {
+        this.timeout(30000);
+        recordBase.createApp(exampleApp).then(function (appResponse) {
+            var app = JSON.parse(appResponse.body);
+            var textField;
+            app.tables[0].fields.forEach(function (field) {
+                if (field.name === 'text') {
+                    textField = field;
+                }
+            });
+            assert(textField, 'failed to find text field');
+            var records = textRecordsDataProvider(textField.id);
+            //For each of the cases, create the record and execute the request
+            var fetchRecordPromises = [];
+            records.forEach(function (currentRecord) {
+                var recordsEndpoint = recordBase.apiBase.resolveRecordsEndpoint(app.id, app.tables[0].id);
+                fetchRecordPromises.push(recordBase.createAndFetchRecord(recordsEndpoint, currentRecord.record, '?format='+currentRecord.format));
+            });
+
+            //When all the records have been created and fetched, assert the values match expectations
+            Promise.all(fetchRecordPromises)
+                .then(function (results) {
+                    for (var i = 0; i < results.length; i++) {
+                        var currentRecord = results[i];
+                        if(results[i].record) {
+                            currentRecord = results[i].record;
+                        }
+                        currentRecord.forEach(function (fieldValue) {
+                            if (fieldValue.id === records[i].expectedFieldValue.id) {
+                                assert.deepEqual(fieldValue, records[i].expectedFieldValue, 'Unexpected field value returned: '
+                                + JSON.stringify(fieldValue) + ', ' + JSON.stringify(records[i].expectedFieldValue));
+                            }
+                        });
+                    }
+                    done();
+                }).catch(function (errorMsg) {
+                    assert(false, 'unable to resolve all records: ' + JSON.stringify(errorMsg));
+                    done();
+                });
+        });
+    });
+
+    //Cleanup the test realm after all tests in the block
+    after(function (done) {
+        //Realm deletion takes time, bump the timeout
+        this.timeout(20000);
+        recordBase.apiBase.cleanup().then(function () {
+            done();
+        });
+    });
+});
