2e31bad9038f483a72d5d218c7f1aec31917e077
Mark Roper <mark_roper@intuit.com>
2015-05-01 11:17:02 -0400

    Updates phone number formatter.

--- ui/server/api/quickbase/formatter/test/recordFormatter.phoneNumber.unit.spec.js
+++ ui/server/api/quickbase/formatter/test/recordFormatter.phoneNumber.unit.spec.js
@@ -13,69 +13,6 @@ describe('Phone number record formatter unit test', function () {
      * DataProvider containing Records, FieldProperties and record display expectations PhoneNumber fields
      */
     function provider() {
-        //Incomplete number
-        var recordsInput =  [[{
-                "id": 7,
-                "value": "12345678"}]];
-        var expectedRecords =
-            [[{
-                "id": 7,
-                "value": "12345678",
-                "display": "(1) 234-5678"}]];
-
-        // 10 digit number
-        var standardInput =  [[{
-            "id": 7,
-            "value": "5557779999"}]];
-        var expectedStandardExpected =
-            [[{
-                "id": 7,
-                "value": "5557779999",
-                "display": "(555) 777-9999"}]];
-
-        //More than 10 digit number
-        var largeInput =  [[{
-            "id": 7,
-            "value": "1234567890123"}]];
-        var largeExpected =
-            [[{
-                "id": 7,
-                "value": "1234567890123",
-                "display": "123 (456) 789-0123"}]];
-
-        //Empty records
-        var emptyRecords =  [[{
-            "id": 7,
-            "value": ""}]];
-        var expectedEmptyRecords =
-            [[{
-                "id": 7,
-                "value": "",
-                "display": ""}]];
-
-        //null record value
-        var nullRecords =  [[{
-            "id": 7,
-            "value": null}]];
-        var nullExpectedRecords =
-            [[{
-                "id": 7,
-                "value": null,
-                "display": ""}]];
-
-        return [
-            { message: "small phone number", records: recordsInput, expectedRecords: expectedRecords },
-            { message: "standard phone number", records: standardInput, expectedRecords: expectedStandardExpected },
-            { message: "empty phone number", records: emptyRecords, expectedRecords: expectedEmptyRecords},
-            { message: "too-long phone number", records: largeInput, expectedRecords: largeExpected },
-            { message: "null phone number", records: nullRecords, expectedRecords: nullExpectedRecords }
-        ];
-    }
-
-    /**
-     * Unit test that validates PhoneNumber records formatting
-     */
-    describe('should format a phone number for display',function(){
         var fieldInfo = [
             {
                 "id": 7,
@@ -83,10 +20,57 @@ describe('Phone number record formatter unit test', function () {
                 "type": "PHONE_NUMBER"
             }
         ];
+
+        var fieldInfoHideExtension = [
+            {
+                "id": 7,
+                "name": "phone",
+                "type": "PHONE_NUMBER",
+                "includeExtension": false
+            }
+        ];
+        //Incomplete number
+        var recordsInput =  [[{
+                "id": 7,
+                "value": "(1) 234-5678"}]];
+        var expectedRecords =
+            [[{
+                "id": 7,
+                "value": "(1) 234-5678",
+                "display": "(1) 234-5678"}]];
+
+        // 10 digit number
+        var standardInput =  [[{
+            "id": 7,
+            "value": "(555) 777-9999 x1234"}]];
+        var expectedStandardExpected =
+            [[{
+                "id": 7,
+                "value": "(555) 777-9999 x1234",
+                "display": "(555) 777-9999 x1234"}]];
+        var expectedStandardExpectedNoExtension =
+            [[{
+                "id": 7,
+                "value": "(555) 777-9999 x1234",
+                "display": "(555) 777-9999"}]];
+
+        return [
+            { message: "small phone number", fieldInfo: fieldInfo, records: recordsInput, expectedRecords: expectedRecords },
+            { message: "phone number with extension", fieldInfo: fieldInfo, records: standardInput, expectedRecords: expectedStandardExpected },
+            { message: "phone number hide extenstion", fieldInfo: fieldInfoHideExtension, records: standardInput, expectedRecords: expectedStandardExpectedNoExtension }
+        ];
+    }
+
+    /**
+     * Unit test that validates PhoneNumber records formatting
+     */
+    describe('should format a phone number for display',function(){
+
         provider().forEach(function(entry){
             it('Test case: ' + entry.message, function (done) {
-                var formattedRecords = recordFormatter.formatRecords(entry.records, fieldInfo);
-                //console.log('entry: ' + JSON.stringify(entry));
+                var formattedRecords = recordFormatter.formatRecords(entry.records, entry.fieldInfo);
+                console.log('entry: ' + JSON.stringify(entry));
+                console.log('actual value: ' + JSON.stringify(formattedRecords));
                 assert.deepEqual(formattedRecords, entry.expectedRecords, entry.message);
                 done();
             });
