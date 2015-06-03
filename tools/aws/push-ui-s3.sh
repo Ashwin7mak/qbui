d295b8c57492231ca9c7ec3f3a02c9b7c73a443b
Claire Martinez <claire_martinez@intuit.com>
2015-06-03 14:58:46 -0400

    use environment params to control how artifact is pushed to support ui try build testing

--- tools/aws/push-ui-s3.sh
+++ tools/aws/push-ui-s3.sh
@@ -3,9 +3,9 @@
 export http_proxy=http://qypprdproxy02.ie.intuit.net:80
 export no_proxy='.intuit.net, .intuit.com, 10.*.*.*, localhost, 127.0.0.1'
 
-BUCKET=s3://quickbase-preprod-software
+BUCKET=s3://${S3_BUCKET:="quickbase-preprod-software"}
+cd ${WORKSPACE}
 
-cd /app/jenkins/workspace/ui_development/
 FILES=$(find -name 'ui-phoenix*.zip')
 S3=/app/jenkins/s3cmd-1.5.0-rc1/s3cmd
 
