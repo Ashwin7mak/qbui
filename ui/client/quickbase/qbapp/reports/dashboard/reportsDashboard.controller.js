75179f3ef268ae203dc82ced36db699904115801
Don Hatch <donald_hatch@intuit.com>
2015-07-10 13:34:04 -0400

    improve navigation styling

--- ui/client/quickbase/qbapp/reports/dashboard/reportsDashboard.controller.js
+++ ui/client/quickbase/qbapp/reports/dashboard/reportsDashboard.controller.js
@@ -10,14 +10,20 @@
 
         $scope.appId = $stateParams.appId;
         $scope.tableId = $stateParams.tableId;
+        $scope.reportId;
 
         if ($scope.appId && $scope.tableId) {
             $scope.reports = [];
             ReportsDashboardModel.get($scope.appId, $scope.tableId).then(
                 function (reports) {
+                    var defaultReport;
                     reports.forEach(function (report) {
+                        if (!defaultReport) { defaultReport = report; }
                         $scope.reports.push({id: report.id, name: report.name});
                     });
+                    if (defaultReport) {
+                        $scope.goToPage(defaultReport);
+                    }
                 },
                 function (resp) {
                     console.log('Error getting report list.  Status: ' + resp.status);
@@ -37,6 +43,11 @@
 
         $scope.goToPage = function(report) {
             $state.transitionTo('reports/report', {appId:$scope.appId, tableId:$scope.tableId,id: report.id});
+            $scope.reportId = report.id;
+        };
+
+        $scope.isSelected = function(report) {
+            return report.id === $scope.reportId;
         };
     }
 
