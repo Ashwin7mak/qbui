3b61ff196b9daee6b26f3dbc449ca33f491bf611
Hatch <dhatch@CMBL1391622b7.local>
2015-09-25 10:12:18 -0400

    incorporate router with initial flux implementation

--- ui/client-react/src/locales/en_us.js
+++ ui/client-react/src/locales/en_us.js
@@ -1,19 +1,19 @@
 export default {
-    locales: "en-us",
+    locales: 'en-us',
 
     messages: {
-        "footer": {
-            "copyright": "{year} Intuit Inc. All rights reserved."
+        'footer': {
+            'copyright': '{year} Intuit Inc. All rights reserved.'
         },
-        "lighthouse": {
-            "stage": {
-                "header":"A Very Import Report",
-                "sub_header":"This is a snapshot of your original report from your app",
-                "content":"Congratulations and welcome! You’re part of a small, early-access program. Your feedback now, will shape the way QuickBase looks and behaves in the future. Below is a snapshot of one of your reports. Browse through it as you normally would. How does it look? Does your data display the way you expect it to? Use the big button, to the right, to let us know. We look forward to hearing what you have to say.",
-                "feedback" : {
-                    "header":"Remember",
-                    "sub_header":"Your Feedback Matters",
-                    "button":"Send your feedback"
+        'lighthouse': {
+            'stage': {
+                'header':'A Very Import Report',
+                'sub_header':'This is a snapshot of your original report from your app',
+                'content':'Congratulations and welcome! You’re part of a small, early-access program. Your feedback now, will shape the way QuickBase looks and behaves in the future. Below is a snapshot of one of your reports. Browse through it as you normally would. How does it look? Does your data display the way you expect it to? Use the big button, to the right, to let us know. We look forward to hearing what you have to say.',
+                'feedback' : {
+                    'header':'Remember',
+                    'sub_header':'Your Feedback Matters',
+                    'button':'Send your feedback'
                 }
             }
         }
--- ui/server/routes/qbClientRoutes.js
+++ ui/server/routes/qbClientRoutes.js
@@ -4,8 +4,19 @@
 
     module.exports = function(app) {
 
-        app.route('/home').get(function(req, res) {
-            log.info('..opening home page.');
+
+        app.route('/app/table').get(function(req, res) {
+            log.info('..opening specific app report request: ' + app.get('appPath') + '/index.html');
+            res.sendfile(app.get('appPath') + '/index.html');
+        });
+
+        app.route('/app/:appId/table/:tblId/report/:rptId').get(function(req, res) {
+            log.info('..opening specific app report request');
+            res.sendfile(app.get('appPath') + '/index.html');
+        });
+
+        app.route('/apps').get(function(req, res) {
+            log.info('..opening apps home page.');
             res.sendfile(app.get('appPath') + '/index.html');
         });
 
