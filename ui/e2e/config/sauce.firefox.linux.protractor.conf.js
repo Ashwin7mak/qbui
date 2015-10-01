9eabb39a203767e1d39067ee34ff73002b4337bc
Ken LaBak <Ken_LaBak@intuit.com>
2015-10-01 12:10:20 -0400

    Code review feedback. Created a global config file for common params when running in Sauce Labs. Local config will still be separate.

--- ui/e2e/config/sauce.firefox.linux.protractor.conf.js
+++ ui/e2e/config/sauce.firefox.linux.protractor.conf.js
@@ -1,62 +1,12 @@
 // Protractor configuration
 // https://github.com/angular/protractor/blob/master/referenceConf.js
 (function() {
-
     'use strict';
-
-    exports.config = {
-        // The timeout for each script run on the browser. This should be longer
-        // than the maximum time your application needs to stabilize between tasks.
-        allScriptsTimeout: 120000,
-
-        //The sauce user and access key allow us to run our browser tests remotely on a SauceLabs VM
-        sauceUser           : 'sbg_qbse',
-        sauceKey            : process.env.SAUCE_KEY,
-        //we have to specify the selenium address to point locally so that we use the tunnel properly
-        sauceSeleniumAddress: 'localhost:4445/wd/hub',
-
-        // A base URL for your application under test will be passed in via grunt config so that we can use whatever url we please
-
-        // list of files / patterns to load in the browser
-        specs: [
-            '../qbapp/**/*.e2e.spec.js'
-        ],
-
-        // Patterns to exclude.
-        exclude: [],
-
-        // ----- Capabilities to be passed to the webdriver instance ----
-        //
-        // For a full list of available capabilities, see
-        // https://code.google.com/p/selenium/wiki/DesiredCapabilities
-        // and
-        // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
-        capabilities: {
-            browserName     : 'firefox',
-            tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-            name            : process.env.SAUCE_JOB_NAME + '_Linux_Firefox'
-        },
-
-        // ----- The test framework -----
-        //
-        // Jasmine and Cucumber are fully supported as a test and assertion framework.
-        // Mocha has limited beta support. You will need to include your own
-        // assertion framework if working with mocha.
-        framework: 'jasmine2',
-
-        // ----- Options to be passed to minijasminenode -----
-        //
-        // See the full list at https://github.com/juliemr/minijasminenode
-        jasmineNodeOpts: {
-            defaultTimeoutInterval: 90000
-        },
-        // Globally accessible variables (params is a property of the Protractor instance)
-        // Used for running tests slower / faster if running in Sauce Labs
-        params: {
-            tinySleep : 1000,
-            smallSleep : 10000,
-            mediumSleep : 20000,
-            largeSleep :40000
-        }
+    var globalSauceConfig = require('./sauce.global.conf.js');
+    globalSauceConfig.capabilities = {
+        browserName     : 'firefox',
+        tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
+        name            : process.env.SAUCE_JOB_NAME + '_Linux_Firefox'
     };
+    exports.config = globalSauceConfig;
 }());
--- ui/e2e/config/sauce.global.conf.js
+++ ui/e2e/config/sauce.global.conf.js
@@ -1,51 +1,48 @@
 // Protractor configuration
 // https://github.com/angular/protractor/blob/master/referenceConf.js
-
 (function() {
     'use strict';
-    exports.config = {
+    // Global properties file with params common to all Sauce lab config files
+    module.exports = {
         // The timeout for each script run on the browser. This should be longer
         // than the maximum time your application needs to stabilize between tasks.
-        allScriptsTimeout: 120000,
+        allScriptsTimeout: 300000,
+
+        //The sauce user and access key allow us to run our browser tests remotely on a SauceLabs VM
+        sauceUser           : 'sbg_qbse',
+        sauceKey            : process.env.SAUCE_KEY,
+        //we have to specify the selenium address to point locally so that we use the tunnel properly
+        sauceSeleniumAddress: 'localhost:4445/wd/hub',
+
         // A base URL for your application under test will be passed in via grunt config so that we can use whatever url we please
-        baseUrl: process.env.DOMAIN,
+        //baseUrl: process.env.DOMAIN,
         // list of files / patterns to load in the browser
         specs: [
             '../qbapp/**/*.e2e.spec.js'
         ],
+
         // Patterns to exclude.
         exclude: [],
-        // ----- Capabilities to be passed to the webdriver instance ----
-        //
-        // For a full list of available capabilities, see
-        // https://code.google.com/p/selenium/wiki/DesiredCapabilities
-        // and
-        // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
-        capabilities: {
-            browserName: 'chrome'
-        },
         // ----- The test framework -----
         //
         // Jasmine and Cucumber are fully supported as a test and assertion framework.
         // Mocha has limited beta support. You will need to include your own
         // assertion framework if working with mocha.
         framework: 'jasmine2',
+
         // ----- Options to be passed to minijasminenode -----
         //
         // See the full list at https://github.com/juliemr/minijasminenode
         jasmineNodeOpts: {
-            // If true, print colors to the terminal.
-            showColors: true,
-            // Default time to wait in ms before a test fails.
-            defaultTimeoutInterval: 60000
+            defaultTimeoutInterval: 150000
         },
         // Globally accessible variables (params is a property of the Protractor instance)
         // Used for running tests slower / faster if running in Sauce Labs
-        params: {
-            tinySleep : 100,
-            smallSleep : 1000,
-            mediumSleep : 5000,
-            largeSleep :10000
+        params : {
+            tinySleep : 5000,
+            smallSleep : 10000,
+            mediumSleep : 30000,
+            largeSleep :60000
         }
     };
 }());
