0e2d393561587cf5b58bace4c167b324f90e56e6
Drew Stevens <drew_stevens@intuit.com>
2015-10-15 15:56:06 -0400

    fix typo, remove font-awesome

--- ui/server/routes/html.jsx
+++ ui/server/routes/html.jsx
@@ -14,7 +14,7 @@ var Html = React.createClass({
                 <head>
                     <meta charSet="UTF-8" />
                         <title>{this.props.title}</title>
-                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+
                         <link rel="icon" type="image/png" href={this.props.hostBase + this.props.favicon} />
                         {this.props.styleTagStringContent ? (
                                 <style type="text/css">
