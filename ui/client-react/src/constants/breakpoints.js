11fbcff890b6925a5f720b269cb134827c5860f4
Drew Stevens <drew_stevens@intuit.com>
2015-10-28 18:31:30 -0400

    convert everything to responsive

---
+++ ui/client-react/src/constants/breakpoints.js
@@ -0,0 +1,4 @@
+export const SMALL_BREAKPOINT = 'small-breakpoint';
+export const MEDIUM_BREAKPOINT = 'medium-breakpoint';
+export const LARGE_BREAKPOINT = 'large-breakpoint';
+export const XLARGE_BREAKPOINT = 'xlarge-breakpoint';
--- ui/client-react/src/scripts/router.js
+++ ui/client-react/src/scripts/router.js
@@ -4,14 +4,7 @@ import {Router, Route, IndexRoute} from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 import Nav from '../components/nav/nav';
-import LeftNav from '../components/nav/leftNav';
-import MobileLeftNav from '../components/nav/mobileLeftNav';
 
-import TopNav from '../components/header/topNav';
-import MobileTopNav from '../components/header/mobileTopNav';
-
-import Footer from '../components/footer/footer';
-import MobileAddFooter from '../components/footer/mobileAddFooter';
 import Fluxxor from 'fluxxor';
 
 import ReportsStore from '../stores/reportsStore';
@@ -33,6 +26,8 @@ import RecordRoute from '../components/record/recordRoute';
 import TableHomePageRoute from '../components/table/tableHomePageRoute';
 import DashboardRoute from '../components/dashboard/dashboardRoute';
 
+import * as breakpoints from '../constants/breakpoints';
+
 let stores = {
     ReportsStore: new ReportsStore(),
     ReportDataStore: new ReportDataStore(),
@@ -45,22 +40,49 @@ flux.addActions(reportDataActions);
 flux.addActions(appsActions);
 flux.addActions(navActions);
 
-function isMobileRoute(route) {
-    return route.path.indexOf('m/') === 0;
-}
-
 let NavWrapper = React.createClass({
+    getInitialState() {
+        return {
+            breakpoint: this.getCurrentBreakpointClass()
+        };
+    },
+    getCurrentBreakpointClass: function() {
+        let w = window.innerWidth;
+
+        if (w <= 640) {
+            return breakpoints.SMALL_BREAKPOINT;
+        } else if (w <= 1024) {
+            return breakpoints.MEDIUM_BREAKPOINT;
+        } else if (w <= 1440) {
+            return breakpoints.LARGE_BREAKPOINT;
+        } else {
+            return breakpoints.XLARGE_BREAKPOINT;
+        }
+    },
     render: function() {
-        return <Nav flux={flux} {...this.props} mobile={isMobileRoute(this.props.route)} />;
+        return <Nav flux={flux} {...this.props}  breakpoint={this.state.breakpoint} />;
+    },
+
+    //need to debounce this...
+    handleResize: function() {
+
+        let breakpoint = this.getCurrentBreakpointClass();
+
+        this.setState({breakpoint});
     },
     componentDidMount: function() {
         flux.actions.loadReports({appId: this.props.params.appId, tblId: this.props.params.tblId});
+
+        window.addEventListener('resize', this.handleResize);
+    },
+    componentWillUnmount: function() {
+        window.removeEventListener('resize', this.handleResize);
     }
 });
 
 let Apps = React.createClass({
     render: function() {
-        return <AppsHome flux={flux} mobile={isMobileRoute(this.props.route)}/>;
+        return <AppsHome flux={flux} />;
     },
     componentDidMount: function() {
         flux.actions.loadAppsWithTables();
@@ -74,17 +96,12 @@ render((
         <Route path="m/apps" component={Apps} />
 
         <Route path="app/:appId/table/:tblId" component={NavWrapper} >
-            <IndexRoute components={{main: TableHomePageRoute, topNav: TopNav, leftNav: LeftNav, footer: Footer}} />
-            <Route path="report/:rptId" components={{main: ReportRoute, topNav: TopNav,  leftNav: LeftNav, footer: Footer}} />
-            <Route path="record/:recordId" components={{main: RecordRoute, topNav: TopNav,  leftNav: LeftNav, footer: Footer}} />
-            <Route path="dashboardDemo/:rptId" components={{main: DashboardRoute, topNav: TopNav,  leftNav: LeftNav, footer: Footer}} />
+            <IndexRoute components={{main: TableHomePageRoute}} />
+            <Route path="report/:rptId" components={{main: ReportRoute}} />
+            <Route path="record/:recordId" components={{main: RecordRoute}} />
+            <Route path="dashboardDemo/:rptId" components={{main: DashboardRoute}} />
         </Route>
 
-        <Route path="m/app/:appId/table/:tblId" component={NavWrapper} >
-            <IndexRoute components={{main: TableHomePageRoute, leftNav: MobileLeftNav, footer: MobileAddFooter}} />
-            <Route path="report/:rptId" components={{main: ReportRoute, topNav: MobileTopNav, leftNav: MobileLeftNav, footer: MobileAddFooter}} />
-            <Route path="record/:recordId" components={{main: RecordRoute, topNav: MobileTopNav, leftNav: MobileLeftNav, footer: MobileAddFooter}} />
-        </Route>
 
     </Router>
 ), document.getElementById('content'));
