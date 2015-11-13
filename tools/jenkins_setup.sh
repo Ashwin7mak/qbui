abb98651d415dd41d2cce406253673b86bf12c33
Ken LaBak <Ken_LaBak@intuit.com>
2015-11-13 10:22:15 -0500

    Updated jenkins_setup.sh script to rebuild node (for AWS E2E Pipeline job)

--- tools/jenkins_setup.sh
+++ tools/jenkins_setup.sh
@@ -40,4 +40,7 @@ echo copy phantomjs to node_module
 cp $PHANTOMJS_INSTALL_DIR/bin/phantomjs $PHANTOMJS_NODEMODULE
 ls -l "$PHANTOMJS_NODEMODULE"
 
+echo running rebuild of node
+npm rebuild node-sass
+
 
