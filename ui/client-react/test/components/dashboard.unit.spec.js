0cb775c8f1fad03bb787c51a16033f32dec3c11c
Drew Stevens <drew_stevens@intuit.com>
2015-11-24 10:37:33 -0500

    remove obsolete unit tests for table dashboard

--- ui/client-react/test/components/dashboard.unit.spec.js
+++ ui/client-react/test/components/dashboard.unit.spec.js
@@ -75,53 +75,7 @@ describe('Dashboard functions', () => {
     it('test render of component', () => {
         component = TestUtils.renderIntoDocument(<Dashboard flux={flux} reportData={fakeReportData_Empty}/>);
         expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
-        expect(TestUtils.scryRenderedComponentsWithType(component, ContentMock).length).toEqual(4);
+
     });
 
-    /* tests for missing params */
-    it('test render of report with no app', () => {
-        component = TestUtils.renderIntoDocument(<Dashboard flux={flux} reportData={fakeReportData} params={params.noapp}/>);
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
-        expect(TestUtils.scryRenderedComponentsWithType(component, ContentMock).length).toEqual(4);
-        expect(flux.actions.loadReport).not.toHaveBeenCalled();
-    });
-    it('test render of report with no tbl', () => {
-        component = TestUtils.renderIntoDocument(<Dashboard flux={flux} reportData={fakeReportData} params={params.notbl}/>);
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
-        expect(TestUtils.scryRenderedComponentsWithType(component, ContentMock).length).toEqual(4);
-        expect(flux.actions.loadReport).not.toHaveBeenCalled();
-    });
-    it('test render of report with no rpt', () => {
-        component = TestUtils.renderIntoDocument(<Dashboard flux={flux} reportData={fakeReportData} params={params.norpt}/>);
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
-        expect(TestUtils.scryRenderedComponentsWithType(component, ContentMock).length).toEqual(4);
-        expect(flux.actions.loadReport).not.toHaveBeenCalled();
-    });
-    it('test render of report with valid params', () => {
-        component = TestUtils.renderIntoDocument(<Dashboard flux={flux} reportData={fakeReportData} params={params.valid}/>);
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
-        expect(TestUtils.scryRenderedComponentsWithType(component, ContentMock).length).toEqual(4);
-        expect(flux.actions.loadReport).not.toHaveBeenCalled();
-    });
-
-    it('test render of report with updated params', () => {
-        var TestParent = React.createFactory(React.createClass({
-            getInitialState() {
-                return {
-                    params: params.valid
-                };
-            },
-            render() {
-                return <Dashboard flux={flux} reportData={fakeReportData} params={this.state.params}/>;
-            }
-        }));
-
-        var parent = TestUtils.renderIntoDocument(TestParent());
-
-        parent.setState({
-            params: params.changed
-        });
-
-        expect(flux.actions.loadReport).toHaveBeenCalledWith(params.changed.appId, params.changed.tblId, params.changed.rptId, true);
-    });
 });
