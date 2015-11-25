385f85a76999e69b9cf884aac4cb774a3f230fcf
Don Hatch <donald_hatch@intuit.com>
2015-11-25 14:16:55 -0500

    unit tests for client routes

---
+++ ui/server/routes/test/qbClientRoutes.unit.spec.js
@@ -0,0 +1,94 @@
+
+var sinon = require('sinon');
+var request = require('supertest');
+
+var log = require('../../logger').getLogger();
+var errors = require('../../components/errors');
+var routeGroups = require('../../routes/routeGroups');
+
+var express = require('express');
+var app = express();
+
+var mockConfig = {
+    routeGroup: routeGroups.DEBUG,
+};
+
+require('../../routes')(app, mockConfig);
+
+/**
+ * Unit tests for app generator
+ */
+describe('Express Client Routes', function() {
+
+    var stubLog;
+    var server;
+    var spySetTid;
+
+    beforeEach(function() {
+        stubLog = sinon.stub(log, 'logRequest').returns(true);
+    });
+    afterEach(function() {
+        stubLog.restore();
+    });
+
+    it('Validate default client route', function(done) {
+        request(app).
+            get('/').
+            expect(200).
+            end(function(err, res) {
+                if (err) {return done(err);}
+                done();
+            });
+    });
+
+    it('Validate get apps route', function(done) {
+        request(app).
+            get('/apps').
+            expect(200).
+            end(function(err, res) {
+                if (err) {return done(err);}
+                done();
+            });
+    });
+
+    it('Validate get tables route', function(done) {
+        request(app).
+            get('/app/1/table/2').
+            expect(200).
+            end(function(err, res) {
+                if (err) {return done(err);}
+                done();
+            });
+    });
+
+    it('Validate get table reports route', function(done) {
+        request(app).
+            get('/app/1/table/2/reports').
+            expect(200).
+            end(function(err, res) {
+                if (err) {return done(err);}
+                done();
+            });
+    });
+
+    it('Validate get record route', function(done) {
+        request(app).
+            get('/app/1/table/2/record/3').
+            expect(200).
+            end(function(err, res) {
+                if (err) {return done(err);}
+                done();
+            });
+    });
+
+    it('Validate get table report route', function(done) {
+        request(app).
+            get('/app/1/table/2/report/3').
+            expect(200).
+            end(function(err, res) {
+                if (err) {return done(err);}
+                done();
+            });
+    });
+
+});
