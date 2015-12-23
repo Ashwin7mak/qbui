b04f40f025983b0ccd8a65be8322efdf831d8f46
Beyer <fbeyer@CMBL130e9e44d.local>
2015-12-23 11:36:52 -0500

    updating import path, because OSx isnt case sensitive but linux is

--- ui/client-react/src/components/record/record.js
+++ ui/client-react/src/components/record/record.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './record.scss';
 
-import QBForm from './../qbForm/qbform.js';
+import QBForm from './../QBForm/qbform.js';
 
 let Record = React.createClass({
 
