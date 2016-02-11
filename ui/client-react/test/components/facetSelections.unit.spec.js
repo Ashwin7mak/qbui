b3f2698e1ceca947acdcf680ae018951d3fa675b
Claire Martinez <claire_martinez@intuit.com>
2016-02-11 18:36:52 -0500

    test messaging cleanup

--- ui/client-react/test/components/facetSelections.unit.spec.js
+++ ui/client-react/test/components/facetSelections.unit.spec.js
@@ -93,13 +93,13 @@ describe('FacetSelections', () => {
             testSelections = new FacetSelections();
         });
 
-        it('getFieldSelections', () => {
+        it('works', () => {
             testSelections.initSelections(mySelections);
             var expected = mySelections.fieldid1;
             expect(testSelections.getFieldSelections('fieldid1')).toEqual(expected);
         });
 
-        it('doesn\'t getFieldSelections invalid input', () => {
+        it(' doesn\'t getFieldSelections invalid input', () => {
             testSelections.initSelections(mySelections);
             expect(testSelections.getFieldSelections('fieldidNotThere')).toEqual([]);
         });
@@ -111,12 +111,12 @@ describe('FacetSelections', () => {
             testSelections = new FacetSelections();
         });
 
-        it('getSelections', () => {
+        it('works', () => {
             testSelections.initSelections(mySelections);
             expect(testSelections.getSelections()).toEqual(mySelections);
         });
 
-        it(' getSelections empty input', () => {
+        it(' with empty input', () => {
             testSelections.initSelections([]);
             expect(testSelections.getSelections()).toEqual([]);
         });
@@ -128,13 +128,13 @@ describe('FacetSelections', () => {
             testSelections = new FacetSelections();
         });
 
-        it('addSelection to field with existing facets', () => {
+        it(' to field with existing facets', () => {
             testSelections.initSelections(mySelections);
             testSelections.addSelection('fieldid1', 'addedVal');
             expect(_.includes(testSelections.getFieldSelections('fieldid1'), 'addedVal')).toBeTruthy();
         });
 
-        it('addSelection several to existing', () => {
+        it(' several to existing', () => {
             testSelections.initSelections(mySelections);
             testSelections.addSelection('fieldid1', 'addedVal1');
             testSelections.addSelection('fieldid1', 'addedVal2');
@@ -146,13 +146,13 @@ describe('FacetSelections', () => {
             expect(_.includes(testSelections.getFieldSelections('fieldid1'), 'addedVal4')).toBeTruthy();
         });
 
-        it('addSelection to field with no facets', () => {
+        it(' to field with no facets', () => {
             testSelections.initSelections();
             testSelections.addSelection('newFid', 'addedVal');
             expect(_.includes(testSelections.getFieldSelections('newFid'), 'addedVal')).toBeTruthy();
         });
 
-        it('addSelection to field with empty facets', () => {
+        it(' to field with empty facets', () => {
             testSelections.initSelections();
             testSelections.addSelection('fieldid4', 'addedVal');
             expect(_.includes(testSelections.getFieldSelections('fieldid4'), 'addedVal')).toBeTruthy();
@@ -164,14 +164,14 @@ describe('FacetSelections', () => {
             testSelections = new FacetSelections();
         });
 
-        it('removeSelection from field with existing facets', () => {
+        it(' from field with existing facets', () => {
             testSelections.initSelections(mySelections);
             testSelections.removeSelection('fieldid1', 'selectedValueB');
             expect(_.includes(testSelections.getFieldSelections('fieldid1'), 'selectedValueB')).toBeFalsy();
             expect(_.includes(testSelections.getFieldSelections('fieldid1'), 'selectedValueA')).toBeTruthy();
         });
 
-        it('removeSelection several times from existing', () => {
+        it(' several times from existing', () => {
             testSelections.initSelections(mySelections);
             testSelections.removeSelection('fieldid1', 'selectedValueB');
             testSelections.removeSelection('fieldid1', 'selectedValueA');
@@ -181,13 +181,13 @@ describe('FacetSelections', () => {
             expect(_.includes(mySelections.fieldid1, 'selectedValueA')).toBeTruthy();
         });
 
-        it('removeSelection from field with no facets', () => {
+        it(' from field with no facets', () => {
             testSelections.initSelections();
             let result = testSelections.removeSelection('newFid', 'addedVal');
             expect(result).toEqual(null);
         });
 
-        it('removeSelection from field with empty facets', () => {
+        it(' from field with empty facets', () => {
             testSelections.initSelections();
             testSelections.removeSelection('fieldid4', 'addedVal');
             expect(_.includes(testSelections.getFieldSelections('fieldid4'), 'addedVal')).toBeFalsy();
@@ -199,14 +199,14 @@ describe('FacetSelections', () => {
             testSelections = new FacetSelections();
         });
 
-        it('removeAllFieldSelections from field with existing facets', () => {
+        it(' from field with existing facets', () => {
             testSelections.initSelections(mySelections);
             testSelections.removeAllFieldSelections('fieldid1');
             expect(_.includes(testSelections.getFieldSelections('fieldid1'), 'selectedValueB')).toBeFalsy();
             expect(_.includes(testSelections.getFieldSelections('fieldid1'), 'selectedValueA')).toBeFalsy();
         });
 
-        it('removeAllFieldSelections several from existing', () => {
+        it(' several from existing', () => {
             testSelections.initSelections(mySelections);
             testSelections.removeAllFieldSelections('fieldid1');
             testSelections.removeAllFieldSelections('fieldid1');
@@ -217,13 +217,13 @@ describe('FacetSelections', () => {
             expect(_.includes(mySelections.fieldid1, 'selectedValueA')).toBeTruthy();
         });
 
-        it('removeAllFieldSelections from field with no facets', () => {
+        it(' from field with no facets', () => {
             testSelections.initSelections();
             let result = testSelections.removeAllFieldSelections('newFid');
             expect(result).toEqual(null);
         });
 
-        it('removeSelection from field with empty facets', () => {
+        it(' from field with empty facets', () => {
             testSelections.initSelections();
             testSelections.removeAllFieldSelections('fieldid4');
             expect(testSelections.getFieldSelections('fieldid4').length).toEqual(0);
@@ -235,7 +235,7 @@ describe('FacetSelections', () => {
             testSelections = new FacetSelections();
         });
 
-        it('removeAllSelections from field with existing facets', () => {
+        it(' from field with existing facets', () => {
             testSelections.initSelections(mySelections);
             testSelections.removeAllSelections();
             expect(_.includes(testSelections.getFieldSelections('fieldid1'), 'selectedValueB')).toBeFalsy();
@@ -243,7 +243,7 @@ describe('FacetSelections', () => {
             expect(testSelections.hasAnySelections()).toBeFalsy();
         });
 
-        it('removeAllSelections several times from existing', () => {
+        it(' several times from existing', () => {
             testSelections.initSelections(mySelections);
             testSelections.removeAllSelections();
             testSelections.removeAllSelections();
@@ -255,14 +255,14 @@ describe('FacetSelections', () => {
             expect(_.includes(mySelections.fieldid1, 'selectedValueA')).toBeTruthy();
         });
 
-        it('removeAllSelections from field with no facets', () => {
+        it(' from field with no facets', () => {
             testSelections.initSelections();
             let result = testSelections.removeAllSelections();
             expect(result).toEqual({});
             expect(testSelections.hasAnySelections()).toBeFalsy();
         });
 
-        it('removeSelection from field with empty facets', () => {
+        it(' from field with empty facets', () => {
             testSelections.initSelections();
             testSelections.removeAllSelections();
             expect(testSelections.getFieldSelections('fieldid4').length).toEqual(0);
