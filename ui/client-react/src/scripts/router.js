b455d3b9140f727064865ce912c1492abe4e8884
Claire <claire_martinez@intuit.com>
2016-04-04 19:09:43 -0400

    case sensitive filename in import

--- ui/client-react/src/scripts/router.js
+++ ui/client-react/src/scripts/router.js
@@ -15,7 +15,7 @@ import ReportsStore from '../stores/reportsStore';
 import reportActions from'../actions/reportActions';
 
 import ReportDataStore from '../stores/reportDataStore';
-import ReportDataSearchStore from '../stores/ReportDataSearchStore';
+import ReportDataSearchStore from '../stores/reportDataSearchStore';
 import reportDataActions from'../actions/reportDataActions';
 
 import AppsStore from '../stores/appsStore';
