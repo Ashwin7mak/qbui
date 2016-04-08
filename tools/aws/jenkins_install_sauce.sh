942bb9c7fdb9ae0213794b4dd5a19e3c61f81e37
Beyer <fbeyer@CMBL130e9e44d.local>
2016-04-08 09:08:01 -0400

    updated our properties file to include information about where we keep saucelabs_sc binary. Added a new shell script to fetch it from nexus and install it. hopefully its coded correctly, I hate shell

---
+++ tools/aws/jenkins_install_sauce.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# this is a script used by Jenkins in AWS to download the sauce binary from nexus
+#set -x #echo on
+echo PATH=$PATH
+
+#get sauce-sc
+
+if [ ! -d "SAUCELABS_SC_INSTALL_DIR" ]; then
+  echo creating tmp dir  [SAUCELABS_SC_INSTALL_DIR for saucelabs_sc
+  mkdir "$SAUCELABS_SC_INSTALL_DIR"
+else
+  echo $SAUCELABS_SC_INSTALL_DIR already exists
+fi
+
+if [ ! -f "$SAUCELABS_SC_INSTALL_DIR/$SAUCELABS_SC_TARBALL" ]; then
+  echo fetching saucelabs_sc
+  wget -q "--directory-prefix=$SAUCELABS_SC_INSTALL_DIR" "https://nexus1.ci.quickbaserocks.com/nexus/content/repositories/thirdparty/phantomjs/phantomjs/1.9.8-linux/phantomjs-1.9.8-linux-x86_64.tar.bz2"
+  ls "$SAUCELABS_SC_INSTALL_DIR"
+else
+  echo "SauceLabs_sc archive $SAUCELABS_SC_INSTALL_DIR/$SAUCELABS_SC_TARBALL already exists"
+fi
+
+
+if [ ! -d "$SAUCELABS_SC_INSTALL_DIR" ]; then
+ echo unpacking saucelabs_sc
+  tar -xf  $SAUCELABS_SC_INSTALL_DIR/$SAUCELABS_SC_TARBALL -C $SAUCELABS_SC_INSTALL_DIR
+  ls "$SAUCELABS_SC_INSTALL_DIR"
+else
+  echo "Saucelabs_sc install dir $SAUCELABS_SC_INSTALL_DIR already exists"
+fi
