6efc728f353002bf6392020745f25bee4ff64797
Goel, Aditi <Aditi_Goel@intuit.com>
2016-04-25 12:56:20 -0400

    Changes for  QBSE­-21617

--- ui/client-react/src/locales/bundles/fr_fr.js
+++ ui/client-react/src/locales/bundles/fr_fr.js
@@ -25,15 +25,6 @@ export default {
         footer: {
             copyright: "{year} Intuit Inc. Tous droits réservés."
         },
-        stage: {
-            header: "Un rapport très à l'importation",
-            content: "Félicitations et bienvenue! Vous faites partie d'un petit programme, début de l'accès. Vos commentaires maintenant, va façonner la manière dont QuickBase ressemble et se comporte à l'avenir. Voici un aperçu de l'un de vos rapports. Parcourez -le comme vous le feriez normalement. De quoi ça a l'air? Ne affiche vos données comme vous le souhaitez? Utilisez le gros bouton, à droite, à nous le faire savoir. Nous avons hâte d'entendre ce que vous avez à dire.",
-            feedback: {
-                header: "Rappelez-vous",
-                sub_header: "Vos commentaires sont important",
-                button: "Envoyer vos commentaires"
-            }
-        },
         nav: {
             home: "Accueil",
             users: "Utilisateurs",
