ea9f55416be21fbde9adebe8d85b41bc7f9e3b80
Beyer <rbeyer@CMBL130e9e44d.local>
2016-05-25 09:51:59 -0400

    wrong filepath

--- ui/server/components/errors/index.js
+++ ui/server/components/errors/index.js
@@ -7,7 +7,7 @@
     var log = require('../../logger').getLogger();
 
     module.exports[401] = function unauthorized(req, res) {
-        var viewFilePath = '403.html';
+        var viewFilePath = '401.html';
         var statusCode = 401;
         processError(req, res, viewFilePath, statusCode);
     };
