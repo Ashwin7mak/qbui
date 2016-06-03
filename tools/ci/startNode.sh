2143e874932dd139ad4dd7de1971e7c5e3a2f130
Ken LaBak <Ken_LaBak@intuit.com>
2016-06-03 10:12:47 -0400

    Reverting test.js config file. Adding shell script that starts node. Added a health check for node in the gradle task.

---
+++ tools/ci/startNode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#set -x #echo on
+echo PATH=$PATH
+
+cd ../../ui
+pwd
+
+NODE_ENV=test npm start
--- ui/server/config/environment/test.js
+++ ui/server/config/environment/test.js
@@ -46,7 +46,7 @@
 
         //REST endpoint (protocol,server,port)
         //javaHost: 'https://quickbase-dev.com:8443',
-        javaHost: 'http://quickbase-dev.com:8082',
+        javaHost: 'http://quickbase-dev.com:' + javaHostPort,
 
         //Express Server
         //DOMAIN: 'https://quickbase-dev.com:9443'
