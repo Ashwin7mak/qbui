b15520251b38ad46baa39416a8d4e865c2f36cb8
Beyer <rbeyer@cmbl130e9e44d.corp.quickbase.net>
2016-06-08 10:53:43 -0400

    updating how we remove the ticket upon signout, this should work in prod, it does locally

--- ui/server/components/authentication/index.js
+++ ui/server/components/authentication/index.js
@@ -10,7 +10,7 @@
         var viewFilePath = 'signout.html';
         var statusCode = 200;
         var message = "User is signing out";
-        res.clearCookie('ticket',  {path: '/'});
+        res.cookie('ticket', {expires: new Date(Date.now())});
         processAuthentication(req, res, viewFilePath, statusCode, message);
     };
 
