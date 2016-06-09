5414a0deff6ecac0250ab8deb6e7618e5f4eda28
Claire Martinez <cmartinez@quickbase.com>
2016-06-08 21:09:57 -0400

    fluxify it

---
+++ ui/client-react/src/stores/perfStore.js
@@ -0,0 +1,82 @@
+import * as actions from "../constants/actions";
+import PerfLogUtils from "../utils/perf/perfLogUtils";
+import ReactPerfUtils from "../utils/perf/reactPerfUtils";
+import Configuration from "../config/app.config.js";
+import _ from "lodash";
+import Fluxxor from "fluxxor";
+import Logger from "../utils/logger";
+var logger = new Logger();
+
+let PerfStore = Fluxxor.createStore({
+
+    initialize() {
+        this.bindActions(
+            actions.LOAD_APPS,             () => {this.onLoadStart(actions.LOAD_APPS);},
+            actions.LOAD_APPS_SUCCESS,     () => {this.onLoadEnd(actions.LOAD_APPS);},
+            actions.LOAD_APPS_FAILED,      () => {this.onLoadEnd(actions.LOAD_APPS);},
+            actions.LOAD_REPORTS,          () => {this.onLoadStart(actions.LOAD_REPORTS);},
+            actions.LOAD_REPORTS_SUCCESS,  () => {this.onLoadEnd(actions.LOAD_REPORTS);},
+            actions.LOAD_REPORTS_FAILED,   () => {this.onLoadEnd(actions.LOAD_REPORTS);},
+            actions.LOAD_REPORT,           () => {this.onLoadStart(actions.LOAD_REPORT);},
+            actions.LOAD_REPORT_SUCCESS,   () => {this.onLoadEnd(actions.LOAD_REPORT);},
+            actions.LOAD_REPORT_FAILED,    () => {this.onLoadEnd(actions.LOAD_REPORT);},
+            actions.LOAD_FIELDS,           () => {this.onLoadStart(actions.LOAD_FIELDS);},
+            actions.LOAD_FIELDS_SUCCESS,   () => {this.onLoadEnd(actions.LOAD_FIELDS);},
+            actions.LOAD_FIELDS_FAILED,    () => {this.onLoadEnd(actions.LOAD_FIELDS);},
+            actions.LOAD_RECORDS,          () => {this.onLoadStart(actions.LOAD_RECORDS);},
+            actions.LOAD_RECORDS_SUCCESS,  () => {this.onLoadEnd(actions.LOAD_RECORDS);},
+            actions.LOAD_RECORDS_FAILED,   () => {this.onLoadEnd(actions.LOAD_RECORDS);},
+
+            actions.MARK_PERF, this.onMark,
+            actions.MEASURE_PERF, this.onMeasure,
+            actions.LOG_MEASUREMENTS_PERF, this.onLogMeasurements,
+            actions.DONE_ROUTE_PERF, this.onDone,
+            actions.NEW_ROUTE_PERF, this.onNewRoute,
+
+        );
+        this.ReactPerf = null;
+        this.initInitalStats =  _.once(this.initDevConsoleStats);
+        this.logInitalStats =  _.once(this.devConsoleStats);
+        this.logger = new Logger();
+    },
+    onMark(payload) {
+        PerfLogUtils.mark(payload);
+    },
+    onMeasure(payload) {
+        PerfLogUtils.measure(payload.label, payload.start, payload.end);
+    },
+    onLogMeasurements(payload) {
+        PerfLogUtils.send(payload);
+    },
+    devConsoleStats() {
+        //browser console the performance measurements
+        ReactPerfUtils.devPerfPrint(Configuration, this.ReactPerf) ;
+    },
+    initDevConsoleStats() {
+        //init the perf measurements for development environment
+        this.ReactPerf = ReactPerfUtils.devPerfInit(Configuration, window);
+    },
+
+    onDone(payload) {
+        this.logInitalStats();
+        PerfLogUtils.done(payload);
+    },
+    onNewRoute(payload) {
+        this.initInitalStats();
+        PerfLogUtils.mark(payload);
+
+    },
+    onLoadStart(type) {
+        this.onMark("loading-" + type);
+    },
+    onLoadEnd(type) {
+        this.onMeasure({label:type, start:"loading-" + type});
+    },
+
+    getState() {
+        return {
+        };
+    },
+});
+
+export default PerfStore;
