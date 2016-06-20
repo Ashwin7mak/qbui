abea6b4f8d6a78db88cfc661bbe241157a388c6d
Matt Saforrian <matthew_saforrian@intuit.com>
2016-06-20 14:58:45 -0400

    Fix for build problem

--- ui/client-react/src/components/componentLibrary/src/Metadata.js
+++ ui/client-react/src/components/componentLibrary/src/Metadata.js
@@ -1,5 +1,5 @@
 import QBPanelMetadata from 'component-metadata!../../QBPanel/qbpanel.js';
-import QBIconMetadata from 'component-metadata!../../qbIcon/qbicon.js';
+import QBIconMetadata from 'component-metadata!../../qbIcon/qbIcon.js';
 import _ from 'lodash';
 
 var Metadata = _.merge(QBPanelMetadata, QBIconMetadata);
