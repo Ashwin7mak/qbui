d76e332d5329b7cdfb9a55e9fe3259f77f4c2df7
Ken LaBak <Ken_LaBak@intuit.com>
2016-06-28 09:16:26 -0400

    New src directory for app.js

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -109,13 +109,13 @@ module.exports = function(grunt) {
             prod   : {
                 options: {
                     debug   : false,
-                    script  : '<%= quickbase.distDir %>/<%= express.root %>/app.js',
+                    script  : '<%= quickbase.distDir %>/<%= express.root %>/src/app.js',
                     node_env: 'production'
                 }
             },
             test   : {
                 options: {
-                    script  : '<%= quickbase.distDir %>/<%= express.root %>/app.js',
+                    script  : '<%= quickbase.distDir %>/<%= express.root %>/src/app.js',
                     node_env: 'test'
                 }
             }
@@ -251,7 +251,7 @@ module.exports = function(grunt) {
         // Use nodemon to run server in debug mode with an initial breakpoint
         nodemon: {
             debug: {
-                script : '<%= express.root %>/app.js',
+                script : '<%= express.root %>/src/app.js',
                 options: {
                     nodeArgs: ['--debug-brk'],
                     env     : {
