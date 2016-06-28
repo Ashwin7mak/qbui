1b96206daad49a9dafd3709290897458cf4b179f
Beyer <rbeyer@CMBL130e9e44d.home>
2016-06-28 18:39:48 -0400

    removing how I was adding the userId to requests. Moving it to a central location within the same block of code for responses

--- ui/server/src/routes.js
+++ ui/server/src/routes.js
@@ -128,8 +128,6 @@
                         //  generate a new Transaction Id(TID) and add to the request header.
                         requestHelper.setTidHeader(req);
                         
-                        // find the userId and add to the request object
-                        requestHelper.setUserIdHeader(req);
                         //
                         //  for readability in the outputted log message, convert all double quotes to single quotes.
                         //
