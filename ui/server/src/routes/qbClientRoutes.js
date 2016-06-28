40849894f065b005f7ea60f7cc918a293f37ceff
Matt Saforrian <msaforrian@quickbase.com>
2016-06-28 10:49:22 -0400

    Fix for serving the minimized version of the file.

--- ui/server/src/routes/qbClientRoutes.js
+++ ui/server/src/routes/qbClientRoutes.js
@@ -81,11 +81,13 @@ var lodash = require('lodash');
         });
 
         app.route('/components').get(function(req, res) {
-            renderIndex(req, res, {bundleFileName: 'componentLibrary.js'});
+            var compBundleFileName = config.isProduction ? 'componentLibrary.min.js' : 'componentLibrary.js';
+            renderIndex(req, res, {bundleFileName: compBundleFileName});
         });
 
         app.route('/components/:componentName').get(function(req, res) {
-            renderIndex(req, res, {bundleFileName: 'componentLibrary.js'});
+            var compBundleFileName = config.isProduction ? 'componentLibrary.min.js' : 'componentLibrary.js';
+            renderIndex(req, res, {bundleFileName: compBundleFileName});
         });
 
         //  default application dashboard
