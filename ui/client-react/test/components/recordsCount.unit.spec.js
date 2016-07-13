d00729a1ba967974d3489f89fd162aa2eafe2924
Beyer <rbeyer@cmbl13b741054.corp.quickbase.net>
2016-07-13 11:47:53 -0400

    updated recordsCount to be cleaner, had unused variables in it. Added a new unit test to cover this component better

---
+++ ui/client-react/test/components/recordsCount.unit.spec.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import RecordCount from '../../src/components/report/recordsCount';
+
+const fakeRecordCountData = {
+    valid: {
+        recordCount: 10,
+        isFiltered: true,
+        filteredRecordCount: 5,
+        nameForRecords: "Duder"
+    },
+    invalid: {
+        recordCount: null,
+        isFiltered: false,
+        filteredRecordCount: null,
+        nameForRecords: "duder"
+    }
+};
+
+describe('Record Count tests', () => {
+    'use strict';
+
+    var component;
+
+    it('test render of component', () => {
+        component = TestUtils.renderIntoDocument(<RecordCount recordCount={fakeRecordCountData.valid.recordCount} isFiltered={fakeRecordCountData.valid.isFiltered}
+                                                              filteredRecordCount={fakeRecordCountData.valid.filteredRecordCount} nameForRecords={fakeRecordCountData.valid.nameForRecords}/>);
+        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+        var recordsCount = ReactDOM.findDOMNode(component);
+        expect(recordsCount).toBeDefined();
+    });
+
+    it('test has records', () => {
+        component = TestUtils.renderIntoDocument(<RecordCount recordCount={fakeRecordCountData.valid.recordCount} isFiltered={fakeRecordCountData.valid.isFiltered}
+                                                              filteredRecordCount={fakeRecordCountData.valid.filteredRecordCount} nameForRecords={fakeRecordCountData.valid.nameForRecords}/>);
+        var node = ReactDOM.findDOMNode(component);
+        var recordsCount = node.getElementsByClassName("recordsCount");
+        expect(recordsCount).toBeDefined();
+    });
+
+    it('test no records', () => {
+        component = TestUtils.renderIntoDocument(<RecordCount recordCount={fakeRecordCountData.invalid.recordCount} isFiltered={fakeRecordCountData.invalid.isFiltered}
+                                                                     filteredRecordCount={fakeRecordCountData.invalid.filteredRecordCount} nameForRecords={fakeRecordCountData.invalid.nameForRecords}/>);
+        var node = ReactDOM.findDOMNode(component);
+        expect(node).toBe(null);
+    });
+});
+
