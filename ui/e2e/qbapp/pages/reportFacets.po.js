8ddb8823175b41c9987731a6fb91664bc8a7c960
skamineni <skamineni@quickbase.com>
2016-07-13 10:43:24 -0400

    eslint spacing fixes

--- ui/e2e/qbapp/pages/reportFacets.po.js
+++ ui/e2e/qbapp/pages/reportFacets.po.js
@@ -181,11 +181,11 @@
             return e2ePageBase.waitForElement(element(by.className('facetSelections'))).then(function() {
                 //return e2eRetry.run(function() {
                 return element.all(by.className('selectedToken')).then(function(items) {
-                        for (var i = (items.length) - 1; i >= 0; --i) {
-                            items[i].element(by.className('clearFacet')).click();
-                            e2ePageBase.waitForElement(reportServicePage.loadedContentEl);
-                        }
-                    });
+                    for (var i = (items.length) - 1; i >= 0; --i) {
+                        items[i].element(by.className('clearFacet')).click();
+                        e2ePageBase.waitForElement(reportServicePage.loadedContentEl);
+                    }
+                });
                 //});
             });
         };
