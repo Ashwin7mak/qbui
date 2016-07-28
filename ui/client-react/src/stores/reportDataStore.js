c58e12d5fe4fb7c0b7c76527f06727e55bda99b3
Beyer <rbeyer@cmbl07558f785.corp.quickbase.net>
2016-07-28 15:46:24 -0400

    fixed a lint issue and added tests for deleteSingleRecord in recordsApi

--- ui/client-react/src/stores/reportDataStore.js
+++ ui/client-react/src/stores/reportDataStore.js
@@ -639,7 +639,7 @@ let ReportDataStore = Fluxxor.createStore({
     onDeleteReportRecordSuccess(recId) {
         const model = this.reportModel.get();
         const index = _.findIndex(model.filteredRecords, {"Record ID#": {value: recId}});
-        if (index != -1) {
+        if (index !== -1) {
             model.filteredRecords.splice(index, 1);
         } else {
             logger.error('the record to delete does not exist in the list of currently viewed records index: ${index}');
