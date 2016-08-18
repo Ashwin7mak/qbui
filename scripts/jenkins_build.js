a14af7a78cac7dfd1d79d8114634ce9f8686ee22
Beyer <rbeyer@cmbl0753fb2d3.corp.quickbase.net>
2016-08-18 12:15:04 -0400

    removing a file i committed by accident

--- scripts/jenkins_build.js
+++
@@ -1,8 +0,0 @@
-var jenkinsapi = require('jenkins-api');
-
-var jenkins = jenkinsapi.init("https://fbeyer:quickbase1@jenkins1.ci.quickbaserocks.com");
-
-jenkins.build('try-flow-ui', {GIT_BRANCH: 'delete_fit_finish'}, function(err, data) {
-    if (err){ return console.log(err); }
-    console.log(data)
-});
