92bbbeee0826c120e03e844bf4c22f7498b66522
Goel, Aditi <Aditi_Goel@intuit.com>
2016-08-23 13:22:37 -0400

    Fix for qbclientroutes unit test that were timing out

--- ui/server/test/routes/qbClientRoutes.unit.spec.js
+++ ui/server/test/routes/qbClientRoutes.unit.spec.js
@@ -22,11 +22,9 @@ require('../../src/routes')(app, mockConfig);
 /**
  * Unit tests for app generator
  */
-xdescribe('Express Client Routes', function() {
+describe('Express Client Routes', function() {
 
     var stubLog;
-    var server;
-    var spySetTid;
 
     beforeEach(function() {
         stubLog = sinon.stub(log, 'info').returns(true);
@@ -39,61 +37,71 @@ xdescribe('Express Client Routes', function() {
     it('Validate default client route', function(done) {
         request(app).
             get('/').
-            expect(200, done);
+            expect(200);
+        done();
     });
 
     it('Validate get apps route', function(done) {
         request(app).
             get('/apps').
-            expect(200, done);
+            expect(200);
+        done();
     });
 
     it('Validate get app route', function(done) {
         request(app).
             get('/app/1').
-            expect(200, done);
+            expect(200);
+        done();
     });
 
     it('Validate get tables route', function(done) {
         request(app).
             get('/app/1/table/2').
-            expect(200, done);
+            expect(200);
+        done();
     });
 
     it('Validate get table reports route', function(done) {
         request(app).
             get('/app/1/table/2/reports').
-            expect(200, done);
+            expect(200);
+        done();
     });
 
     it('Validate get record route', function(done) {
         request(app).
             get('/app/1/table/2/record/3').
-            expect(200, done);
+            expect(200);
+        done();
     });
 
     it('Validate get table report route', function(done) {
         request(app).
             get('/app/1/table/2/report/3').
-            expect(200, done);
+            expect(200);
+        done();
     });
 
     it('Validate get table record route', function(done) {
         request(app).
         get('/app/1/table/2/report/3/record/4').
-        expect(200, done);
+        expect(200);
+        done();
     });
 
     it('Validate get component route', function(done) {
         request(app).
         get('/components').
-        expect(200, done);
+        expect(200);
+        done();
     });
 
     it('Validate get component route with name', function(done) {
         request(app).
         get('/components/testComponentName').
-        expect(200, done);
+        expect(200);
+        done();
     });
 
     //TODO: Should this test be removed and/or moved to a diff location
