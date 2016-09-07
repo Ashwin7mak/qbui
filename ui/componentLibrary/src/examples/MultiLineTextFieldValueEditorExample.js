35af09d51bd4420d8f0939e365e2ed4ca3d7534e
Stookey, Brandon <bstookey@quickbase.com>
2016-09-07 16:05:30 -0400

    Remove unnecessary let

--- ui/componentLibrary/src/examples/MultiLineTextFieldValueEditorExample.js
+++ ui/componentLibrary/src/examples/MultiLineTextFieldValueEditorExample.js
@@ -1,5 +1,3 @@
-let exampleList = 'Aaliyah \n Abigail \n Addison \n Agatha \n Alex \n Alexa \n Alexandra \n Alexis';
-
 const basicMultiLineTextFieldValueEditor = (
     <div>
         <dt>Empty Text Field Editor:</dt>
