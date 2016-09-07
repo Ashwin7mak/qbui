d4a8f32372016b06b1a7d693908e507104f3ff4d
Goel, Aditi <Aditi_Goel@intuit.com>
2016-09-07 16:09:58 -0400

    Add some more relevant examples for component library

--- ui/componentLibrary/src/examples/NumericFieldValueEditorExample.js
+++ ui/componentLibrary/src/examples/NumericFieldValueEditorExample.js
@@ -1,6 +1,78 @@
 
 let editValue1 = '45.67';
 
+let EmptyNumericEditor = React.createClass({
+    getInitialState() {
+        return {
+            inputValue: editValue1
+        };
+    },
+    onChange(value) {
+        this.setState({inputValue: value});
+    },
+    onBlur(values) {
+        this.setState({inputValue: values.display});
+    },
+    render() {
+        return (
+            <div>
+                <dt>Empty Numeric Field Editor:</dt>
+                <dd>
+                    <NumericFieldValueEditor onChange={this.onChange} onBlur={this.onBlur}/>
+                </dd>
+            </div>
+        );
+    }
+});
+
+let PlaceholderNumericEditor = React.createClass({
+    getInitialState() {
+        return {
+            inputValue: editValue1
+        };
+    },
+    onChange(value) {
+        this.setState({inputValue: value});
+    },
+    onBlur(values) {
+        this.setState({inputValue: values.display});
+    },
+    render() {
+        return (
+            <div>
+                <dt>Empty Numeric Field Editor with placeholder text:</dt>
+                <dd>
+                    <NumericFieldValueEditor placeholder="placeholder text" onChange={this.onChange} onBlur={this.onBlur}/>
+                </dd>
+            </div>
+        );
+    }
+});
+
+let ValuedNumericEditor = React.createClass({
+    getInitialState() {
+        return {
+            inputValue: editValue1
+        };
+    },
+    onChange(value) {
+        this.setState({inputValue: value});
+    },
+    onBlur(values) {
+        this.setState({inputValue: values.display});
+    },
+    render() {
+        return (
+            <div>
+                <dt>Numeric Field Editor with a value (type in and tab out to see formatted values):</dt>
+                <dd>
+                    <NumericFieldValueEditor value={this.state.inputValue} onChange={this.onChange} onBlur={this.onBlur}/>
+                </dd>
+            </div>
+        );
+    }
+});
+
 var BasicNumericFieldValueEditor = React.createClass({
     getInitialState() {
         return {
@@ -10,26 +82,15 @@ var BasicNumericFieldValueEditor = React.createClass({
     onChange(value) {
         this.setState({inputValue: value});
     },
+    onBlur(values) {
+        this.setState({inputValue: values.display});
+    },
     render() {
         return (
             <div>
-                <dt>Empty Numeric Field Editor:</dt>
-                <dd>
-                    <NumericFieldValueEditor/>
-                </dd>
-
-
-                <dt>Empty Numeric Field Editor with placeholder text:</dt>
-                <dd>
-                    <NumericFieldValueEditor placeholder="placeholder text"/>
-                </dd>
-
-
-                <dt>Numeric Field Editor with a value:</dt>
-                <dd>
-                    <NumericFieldValueEditor value={this.state.inputValue} onChange={this.onChange}/>
-                </dd>
-
+                <EmptyNumericEditor />
+                <PlaceholderNumericEditor />
+                <ValuedNumericEditor />
             </div>
         );
     }
