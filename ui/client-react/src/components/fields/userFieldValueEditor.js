ba43491cc2d4e2ba97e8ae1d3fd5e852ebd18294
Stevens <dstevens@cmbl13b0a44d5.corp.quickbase.net>
2016-09-13 16:48:06 -0400

    update icons, styling user picker, add blank item for non-required user fields

--- ui/client-react/src/components/fields/userFieldValueEditor.js
+++ ui/client-react/src/components/fields/userFieldValueEditor.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Select from 'react-select';
+import QbIcon from '../qbIcon/qbIcon';
+
 import * as userFormatter from '../../../../common/src/formatter/userFormatter';
 import 'react-select/dist/react-select.min.css';
 import './userFieldValueEditor.scss';
@@ -22,7 +24,9 @@ const UserFieldValueEditor = React.createClass({
 
     selectUser(user) {
 
-        this.setState({selectedUserId: user ? user.value : null});
+        if (user || !this.props.fieldDef.required) {
+            this.setState({selectedUserId: user ? user.value : null});
+        }
     },
 
     getAppUser(id) {
@@ -33,13 +37,17 @@ const UserFieldValueEditor = React.createClass({
     getSelectItems() {
         const datatypeAttributes = this.props.fieldDef && this.props.fieldDef.datatypeAttributes ? this.props.fieldDef.datatypeAttributes : {};
 
-        return this.props.appUsers ?
+        const appUserItems = this.props.appUsers ?
             this.props.appUsers.map(user => {
                 const label = userFormatter.format({value: user}, datatypeAttributes);
                 return {
                     value: user.userId,
                     label};
             }) : [];
+
+        const blankItems = [{value:null,label:"empty"}];
+
+        return this.props.fieldDef.required ? appUserItems : blankItems.concat(appUserItems);
     },
 
     onBlur() {
@@ -57,28 +65,54 @@ const UserFieldValueEditor = React.createClass({
 
     renderOption(option) {
 
+        if (option.value === null) {
+            return <div>&nbsp;</div>;
+        }
         const user = this.getAppUser(option.value);
         const datatypeAttributes = this.props.fieldDef && this.props.fieldDef.datatypeAttributes ? this.props.fieldDef.datatypeAttributes : {};
         const userLabel = userFormatter.format({value: user}, datatypeAttributes);
 
         return (
             <div className="userOption">
+                {this.state.selectedUserId === user.userId && <QbIcon icon="check-reversed"/>}
                 <div className="userLabel">{userLabel}</div>
                 <div className="email">{user.email}</div>
             </div>);
+    },
+
+    filterOption(option, filter) {
+
+        if (option.value === null) {
+            return filter === '';
+        }
+
+        const user = this.getAppUser(option.value);
+
+        filter = filter.toLowerCase();
+
+        return user.value === null || user.email.toLowerCase().startsWith(filter) ||
+            user.firstName.toLowerCase().startsWith(filter) ||
+            user.lastName.toLowerCase().startsWith(filter) ||
+            user.screenName.toLowerCase().startsWith(filter);
     },
 
     render() {
 
         return (
             <Select
+                isOpen={true}
                 className="cellEdit"
                 tabIndex="0"
-                matchPos="start"
+                filterOption={this.filterOption}
+                autoSize={true}
                 value={this.state.selectedUserId}
                 optionRenderer={this.renderOption}
                 options={this.getSelectItems()}
                 onChange={this.selectUser}
+                placeholder="Search..."
+                noResultsText="Not found"
+                autosize={false}
+                clearable={!this.props.fieldDef.required}
                 onBlur={this.onBlur} />
         );
     }
