863230621328dd1d35a3c3d99d8b84f5fb43e82a
nvanselow <nvanselow@quickbase.com>
2016-09-19 13:55:06 -0400

    No longer need uniqueId mixin

--- ui/client-react/src/components/fields/checkBoxFieldValueEditor.js
+++ ui/client-react/src/components/fields/checkBoxFieldValueEditor.js
@@ -10,7 +10,6 @@ import './checkbox.scss';
  */
 const CheckBoxFieldValueEditor = React.createClass({
     displayName: 'CheckBoxFieldValueEditor',
-    mixins: [UniqueIdMixin],
     propTypes: {
         value: PropTypes.bool,
         onChange: PropTypes.func,
@@ -35,7 +34,14 @@ const CheckBoxFieldValueEditor = React.createClass({
     },
 
     onChange(ev) {
-        const newValue = ev.target.checked;
+        ev.preventDefault();
+
+        // Don't change the value the checkbox is disabled/read only
+        if(this.props.disabled || this.props.readyOnly){
+            return;
+        }
+
+        const newValue = !this.props.value;
         if(this.props.onChange){
             this.props.onChange(newValue);
         }
@@ -70,23 +76,23 @@ const CheckBoxFieldValueEditor = React.createClass({
             requiredSymbol = <span className={requiredSymbolClasses}>*</span>;
         }
 
-        // If a checkbox is readonly, return the renderer instead
-        if(this.props.readOnly) {
+        // If a checkbox is disabled/readonly, return the renderer instead
+        if(this.props.disabled || this.props.readOnly) {
             return <CheckBoxFieldValueRenderer {...this.props} />;
         }
-        
+
         return (
             <div className={classes}>
-                <input id={this.getNextHtmlFor()}
-                       className={checkBoxClasses}
+                <input className={checkBoxClasses}
+                       checked={this.props.value}
                        ref="fieldInput"
                        type="checkbox"
                        onChange={this.onChange}
                        onBlur={this.props.onBlur}
                        tabIndex="0"
-                       defaultChecked={this.props.value} // react requirement
                        disabled={this.props.disabled} />
-                <label className="label" htmlFor={this.getNextHtmlFor()}>
+                <label className="label"
+                       onClick={this.onChange}>
                     {this.props.label}
                 </label>
                 {requiredSymbol}
