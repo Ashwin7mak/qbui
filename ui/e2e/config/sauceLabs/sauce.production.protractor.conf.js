a6b23a303cb6e81689df6d1eee0ae17e7a3533ba
Ken LaBak <Ken_LaBak@intuit.com>
2016-09-21 11:17:13 -0400

    Added missing capability setting to config file.

--- ui/e2e/config/sauceLabs/sauce.production.protractor.conf.js
+++ ui/e2e/config/sauceLabs/sauce.production.protractor.conf.js
@@ -8,5 +8,19 @@
     globalSauceConfig.specs = [
         baseE2EPath + 'qbapp/tests/reports/prodSmokeTest.e2e.spec.js'
     ];
+    // Set the capability object to use for Sauce Labs
+    globalSauceConfig.multiCapabilities = [
+        {
+            platform : 'OS X 10.11',
+            browserName     : 'chrome',
+            tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
+            name            : process.env.SAUCE_JOB_NAME + '_OSX_Chrome',
+            screenResolution : '1600x1200',
+            //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
+            idleTimeout: '180',
+            maxDuration: 10800,
+            breakpointSize: 'large'
+        }
+    ];
     exports.config = globalSauceConfig;
 }());
