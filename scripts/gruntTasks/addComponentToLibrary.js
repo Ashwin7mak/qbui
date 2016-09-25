c4b5ac87bcc756b33af3d6badce5637a21ebc8f8
nvanselow <nvanselow@quickbase.com>
2016-09-25 11:48:36 -0400

    Separate massive function into smaller ones

--- scripts/gruntTasks/addComponentToLibrary.js
+++ scripts/gruntTasks/addComponentToLibrary.js
@@ -11,13 +11,22 @@
 /* and improve your example in src/examples
 */
 module.exports = function(grunt) {
-    grunt.registerTask('addComponentToLibrary', 'Add a React component to the component library', function(){
-        // Current Component Info
-        var componentName = grunt.option('name');
-        var componentPath = grunt.option('path');
-        var componentPathArray = componentPath.split('/');
-        var componentFileName = componentPathArray.pop();
+    // The following ables hold the comments that allow this grunt task
+    // to locate the appropriate places in each file where import (and other)
+    // statements should be placed
+    //   Common locator across files
+    var IMPORT_COMMENT = '// END OF IMPORT STATEMENTS';
+    //   Locator comments in Metadata.js (extra spaces are needed to match tab indentation)
+    var METADATA_MERGE_COMMENT = '    // END OF METADATA MERGE';
+    //   Locator comments in Examples.js (extra spaces are needed to match tab indentation)
+    var EXAMPLES_END_EXPORT_COMMENT = '    // END OF EXPORT';
 
+    function getComponentFileName(componentPath) {
+        var componentPathArray = componentPath.split('/');
+        return componentPathArray.pop();
+    }
+
+    function checkForRequiredOptions(componentName, componentPath) {
         // Must provide a component class name
         if(!componentName){
             grunt.fail.fatal('Please provide a component name with the flag --name');
@@ -32,84 +41,118 @@ module.exports = function(grunt) {
         if(!grunt.file.exists(componentPath)){
             grunt.fail.fatal('A component file does not exist at /qbui/ui/' + componentPath);
         }
+    }
 
-        // Component Library Info
-        var componentLibraryPath = 'componentLibrary/';
-        var componentLibrarySrcPath = componentLibraryPath + 'src/'
-        var componentLibraryTemplatePath = componentLibraryPath + 'templates/';
-        var docFile = componentLibrarySrcPath + 'docs/' + componentFileName;
-        var metaDataFilePath = componentLibrarySrcPath + 'components/Metadata.js';
-        var metaDataFile = grunt.file.read(metaDataFilePath);
-        var exampleFile = componentLibrarySrcPath + 'examples/' + componentName + 'Example.js';
-        var examplesFile = componentLibrarySrcPath + 'components/Examples.js';
-        var playgroundFile = componentLibrarySrcPath + 'components/ReactPlayground.js';
-        var indexFile = componentLibrarySrcPath + 'index.js';
-        var routesFile = componentLibrarySrcPath + 'components/componentLibrary.js';
-
-        var componentData = {
-            componentName: componentName,
-            componentPath: componentPath
-        };
-
+    function createDocFile(componentData) {
         // ---  Create Component Doc File ---
-        var docTemplate = grunt.file.read(componentLibraryTemplatePath + 'doc.tmpl');
-        grunt.file.write(docFile, grunt.template.process(docTemplate, {data: componentData}));
+        var docTemplate = grunt.file.read(componentData.componentLibraryTemplatePath + 'doc.tmpl');
+        grunt.file.write(componentData.docFile, grunt.template.process(docTemplate, {data: componentData}));
+    }
 
+    function updateMetadataFile(componentData) {
         // --- Update Metadata.js ---
+        var metaDataFile = grunt.file.read(componentData.metaDataFilePath);
         var metaDataFileArray = metaDataFile.split("\n");
 
-        var endOfImport = metaDataFileArray.indexOf('// END OF IMPORT STATEMENTS');
-        metaDataFileArray.splice(endOfImport, 0, 'import ' + componentName + "Metadata from 'component-metadata!../../../" + componentPath + "';");
+        var endOfImport = metaDataFileArray.indexOf(IMPORT_COMMENT);
+        metaDataFileArray.splice(endOfImport, 0, 'import ' + componentData.componentName + "Metadata from 'component-metadata!../../../" + componentData.componentPath + "';");
 
-        var endOfMerge = metaDataFileArray.indexOf('    // END OF METADATA MERGE');
+        var endOfMerge = metaDataFileArray.indexOf(METADATA_MERGE_COMMENT);
         metaDataFileArray[endOfMerge - 1] = metaDataFileArray[endOfMerge - 1] + ',';
-        metaDataFileArray.splice(endOfMerge, 0, '    ' + componentName + 'Metadata');
+        metaDataFileArray.splice(endOfMerge, 0, '    ' + componentData.componentName + 'Metadata');
 
-        grunt.file.write(metaDataFilePath, metaDataFileArray.join("\n"));
+        grunt.file.write(componentData.metaDataFilePath, metaDataFileArray.join("\n"));
+    }
 
-        // // --- Generate default example ---
-        var exampleTemplate = grunt.file.read(componentLibraryTemplatePath + 'example.tmpl');
-        grunt.file.write(exampleFile, grunt.template.process(exampleTemplate, {data: componentData}));
+    function createExample(componentData) {
+        // --- Generate default example ---
+        var exampleTemplate = grunt.file.read(componentData.componentLibraryTemplatePath + 'example.tmpl');
+        grunt.file.write(componentData.exampleFile, grunt.template.process(exampleTemplate, {data: componentData}));
+    }
 
-        // // --- Add example to Examples.js ---
-        var examplesFileArray = grunt.file.read(examplesFile).split("\n");
-        endOfImport = examplesFileArray.indexOf('// END OF IMPORT STATEMENTS');
-        examplesFileArray.splice(endOfImport, 0, 'import ' + componentName + "Example from 'raw!../examples/" + componentName + "Example.js';");
+    function addToExamples(componentData) {
+        // --- Add example to Examples.js ---
+        var examplesFileArray = grunt.file.read(componentData.examplesFile).split("\n");
+        endOfImport = examplesFileArray.indexOf(IMPORT_COMMENT);
+        examplesFileArray.splice(endOfImport, 0, 'import ' + componentData.componentName + "Example from 'raw!../examples/" + componentData.componentName + "Example.js';");
 
-        endOfMerge = examplesFileArray.indexOf('    // END OF EXPORT');
-        examplesFileArray.splice(endOfMerge, 0, '    ' + componentName + ': ' + componentName + 'Example,');
+        var endOfExport = examplesFileArray.indexOf(EXAMPLES_END_EXPORT_COMMENT);
+        examplesFileArray.splice(endOfExport, 0, '    ' + componentData.componentName + ': ' + componentData.componentName + 'Example,');
 
-        grunt.file.write(examplesFile, examplesFileArray.join("\n"));
+        grunt.file.write(componentData.examplesFile, examplesFileArray.join("\n"));
+    }
 
-        // // --- Import component to ReactPlayground.js ---
-        var playgroundFileArray = grunt.file.read(playgroundFile).split("\n");
-        endOfImport = playgroundFileArray.indexOf('// END OF REQUIRE STATEMENTS');
-        playgroundFileArray.splice(endOfImport, 0, 'const ' + componentName + " = require('../../../" + componentPath + "');");
-        grunt.file.write(playgroundFile, playgroundFileArray.join("\n"));
+    function addToPlayground(componentData) {
+        // --- Import component to ReactPlayground.js ---
+        var playgroundFileArray = grunt.file.read(componentData.playgroundFile).split("\n");
+        endOfImport = playgroundFileArray.indexOf(IMPORT_COMMENT);
+        playgroundFileArray.splice(endOfImport, 0, 'const ' + componentData.componentName + " = require('../../../" + componentData.componentPath + "');");
+        grunt.file.write(componentData.playgroundFile, playgroundFileArray.join("\n"));
+    }
 
-        // // --- Add a route to index.js ---
-        var indexFileArray = grunt.file.read(indexFile).split("\n");
+    function addToIndex(componentData) {
+        // --- Add a route to index.js ---
+        var indexFileArray = grunt.file.read(componentData.indexFile).split("\n");
 
-        endOfImport = indexFileArray.indexOf('// END OF IMPORT STATEMENTS');
-        indexFileArray.splice(endOfImport, 0, 'import ' + componentName + "Doc from './docs/" + componentFileName + "';")
+        endOfImport = indexFileArray.indexOf(IMPORT_COMMENT);
+        indexFileArray.splice(endOfImport, 0, 'import ' + componentData.componentName + "Doc from './docs/" + componentData.componentFileName + "';")
 
+        // Extra spacing here to match indentation
         var endOfRoutes = indexFileArray.indexOf('        </Route>');
-        var routeTemplate = grunt.file.read(componentLibraryTemplatePath + 'route.tmpl');
+        var routeTemplate = grunt.file.read(componentData.componentLibraryTemplatePath + 'route.tmpl');
         // Slice on the end removes extra newline
         indexFileArray.splice(endOfRoutes, 0, grunt.template.process(routeTemplate, {data: componentData}).slice(0, -1));
 
-        grunt.file.write(indexFile, indexFileArray.join("\n"));
+        grunt.file.write(componentData.indexFile, indexFileArray.join("\n"));
+    }
 
-        // // --- Add link to componentLibrary.js ---
-        var routesFileArray = grunt.file.read(routesFile).split("\n");
+    function addLinkToRoutes(componentData) {
+        // --- Add link to componentLibrary.js ---
+        var routesFileArray = grunt.file.read(componentData.routesFile).split("\n");
+        // Extra spaces here to match indentation
         var endOfLinks = routesFileArray.indexOf('                    </nav>') - 1;
-        var linkTemplate = grunt.file.read(componentLibraryTemplatePath + 'link.tmpl');
+        var linkTemplate = grunt.file.read(componentData.componentLibraryTemplatePath + 'link.tmpl');
         routesFileArray.splice(endOfLinks, 0, grunt.template.process(linkTemplate, {data: componentData}).slice(0, -1));
-        grunt.file.write(routesFile, routesFileArray.join("\n"));
+        grunt.file.write(componentData.routesFile, routesFileArray.join("\n"));
+    }
+
+    grunt.registerTask('addComponentToLibrary', 'Add a React component to the component library', function(){
+        // Current Component Info
+        var componentName = grunt.option('name');
+        var componentPath = grunt.option('path');
+        checkForRequiredOptions(componentName, componentPath);
+        var componentFileName = getComponentFileName(componentPath);
+
+        // Create an object for passing information to templates and subfunctions
+        var componentLibraryPath = 'componentLibrary/';
+        var componentLibrarySrcPath = componentLibraryPath + 'src/';
+        var componentData = {
+            componentName: componentName,
+            componentPath: componentPath,
+            // Component Library Info
+            componentLibraryPath: componentLibraryPath,
+            componentLibrarySrcPath: componentLibrarySrcPath,
+            componentLibraryTemplatePath: componentLibraryPath + 'templates/',
+            docFile: componentLibrarySrcPath + 'docs/' + componentFileName,
+            metaDataFilePath: componentLibrarySrcPath + 'components/Metadata.js',
+            exampleFile: componentLibrarySrcPath + 'examples/' + componentName + 'Example.js',
+            examplesFile: componentLibrarySrcPath + 'components/Examples.js',
+            playgroundFile: componentLibrarySrcPath + 'components/ReactPlayground.js',
+            indexFile: componentLibrarySrcPath + 'index.js',
+            routesFile: componentLibrarySrcPath + 'components/componentLibrary.js'
+        };
+
+        createDocFile(componentData);
+        updateMetadataFile(componentData);
+        createExample(componentData);
+        addToExamples(componentData);
+        addToPlayground(componentData);
+        addToIndex(componentData);
+        addLinkToRoutes(componentData);
 
         grunt.log.ok('Completed! Your component ' + componentName + ' is now in the library.');
         grunt.log.subhead('Next steps:');
-        grunt.log.writeln('1) Double check the placement of the link in the nav menu in ' + routesFile);
-        grunt.log.writeln('2) Improve your example in ' + exampleFile);
+        grunt.log.writeln('1) Double check the placement of the link in the nav menu in ' + componentData.routesFile);
+        grunt.log.writeln('2) Improve your example in ' + componentData.exampleFile);
     });
 }
