8389b18a6008dac4b1a963df11ee4c527be6ec6a
nvanselow <nvanselow@quickbase.com>
2016-09-26 18:26:28 -0400

    Add basic URL Field Renderer

---
+++ ui/client-react/src/components/fields/urlFieldValueRenderer.js
@@ -0,0 +1,42 @@
+import React, {PropTypes} from 'react';
+import TextFieldValueRenderer from './TextFieldValueRenderer';
+
+const UrlFieldValueRenderer = React.createClass({
+    displayName: 'UrlFieldValueRenderer',
+    propTypes: {
+        /*
+        * The underlying value for the URL */
+        value: PropTypes.string,
+
+        /*
+        * The value displayed to the user */
+        display: PropTypes.string,
+
+        /*
+        * If true, clicking the link will open a new window/tab */
+        openInNewWindow: PropTypes.bool,
+
+        /*
+        * If true, the link will be a button rather than a text link */
+        showAsButton: PropTypes.bool,
+
+        /*
+        * If true, the link will be clickable. Otherwise, the link will only
+        * appear as text */
+        clickable: PropTypes.bool
+    },
+    getDefaultProps() {
+        return {
+            clickable: true,
+            display: '',
+            openInNewWindow: true,
+            showAsButton: false,
+            value: ''
+        };
+    },
+    render() {
+        return <TextFieldValueRenderer {...this.props} />;
+    }
+});
+
+export default UrlFieldValueRenderer;
