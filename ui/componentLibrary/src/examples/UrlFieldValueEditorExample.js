f350ee609cab8df249310c63c120e744d67bf800
nvanselow <nvanselow@quickbase.com>
2016-09-28 14:11:19 -0400

    Better example for URL field editor in component library

--- ui/componentLibrary/src/examples/UrlFieldValueEditorExample.js
+++ ui/componentLibrary/src/examples/UrlFieldValueEditorExample.js
@@ -1,18 +1,37 @@
 // This is a basic example for the React playground
 // Please update to include other properties or states for your component
 
-const basicUrlFieldValueEditorExample = (
-    <div>
-        <dt>Default: </dt>
-        <dd>
-            <UrlFieldValueEditor />
-        </dd>
+const UrlFieldValueEditorExample = React.createClass({
+    getInitialState() {
+        return {
+            value: 'https://www.quickbase.com',
+            invalidValue: 'quickbase'
+        }
+    },
+    onChange(newValue) {
+        this.setState({ value: newValue });
+    },
+    onChangeInvalidValue(newValue) {
+        this.setState({ value: newValue });
+    },
+    render() {
+        return (
+            <div>
+                <dt>Default: </dt>
+                <dd>
+                    <UrlFieldValueEditor value={this.state.value} onChange={this.onChange} />
+                </dd>
 
-        <dt>Invalid: </dt>
-        <dd>
-            <UrlFieldValueEditor isInvalid={true} />
-        </dd>
-    </div>
-);
+                <dt>Invalid: </dt>
+                <dd>
+                    <UrlFieldValueEditor isInvalid={true}
+                                         invalidMessage="Invalid url"
+                                         value={this.state.invalidValue}
+                                         onChange={this.onChangeInvalidValue} />
+                </dd>
+            </div>
+        );
+    }
+});
 
-ReactDOM.render(basicUrlFieldValueEditorExample, mountNode);
+ReactDOM.render(<UrlFieldValueEditorExample />, mountNode);
