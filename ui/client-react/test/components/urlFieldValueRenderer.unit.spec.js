22c1d0bf29ece0b4d1cc48c39147b31f93e9124f
nvanselow <nvanselow@quickbase.com>
2016-09-30 18:40:52 -0400

    Unit tests for UrlFieldValueRenderer

---
+++ ui/client-react/test/components/urlFieldValueRenderer.unit.spec.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils, {Simulate} from 'react-addons-test-utils';
+
+import UrlFieldValueRenderer from '../../src/components/fields/UrlFieldValueRenderer';
+
+describe('UrlFieldValueRenderer', () => {
+    let component, domComponent;
+    let alternateText = 'Click Me!';
+    let urlWithProtocol = 'http://www.quickbase.com/';
+    let urlWithoutProtocol = 'www.quickbase.com';
+    let telephoneNumber = 'tel:555-555-5555';
+
+    it('displays a url as a clickable link by default', () => {
+        component = TestUtils.renderIntoDocument(<UrlFieldValueRenderer value={urlWithProtocol} display={urlWithProtocol} />);
+        domComponent = ReactDOM.findDOMNode(component).querySelector('a');
+        expect(domComponent.href).toEqual(urlWithProtocol);
+        expect(domComponent.text).toEqual(urlWithProtocol);
+    });
+
+    it('opens in a new window by default', () => {
+        component = TestUtils.renderIntoDocument(<UrlFieldValueRenderer value={urlWithProtocol} display={urlWithProtocol} />);
+        domComponent = ReactDOM.findDOMNode(component).querySelector('a');
+        expect(domComponent.target).toEqual('_blank');
+    });
+
+    it('can open a link in the same window', () => {
+        component = TestUtils.renderIntoDocument(<UrlFieldValueRenderer value={urlWithProtocol}
+                                                                        display={urlWithProtocol}
+                                                                        openInNewWindow={false} />);
+        domComponent = ReactDOM.findDOMNode(component).querySelector('a');
+        expect(domComponent.target).toEqual('_self');
+    });
+
+    it('displays only text if disabled', () => {
+        component = TestUtils.renderIntoDocument(<UrlFieldValueRenderer value={urlWithProtocol}
+                                                                        display={urlWithProtocol}
+                                                                        disabled={true} />);
+        domComponent = ReactDOM.findDOMNode(component);
+        let aTag = domComponent.querySelector('a');
+        let spanTag = domComponent.querySelector('span');
+
+        expect(aTag).toEqual(null);
+        expect(spanTag.textContent).toEqual(urlWithProtocol);
+    });
+
+    it('can display the link as a button', () => {
+        component = TestUtils.renderIntoDocument(<UrlFieldValueRenderer value={urlWithProtocol}
+                                                                        display={urlWithProtocol}
+                                                                        showAsButton={true} />);
+
+        domComponent = ReactDOM.findDOMNode(component).querySelector('a');
+
+        expect(domComponent.classList).toContain('btn');
+    });
+
+    it('can display the link as a disabled button', () => {
+        component = TestUtils.renderIntoDocument(<UrlFieldValueRenderer value={urlWithProtocol}
+                                                                        display={urlWithProtocol}
+                                                                        showAsButton={true}
+                                                                        disabled={true} />);
+
+        domComponent = ReactDOM.findDOMNode(component);
+        let aTag = domComponent.querySelector('a');
+        let spanTag = domComponent.querySelector('span');
+
+        expect(aTag).toEqual(null);
+        expect(spanTag.classList).toContain('btn');
+        expect(spanTag.classList).toContain('disabled');
+    });
+
+    it('will not display an empty button (i.e., a button without text)', () => {
+        component = TestUtils.renderIntoDocument(<UrlFieldValueRenderer value={urlWithProtocol}
+                                                                        display={null}
+                                                                        showAsButton={true} />);
+
+        domComponent = ReactDOM.findDOMNode(component).querySelector('span');
+        expect(domComponent.textContent).toEqual('');
+    });
+
+    it('adds a protocol to the url if one is missing', () => {
+        component = TestUtils.renderIntoDocument(<UrlFieldValueRenderer value={urlWithoutProtocol}
+                                                                        display={urlWithoutProtocol} />);
+
+        domComponent = ReactDOM.findDOMNode(component).querySelector('a');
+
+        expect(domComponent.href).toEqual(urlWithProtocol);
+        expect(domComponent.textContent).toEqual(urlWithoutProtocol);
+    });
+
+    it('displays alternate text for the link', () => {
+        component = TestUtils.renderIntoDocument(<UrlFieldValueRenderer value={urlWithProtocol}
+                                                                        display={alternateText} />);
+
+        domComponent = ReactDOM.findDOMNode(component).querySelector('a');
+
+        expect(domComponent.href).toEqual(urlWithProtocol);
+        expect(domComponent.textContent).toEqual(alternateText);
+    });
+
+    it('displays an icon for special protocols such as tel, email, and sms', () => {
+        // Tests for determining specific icons is in utils/urlUtils.unit.spec.js
+        component = TestUtils.renderIntoDocument(<UrlFieldValueRenderer value={telephoneNumber}
+                                                                        display={telephoneNumber} />);
+
+        domComponent = ReactDOM.findDOMNode(component).querySelector('.qbIcon');
+
+        expect(domComponent).not.toEqual(null);
+    });
+});
