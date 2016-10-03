6301b070d2a9a617c215f6814cb422e007abaa9c
nvanselow <nvanselow@quickbase.com>
2016-10-03 10:13:37 -0400

    Unit Tests for urlUtils

---
+++ ui/client-react/test/utils/urlUtils.unit.spec.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import UrlUtils from '../../src/utils/urlUtils';
+import QBicon from '../../src/components/qbIcon/qbIcon';
+
+describe('UrlUtils', () => {
+    let phoneIcon = 'phone-outline';
+    let mailIcon = 'mail';
+    let messageIcon = 'speechbubble-outline';
+    let fileIcon = 'report-summary';
+
+    describe('getIconForProtocol', () => {
+        let testCases = [
+            {
+                name: 'Returns phone icon for tel',
+                protocol: 'tel',
+                expectation: phoneIcon
+            },
+            {
+                name: 'Returns phone icon for callto',
+                protocol: 'callto',
+                expectation: phoneIcon
+            },
+            {
+                name: 'Returns the phone icon for skype',
+                protocol: 'skype',
+                expectation: phoneIcon
+            },
+            {
+                name: 'Returns the mail icon for mailto',
+                protocol: 'mailto',
+                expectation: mailIcon
+            },
+            {
+                name: 'Returns the message icon for sms',
+                protocol: 'sms',
+                expectation: messageIcon
+            },
+            {
+                name: 'Returns the message icon for imessage',
+                protocol: 'imessage',
+                expectation: messageIcon
+            },
+            {
+                name: 'Returns the file icon for files',
+                protocol: 'file',
+                expectation: fileIcon
+            },
+            {
+                name: 'Returns no icon for http',
+                protocol: 'http',
+                expectation: ''
+            },
+            {
+                name: 'Returns no icon for anything else',
+                protocol: '',
+                expectation: ''
+            }
+        ];
+
+        testCases.forEach(testCase => {
+            it(testCase.name, () => {
+                expect(UrlUtils.getIconForProtocol(testCase.protocol)).toEqual(testCase.expectation);
+            });
+        });
+    });
+
+    describe('renderIconForUrl', () => {
+        it('returns a QBicon if the provided url has a protocol', () => {
+            expect(UrlUtils.renderIconForUrl('mailto:test@quickbase.com')).toEqual(<QBicon icon={mailIcon} />);
+        });
+
+        it('returns an empty span if the provided url does not have a protocl', () => {
+            expect(UrlUtils.renderIconForUrl('www.quickbase.com')).toEqual(<span />);
+        });
+    });
+});
