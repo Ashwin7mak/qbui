b11cee18e6f2262e1922937e3a6ee0f729280f6a
Don Hatch <donald_hatch@intuit.com>
2016-10-05 09:13:58 -0400

    adjust expectation for reportsAPi integration tests with new json output on report result endpoint

--- ui/server/test/api/reportsApi.sorting.integration.spec.js
+++ ui/server/test/api/reportsApi.sorting.integration.spec.js
@@ -251,7 +251,7 @@
                             // Sort the expected records
                             var sortedExpectedRecords = sortRecords(records, testcase.sortFids, testcase.sortOrder);
                             // Verify sorted records
-                            verifyRecords(results.records.records, sortedExpectedRecords);
+                            verifyRecords(results.records, sortedExpectedRecords);
                             // No grouping
                             assert.equal(results.records.groups, null, 'Excepted groups object to be null when testing sort only report');
                             done();
