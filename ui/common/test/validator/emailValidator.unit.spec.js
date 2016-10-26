72d562883153bcb5b6c0dc5b45f240671f80ca41
nvanselow <nvanselow@quickbase.com>
2016-10-26 11:23:41 -0400

    Fix email validation to match current spec and add additional tests

--- ui/common/test/validator/emailValidator.unit.spec.js
+++ ui/common/test/validator/emailValidator.unit.spec.js
@@ -5,43 +5,88 @@ var sinon = require('sinon');
 var emailValidator = require('../../src/validator/emailValidator');
 
 describe('emailValidator', () => {
-    describe('validate', () => {
+    describe.only('validate', () => {
         let testCases = [
             {
                 name: 'returns true if an email is valid',
                 email: 'valid@quickbase.com',
-                validationType: null,
                 expectation: true
             },
             {
                 name: 'returns false if an email is invalid',
                 email: 'somethinginvalid./',
-                validationType: null,
+                expectation: false
+            },
+            {
+                name: 'returns false if the hostname is missing',
+                email: 'something.com',
+                expectation: false
+            },
+            {
+                name: 'returns false if the TLD is missing',
+                email: 'something@something',
+                expectation: false
+            },
+            {
+                name: 'returns false if the mailbox is missing',
+                email: '@something.com',
                 expectation: false
             },
             {
                 name: 'does not validate an empty string',
                 email: '',
-                validationType: null,
                 expectation: true
             },
             {
-                name: 'can optionally only check the domain',
-                email: '@domain.com',
-                validationType: emailValidator.ONLY_VALIDATE_DOMAIN,
+                name: 'correctly validates international TLDs',
+                email: 'test@test.de',
                 expectation: true
             },
             {
-                name: 'can optionally only check the mailbox',
-                email: 'mailbox',
-                validationType: emailValidator.ONLY_VALIDATE_MAILBOX,
+                name: 'correctly validates long TLDs',
+                email: 'test@test.expert',
                 expectation: true
+            },
+            {
+                name: 'correctly validates emails with multiple subdomains',
+                email: 'test@test.subdomain.quickbase.com',
+                expectation: true
+            },
+            {
+                name: 'correctly validates emails with special characters as part of the mailbox',
+                email: 'test!&$!@emai-l.com',
+                expectation: true
+            },
+            {
+                name: 'returns false if there are special characters in the domain (not including hyphen)',
+                email: 'test@em&$%^.com',
+                expectation: false
+            },
+            {
+                name: 'returns false if there are special characters in the TLD',
+                email: 'test@email.&%^em',
+                expectation: false
+            },
+            {
+                name: 'returns false if the domain does not start with a letter, number',
+                email: 'test@-test.com',
+                expectation: false
+            },
+            {
+                name: 'returns true if all emails in a list are valid',
+                email: 'test@test.com; me@quickbase.com; whatsup@chocalat.fr',
+                expectation: true
+            },
+            {
+                name: 'returns false if any email in a list is invalid',
+                email: 'test@test.com; me@quickbaseinvalid; whatsup@chocolat.fr',
+                expectation: false
             }
         ];
 
         testCases.forEach((testCase) => {
             it(testCase.name, () => {
-                var result = emailValidator.isValid(testCase.email, testCase.validationType);
+                var result = emailValidator.isValid(testCase.email);
                 assert.equal(result, testCase.expectation, 'Validator did not return the correct result: ' + result + ' (actual) is not ' + testCase.expectation);
             });
         });
