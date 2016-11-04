4609fac16af7dc599e2285d4544ac8ec79c6ffd4
Stevens <dstevens@CMBL1289dd98c.local>
2016-11-04 10:59:26 -0400

    disable some more unimplemented menu items/icons

--- ui/client-react/src/components/report/reportToolsAndContent.js
+++ ui/client-react/src/components/report/reportToolsAndContent.js
@@ -126,11 +126,8 @@ const ReportToolsAndContent = React.createClass({
     getPageActions(maxButtonsBeforeMenu) {
         const actions = [
             {msg: 'pageActions.addRecord', icon:'add'},
-            {msg: 'pageActions.favorite', icon:'star'},
-            {msg: 'pageActions.gridEdit', icon:'report-grid-edit'},
-            {msg: 'pageActions.email', icon:'mail'},
-            {msg: 'pageActions.print', icon:'print'},
-            {msg: 'pageActions.customizeReport', icon:'settings-hollow'},
+            {msg: 'pageActions.favorite', icon:'star', disabled: true},
+            {msg: 'pageActions.print', icon:'print', disabled: true},
         ];
         return (<IconActions className="pageActions" actions={actions} maxButtonsBeforeMenu={maxButtonsBeforeMenu}/>);
     },
