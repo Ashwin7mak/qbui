6730ca827e8f8a6c21d5245c30e17fbe3782c88a
Stookey, Brandon <bstookey@quickbase.com>
2016-11-10 14:55:52 -0500

    remove break tags

--- ui/client-react/src/components/dts/dtsErrorModal.js
+++ ui/client-react/src/components/dts/dtsErrorModal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import QBModal from '../qbModal/qbModal';
 import UrlUtils from '../../utils/urlUtils';
 import {I18nMessage} from '../../../src/utils/i18nMessage';
+import './dtsErrorModal.scss';
 
 const DTSErrorModal = React.createClass({
     ///There will be no state
@@ -15,10 +16,17 @@ const DTSErrorModal = React.createClass({
     },
     render() {
         const title = <I18nMessage message={'dtsErrorModal.dtsErrorTitle'} />;
-        const errorMessage = <div> <I18nMessage message={'dtsErrorModal.dtsErrorBodyMessage'} /><br/>
-                            <I18nMessage message={'dtsErrorModal.dtsErrorSecondErrorBodyMessage'} />
-                            <br /><br />
-                            <I18nMessage message={'dtsErrorModal.dtsErrorTID'} /><p>{this.props.tid}</p>
+        const errorMessage = <div className="dtsErrorModal">
+                                <span className="dtsErrorBodyMessageOne">
+                                    <I18nMessage message={'dtsErrorModal.dtsErrorBodyMessage'} />
+                                </span>
+                                <span className="dtsErrorBodyMessageTwo">
+                                    <I18nMessage message={'dtsErrorModal.dtsErrorSecondErrorBodyMessage'} />
+                                </span>
+                                <span className="dtsErrorTID">
+                                    <I18nMessage message={'dtsErrorModal.dtsErrorTID'} />
+                                    <p>{this.props.tid}</p>
+                                </span>
                             </div>;
         const primaryButtonName = <I18nMessage message={'dtsErrorModal.dtsErrorPrimaryButtonText'} />;
         return (
