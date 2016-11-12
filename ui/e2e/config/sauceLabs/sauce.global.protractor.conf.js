6eb503def82e9da9967fd774dda9cd0d0c90d6e4
lakshmi kamineni <skamineni@quickbase.com>
2016-11-11 22:03:17 -0500

    enabled formAdd and formEdit

--- ui/e2e/config/sauceLabs/sauce.global.protractor.conf.js
+++ ui/e2e/config/sauceLabs/sauce.global.protractor.conf.js
@@ -22,11 +22,10 @@
         // sauceSeleniumAddress: 'localhost:4445/wd/hub',
         // list of files / patterns to load in the browser
         specs: [
-            baseE2EPath + 'qbapp/tests/forms/*.e2e.spec.js',
-            //baseE2EPath + 'qbapp/tests/reports/reportFacets.e2e.spec.js',
-            //baseE2EPath + 'qbapp/tests/reports/reportSortingViaColumnHeader.e2e.spec.js',
-            //baseE2EPath + 'qbapp/tests/forms/formAdd.e2e.spec.js',
-            //baseE2EPath + 'qbapp/tests/forms/formEdit.e2e.spec.js'
+            baseE2EPath + 'qbapp/tests/reports/reportFacets.e2e.spec.js',
+            baseE2EPath + 'qbapp/tests/reports/reportSortingViaColumnHeader.e2e.spec.js',
+            baseE2EPath + 'qbapp/tests/forms/formAdd.e2e.spec.js',
+            baseE2EPath + 'qbapp/tests/forms/formEdit.e2e.spec.js'
         ],
         // Patterns to exclude.
         exclude: [
