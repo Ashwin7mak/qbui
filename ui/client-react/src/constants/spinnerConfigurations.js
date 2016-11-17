f145d41bf3239b177a6b2fa671720063a2de3392
Claire Martinez <cmartinez@quickbase.com>
2016-11-17 17:19:04 -0500

    colors for spinners

--- ui/client-react/src/constants/spinnerConfigurations.js
+++ ui/client-react/src/constants/spinnerConfigurations.js
@@ -3,6 +3,11 @@
  */
 import _ from 'lodash';
 
+//TODO  need a way to share scss vars in js
+let colorBlue500 = '#7090ab';
+let colorBlack100 = '#ffffff';
+let spinnerZindex = 2e9
+
 const BASE_SPINNER = {
     //http://spin.js.org/#?lines=7&length=0&width=16&radius=17&scale=1.00&corners=1.0&opacity=0&rotate=0&direction=1&speed=1.0&trail=60&top=50&left=50
     lines: 7,
@@ -17,7 +22,7 @@ const BASE_SPINNER = {
     speed: 1.0,
     trail: 60,
     fps: 20,
-    zIndex: 2000000000,
+    zIndex: spinnerZindex,
     className: 'spinner',
     shadow: false,
     hwaccel: false,
@@ -46,7 +51,9 @@ export const SMALL_SPINNER =  _.merge({}, BASE_SPINNER, {
 export const LARGE_BREAKPOINT_REPORT = LARGE_ABSOLUTE_CENTERED_SPINNER;
 
 // Used in the left navbar when apps or tables are loading
-export const LEFT_NAV_BAR = LARGE_RELATIVE_SPINNER;
+export const LEFT_NAV_BAR =  _.merge({}, LARGE_RELATIVE_SPINNER, {
+    color:colorBlack100
+});
 
 // Used in records count is not initially available very large datasets
 export const RECORD_COUNT = SMALL_SPINNER;
@@ -55,5 +62,8 @@ export const RECORD_COUNT = SMALL_SPINNER;
 export const INLINE_SAVING = SMALL_SPINNER;
 
 // Use on next swipe area in CARD_VIEW
-export const CARD_VIEW_NAVIGATION = SMALL_SPINNER;
+export const CARD_VIEW_NAVIGATION =  _.merge({}, SMALL_SPINNER, {
+    color:colorBlue500
+});
+
 
