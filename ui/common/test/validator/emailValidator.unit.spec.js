1f5a4332b9e2c76cd458bf54fc4c7eae0436d489
nvanselow <nvanselow@quickbase.com>
2016-11-21 19:50:10 -0500

    MB1246-Update email validator so that it can identify which emails in a list are invalid

--- ui/common/test/validator/emailValidator.unit.spec.js
+++ ui/common/test/validator/emailValidator.unit.spec.js
@@ -6,101 +6,133 @@ var emailValidator = require('../../src/validator/emailValidator');
 var dataErrorCodes = require('../../src/dataEntryErrorCodes');
 
 describe('emailValidator', () => {
-    describe('validate', () => {
+    describe('isSingleEmailValid', () => {
         let testCases = [
             {
-                name: 'returns true if an email is valid',
+                description: 'returns true if an email is valid',
                 email: 'valid@quickbase.com',
                 expectation: true
             },
             {
-                name: 'returns false if an email is invalid',
+                description: 'returns false if an email is invalid',
                 email: 'somethinginvalid./',
                 expectation: false
             },
             {
-                name: 'returns false if the hostname is missing',
+                description: 'returns false if the hostname is missing',
                 email: 'something.com',
                 expectation: false
             },
             {
-                name: 'returns false if the TLD is missing',
+                description: 'returns false if the TLD is missing',
                 email: 'something@something',
                 expectation: false
             },
             {
-                name: 'returns false if the mailbox is missing',
+                description: 'returns false if the mailbox is missing',
                 email: '@something.com',
                 expectation: false
             },
             {
-                name: 'does not validate an empty string',
+                description: 'does not validate an empty string',
                 email: '',
                 expectation: true
             },
             {
-                name: 'correctly validates international TLDs',
+                description: 'correctly validates international TLDs',
                 email: 'test@test.de',
                 expectation: true
             },
             {
-                name: 'correctly validates long TLDs',
+                description: 'correctly validates long TLDs',
                 email: 'test@test.expert',
                 expectation: true
             },
             {
-                name: 'correctly validates emails with multiple subdomains',
+                description: 'correctly validates emails with multiple subdomains',
                 email: 'test@test.subdomain.quickbase.com',
                 expectation: true
             },
             {
-                name: 'correctly validates emails with special characters as part of the mailbox',
+                description: 'correctly validates emails with special characters as part of the mailbox',
                 email: 'test!&$!@emai-l.com',
                 expectation: true
             },
             {
-                name: 'returns false if there are special characters in the domain (not including hyphen)',
+                description: 'returns false if there are special characters in the domain (not including hyphen)',
                 email: 'test@em&$%^.com',
                 expectation: false
             },
             {
-                name: 'returns false if there are special characters in the TLD',
+                description: 'returns false if there are special characters in the TLD',
                 email: 'test@email.&%^em',
                 expectation: false
             },
             {
-                name: 'returns false if the domain does not start with a letter, number',
+                description: 'returns false if the domain does not start with a letter, number',
                 email: 'test@-test.com',
                 expectation: false
             },
-            {
-                name: 'returns true if all emails in a list are valid',
-                email: 'test@test.com; me@quickbase.com; whatsup@chocalat.fr',
-                expectation: true
-            },
-            {
-                name: 'returns false if any email in a list is invalid',
-                email: 'test@test.com; me@quickbaseinvalid; whatsup@chocolat.fr',
-                expectation: false
-            }
         ];
 
         testCases.forEach((testCase) => {
-            it(testCase.name, () => {
-                var result = emailValidator.isValid(testCase.email);
+            it(testCase.description, () => {
+                var result = emailValidator.isSingleEmailValid(testCase.email);
                 assert.equal(result, testCase.expectation, 'Validator did not return the correct result: ' + result + ' (actual) is not ' + testCase.expectation);
             });
         });
     });
 
-    describe('isInvalid', () => {
+    describe('isSingleEmailInvalid', () => {
         it('is a helper method to match the React property isInvalid (returns opposite result of validate)', () => {
-            sinon.spy(emailValidator, 'isValid');
+            sinon.spy(emailValidator, 'isSingleEmailValid');
 
-            var result = emailValidator.isInvalid('test@quickbase.com');
+            var result = emailValidator.isSingleEmailInvalid('test@quickbase.com');
 
             assert.equal(result, false);
-            assert(emailValidator.isValid.calledOnce);
+            assert(emailValidator.isSingleEmailValid.calledOnce);
+        });
+    });
+
+    describe('validateArrayOfEmails', () => {
+        let invalidEmailA = 'invalidEmail';
+        let invalidEmailB = 'foo@test';
+
+        let testCases = [
+            {
+                description: 'returns as valid if all emails in the Array are valid',
+                emails: ['valid@test.com', 'bob@quickbase.com', 'sue@gmail.com'],
+                expectedIsValidResult: true,
+                expectedArrayOfInvalidEmails: []
+            },
+            {
+                description: 'returns as valid if the array of emails is empty',
+                emails: [],
+                expectedIsValidResult: true,
+                expectedArrayOfInvalidEmails: []
+            },
+            {
+                description: 'returns as valid if null is passed in as an argument',
+                emails: null,
+                expectedIsValidResult: true,
+                expectedArrayOfInvalidEmails: []
+            },
+            {
+                description: 'returns as invalid if any email in the array is invalid (And provides an array of emails with their index in the array)',
+                emails: ['valid@test.com', invalidEmailA, 'anotherValidEmail@quickbase.de', invalidEmailB],
+                expectedIsValidResult: false,
+                expectedArrayOfInvalidEmails: [{index: 1, email: invalidEmailA}, {index: 3, email: invalidEmailB}]
+            },
+        ];
+
+        testCases.forEach((testCase) => {
+            it(testCase.description, () => {
+                let validationResult = emailValidator.validateArrayOfEmails(testCase.emails);
+
+                assert.equal(validationResult.isValid, testCase.expectedIsValidResult);
+                assert.equal(validationResult.isInvalid, !testCase.expectedIsValidResult);
+                assert.deepEqual(validationResult.invalidEmails, testCase.expectedArrayOfInvalidEmails);
+            });
         });
     });
 
@@ -108,20 +140,22 @@ describe('emailValidator', () => {
         let fieldName = 'email';
 
         it('returns a result, to be used with validatorUtils, if the email is invalid', () => {
+            let invalidEmail = 'invalidEmail';
+
             let expectedResult = {
                 isInvalid: true,
                 error: {
                     code: dataErrorCodes.INVALID_ENTRY,
                     messageId: 'invalidMsg.email',
-                    data: {fieldName: fieldName}
+                    data: {fieldName: fieldName, invalidEmails: [{index: 0, email: invalidEmail}]}
                 }
             };
 
-            assert.deepEqual(emailValidator.validateAndReturnResults('invalidemail', fieldName), expectedResult);
+            assert.deepEqual(emailValidator.validateAndReturnResults(invalidEmail, fieldName), expectedResult);
         });
 
         it('passes through the result if the email is valid', () => {
-            let requiredResult = {
+            let expectedResult = {
                 isInvalid: true,
                 error: {
                     messageId: 'invalidMsg.required',
@@ -133,7 +167,7 @@ describe('emailValidator', () => {
                 }
             };
 
-            assert.deepEqual(emailValidator.validateAndReturnResults('valid@test.com', fieldName, requiredResult), requiredResult);
+            assert.deepEqual(emailValidator.validateAndReturnResults('valid@test.com', fieldName, expectedResult), expectedResult);
         });
     });
 });
