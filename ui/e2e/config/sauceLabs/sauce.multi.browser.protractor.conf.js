d92e09b88c877fc73e8bfed6e5732894d4494850
lakshmi kamineni <skamineni@quickbase.com>
2016-11-23 10:41:11 -0500

    Fixed the config and added version

--- ui/e2e/config/sauceLabs/sauce.multi.browser.protractor.conf.js
+++ ui/e2e/config/sauceLabs/sauce.multi.browser.protractor.conf.js
@@ -61,8 +61,9 @@
         {
             platform: 'OS X 10.11',
             browserName: 'MicrosoftEdge',
+            version: '14.14393',
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-            name: 'aws_' + process.env.SAUCE_JOB_NAME + '_OSX_MicrosoftEdge',
+            name: 'aws_' + process.env.SAUCE_JOB_NAME + '_Win10_MicrosoftEdge',
             screenResolution : '1600x1200',
             //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
             idleTimeout: '180',
