71387e92272026c805bc210e2b55d564f2c74e2d
Brandon Stookey <bstookey@quickbase.com>
2016-11-23 13:21:42 -0500

    Clean up comments

--- ui/client-react/src/components/fields/phoneFieldValueRenderer.js
+++ ui/client-react/src/components/fields/phoneFieldValueRenderer.js
@@ -3,23 +3,13 @@ import QBicon from '../../components/qbIcon/qbIcon.js';
 import Breakpoints from "../../utils/breakpoints";
 import './fields.scss';
 import './phoneField.scss';
-/**
- * # TextFieldValueRenderer
- *
- * A TextFieldValueRenderer is a read only rendering of the field containing a single line text.
- *
- * The value can be rendered as bold or not and classes can be optionally pass in for custom styling.
- */
+
 const PhoneFieldValueRenderer = React.createClass({
     displayName: 'PhoneFieldValueRenderer',
     propTypes: {
         /**
          * the value to render */
         value: React.PropTypes.any,
-
-        /**
-         * optional additional classes for the input to customize styling */
-        classes: React.PropTypes.string,
         /**
          * text field attributes
          */
@@ -61,9 +51,6 @@ const PhoneFieldValueRenderer = React.createClass({
         }
     },
     render() {
-        // Remove value from props to be replaced by the formatted email
-        let {value} = this.props;
-        value = (value || this.props.display);
         return (
             <div className = "phoneWrapper">
                 {this.renderLink()}
