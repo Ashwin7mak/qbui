8da6f28d57261c98256afdee5f5fba0f807b802c
lakshmi kamineni <skamineni@quickbase.com>
2016-11-23 10:27:05 -0500

    Fixed the name

--- ui/e2e/config/sauceLabs/sauce.multi.browser.protractor.conf.js
+++ ui/e2e/config/sauceLabs/sauce.multi.browser.protractor.conf.js
@@ -62,7 +62,7 @@
             platform: 'OS X 10.11',
             browserName: 'MicrosoftEdge',
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-            name: 'aws_' + process.env.SAUCE_JOB_NAME + '_OSX_Firefox',
+            name: 'aws_' + process.env.SAUCE_JOB_NAME + '_OSX_MicrosoftEdge',
             screenResolution : '1600x1200',
             //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
             idleTimeout: '180',
