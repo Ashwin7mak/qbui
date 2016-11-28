952f8fd7cdd94c2e54303202d6959a64290aa913
lakshmi kamineni <skamineni@quickbase.com>
2016-11-28 13:00:38 -0500

    fixed to work on edge

--- ui/e2e/qbapp/tests/forms/formAdd.e2e.spec.js
+++ ui/e2e/qbapp/tests/forms/formAdd.e2e.spec.js
@@ -42,41 +42,43 @@
             var origRecordCount;
             var fieldTypeClassNames = ['textField', 'numericField', 'dateCell', 'timeCell', 'checkbox'];
 
-            // Load the List All report
-            e2eBase.reportService.loadReportByIdInBrowser(realmName, app.id, app.tables[e2eConsts.TABLE1].id, 1);
-            reportContentPage.waitForReportContent().then(function() {
-                // Count the number of records before adding
-                reportContentPage.agGridRecordElList.then(function(records) {
-                    origRecordCount = records.length;
-                });
-            }).then(function() {
-                // Click on add record button
-                reportServicePage.clickAddRecordOnStage();
-            }).then(function() {
-                // Get the fields from the from and create a new record
-                for (var i = 0; i < fieldTypeClassNames.length; i++) {
-                    formsPage.enterFormValues(fieldTypeClassNames[i]);
-                }
-            }).then(function() {
-                // Save the form
-                formsPage.clickFormSaveBtn();
-                reportContentPage.waitForReportContent();
-            }).then(function() {
-                // Reload the report
+            reportServicePage.waitForElement(reportServicePage.appsListDivEl).then(function() {
+                // Load the List All report
                 e2eBase.reportService.loadReportByIdInBrowser(realmName, app.id, app.tables[e2eConsts.TABLE1].id, 1);
-                reportContentPage.waitForReportContent();
-            }).then(function() {
-                // Check the record count
-                reportContentPage.agGridRecordElList.then(function(records) {
-                    expect(records.length).toBe(origRecordCount + 1);
+                reportContentPage.waitForReportContent().then(function() {
+                    // Count the number of records before adding
+                    reportContentPage.agGridRecordElList.then(function(records) {
+                        origRecordCount = records.length;
+                    });
+                }).then(function() {
+                    // Click on add record button
+                    reportServicePage.clickAddRecordOnStage();
+                }).then(function() {
+                    // Get the fields from the from and create a new record
+                    for (var i = 0; i < fieldTypeClassNames.length; i++) {
+                        formsPage.enterFormValues(fieldTypeClassNames[i]);
+                    }
+                }).then(function() {
+                    // Save the form
+                    formsPage.clickFormSaveBtn();
+                    reportContentPage.waitForReportContent();
+                }).then(function() {
+                    // Reload the report
+                    e2eBase.reportService.loadReportByIdInBrowser(realmName, app.id, app.tables[e2eConsts.TABLE1].id, 1);
+                    reportContentPage.waitForReportContent();
+                }).then(function() {
+                    // Check the record count
+                    reportContentPage.agGridRecordElList.then(function(records) {
+                        expect(records.length).toBe(origRecordCount + 1);
+                    });
+                }).then(function() {
+                    // Verify new record is now the last row in a table
+                    for (var j = 0; j < fieldTypeClassNames.length; j++) {
+                        formsPage.verifyFieldValuesInReportTable(origRecordCount, fieldTypeClassNames[j]);
+                    }
+                }).then(function() {
+                    done();
                 });
-            }).then(function() {
-                // Verify new record is now the last row in a table
-                for (var j = 0; j < fieldTypeClassNames.length; j++) {
-                    formsPage.verifyFieldValuesInReportTable(origRecordCount, fieldTypeClassNames[j]);
-                }
-            }).then(function() {
-                done();
             });
         });
     });
