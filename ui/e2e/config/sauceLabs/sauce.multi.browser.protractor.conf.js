a2ac610c07f853318af1c21edaa4fe39539e3c49
lakshmi kamineni <skamineni@quickbase.com>
2016-12-02 15:35:25 -0500

    removed commented out code

--- ui/e2e/config/sauceLabs/sauce.multi.browser.protractor.conf.js
+++ ui/e2e/config/sauceLabs/sauce.multi.browser.protractor.conf.js
@@ -64,7 +64,6 @@
             platform: 'Windows 10',
             browserName: 'MicrosoftEdge',
             version: '14.14393',
-            //version: '13.10586',
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
             name: 'aws_' + process.env.SAUCE_JOB_NAME + '_Win10_MicrosoftEdge',
             screenResolution : '1600x1200',
