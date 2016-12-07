993c8f3b2fbcf56b50283da072280f5517c2b675
Brandon Stookey <bstookey@quickbase.com>
2016-12-07 16:12:09 -0500

    Remove unneeded value

--- ui/componentLibrary/src/examples/PhoneFieldValueRendererExample.js
+++ ui/componentLibrary/src/examples/PhoneFieldValueRendererExample.js
@@ -6,13 +6,11 @@ const basicPhoneFieldValueRendererExample = (
         <dt>Phone Number Without Ext:</dt>
         <p>(Will Display smsIcon and phoneIcon on smallBreakpoint)</p>
         <dd>
-            <PhoneFieldValueRenderer display="(555) 555-5555"
-                                     value="5555555555" />
+            <PhoneFieldValueRenderer display="(555) 555-5555"/>
         </dd>
         <dt>Phone Number With Ext: </dt>
         <dd>
-            <PhoneFieldValueRenderer display="(555) 555-5555 x5555"
-                                     value="5555555555" />
+            <PhoneFieldValueRenderer display="(555) 555-5555 x5555"/>
         </dd>
     </div>
 );
