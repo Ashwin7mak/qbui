333223c60e0f1d029f9493655075529b7a4b99d6
lakshmi kamineni <skamineni@quickbase.com>
2016-12-07 13:27:29 -0500

    Fixed the prodSmokeTest to take care of walkMe and also added a test as admin to switch from V2 to V3.

--- ui/e2e/qbapp/tests/prodSmokeTest.e2e.spec.js
+++ ui/e2e/qbapp/tests/prodSmokeTest.e2e.spec.js
@@ -12,21 +12,23 @@
     var ReportServicePage = require('../pages/reportService.po');
     var CurrentStackLoginPage = require('../pages/currentStackLogin.po');
     var CurrentStackReportsPage = require('../pages/currentStackReports.po');
+    var WalkMePage = require('../pages/walkMe.po');
+    var v2Tov3Page = require('../pages/v2Tov3.po');
 
     var reportServicePage = new ReportServicePage();
     var currentStackLoginPage = new CurrentStackLoginPage();
     var currentStackReportsPage = new CurrentStackReportsPage();
+    var walkMePage = new WalkMePage();
+    var v2Tov3PO = new v2Tov3Page();
 
     describe('New Stack Production Smoke Test', function() {
 
-        var CURRENT_STACK_PROD_REALM = 'https://team.quickbase.com';
-        var CURRENT_STACK_APPID = 'bkj4bmc3s';
-        var CURRENT_STACK_TABLEID = 'bkj4bmc7r';
+        var PROD_REALM = 'https://team.quickbase.com';
+        var APPID = 'bkj4bmc3s';
+        var TABLEID = 'bkj4bmc7r';
 
         var CURRENT_STACK_USERNAME = 'QuickBaseall';
         var CURRENT_STACK_PASSWORD = 'quickbase1';
-
-        var NEW_STACK_PROD_REALM = 'https://team.newstack.quickbase.com';
 
         var currentStackRecordCount;
         var currentStackRecordValues;
@@ -38,39 +40,35 @@
          */
         beforeAll(function(done) {
             // Log in to current stack env
-            browser.get(CURRENT_STACK_PROD_REALM);
-
-            // Enter login creds
-            currentStackLoginPage.loginUser(CURRENT_STACK_USERNAME, CURRENT_STACK_PASSWORD);
-
-            // Go to Bicycle app
-            browser.get(CURRENT_STACK_PROD_REALM + '/db/' + CURRENT_STACK_APPID);
-
-            // Go to List All report of test table
-            browser.get(CURRENT_STACK_PROD_REALM + '/db/' + CURRENT_STACK_TABLEID + '?a=q&qid=1');
-
-            // Count current stack records in report
-            currentStackReportsPage.getRecordCount().then(function(count) {
-                currentStackRecordCount = count;
-            });
-
-            // Get a record to compare values on the new stack
-            currentStackReportsPage.getRecordFromReportTable(testRecordIndex).then(function(recordEl) {
-                currentStackReportsPage.getRecordValues(recordEl).then(function(values) {
-                    currentStackRecordValues = values;
+            return browser.get(PROD_REALM).then(function() {
+                // Enter login creds
+                return currentStackLoginPage.loginUser(CURRENT_STACK_USERNAME, CURRENT_STACK_PASSWORD);
+            }).then(function() {
+                // Go to Bicycle app
+                return browser.get(PROD_REALM + '/db/' + APPID);
+            }).then(function() {
+                // Go to List All report of test table
+                return browser.get(PROD_REALM + '/db/' + TABLEID + '?a=q&qid=1');
+            }).then(function() {
+                // Count current stack records in report
+                return currentStackReportsPage.getRecordCount().then(function(count) {
+                    currentStackRecordCount = count;
                 });
-            });
-
-            // Log in to the new stack env and load the List All report
-            browser.get(NEW_STACK_PROD_REALM + '/qbase/app/' + CURRENT_STACK_APPID + '/table/' + CURRENT_STACK_TABLEID + '/report/1');
-            done();
-        });
-
-        /**
-         * Before each test starts just make sure the report has loaded in the UI
-         */
-        beforeEach(function(done) {
-            reportServicePage.waitForElement(reportServicePage.agGridContainerEl).then(function() {
+            }).then(function() {
+                // Get a record to compare values on the new stack
+                return currentStackReportsPage.getRecordFromReportTable(testRecordIndex).then(function(recordEl) {
+                    return currentStackReportsPage.getRecordValues(recordEl).then(function(values) {
+                        currentStackRecordValues = values;
+                    });
+                });
+            }).then(function() {
+                // Log in to the new stack env and load the List All report
+                return browser.get(PROD_REALM + '/qbase/apps/').then(function() {
+                    return reportServicePage.waitForElement(reportServicePage.appsListDivEl);
+                });
+            }).then(function() {
+                //dismiss walkme
+                walkMePage.dismissWalkMe();
                 done();
             });
         });
@@ -79,17 +77,38 @@
          * Test methods
          */
         it('Verify the specified record in the List All report matches the one from the current stack', function(done) {
-            // Get the records out of the table report
-            reportServicePage.agGridRecordElList.then(function(records) {
-                // Assert we have all records being displayed from current stack
-                expect(records.length).toBe(currentStackRecordCount);
-                // Check the values of the selected record
-                reportServicePage.getRecordValues(records[testRecordIndex]).then(function(newStackRecordValues) {
-                    // Currently showing record Id column on new stack so don't use that in assert
-                    newStackRecordValues.shift();
-                    expect(newStackRecordValues).toEqual(currentStackRecordValues);
-                    done();
+            browser.get(PROD_REALM + '/qbase/app/' + APPID + '/table/' + TABLEID + '/report/1').then(function() {
+                // Get the records out of the table report
+                reportServicePage.agGridRecordElList.then(function(records) {
+                    // Assert we have all records being displayed from current stack
+                    expect(records.length).toBe(currentStackRecordCount);
+                    // Check the values of the selected record
+                    reportServicePage.getRecordValues(records[testRecordIndex]).then(function(newStackRecordValues) {
+                        // Currently showing record Id column on new stack so don't use that in assert
+                        newStackRecordValues.shift();
+                        expect(newStackRecordValues).toEqual(currentStackRecordValues);
+                        done();
+                    });
                 });
+            });
+        });
+
+        it('Verify admin(quickbaseall) can switch from V3 to V2 from user Menu', function(done) {
+            // Log in to the new stack env and go to Bicycle app
+            browser.get(PROD_REALM + '/qbase/app/' + APPID).then(function() {
+                //wait untill table lists loaded at leftNav
+                reportServicePage.waitForElement(reportServicePage.tablesListDivEl);
+            }).then(function() {
+                //select switch to quickbase classic Toggle under user Menu
+                v2Tov3PO.clickUserMenuItem('Switch to QuickBase Classic');
+            }).then(function() {
+                //verify its switched to classic view (ie V2)
+                browser.getCurrentUrl().then(function(url) {
+                    expect(url).toBe(PROD_REALM + '/db/' + APPID);
+                });
+                //TODO switch back to V3 once the code is available in production
+            }).then(function() {
+                done();
             });
         });
 
