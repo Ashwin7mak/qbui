5bff1fe130d2b76cb9715a632c26cab474deb259
nvanselow <nvanselow@quickbase.com>
2016-12-08 17:24:09 -0500

    MB1732-Use plan table with a loader for loading screen

--- ui/server/src/routes/loadingScreen.jsx
+++ ui/server/src/routes/loadingScreen.jsx
@@ -12,36 +12,75 @@ var LoadingScreen = () => {
                     <div className="addButton"></div>
                 </div>
                 <div className="mainContent">
-                    {/*<div className="sk-circle">*/}
-                    {/*<div className="sk-circle1 sk-child"></div>*/}
-                    {/*<div className="sk-circle2 sk-child"></div>*/}
-                    {/*<div className="sk-circle3 sk-child"></div>*/}
-                    {/*<div className="sk-circle4 sk-child"></div>*/}
-                    {/*<div className="sk-circle5 sk-child"></div>*/}
-                    {/*<div className="sk-circle6 sk-child"></div>*/}
-                    {/*<div className="sk-circle7 sk-child"></div>*/}
-                    {/*<div className="sk-circle8 sk-child"></div>*/}
-                    {/*<div className="sk-circle9 sk-child"></div>*/}
-                    {/*<div className="sk-circle10 sk-child"></div>*/}
-                    {/*<div className="sk-circle11 sk-child"></div>*/}
-                    {/*<div className="sk-circle12 sk-child"></div>*/}
-                    {/*</div>*/}
-
                     <div className="table">
                         <div className="tableHeader"></div>
-                        <div className="tableRow row1"></div>
-                        <div className="tableRow row2"></div>
-                        <div className="tableRow row3"></div>
-                        <div className="tableRow row4"></div>
-                        <div className="tableRow row5"></div>
+                        <div className="tableBody">
+                            <div className="loading-circle">
+                                <div className="loading-circle1 loading-circle-child"></div>
+                                <div className="loading-circle2 loading-circle-child"></div>
+                                <div className="loading-circle3 loading-circle-child"></div>
+                                <div className="loading-circle4 loading-circle-child"></div>
+                                <div className="loading-circle5 loading-circle-child"></div>
+                                <div className="loading-circle6 loading-circle-child"></div>
+                                <div className="loading-circle7 loading-circle-child"></div>
+                                <div className="loading-circle8 loading-circle-child"></div>
+                                <div className="loading-circle9 loading-circle-child"></div>
+                                <div className="loading-circle10 loading-circle-child"></div>
+                                <div className="loading-circle11 loading-circle-child"></div>
+                                <div className="loading-circle12 loading-circle-child"></div>
+                            </div>
+                        </div>
+                        {/*<div className="tableRow row1"></div>*/}
+                        {/*<div className="tableRow row2"></div>*/}
+                        {/*<div className="tableRow row3"></div>*/}
+                        {/*<div className="tableRow row4"></div>*/}
+                        {/*<div className="tableRow row5"></div>*/}
+                        {/*<div className="tableRow row6"></div>*/}
+                        {/*<div className="tableRow row7"></div>*/}
+                        {/*<div className="tableRow row8"></div>*/}
+                        {/*<div className="tableRow row9"></div>*/}
+                        {/*<div className="tableRow row10"></div>*/}
+                        {/*<div className="tableRow row11"></div>*/}
+                        {/*<div className="tableRow row12"></div>*/}
                     </div>
 
                     <div className="cards">
-                        <div className="card row1"></div>
-                        <div className="card row2"></div>
-                        <div className="card row3"></div>
-                        <div className="card row4"></div>
-                        <div className="card row5"></div>
+                        {/*<div className="card row1"></div>*/}
+                        {/*<div className="card row2"></div>*/}
+                        {/*<div className="card row3"></div>*/}
+                        {/*<div className="card row4"></div>*/}
+                        {/*<div className="card row5"></div>*/}
+                        {/*<div className="card row6"></div>*/}
+                        {/*<div className="card row7"></div>*/}
+                        {/*<div className="card row8"></div>*/}
+                        {/*<div className="card row9"></div>*/}
+                        {/*<div className="card row10"></div>*/}
+
+                        {/*<div className="card"></div>*/}
+                        {/*<div className="card"></div>*/}
+                        {/*<div className="card"></div>*/}
+                        {/*<div className="card"></div>*/}
+                        {/*<div className="card"></div>*/}
+                        {/*<div className="card"></div>*/}
+                        {/*<div className="card"></div>*/}
+                        {/*<div className="card"></div>*/}
+                        {/*<div className="card"></div>*/}
+                        {/*<div className="card"></div>*/}
+
+                        <div className="loading-circle">
+                            <div className="loading-circle1 loading-circle-child"></div>
+                            <div className="loading-circle2 loading-circle-child"></div>
+                            <div className="loading-circle3 loading-circle-child"></div>
+                            <div className="loading-circle4 loading-circle-child"></div>
+                            <div className="loading-circle5 loading-circle-child"></div>
+                            <div className="loading-circle6 loading-circle-child"></div>
+                            <div className="loading-circle7 loading-circle-child"></div>
+                            <div className="loading-circle8 loading-circle-child"></div>
+                            <div className="loading-circle9 loading-circle-child"></div>
+                            <div className="loading-circle10 loading-circle-child"></div>
+                            <div className="loading-circle11 loading-circle-child"></div>
+                            <div className="loading-circle12 loading-circle-child"></div>
+                        </div>
                     </div>
 
                     <div className="mobileAddButton"></div>
