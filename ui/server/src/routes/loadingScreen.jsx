8999544a215dfb39306ac5b8eff073f9bc0cd3ff
nvanselow <nvanselow@quickbase.com>
2016-12-08 15:45:45 -0500

    MB1732-Use the table outline as the loader on the loading screen

--- ui/server/src/routes/loadingScreen.jsx
+++ ui/server/src/routes/loadingScreen.jsx
@@ -12,47 +12,28 @@ var LoadingScreen = () => {
                     <div className="addButton"></div>
                 </div>
                 <div className="mainContent">
-                    {/*<div className="loader">*/}
-                        {/*<div className="spinner" role="progressbar" style={{"position":"absolute", "width":"0px", "zIndex":"2000000000", "left":"50%", "top":"50%"}}>*/}
-                            {/*<div style={{"position":"absolute", "top":"-8px", "opacity":"0", "animation":"opacity-60-0-0-7 0.666667s linear infinite"}}>*/}
-                                {/*<div style={{"position":"absolute", "width":"16px", "height":"16px", "background":"rgb(150, 150, 150)", "boxShadow":"rgba(0, 0, 0, 0.0980392) 0px 0px 1px", "transformOrigin":"left center 0px", "transform":"rotate(0deg) translate(17px, 0px)", "borderRadius":"8px"}}></div>*/}
-                            {/*</div>*/}
-                            {/*<div style={{"position":"absolute", "top":"-8px", "opacity":"0", "animation":"opacity-60-0-1-7 0.666667s linear infinite"}}>*/}
-                                {/*<div style={{"position":"absolute", "width":"16px", "height":"16px", "background":"rgb(150, 150, 150)", "boxShadow":"rgba(0, 0, 0, 0.0980392) 0px 0px 1px", "transformOrigin":"left center 0px", "transform":"rotate(51deg) translate(17px, 0px)", "borderRadius":"8px"}}></div>*/}
-                            {/*</div>*/}
-                            {/*<div style={{"position":"absolute", "top":"-8px", "opacity":"0", "animation":"opacity-60-0-2-7 0.666667s linear infinite"}}>*/}
-                                {/*<div style={{"position":"absolute", "width":"16px", "height":"16px", "background":"rgb(150, 150, 150)", "boxShadow":"rgba(0, 0, 0, 0.0980392) 0px 0px 1px", "transformOrigin":"left center 0px", "transform":"rotate(102deg) translate(17px, 0px)", "borderRadius":"8px"}}></div>*/}
-                            {/*</div>*/}
-                            {/*<div style={{"position":"absolute", "top":"-8px", "opacity":"0", "animation":"opacity-60-0-3-7 0.666667s linear infinite"}}>*/}
-                                {/*<div style={{"position":"absolute", "width":"16px", "height":"16px", "background":"rgb(150, 150, 150)", "boxShadow":"rgba(0, 0, 0, 0.0980392) 0px 0px 1px", "transformOrigin":"left center 0px", "transform":"rotate(154deg) translate(17px, 0px)", "borderRadius":"8px"}}></div>*/}
-                            {/*</div>*/}
-                            {/*<div style={{"position":"absolute", "top":"-8px", "opacity":"0", "animation":"opacity-60-0-4-7 0.666667s linear infinite"}}>*/}
-                                {/*<div style={{"position":"absolute", "width":"16px", "height":"16px", "background":"rgb(150, 150, 150)", "boxShadow":"rgba(0, 0, 0, 0.0980392) 0px 0px 1px", "transformOrigin":"left center 0px", "transform":"rotate(205deg) translate(17px, 0px)", "borderRadius":"8px"}}></div>*/}
-                            {/*</div>*/}
-                            {/*<div style={{"position":"absolute", "top":"-8px", "opacity":"0", "animation":"opacity-60-0-5-7 0.666667s linear infinite"}}>*/}
-                                {/*<div style={{"position":"absolute", "width":"16px", "height":"16px", "background":"rgb(150, 150, 150)", "boxShadow":"rgba(0, 0, 0, 0.0980392) 0px 0px 1px", "transformOrigin":"left center 0px", "transform":"rotate(257deg) translate(17px, 0px)", "borderRadius":"8px"}}></div>*/}
-                            {/*</div>*/}
-                            {/*<div style={{"position":"absolute", "top":"-8px", "opacity":"0", "animation":"opacity-60-0-6-7 0.666667s linear infinite"}}>*/}
-                                {/*<div style={{"position":"absolute", "width":"16px", "height":"16px", "background":"rgb(150, 150, 150)", "boxShadow":"rgba(0, 0, 0, 0.0980392) 0px 0px 1px", "transformOrigin":"left center 0px", "transform":"rotate(308deg) translate(17px, 0px)", "borderRadius":"8px"}}></div>*/}
-                            {/*</div>*/}
+                    <div className="table">
+                        <div className="tableHeader"></div>
+                        <div className="tableRow row1"></div>
+                        <div className="tableRow row2"></div>
+                        <div className="tableRow row3"></div>
+                        <div className="tableRow row4"></div>
+                        <div className="tableRow row5"></div>
+
+                        {/*<div className="sk-circle">*/}
+                            {/*<div className="sk-circle1 sk-child"></div>*/}
+                            {/*<div className="sk-circle2 sk-child"></div>*/}
+                            {/*<div className="sk-circle3 sk-child"></div>*/}
+                            {/*<div className="sk-circle4 sk-child"></div>*/}
+                            {/*<div className="sk-circle5 sk-child"></div>*/}
+                            {/*<div className="sk-circle6 sk-child"></div>*/}
+                            {/*<div className="sk-circle7 sk-child"></div>*/}
+                            {/*<div className="sk-circle8 sk-child"></div>*/}
+                            {/*<div className="sk-circle9 sk-child"></div>*/}
+                            {/*<div className="sk-circle10 sk-child"></div>*/}
+                            {/*<div className="sk-circle11 sk-child"></div>*/}
+                            {/*<div className="sk-circle12 sk-child"></div>*/}
                         {/*</div>*/}
-                    {/*</div>*/}
-
-
-                    {/*<span className="loadingSpinner"></span>*/}
-                    <div className="sk-circle">
-                        <div className="sk-circle1 sk-child"></div>
-                        <div className="sk-circle2 sk-child"></div>
-                        <div className="sk-circle3 sk-child"></div>
-                        <div className="sk-circle4 sk-child"></div>
-                        <div className="sk-circle5 sk-child"></div>
-                        <div className="sk-circle6 sk-child"></div>
-                        <div className="sk-circle7 sk-child"></div>
-                        <div className="sk-circle8 sk-child"></div>
-                        <div className="sk-circle9 sk-child"></div>
-                        <div className="sk-circle10 sk-child"></div>
-                        <div className="sk-circle11 sk-child"></div>
-                        <div className="sk-circle12 sk-child"></div>
                     </div>
                     <div className="mobileAddButton"></div>
                 </div>
