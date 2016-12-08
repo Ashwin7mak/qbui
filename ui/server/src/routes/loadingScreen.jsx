af258f5155c257406a8eb2210a840fc5fdb7e8b7
nvanselow <nvanselow@quickbase.com>
2016-12-08 16:18:22 -0500

    MB1732-Better mobile loading screen and css cleanup

--- ui/server/src/routes/loadingScreen.jsx
+++ ui/server/src/routes/loadingScreen.jsx
@@ -12,6 +12,21 @@ var LoadingScreen = () => {
                     <div className="addButton"></div>
                 </div>
                 <div className="mainContent">
+                    {/*<div className="sk-circle">*/}
+                    {/*<div className="sk-circle1 sk-child"></div>*/}
+                    {/*<div className="sk-circle2 sk-child"></div>*/}
+                    {/*<div className="sk-circle3 sk-child"></div>*/}
+                    {/*<div className="sk-circle4 sk-child"></div>*/}
+                    {/*<div className="sk-circle5 sk-child"></div>*/}
+                    {/*<div className="sk-circle6 sk-child"></div>*/}
+                    {/*<div className="sk-circle7 sk-child"></div>*/}
+                    {/*<div className="sk-circle8 sk-child"></div>*/}
+                    {/*<div className="sk-circle9 sk-child"></div>*/}
+                    {/*<div className="sk-circle10 sk-child"></div>*/}
+                    {/*<div className="sk-circle11 sk-child"></div>*/}
+                    {/*<div className="sk-circle12 sk-child"></div>*/}
+                    {/*</div>*/}
+
                     <div className="table">
                         <div className="tableHeader"></div>
                         <div className="tableRow row1"></div>
@@ -19,22 +34,16 @@ var LoadingScreen = () => {
                         <div className="tableRow row3"></div>
                         <div className="tableRow row4"></div>
                         <div className="tableRow row5"></div>
-
-                        {/*<div className="sk-circle">*/}
-                            {/*<div className="sk-circle1 sk-child"></div>*/}
-                            {/*<div className="sk-circle2 sk-child"></div>*/}
-                            {/*<div className="sk-circle3 sk-child"></div>*/}
-                            {/*<div className="sk-circle4 sk-child"></div>*/}
-                            {/*<div className="sk-circle5 sk-child"></div>*/}
-                            {/*<div className="sk-circle6 sk-child"></div>*/}
-                            {/*<div className="sk-circle7 sk-child"></div>*/}
-                            {/*<div className="sk-circle8 sk-child"></div>*/}
-                            {/*<div className="sk-circle9 sk-child"></div>*/}
-                            {/*<div className="sk-circle10 sk-child"></div>*/}
-                            {/*<div className="sk-circle11 sk-child"></div>*/}
-                            {/*<div className="sk-circle12 sk-child"></div>*/}
-                        {/*</div>*/}
                     </div>
+
+                    <div className="cards">
+                        <div className="card row1"></div>
+                        <div className="card row2"></div>
+                        <div className="card row3"></div>
+                        <div className="card row4"></div>
+                        <div className="card row5"></div>
+                    </div>
+
                     <div className="mobileAddButton"></div>
                 </div>
             </div>
