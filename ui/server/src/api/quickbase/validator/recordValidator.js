74a1da4c4905148c7abc90b9e1bcb8d333a4d50e
nvanselow <nvanselow@quickbase.com>
2016-12-14 12:55:20 -0500

    MB186-Add unit test for recordValidator (server side)

--- ui/server/src/api/quickbase/validator/recordValidator.js
+++ ui/server/src/api/quickbase/validator/recordValidator.js
@@ -1,32 +1,32 @@
-(function() {
-    'use strict';
-    var _ = require('lodash');
-    var ValidatorUtils = require('../../../../../common/src/validationUtils');
-    var constants = require('../../../../../common/src/constants');
-    var phoneValidator = require('./phoneValidator');
+// This module is not wrapped in a self-invoking function because it makes it impossible to rewire dependencies
+'use strict';
+var _ = require('lodash');
+var ValidationUtils = require('../../../../../common/src/validationUtils');
+var constants = require('../../../../../common/src/constants');
+var phoneValidator = require('./phoneValidator');
 
-    /**
-     * A wrapper for the common validator utils that has additional server-side only validations
-     * @type {{checkFieldValue: recordValidator.checkFieldValue}}
-     */
-    var recordValidator = {
-        checkFieldValue: function(def, name, value, checkRequired) {
-            var fieldDef = def.fieldDef;
+/**
+ * A wrapper for the common validator utils that has additional server-side only validations
+ * @type {{checkFieldValue: recordValidator.checkFieldValue}}
+ */
+var recordValidator = {
+    checkFieldValue: function(def, name, value, checkRequired) {
+        var fieldDef = def.fieldDef;
 
-            var results = ValidatorUtils.checkFieldValue(def, name, value, checkRequired);
+        var results = ValidationUtils.checkFieldValue(def, name, value, checkRequired);
 
-            // Type specific validators
-            if (_.has(fieldDef, 'datatypeAttributes.type')) {
-                switch (fieldDef.datatypeAttributes.type) {
-                case constants.PHONE_NUMBER :
-                    results = phoneValidator.validateAndReturnResults(value, name, results);
-                    break;
-                }
+        // Type specific validators
+        if (_.has(fieldDef, 'datatypeAttributes.type')) {
+            switch (fieldDef.datatypeAttributes.type) {
+            case constants.PHONE_NUMBER :
+                results = phoneValidator.validateAndReturnResults(value, name, results);
+                break;
             }
-
-            return results;
         }
-    };
 
-    module.exports = recordValidator;
-}());
+        return results;
+    }
+};
+
+module.exports = recordValidator;
+
