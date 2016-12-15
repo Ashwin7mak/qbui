d4881b57bb2d1f71927ae38260135c2b5b4913ad
Beyer <rbeyer@cmbl0753fb2d3.corp.quickbase.net>
2016-12-15 14:11:13 -0500

    added unit tests for numericFormulaFieldRenderer

---
+++ ui/client-react/test/components/numericFormulaFieldRenderer.unit.spec.js
@@ -0,0 +1,50 @@
+/**
+ * Created by rbeyer on 12/15/16.
+ */
+import React from "react";
+import TestUtils from "react-addons-test-utils";
+import NumericFormulaFieldRenderer from "../../src/components/fields/numericFormulaFieldRenderer";
+
+describe('NumericFormulaFieldRenderer: ', () => {
+    'use strict';
+    let component;
+
+    it('test render of component', () => {
+        component = TestUtils.renderIntoDocument(<NumericFormulaFieldRenderer />);
+        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+    });
+
+    it('test render of component with no props', () => {
+        component = TestUtils.renderIntoDocument(<NumericFormulaFieldRenderer />);
+        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+        let textFieldValueRenderer = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
+        expect(textFieldValueRenderer.classList.contains('emptyNumericFormula')).toBeTruthy();
+    });
+
+    it('test render of component with value prop', () => {
+        let numberStr = {numberStr: "123"};
+        component = TestUtils.renderIntoDocument(<NumericFormulaFieldRenderer value={numberStr}/>);
+        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+        let textFieldValueRenderer = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
+        expect(textFieldValueRenderer.classList.contains('filledNumericFormula')).toBeTruthy();
+    });
+
+    it('test render of component with display prop', () => {
+        let numberStr = "1337";
+        component = TestUtils.renderIntoDocument(<NumericFormulaFieldRenderer display={numberStr}/>);
+        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+        let textFieldValueRenderer = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
+        expect(textFieldValueRenderer.classList.contains('filledNumericFormula')).toBeTruthy();
+    });
+
+    it('test render of component with all props', () => {
+        let numberStr = {numberStr: "123"};
+        let moreNumberStr = "1337";
+        component = TestUtils.renderIntoDocument(<NumericFormulaFieldRenderer value={numberStr} display={moreNumberStr}/>);
+        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+        let textFieldValueRenderer = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
+        expect(textFieldValueRenderer.classList.contains('filledNumericFormula')).toBeTruthy();
+    });
+
+
+});
