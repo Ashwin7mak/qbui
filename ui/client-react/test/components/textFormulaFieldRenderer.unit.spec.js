a4d671431be9eafb825f36adf602f4dc866b2124
Beyer <rbeyer@cmbl0753fb2d3.corp.quickbase.net>
2016-12-15 13:43:10 -0500

    getting rid of unused import

--- ui/client-react/test/components/textFormulaFieldRenderer.unit.spec.js
+++ ui/client-react/test/components/textFormulaFieldRenderer.unit.spec.js
@@ -2,7 +2,6 @@
  * Created by rbeyer on 12/15/16.
  */
 import React from "react";
-import ReactDOM from "react-dom";
 import TestUtils from "react-addons-test-utils";
 import TextFormulaFieldRenderer from "../../src/components/fields/textFormulaFieldRenderer";
 
