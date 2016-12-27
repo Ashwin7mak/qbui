2902716d292e1d1b0095e2bd46f9550f97ecf4e3
nvanselow <nvanselow@quickbase.com>
2016-12-27 16:20:23 -0500

    MB1770-Refactor apiResponseFormatter and account for editing a record

--- ui/server/src/api/quickbase/formatter/apiResponseFormatter.js
+++ ui/server/src/api/quickbase/formatter/apiResponseFormatter.js
@@ -1,64 +1,121 @@
+var Promise = require('bluebird');
 var _ = require('lodash');
 var errorCodes = require('../../../../../common/src/dataEntryErrorCodes');
-var Promise = require('bluebird');
-
-var defaultValidationResult = {
-    value: null,
-    def: null,
-    isInvalid : false,
-    error : {
-        messageId : null,
-        code : null,
-        data: {}
-    },
-};
+var apiResponseErrors = require('../../../constants/apiResponseErrors');
 
 function parseRequestBodyAsJson(payload) {
+    if (_.has(payload, 'request.body')) {
+        if (_.isString(payload.request.body)) {
+            return JSON.parse(payload.request.body);
+        } else {
+            return payload.request.body;
+        }
+    }
 
+    return [];
 }
 
-// function convertApiErrorToI18nMessageKey(error, subtype) {
-//     switch (error) {
-//     case 'NotUniqueKeyFieldValue' :
-//         if (subtype === 'singleField') {
-//             return 'invalidMsg.api.notUniqueSingleField';
-//         } else if (subtype === 'multichoice') {
-//             return 'invalidMsg.api.notUniqueMultiChoice';
-//         }
-//         break;
-//     }
-// }
+function parseResponseBodyAsJson(payload) {
+    if (_.has(payload, 'body')) {
+        if (_.isString(payload.body)) {
+            return JSON.parse(payload.body);
+        } else {
+            return payload.body;
+        }
+    }
+
+    return [];
+}
 
 function formatFieldAsValidationError(field, i18nErrorKey, data) {
-    var resultsCopy = _.assign({}, defaultValidationResult);
+    var resultsCopy = {error: {}};
     resultsCopy.id = field.id;
     resultsCopy.value = field.value;
     resultsCopy.def = field;
     resultsCopy.error.code = errorCodes.INVALID_ENTRY;
     resultsCopy.error.messageId = i18nErrorKey;
-    resultsCopy.error.data = Object.assign({}, {fieldName: field.fieldName}, data);
+    resultsCopy.error.data = _.assign({}, {fieldName: field.fieldName}, data);
     resultsCopy.isInvalid = true;
     return resultsCopy;
 }
 
-var ApiResponseFormatter = {
+function hasValidResponseBody(responseBody) {
+    return _.isArray(responseBody) && _.isObject(responseBody[0]);
+}
 
+function getErrorCode(responseBody) {
+    if (hasValidResponseBody(responseBody)) {
+        return responseBody[0].code;
+    }
+
+    return null;
+}
+
+function getErrorMessage(responseBody) {
+    if (hasValidResponseBody(responseBody)) {
+        return responseBody[0].message;
+    }
+
+    return null;
+}
+
+function responseHasUniqueValidationErrors(responseBody) {
+    // Core API returns a NOT_UNIQUE_VALUE error when creating a new record,
+    // but returns a 404 and a NOT_UNIQUE_VALUE_MESSAGE when editing a record
+    // if the there is a unique validation error
+    return getErrorCode(responseBody) === apiResponseErrors.NOT_UNIQUE_VALUE || getErrorMessage(responseBody) === apiResponseErrors.NOT_UNIQUE_VALUE_MESSAGE;
+}
+
+function formatUniqueValidationErrors(requestBody) {
+    var uniqueValidationErrors = [];
+
+    requestBody.forEach(field => {
+        // The Core API does not currently specify which fields failed the unique test, so this function
+        // only applies the validation error to fields that are set as unique.
+        if (_.has(field, 'fieldDef.unique') && field.fieldDef.unique) {
+            uniqueValidationErrors.push(formatFieldAsValidationError(field, 'invalidMsg.api.notUniqueSingleField', {recordName: 'record'}));
+        }
+    });
+
+    return uniqueValidationErrors;
+}
+
+/**
+ * The promise needs to be rejected if the response is not ok. If the promise is not rejected,
+ * the response will be incorrectly marked as 200.
+ * @param payload
+ * @returns {*}
+ */
+function returnPayload(payload) {
+    if (payload.statusCode >= 300) {
+        return Promise.reject(payload);
+    }
+
+    return payload;
+}
+
+var ApiResponseFormatter = {
+    /**
+     * Accepts an error response from the Core API and formats it for use in the UI layer.
+     * Consider carefully before adding an error handler here. Could this be caught in a Node layer validator? If so, add it there.
+     * This only affects errors which cannot be evaluated by the Node server layer (e.g., unique values).
+     * @param payload
+     * @returns {*}
+     */
     formatResponseError: function(payload) {
         var formattedErrors = [];
-        var requestBodyAsJson = [];
-        if (_.has(payload, 'request.body') && _.isString(payload.request.body)) {
-            requestBodyAsJson = JSON.parse(payload.request.body);
+        var requestBodyAsJson = parseRequestBodyAsJson(payload);
+        var responseBodyAsJson = parseResponseBodyAsJson(payload);
+
+        if (responseHasUniqueValidationErrors(responseBodyAsJson)) {
+            formattedErrors = formattedErrors.concat(formatUniqueValidationErrors(requestBodyAsJson));
         }
 
-        requestBodyAsJson.forEach(field => {
-            if (_.has(field, 'fieldDef.unique') && field.fieldDef.unique) {
-                formattedErrors.push(formatFieldAsValidationError(field, 'invalidMsg.api.notUniqueSingleField', {recordName: 'record'}));
-            }
-        });
+        if (formattedErrors.length > 0) {
+            return Promise.reject({response:{message:'validation error', status: 422, errors: formattedErrors}});
+        }
 
-        var payloadWithValidationErrors = _.assign({}, payload);
-        payloadWithValidationErrors.body = JSON.stringify([_.assign(JSON.parse(payload.body)[0], {response: {errors: formattedErrors}})]);
-        return Promise.reject({response:{message:'validation error', status: 422, errors: formattedErrors}});
+        return returnPayload(payload);
     }
 };
 
---
+++ ui/server/src/constants/apiResponseErrors.js
@@ -0,0 +1,4 @@
+module.exports = {
+    NOT_UNIQUE_VALUE: 'NotUniqueKeyFieldValue',
+    NOT_UNIQUE_VALUE_MESSAGE: 'The record value is in violation of referential integrity constraints.'
+};
