233c60c6f46ec616fad71a284e2940e31ade951f
lakshmi kamineni <skamineni@quickbase.com>
2016-12-28 17:07:04 -0500

    only enabled form tests

--- ui/wdio/config/wdioSauce.conf.js
+++ ui/wdio/config/wdioSauce.conf.js
@@ -53,7 +53,11 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './ui/wdio/tests/forms/*.e2e.spec.js',
+        './ui/wdio/tests/forms/formAdd.e2e.spec.js',
+        './ui/wdio/tests/forms/formEdit.e2e.spec.js',
+        './ui/wdio/tests/forms/formAddValidation.e2e.spec.js',
+        './ui/wdio/tests/forms/formPermissionsParticipantRole.e2e.spec.js',
+        './ui/wdio/tests/forms/formPermissionsViewerRole.e2e.spec.js',
         //'./ui/wdio/tests/reportAddRecord.e2e.spec.js'
     ],
     // Patterns to exclude.
