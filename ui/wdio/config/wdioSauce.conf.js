6556f70d295a0c0d5d5c8bd909b20a847f694fd5
lakshmi kamineni <skamineni@quickbase.com>
2016-12-30 20:13:26 -0500

    changed chrome version to 53

--- ui/wdio/config/wdioSauce.conf.js
+++ ui/wdio/config/wdioSauce.conf.js
@@ -85,7 +85,7 @@ exports.config = {
         {
             platform : 'OS X 10.11',
             browserName     : 'chrome',
-            version: '54.0',
+            version: '53.0',
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
             name            : process.env.SAUCE_JOB_NAME + '_OSX_Chrome',
             //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
