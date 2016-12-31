3e7e967f4a8af425550e7bd59194ec0180d2a86c
lakshmi kamineni <skamineni@quickbase.com>
2016-12-30 20:28:43 -0500

    changed chrome version to 54 and large

--- ui/wdio/config/wdioSauce.conf.js
+++ ui/wdio/config/wdioSauce.conf.js
@@ -85,14 +85,14 @@ exports.config = {
         {
             platform : 'OS X 10.11',
             browserName     : 'chrome',
-            version: '53.0',
+            version: '54.0',
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
             name            : process.env.SAUCE_JOB_NAME + '_OSX_Chrome',
             //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
             idleTimeout: '120',
             screenResolution : '1600x1200',
             maxDuration: 10800,
-            breakpointSize: 'large',
+            breakpointSize: 'xlarge',
             // These two values enable parallel testing which will run a spec file per instance
             shardTestFiles: true,
             maxInstances: 2
