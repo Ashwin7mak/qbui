60bb713ae814666b410ff2215138221a2a0c80e6
Claire Martinez <cmartinez@quickbase.com>
2017-01-06 08:51:39 -0500

    changes to match new output from rewire plugin

--- ui/plugins/babel-plugin-rewire-ignore-coverage/index.js
+++ ui/plugins/babel-plugin-rewire-ignore-coverage/index.js
@@ -1,33 +1,26 @@
 // support for commenting out rwire code for istanbul coverage
-module.exports = function ({types: t}) {
+module.exports = function({types: t}) {
     var commentToIgnoreCoverage = " istanbul ignore next ";
-
+    var methodsToIgnoreRegEx =
+        /_get__|_get_original__|_assign__|_set_original__|_update_operation__|_set__|_reset__|_with__/;
     function addIgnoreComment(context) {
         context.addComment("leading", commentToIgnoreCoverage, true);
     }
     return {
         visitor: {
-            Function: {
-                exit(path) {
+            Function(path) {
                     // add istanbul ignore on the the rewire injected function statements
-                    if (path.id && path.id.name) {
-                        if (path.id.name.match(/__GetDependency__|__Rewire__|__ResetDependency__/)) {
-                            //console.log('adding comment for path '+ path.id.name);
-                            addIgnoreComment(this);
-                        }
-                    //  add istanbul ignore for the functions assignments for $Getter, $Setter, $Resetter injections
-                    } else if (this.container !== undefined &&
-                      this.container.type  !== undefined &&
-                      this.container.type.match(/AssignmentExpression/) &&
-                      this.container.left !== undefined &&
-                      this.container.left.object !== undefined &&
-                      this.container.left.object.name.match(/__\$Getters__|__\$Setters__|__\$Resetters__/)) {
-                        //console.log('adding comment for this.container.left.object.name '+ this.container.left.object.name);
-                        addIgnoreComment(this);
+                if (path.node.id && path.node.id.name) {
+                        //console.log('!adding comment for function '+ path.node.id.name);
+                    if (methodsToIgnoreRegEx.exec(path.node.id.name) !== null) {
+                        addIgnoreComment(path);
                     }
+                    //  add istanbul ignore for the functions assignments for $Getter, $Setter, $Resetter injections
                 }
             }
         }
-    }
+
+    };
 };
+//To debug run `babel client-react/src/components/fields/fieldValueRenderers.js --out-file outfilename.js
 
