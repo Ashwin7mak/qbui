d8626172a9e57c60eb35f8d1d26e44dd5956f433
lakshmi kamineni <skamineni@quickbase.com>
2017-01-11 12:51:24 -0500

    removed if Edge

--- ui/wdio/tests/reports/reportEditRecord.e2e.spec.js
+++ ui/wdio/tests/reports/reportEditRecord.e2e.spec.js
@@ -16,177 +16,173 @@
         var realmName;
         var realmId;
         var testApp;
-        //TODO currently Anshu mentioned this works only on edge. Adding this so it wont break the build which has forms tests that work on 3 different browsers
-        if (browser === 'MicrosoftEdge') {
-
-            beforeAll(function() {
-                browser.logger.info('beforeAll spec function - Generating test data and logging in');
-                // Need to return here. beforeAll is completely async, need to return the Promise chain in any before or after functions!
-                // No need to call done() anymore
-                return e2eBase.basicAppSetup().then(function(createdApp) {
-                    // Set your global objects to use in the test functions
-                    testApp = createdApp;
-                    realmName = e2eBase.recordBase.apiBase.realm.subdomain;
-                    realmId = e2eBase.recordBase.apiBase.realm.id;
-                }).then(function() {
-                    // Auth into the new stack
-                    return NewStackAuthPO.realmLogin(realmName, realmId);
-                }).catch(function(error) {
-                    // Global catch that will grab any errors from chain above
-                    // Will appropriately fail the beforeAll method so other tests won't run
-                    Promise.reject(new Error('Error during test setup beforeAll: ' + error.message));
-                });
+        beforeAll(function() {
+            browser.logger.info('beforeAll spec function - Generating test data and logging in');
+            // Need to return here. beforeAll is completely async, need to return the Promise chain in any before or after functions!
+            // No need to call done() anymore
+            return e2eBase.basicAppSetup().then(function(createdApp) {
+                // Set your global objects to use in the test functions
+                testApp = createdApp;
+                realmName = e2eBase.recordBase.apiBase.realm.subdomain;
+                realmId = e2eBase.recordBase.apiBase.realm.id;
+            }).then(function() {
+                // Auth into the new stack
+                return NewStackAuthPO.realmLogin(realmName, realmId);
+            }).catch(function(error) {
+                // Global catch that will grab any errors from chain above
+                // Will appropriately fail the beforeAll method so other tests won't run
+                Promise.reject(new Error('Error during test setup beforeAll: ' + error.message));
             });
+        });
 
 
-            /**
-             * Before each test starts just make sure the report has loaded with records visible
-             */
-            beforeEach(function() {
-                e2ePageBase.loadReportByIdInBrowser(realmName, testApp.id, testApp.tables[e2eConsts.TABLE1].id, 1);
-                return ReportContentPO.waitForReportContent();
+        /**
+         * Before each test starts just make sure the report has loaded with records visible
+         */
+        beforeEach(function() {
+            e2ePageBase.loadReportByIdInBrowser(realmName, testApp.id, testApp.tables[e2eConsts.TABLE1].id, 1);
+            return ReportContentPO.waitForReportContent();
 
-            });
+        });
 
-            /**
-             * Test Method - In-line edit menu should have three actions
-             */
+        /**
+         * Test Method - In-line edit menu should have three actions
+         */
 
-            it('In-line edit menu should have three actions (save, save and add new, cancel)', function(done) {
+        it('In-line edit menu should have three actions (save, save and add new, cancel)', function(done) {
 
-                browser.logger.info('it spec function - Running the test');
+            browser.logger.info('it spec function - Running the test');
 
-                //Step 1 -  Open the in-line edit menu for the first record in the report
-                ReportInLineEditPO.openRecordEditMenu(0);
+            //Step 1 -  Open the in-line edit menu for the first record in the report
+            ReportInLineEditPO.openRecordEditMenu(0);
 
-                //Step 2 -  Check that the three buttons are visible
-                ReportInLineEditPO.assertInlineEditMenuButtonsTobeTrue();
+            //Step 2 -  Check that the three buttons are visible
+            ReportInLineEditPO.assertInlineEditMenuButtonsTobeTrue();
 
-                //step 3 - Close the edit menu
-                ReportInLineEditPO.clickCancelButton();
-            });
+            //step 3 - Close the edit menu
+            ReportInLineEditPO.clickCancelButton();
+        });
 
-            /**
-             * Test Method. Record in-line editing test on a report.
-             */
-            it('Should allow you to in-line edit multiple fields in a report record', function() {
+        /**
+         * Test Method. Record in-line editing test on a report.
+         */
+        it('Should allow you to in-line edit multiple fields in a report record', function() {
 
-                //Variable declarations
-                var textToEnter = 'My new text';
-                var dateToEnter = '03-11-1985';
-                var dateToExpect = '03-12-1985';
-                var successMessage = 'Record saved';
+            //Variable declarations
+            var textToEnter = 'My new text';
+            var dateToEnter = '03-11-1985';
+            var dateToExpect = '03-12-1985';
+            var successMessage = 'Record saved';
 
-                //Step 1 - Open the in-line edit menu for the first record in the report
-                ReportInLineEditPO.openRecordEditMenu(0);
+            //Step 1 - Open the in-line edit menu for the first record in the report
+            ReportInLineEditPO.openRecordEditMenu(0);
 
-                // Step 2 - Edit the Text Field
-                ReportInLineEditPO.editTextField(0, textToEnter);
+            // Step 2 - Edit the Text Field
+            ReportInLineEditPO.editTextField(0, textToEnter);
 
-                // Steps 3 - Edit the Date Field
-                // Set the value of the input box so the calendar widget will be set
-                ReportInLineEditPO.editDateField(0, dateToEnter);
-                // Open the calendar widget and advance the present date by 1 day
-                if (browserName !== 'safari') {
-                    ReportInLineEditPO.openDateFieldCalWidget(0);
-                    ReportInLineEditPO.advanceCurrentlySelectedDate(0);
-                }
+            // Steps 3 - Edit the Date Field
+            // Set the value of the input box so the calendar widget will be set
+            ReportInLineEditPO.editDateField(0, dateToEnter);
+            // Open the calendar widget and advance the present date by 1 day
+            if (browserName !== 'safari') {
+                ReportInLineEditPO.openDateFieldCalWidget(0);
+                ReportInLineEditPO.advanceCurrentlySelectedDate(0);
+            }
 
-                // Step 4 - Save the edit
-                ReportInLineEditPO.clickSaveChangesButton();
-                ReportInLineEditPO.assertSuccessMessage(successMessage);
+            // Step 4 - Save the edit
+            ReportInLineEditPO.clickSaveChangesButton();
+            ReportInLineEditPO.assertSuccessMessage(successMessage);
 
-                // Step 5 - Reload the report after saving
-                e2ePageBase.loadReportByIdInBrowser(realmName, testApp.id, testApp.tables[e2eConsts.TABLE1].id, 1);
+            // Step 5 - Reload the report after saving
+            e2ePageBase.loadReportByIdInBrowser(realmName, testApp.id, testApp.tables[e2eConsts.TABLE1].id, 1);
 
-                // Step 6 - Check for the edited value at the edited row
-                var recordValues = ReportContentPO.getRecordValues(0);
-                expect(recordValues[1]).toBe(textToEnter, 'Expected Text is not present');
+            // Step 6 - Check for the edited value at the edited row
+            var recordValues = ReportContentPO.getRecordValues(0);
+            expect(recordValues[1]).toBe(textToEnter, 'Expected Text is not present');
 
-                //If browser is safari then we are not advancing the present date by 1 day
-                if (browserName !== 'safari') {
-                    expect(recordValues[6]).toBe(dateToExpect, 'Expected date is not present');
-                } else {
-                    expect(recordValues[6]).toBe(dateToEnter, 'Expected date is not present');
-                }
+            //If browser is safari then we are not advancing the present date by 1 day
+            if (browserName !== 'safari') {
+                expect(recordValues[6]).toBe(dateToExpect, 'Expected date is not present');
+            } else {
+                expect(recordValues[6]).toBe(dateToEnter, 'Expected date is not present');
+            }
 
-            });
+        });
 
-            /**
-             * Test Method. Tests in-line editing with Pagination.
-             */
-            it('Should allow you to in-line edit a record on the second page', function(done) {
-                //Variable declarations
-                var textToEnter = 'My new text on page 2';
-                var successMessage = 'Record saved';
+        /**
+         * Test Method. Tests in-line editing with Pagination.
+         */
+        it('Should allow you to in-line edit a record on the second page', function(done) {
+            //Variable declarations
+            var textToEnter = 'My new text on page 2';
+            var successMessage = 'Record saved';
 
-                // Steps 1- Go to the second page of records
-                ReportPagingPO.clickPagingNavButton(ReportPagingPO.pagingToolbarNextButton);
+            // Steps 1- Go to the second page of records
+            ReportPagingPO.clickPagingNavButton(ReportPagingPO.pagingToolbarNextButton);
 
-                // Step 2 - Open the in-line edit menu for the first record on that page
-                ReportInLineEditPO.openRecordEditMenu(0);
+            // Step 2 - Open the in-line edit menu for the first record on that page
+            ReportInLineEditPO.openRecordEditMenu(0);
+
+            // Step 3 - Edit the Text Field
+            ReportInLineEditPO.editTextField(0, textToEnter);
+
+            // Step 4 - Save the edit
+            ReportInLineEditPO.clickSaveChangesButton();
+            // browser.waitForText(textToEnter);
+            expect(browser.isVisible('.ag-row.editing .saveRecord')).toBeFalsy();
+            expect(browser.isVisible('.ag-row.editing .cancelSelection')).toBeFalsy();
+            expect(browser.isVisible('.ag-row.editing .addRecord')).toBeFalsy();
+
+            // Step 5 - Check for the success message 'Record added'
+            ReportInLineEditPO.assertSuccessMessage(successMessage);
+
+            // Step 6 - Check that the edit persisted on the report
+            var recordValues = ReportContentPO.getRecordValues(0);
+            expect(recordValues[1]).toBe(textToEnter);
+
+            // Step 7 - Go back to the first page of records
+            ReportPagingPO.clickPagingNavButton(ReportPagingPO.pagingToolbarPrevButton);
+        });
+
+        /**
+         * Test Method. Cancel button test for in-line editing.
+         */
+        it('Cancel button should not save edit updates to a record', function(done) {
+            //Note this is not working on safari
+            if (browserName === 'chrome' || browserName === 'firefox') {
+                var textToEnter = 'My new text 2';
+
+                //Step 1 - Get the original value of the text field on the second record
+                var fieldValues = ReportContentPO.getRecordValues(1);
+                var originalText = fieldValues[1];
+                var elemForText = 'div=' + originalText;
+
+                //Step 2 - Open the in-line edit menu for the second record
+                ReportInLineEditPO.openRecordEditMenu(1);
 
                 // Step 3 - Edit the Text Field
                 ReportInLineEditPO.editTextField(0, textToEnter);
 
-                // Step 4 - Save the edit
-                ReportInLineEditPO.clickSaveChangesButton();
-                // browser.waitForText(textToEnter);
+                // Step 4 - Click the cancel button
+                ReportInLineEditPO.clickCancelButton();
+
+                //Step 5 - Extra Assertions for inline Edit to be invisible
+                browser.waitForText(elemForText);
                 expect(browser.isVisible('.ag-row.editing .saveRecord')).toBeFalsy();
                 expect(browser.isVisible('.ag-row.editing .cancelSelection')).toBeFalsy();
                 expect(browser.isVisible('.ag-row.editing .addRecord')).toBeFalsy();
 
-                // Step 5 - Check for the success message 'Record added'
-                ReportInLineEditPO.assertSuccessMessage(successMessage);
-
-                // Step 6 - Check that the edit persisted on the report
-                var recordValues = ReportContentPO.getRecordValues(0);
-                expect(recordValues[1]).toBe(textToEnter);
-
-                // Step 7 - Go back to the first page of records
-                ReportPagingPO.clickPagingNavButton(ReportPagingPO.pagingToolbarPrevButton);
-            });
-
-            /**
-             * Test Method. Cancel button test for in-line editing.
-             */
-            it('Cancel button should not save edit updates to a record', function(done) {
-                //Note this is not working on safari
-                if (browserName === 'chrome' || browserName === 'firefox') {
-                    var textToEnter = 'My new text 2';
-
-                    //Step 1 - Get the original value of the text field on the second record
-                    var fieldValues = ReportContentPO.getRecordValues(1);
-                    var originalText = fieldValues[1];
-                    var elemForText = 'div=' + originalText;
-
-                    //Step 2 - Open the in-line edit menu for the second record
-                    ReportInLineEditPO.openRecordEditMenu(1);
-
-                    // Step 3 - Edit the Text Field
-                    ReportInLineEditPO.editTextField(0, textToEnter);
-
-                    // Step 4 - Click the cancel button
-                    ReportInLineEditPO.clickCancelButton();
-
-                    //Step 5 - Extra Assertions for inline Edit to be invisible
-                    browser.waitForText(elemForText);
-                    expect(browser.isVisible('.ag-row.editing .saveRecord')).toBeFalsy();
-                    expect(browser.isVisible('.ag-row.editing .cancelSelection')).toBeFalsy();
-                    expect(browser.isVisible('.ag-row.editing .addRecord')).toBeFalsy();
-
-                    // Check that the edit was not persisted on report
-                    var fieldValues2 = ReportContentPO.getRecordValues(1);
-                    expect(fieldValues2[1]).toBe(originalText);
-                }
-            });
+                // Check that the edit was not persisted on report
+                var fieldValues2 = ReportContentPO.getRecordValues(1);
+                expect(fieldValues2[1]).toBe(originalText);
+            }
+        });
 
 
-            //TODO: Required field test, Need to extend setup data for this
+        //TODO: Required field test, Need to extend setup data for this
 
-            //TODO: Invalid input value tests (text in a date field)
+        //TODO: Invalid input value tests (text in a date field)
 
-            //TODO: Check that record ID isn't editable
-        }
+        //TODO: Check that record ID isn't editable
     });
 }());
