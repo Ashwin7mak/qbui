05d412431251deae722679649a851b810acc2c89
nvanselow <nvanselow@quickbase.com>
2017-01-11 18:16:16 -0500

    MB1905-Unit test for qbGrid's RowTransformer

--- ui/client-react/src/components/dataTable/qbGrid/rowTransformer.js
+++ ui/client-react/src/components/dataTable/qbGrid/rowTransformer.js
@@ -1,3 +1,12 @@
+const defaultOptions = {
+    isSubHeader: false, // If set to true, row will appear as a subheader row instead of a regular row
+    subHeaderLabel: null, // The label for a subheader row
+    subHeaderLevel: null, // The level (i.e., indentation for a subheader, 1-6 is currently supported via styling)
+    isEditing: false, // Whether the row is in an editing mode. (Adds an 'editing' class when true)
+    isSelected: false, // Denotes when a particular row has been selected
+    classes: [] // Any additional css classes to add to the row
+};
+
 /**
  * A class that holds information about properties that you may want to set for a row to be used
  * in the QbGrid. The rowId is required and a unique identifier for the row. The options are options,
@@ -14,17 +23,14 @@ class RowTransformer {
      */
     constructor(
         rowId, // Required unique identifier for a row.
-        options = {
-            isSubHeader: false, // If set to true, row will appear as a subheader row instead of a regular row
-            subHeaderLabel: null, // The label for a subheader row
-            subHeaderLevel: null, // The level (i.e., indentation for a subheader, 1-6 is currently supported via styling)
-            isEditing: false, // Whether the row is in an editing mode. (Adds an 'editing' class when true)
-            isSelected: false, // Denotes when a particular row has been selected
-            classes: [] // Any additional css classes to add to the row
-        }) {
+        options = defaultOptions) {
 
         this.id = rowId;
-        Object.assign(this, options);
+        this.isSubHeader = options.isSubHeader;
+        this.isEditing = options.isEditing;
+        this.isSelected = options.isSelected;
+
+        Object.assign(this, defaultOptions, options);
     }
 }
 
