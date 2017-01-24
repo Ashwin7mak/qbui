2d6010329be8d1c9fe3a73f4f2e46a8074e80bd9
Don Hatch <donald_hatch@intuit.com>
2017-01-24 09:14:00 -0500

    update doc on action context

--- ui/client-react/src/actions/context.js
+++ ui/client-react/src/actions/context.js
@@ -1,11 +1,14 @@
 /*
- *  Define context's for redux actions.  Defining a context will allow for an action to be
- *  used in multiple places within the same user context.
+ *  Define the context for a redux action.
+ *
  */
 
 export const CONTEXT = {
-    //  **** load reports actions context's ****
     LOAD_REPORTS: {
-        NAV: 'LOAD_REPORTS_NAV'
+        NAV: 'NAV'
+    },
+    REPORTS: {
+        GRID: 'GRID',
+        EMBEDDED: 'EMBEDDED'
     }
 };
