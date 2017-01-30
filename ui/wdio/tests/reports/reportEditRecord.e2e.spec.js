c7eb476df2f5e70f52bd291ac61a9138748a821f
Brandon Stookey <bstookey@quickbase.com>
2017-01-30 14:27:16 -0500

    run only In-line edit menu should have three actions (save, save and add new, cancel

--- ui/wdio/tests/reports/reportEditRecord.e2e.spec.js
+++ ui/wdio/tests/reports/reportEditRecord.e2e.spec.js
@@ -65,126 +65,126 @@
         /**
          * Test Method. Record in-line editing test on a report.
          */
-        it('Should allow you to in-line edit multiple fields in a report record', function() {
-
-            //Variable declarations
-            var textToEnter = 'My new text';
-            var dateToEnter = '03-11-1985';
-            var dateToExpect = '03-12-1985';
-            var successMessage = 'Record saved';
-
-            //Step 1 - Open the in-line edit menu for the first record in the report
-            ReportInLineEditPO.openRecordEditMenu(0);
-
-            // Step 2 - Edit the Text Field
-            ReportInLineEditPO.editTextField(0, textToEnter);
-
-            // Steps 3 - Edit the Date Field
-            // Set the value of the input box so the calendar widget will be set
-            ReportInLineEditPO.editDateField(0, dateToEnter);
-            // Open the calendar widget and advance the present date by 1 day
-            if (browserName !== 'safari') {
-                ReportInLineEditPO.openDateFieldCalWidget(0);
-                ReportInLineEditPO.advanceCurrentlySelectedDate(0);
-            }
-
-            // Step 4 - Save the edit
-            ReportInLineEditPO.clickSaveChangesButton();
-            //TODO: See if we can handle this a different way so it will work 100%. Would like to have this assertion
-            //ReportInLineEditPO.assertSuccessMessage(successMessage);
-            expect(browser.isVisible('.qbRow.editing .saveRecord')).toBeFalsy();
-            expect(browser.isVisible('.qbRow.editing .cancelSelection')).toBeFalsy();
-            expect(browser.isVisible('.qbRow.editing .addRecord')).toBeFalsy();
-
-            // Step 5 - Reload the report after saving
-            e2ePageBase.loadReportByIdInBrowser(realmName, testApp.id, testApp.tables[e2eConsts.TABLE1].id, 1);
-
-            // Step 6 - Check for the edited value at the edited row
-            var recordValues = ReportContentPO.getRecordValues(0);
-            expect(recordValues[1]).toBe(textToEnter, 'Expected Text is not present');
-
-            //If browser is safari then we are not advancing the present date by 1 day
-            if (browserName !== 'safari') {
-                expect(recordValues[6]).toBe(dateToExpect, 'Expected date is not present');
-            } else {
-                expect(recordValues[6]).toBe(dateToEnter, 'Expected date is not present');
-            }
-
-        });
-
-        /**
-         * Test Method. Tests in-line editing with Pagination.
-         */
-        it('Should allow you to in-line edit a record on the second page', function() {
-            //Variable declarations
-            var textToEnter = 'My new text on page 2';
-            var successMessage = 'Record saved';
-
-            // Steps 1- Go to the second page of records
-            ReportPagingPO.clickPagingNavButton(ReportPagingPO.pagingToolbarNextButton);
-
-            // Step 2 - Open the in-line edit menu for the first record on that page
-            ReportInLineEditPO.openRecordEditMenu(0);
-
-            // Step 3 - Edit the Text Field
-            ReportInLineEditPO.editTextField(0, textToEnter);
-
-            // Step 4 - Save the edit
-            ReportInLineEditPO.clickSaveChangesButton();
-            // browser.waitForText(textToEnter);
-            expect(browser.isVisible('.qbRow.editing .saveRecord')).toBeFalsy();
-            expect(browser.isVisible('.qbRow.editing .cancelSelection')).toBeFalsy();
-            expect(browser.isVisible('.qbRow.editing .addRecord')).toBeFalsy();
-
-            // Step 5 - Check for the success message 'Record added'
-            //TODO: See if we can handle this a different way so it will work 100%. Would like to have this assertion
-            //ReportInLineEditPO.assertSuccessMessage(successMessage);
-
-            // Step 6 - Check that the edit persisted on the report
-            var recordValues = ReportContentPO.getRecordValues(0);
-            expect(recordValues[1]).toBe(textToEnter);
-
-            // Step 7 - Go back to the first page of records
-            ReportPagingPO.clickPagingNavButton(ReportPagingPO.pagingToolbarPrevButton);
-        });
-
-        /**
-         * Test Method. Cancel button test for in-line editing.
-         */
-        it('Cancel button should not save edit updates to a record', function() {
-            //Note this is not working on safari
-            if (browserName !== 'safari') {
-                var textToEnter = 'My new text 2';
-
-                //Step 1 - Get the original value of the text field on the second record
-                var fieldValues = ReportContentPO.getRecordValues(1);
-                var originalText = fieldValues[1];
-
-                //Step 2 - Open the in-line edit menu for the second record
-                ReportInLineEditPO.openRecordEditMenu(1);
-
-                // Step 3 - Edit the Text Field
-                ReportInLineEditPO.editTextField(0, textToEnter);
-
-                // Step 4 - Click the cancel button
-                ReportInLineEditPO.clickCancelButton();
-
-                //Step 5 - Extra Assertions for inline Edit to be invisible
-                expect(browser.isVisible('.qbRow.editing .saveRecord')).toBeFalsy();
-                expect(browser.isVisible('.qbRow.editing .cancelSelection')).toBeFalsy();
-                expect(browser.isVisible('.qbRow.editing .addRecord')).toBeFalsy();
-
-                // Check that the edit was not persisted on report
-                var fieldValues2 = ReportContentPO.getRecordValues(1);
-                expect(fieldValues2[1]).toBe(originalText);
-            }
-        });
-
-
-        //TODO: Required field test, Need to extend setup data for this
-
-        //TODO: Invalid input value tests (text in a date field)
-
-        //TODO: Check that record ID isn't editable
+        // it('Should allow you to in-line edit multiple fields in a report record', function() {
+        //
+        //     //Variable declarations
+        //     var textToEnter = 'My new text';
+        //     var dateToEnter = '03-11-1985';
+        //     var dateToExpect = '03-12-1985';
+        //     var successMessage = 'Record saved';
+        //
+        //     //Step 1 - Open the in-line edit menu for the first record in the report
+        //     ReportInLineEditPO.openRecordEditMenu(0);
+        //
+        //     // Step 2 - Edit the Text Field
+        //     ReportInLineEditPO.editTextField(0, textToEnter);
+        //
+        //     // Steps 3 - Edit the Date Field
+        //     // Set the value of the input box so the calendar widget will be set
+        //     ReportInLineEditPO.editDateField(0, dateToEnter);
+        //     // Open the calendar widget and advance the present date by 1 day
+        //     if (browserName !== 'safari') {
+        //         ReportInLineEditPO.openDateFieldCalWidget(0);
+        //         ReportInLineEditPO.advanceCurrentlySelectedDate(0);
+        //     }
+        //
+        //     // Step 4 - Save the edit
+        //     ReportInLineEditPO.clickSaveChangesButton();
+        //     //TODO: See if we can handle this a different way so it will work 100%. Would like to have this assertion
+        //     //ReportInLineEditPO.assertSuccessMessage(successMessage);
+        //     expect(browser.isVisible('.qbRow.editing .saveRecord')).toBeFalsy();
+        //     expect(browser.isVisible('.qbRow.editing .cancelSelection')).toBeFalsy();
+        //     expect(browser.isVisible('.qbRow.editing .addRecord')).toBeFalsy();
+        //
+        //     // Step 5 - Reload the report after saving
+        //     e2ePageBase.loadReportByIdInBrowser(realmName, testApp.id, testApp.tables[e2eConsts.TABLE1].id, 1);
+        //
+        //     // Step 6 - Check for the edited value at the edited row
+        //     var recordValues = ReportContentPO.getRecordValues(0);
+        //     expect(recordValues[1]).toBe(textToEnter, 'Expected Text is not present');
+        //
+        //     //If browser is safari then we are not advancing the present date by 1 day
+        //     if (browserName !== 'safari') {
+        //         expect(recordValues[6]).toBe(dateToExpect, 'Expected date is not present');
+        //     } else {
+        //         expect(recordValues[6]).toBe(dateToEnter, 'Expected date is not present');
+        //     }
+        //
+        // });
+        //
+        // /**
+        //  * Test Method. Tests in-line editing with Pagination.
+        //  */
+        // it('Should allow you to in-line edit a record on the second page', function() {
+        //     //Variable declarations
+        //     var textToEnter = 'My new text on page 2';
+        //     var successMessage = 'Record saved';
+        //
+        //     // Steps 1- Go to the second page of records
+        //     ReportPagingPO.clickPagingNavButton(ReportPagingPO.pagingToolbarNextButton);
+        //
+        //     // Step 2 - Open the in-line edit menu for the first record on that page
+        //     ReportInLineEditPO.openRecordEditMenu(0);
+        //
+        //     // Step 3 - Edit the Text Field
+        //     ReportInLineEditPO.editTextField(0, textToEnter);
+        //
+        //     // Step 4 - Save the edit
+        //     ReportInLineEditPO.clickSaveChangesButton();
+        //     // browser.waitForText(textToEnter);
+        //     expect(browser.isVisible('.qbRow.editing .saveRecord')).toBeFalsy();
+        //     expect(browser.isVisible('.qbRow.editing .cancelSelection')).toBeFalsy();
+        //     expect(browser.isVisible('.qbRow.editing .addRecord')).toBeFalsy();
+        //
+        //     // Step 5 - Check for the success message 'Record added'
+        //     //TODO: See if we can handle this a different way so it will work 100%. Would like to have this assertion
+        //     //ReportInLineEditPO.assertSuccessMessage(successMessage);
+        //
+        //     // Step 6 - Check that the edit persisted on the report
+        //     var recordValues = ReportContentPO.getRecordValues(0);
+        //     expect(recordValues[1]).toBe(textToEnter);
+        //
+        //     // Step 7 - Go back to the first page of records
+        //     ReportPagingPO.clickPagingNavButton(ReportPagingPO.pagingToolbarPrevButton);
+        // });
+        //
+        // /**
+        //  * Test Method. Cancel button test for in-line editing.
+        //  */
+        // it('Cancel button should not save edit updates to a record', function() {
+        //     //Note this is not working on safari
+        //     if (browserName !== 'safari') {
+        //         var textToEnter = 'My new text 2';
+        //
+        //         //Step 1 - Get the original value of the text field on the second record
+        //         var fieldValues = ReportContentPO.getRecordValues(1);
+        //         var originalText = fieldValues[1];
+        //
+        //         //Step 2 - Open the in-line edit menu for the second record
+        //         ReportInLineEditPO.openRecordEditMenu(1);
+        //
+        //         // Step 3 - Edit the Text Field
+        //         ReportInLineEditPO.editTextField(0, textToEnter);
+        //
+        //         // Step 4 - Click the cancel button
+        //         ReportInLineEditPO.clickCancelButton();
+        //
+        //         //Step 5 - Extra Assertions for inline Edit to be invisible
+        //         expect(browser.isVisible('.qbRow.editing .saveRecord')).toBeFalsy();
+        //         expect(browser.isVisible('.qbRow.editing .cancelSelection')).toBeFalsy();
+        //         expect(browser.isVisible('.qbRow.editing .addRecord')).toBeFalsy();
+        //
+        //         // Check that the edit was not persisted on report
+        //         var fieldValues2 = ReportContentPO.getRecordValues(1);
+        //         expect(fieldValues2[1]).toBe(originalText);
+        //     }
+        // });
+        //
+        //
+        // //TODO: Required field test, Need to extend setup data for this
+        //
+        // //TODO: Invalid input value tests (text in a date field)
+        //
+        // //TODO: Check that record ID isn't editable
     });
 }());
