da619b38de4e4c128133c914f9787d54f36a391b
Brandon Stookey <bstookey@quickbase.com>
2017-01-30 14:30:13 -0500

    Running second it block Should allow you to in-line edit multiple fields in a report record

--- ui/wdio/tests/reports/reportEditRecord.e2e.spec.js
+++ ui/wdio/tests/reports/reportEditRecord.e2e.spec.js
@@ -48,69 +48,69 @@
          * Test Method - In-line edit menu should have three actions
          */
 
-        it('In-line edit menu should have three actions (save, save and add new, cancel)', function() {
-
-            browser.logger.info('it spec function - Running the test');
-
-            //Step 1 -  Open the in-line edit menu for the first record in the report
-            ReportInLineEditPO.openRecordEditMenu(0);
-
-            //Step 2 -  Check that the three buttons are visible
-            ReportInLineEditPO.assertInlineEditMenuButtonsTobeTrue();
-
-            //step 3 - Close the edit menu
-            ReportInLineEditPO.clickCancelButton();
-        });
+        // it('In-line edit menu should have three actions (save, save and add new, cancel)', function() {
+        //
+        //     browser.logger.info('it spec function - Running the test');
+        //
+        //     //Step 1 -  Open the in-line edit menu for the first record in the report
+        //     ReportInLineEditPO.openRecordEditMenu(0);
+        //
+        //     //Step 2 -  Check that the three buttons are visible
+        //     ReportInLineEditPO.assertInlineEditMenuButtonsTobeTrue();
+        //
+        //     //step 3 - Close the edit menu
+        //     ReportInLineEditPO.clickCancelButton();
+        // });
 
         /**
          * Test Method. Record in-line editing test on a report.
          */
-        // it('Should allow you to in-line edit multiple fields in a report record', function() {
-        //
-        //     //Variable declarations
-        //     var textToEnter = 'My new text';
-        //     var dateToEnter = '03-11-1985';
-        //     var dateToExpect = '03-12-1985';
-        //     var successMessage = 'Record saved';
-        //
-        //     //Step 1 - Open the in-line edit menu for the first record in the report
-        //     ReportInLineEditPO.openRecordEditMenu(0);
-        //
-        //     // Step 2 - Edit the Text Field
-        //     ReportInLineEditPO.editTextField(0, textToEnter);
-        //
-        //     // Steps 3 - Edit the Date Field
-        //     // Set the value of the input box so the calendar widget will be set
-        //     ReportInLineEditPO.editDateField(0, dateToEnter);
-        //     // Open the calendar widget and advance the present date by 1 day
-        //     if (browserName !== 'safari') {
-        //         ReportInLineEditPO.openDateFieldCalWidget(0);
-        //         ReportInLineEditPO.advanceCurrentlySelectedDate(0);
-        //     }
-        //
-        //     // Step 4 - Save the edit
-        //     ReportInLineEditPO.clickSaveChangesButton();
-        //     //TODO: See if we can handle this a different way so it will work 100%. Would like to have this assertion
-        //     //ReportInLineEditPO.assertSuccessMessage(successMessage);
-        //     expect(browser.isVisible('.qbRow.editing .saveRecord')).toBeFalsy();
-        //     expect(browser.isVisible('.qbRow.editing .cancelSelection')).toBeFalsy();
-        //     expect(browser.isVisible('.qbRow.editing .addRecord')).toBeFalsy();
-        //
-        //     // Step 5 - Reload the report after saving
-        //     e2ePageBase.loadReportByIdInBrowser(realmName, testApp.id, testApp.tables[e2eConsts.TABLE1].id, 1);
-        //
-        //     // Step 6 - Check for the edited value at the edited row
-        //     var recordValues = ReportContentPO.getRecordValues(0);
-        //     expect(recordValues[1]).toBe(textToEnter, 'Expected Text is not present');
-        //
-        //     //If browser is safari then we are not advancing the present date by 1 day
-        //     if (browserName !== 'safari') {
-        //         expect(recordValues[6]).toBe(dateToExpect, 'Expected date is not present');
-        //     } else {
-        //         expect(recordValues[6]).toBe(dateToEnter, 'Expected date is not present');
-        //     }
-        //
-        // });
+        it('Should allow you to in-line edit multiple fields in a report record', function() {
+
+            //Variable declarations
+            var textToEnter = 'My new text';
+            var dateToEnter = '03-11-1985';
+            var dateToExpect = '03-12-1985';
+            var successMessage = 'Record saved';
+
+            //Step 1 - Open the in-line edit menu for the first record in the report
+            ReportInLineEditPO.openRecordEditMenu(0);
+
+            // Step 2 - Edit the Text Field
+            ReportInLineEditPO.editTextField(0, textToEnter);
+
+            // Steps 3 - Edit the Date Field
+            // Set the value of the input box so the calendar widget will be set
+            ReportInLineEditPO.editDateField(0, dateToEnter);
+            // Open the calendar widget and advance the present date by 1 day
+            if (browserName !== 'safari') {
+                ReportInLineEditPO.openDateFieldCalWidget(0);
+                ReportInLineEditPO.advanceCurrentlySelectedDate(0);
+            }
+
+            // Step 4 - Save the edit
+            ReportInLineEditPO.clickSaveChangesButton();
+            //TODO: See if we can handle this a different way so it will work 100%. Would like to have this assertion
+            //ReportInLineEditPO.assertSuccessMessage(successMessage);
+            expect(browser.isVisible('.qbRow.editing .saveRecord')).toBeFalsy();
+            expect(browser.isVisible('.qbRow.editing .cancelSelection')).toBeFalsy();
+            expect(browser.isVisible('.qbRow.editing .addRecord')).toBeFalsy();
+
+            // Step 5 - Reload the report after saving
+            e2ePageBase.loadReportByIdInBrowser(realmName, testApp.id, testApp.tables[e2eConsts.TABLE1].id, 1);
+
+            // Step 6 - Check for the edited value at the edited row
+            var recordValues = ReportContentPO.getRecordValues(0);
+            expect(recordValues[1]).toBe(textToEnter, 'Expected Text is not present');
+
+            //If browser is safari then we are not advancing the present date by 1 day
+            if (browserName !== 'safari') {
+                expect(recordValues[6]).toBe(dateToExpect, 'Expected date is not present');
+            } else {
+                expect(recordValues[6]).toBe(dateToEnter, 'Expected date is not present');
+            }
+
+        });
         //
         // /**
         //  * Test Method. Tests in-line editing with Pagination.
