fa0e43e2bb0ba540040d75f74e177dd1e9aafadf
Gen Edwards <gedwards@quickbase.com>
2017-02-03 14:29:37 -0500

    1952:remove unnecessary code

--- ui/client-react/src/components/QBForm/relatedChildReport.js
+++ ui/client-react/src/components/QBForm/relatedChildReport.js
@@ -5,21 +5,24 @@ import _ from 'lodash';
 import UrlUtils from '../../utils/urlUtils';
 import Breakpoints from '../../utils/breakpoints';
 
+/**
+ * This component renders a link to a child report, which is the behavior in the small breakpoint.
+ * In medium/large breakpoint, we will show an embedded report of the children(not implemented yet).
+ */
 class ChildReport extends React.Component {
     constructor(...args) {
         super(...args);
     }
     render() {
-        // TODO: render embedded report for medium and large breakpoint
         const {appId, childTableId, childReportId, foreignKeyFid, foreignKeyValue} = this.props;
         const validProps = [appId, childTableId, childReportId, foreignKeyFid, foreignKeyValue].every(prop => prop || typeof prop === 'number');
         if (!validProps) {
             return null;
-        } else if (Breakpoints.isSmallBreakpoint() || true) { //TODO: medium/large breakpoint
+        } else if (Breakpoints.isSmallBreakpoint() || true) {
             const link = UrlUtils.getRelatedChildReportLink(appId, childTableId, childReportId, foreignKeyFid, foreignKeyValue);
             return <Link to={link} className="relatedChildReport childReportLink">{this.props.childTableName || "Child Table"}</Link>;
         } else {
-            //TODO: show embedded report in medium/large breakpoint
+            // TODO: render embedded report for medium and large breakpoint
             return null;
         }
     }
@@ -35,9 +38,6 @@ ChildReport.propTypes = {
     foreignKeyFid: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
     /** The value entered in the foreignkey field. */
     foreignKeyValue: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number])
-};
-ChildReport.defaultProps = {
-
 };
 
 export default ChildReport;
