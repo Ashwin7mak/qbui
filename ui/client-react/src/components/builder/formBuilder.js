fd382480838d33ae60eb9521f2100bb4b140ce3e
Brandon Stookey <bstookey@quickbase.com>
2017-02-03 08:36:54 -0500

    add style file

--- ui/client-react/src/components/builder/formBuilder.js
+++ ui/client-react/src/components/builder/formBuilder.js
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+import './formBuilder.scss'
 
 
 const FormBuilder = React.createClass({
@@ -10,7 +11,7 @@ const FormBuilder = React.createClass({
     render() {
         return(
             <div>
-                <h1>Hello!</h1>
+                <h1 className="formBuilderHeader">Welcome To Form Builder</h1>
             </div>
         )
     }
