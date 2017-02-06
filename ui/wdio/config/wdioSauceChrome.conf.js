9141e8e49266374e9ace7507096d92e4b8ffbb24
Ken LaBak <Ken_LaBak@intuit.com>
2017-02-06 15:35:50 -0500

    Updates to config files due to error in CI env.

--- ui/wdio/config/wdioSauceChrome.conf.js
+++ ui/wdio/config/wdioSauceChrome.conf.js
@@ -1,23 +1,19 @@
-const baseConf = require('./wdioSauce.conf');
-const config = {
-    capabilities: [
-        {
-            platform : 'OS X 10.11',
-            browserName     : 'chrome',
-            version: '55.0',
-            tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-            name            : process.env.SAUCE_JOB_NAME + '_OSX_Chrome',
-            //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
-            idleTimeout: '120',
-            screenResolution : '1600x1200',
-            maxDuration: 10800,
-            breakpointSize: 'xlarge',
-            // These two values enable parallel testing which will run a spec file per instance
-            shardTestFiles: true,
-            maxInstances: 2
-        }
-    ]
-};
-
-let exportConfig = Object.assign(baseConf.config, config);
-exports.config = exportConfig;
+var baseConf = require('./wdioSauce.conf');
+baseConf.capabilities = [
+    {
+        platform : 'OS X 10.11',
+        browserName     : 'chrome',
+        version: '55.0',
+        tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
+        name            : process.env.SAUCE_JOB_NAME + '_OSX_Chrome',
+        //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
+        idleTimeout: '120',
+        screenResolution : '1600x1200',
+        maxDuration: 10800,
+        breakpointSize: 'xlarge',
+        // These two values enable parallel testing which will run a spec file per instance
+        shardTestFiles: true,
+        maxInstances: 2
+    }
+];
+exports.config = baseConf;
--- ui/wdio/config/wdioSauceEdge.conf.js
+++ ui/wdio/config/wdioSauceEdge.conf.js
@@ -1,22 +1,18 @@
-const baseConf = require('./wdioSauce.conf');
-const config = {
-    capabilities: [
-        {
-            platform: 'Windows 10',
-            browserName: 'MicrosoftEdge',
-            version: '14.14393',
-            tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-            name: process.env.SAUCE_JOB_NAME + '_Win10_MicrosoftEdge',
-            screenResolution : '1600x1200',
-            //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
-            idleTimeout: '280',
-            maxDuration: 10800,
-            breakpointSize: 'xlarge',
-            shardTestFiles: true,
-            maxInstances: 2
-        }
-    ]
-};
-
-let exportConfig = Object.assign(baseConf.config, config);
-exports.config = exportConfig;
+var baseConf = require('./wdioSauce.conf');
+baseConf.capabilities = [
+    {
+        platform: 'Windows 10',
+        browserName: 'MicrosoftEdge',
+        version: '14.14393',
+        tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
+        name: process.env.SAUCE_JOB_NAME + '_Win10_MicrosoftEdge',
+        screenResolution : '1600x1200',
+        //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
+        idleTimeout: '280',
+        maxDuration: 10800,
+        breakpointSize: 'xlarge',
+        shardTestFiles: true,
+        maxInstances: 2
+    }
+];
+exports.config = baseConf;
