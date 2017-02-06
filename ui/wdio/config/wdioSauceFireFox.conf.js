7ffb65ffcc69697821a5a437865729b1beeaa6e7
Ken LaBak <Ken_LaBak@intuit.com>
2017-02-06 15:36:34 -0500

    Updates to config files due to error in CI env.

--- ui/wdio/config/wdioSauceFireFox.conf.js
+++ ui/wdio/config/wdioSauceFireFox.conf.js
@@ -1,22 +1,18 @@
-const baseConf = require('./wdioSauce.conf');
-const config = {
-    capabilities: [
-        {
-            platform: 'OS X 10.11',
-            browserName: 'firefox',
-            version: '46.0',
-            tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-            name: process.env.SAUCE_JOB_NAME + '_OSX_Firefox',
-            screenResolution : '1600x1200',
-            //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
-            idleTimeout: '180',
-            maxDuration: 10800,
-            breakpointSize: 'large',
-            shardTestFiles: true,
-            maxInstances: 2
-        }
-    ]
-};
-
-let exportConfig = Object.assign(baseConf.config, config);
-exports.config = exportConfig;
+var baseConf = require('./wdioSauce.conf');
+baseConf.capabilities = [
+    {
+        platform: 'OS X 10.11',
+        browserName: 'firefox',
+        version: '46.0',
+        tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
+        name: process.env.SAUCE_JOB_NAME + '_OSX_Firefox',
+        screenResolution : '1600x1200',
+        //Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
+        idleTimeout: '180',
+        maxDuration: 10800,
+        breakpointSize: 'large',
+        shardTestFiles: true,
+        maxInstances: 2
+    }
+];
+exports.config = baseConf;
