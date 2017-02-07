7d7c7fd750f2fc5f1c577f2baddb31684b3bab9a
Brandon Stookey <bstookey@quickbase.com>
2017-02-06 19:01:47 -0500

    test clean up

--- ui/client-react/test/components/recordRoute.unit.spec.js
+++ ui/client-react/test/components/recordRoute.unit.spec.js
@@ -11,7 +11,7 @@ import {loadingForm} from '../../src/actions/formActions';
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-describe('RecordRoute functions', () => {
+describe('RecordRoute', () => {
     'use strict';
 
     let component;
@@ -41,185 +41,189 @@ describe('RecordRoute functions', () => {
         flux.actions.showNextRecord.calls.reset();
     });
 
-    it('test render of component with missing url params', () => {
-        let badRouteParams = {appId:1, tblId:2};
+    describe('Previous/Next/Return functions', () => {
+        it('test render of component with missing url params', () => {
+            let badRouteParams = {appId: 1, tblId: 2};
 
-        component = TestUtils.renderIntoDocument(<RecordRoute params={badRouteParams} flux={flux}/>);
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+            component = TestUtils.renderIntoDocument(<RecordRoute params={badRouteParams} flux={flux}/>);
+            expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
 
-        let qbForm = TestUtils.scryRenderedComponentsWithType(component, QBForm);
-        expect(qbForm.length).toBe(0);
+            let qbForm = TestUtils.scryRenderedComponentsWithType(component, QBForm);
+            expect(qbForm.length).toBe(0);
 
+        });
+
+        it('test render of component without report param', () => {
+
+            const initialState = {};
+            const store = mockStore(initialState);
+
+            let routeParams = {appId: 1, tblId: 2, recordId: 4};
+
+            component = TestUtils.renderIntoDocument(
+                <Provider store={store}>
+                    <ConnectedRecordRoute params={routeParams} flux={flux}/>
+                </Provider>);
+
+            expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+
+            expect(flux.actions.selectTableId).toHaveBeenCalledWith(routeParams.tblId);
+
+            // test Redux actions
+            expect(store.getActions()[0]).toEqual(loadingForm("view"));
+
+            let qbForm = TestUtils.scryRenderedComponentsWithType(component, QBForm);
+            expect(qbForm.length).toBe(1);
+
+            let prevRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "prevRecord");
+            let nextRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "nextRecord");
+            let returnToReport = TestUtils.scryRenderedDOMComponentsWithClass(component, "backToReport");
+
+            // no report param so no nav links
+            expect(prevRecord.length).toBe(0);
+            expect(nextRecord.length).toBe(0);
+            expect(returnToReport.length).toBe(0);
+        });
+
+        it('test render of component with report param', () => {
+
+            const initialState = {};
+            const store = mockStore(initialState);
+
+            let routeParams = {appId: 1, tblId: 2, recordId: 3, rptId: 4};
+
+            component = TestUtils.renderIntoDocument(
+                <Provider store={store}>
+                    <ConnectedRecordRoute params={routeParams} flux={flux}/>
+                </Provider>);
+
+            expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+
+            let prevRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "prevRecord");
+            let nextRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "nextRecord");
+            let returnToReport = TestUtils.scryRenderedDOMComponentsWithClass(component, "backToReport");
+
+            // no next/prev nav links since we don't have props for those records (a bookmarked link)
+            expect(prevRecord.length).toBe(0);
+            expect(nextRecord.length).toBe(0);
+
+            // but we can still nav back to the report
+            expect(returnToReport.length).toBe(1);
+        });
+
+        it('test correct state is pushed to history', () => {
+            const initialState = {};
+            const store = mockStore(initialState);
+
+            let routeParams = {appId: 1, tblId: 2, rptId: 3, recordId: 2};
+            let reportData = {
+                appId: 1,
+                tblId: 2,
+                rptId: 3,
+
+                previousRecordId: 1,
+                currentRecordId: 2,
+                nextRecordId: 3,
+
+                data: {
+                    records: [
+                        {"Record ID#": {id: 1, value: 1, display: "1"}},
+                        {"Record ID#": {id: 2, value: 2, display: "2"}},
+                        {"Record ID#": {id: 3, value: 3, display: "3"}}
+                    ],
+                    columns: [
+                        {
+                            id: 1,
+                            field: "Record ID#",
+                            headerName: "Record ID#",
+                            fieldDef: {datatypeAttributes: {type: "NUMERIC"}}
+                        }
+                    ]
+                }
+            };
+
+            let router = [];
+            let expectedRouter = [];
+
+            component = TestUtils.renderIntoDocument(
+                <Provider store={store}>
+                    <ConnectedRecordRoute params={routeParams} reportData={reportData} flux={flux} router={router}/>
+                </Provider>);
+
+            expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+
+            let prevRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "prevRecord");
+            let nextRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "nextRecord");
+            let returnToReport = TestUtils.scryRenderedDOMComponentsWithClass(component, "backToReport");
+
+            // should have all 3 nav links
+            expect(prevRecord.length).toBe(1);
+            expect(nextRecord.length).toBe(1);
+            expect(returnToReport.length).toBe(1);
+
+            // previous record
+            TestUtils.Simulate.click(prevRecord[0]);
+            expect(flux.actions.showPreviousRecord).toHaveBeenCalled();
+            expectedRouter.push('/qbase/app/1/table/2/report/3/record/1');
+
+            // next record
+            TestUtils.Simulate.click(nextRecord[0]);
+            expect(flux.actions.showNextRecord).toHaveBeenCalled();
+            expectedRouter.push('/qbase/app/1/table/2/report/3/record/3');
+
+            // return to report
+            TestUtils.Simulate.click(returnToReport[0]);
+            expectedRouter.push('/qbase/app/1/table/2/report/3');
+
+            expect(router).toEqual(expectedRouter);
+        });
     });
+    describe('navigateToBuilder function', () => {
 
-    it('test render of component without report param', () => {
+        it('should render a component without form type or form id', () => {
+            const initialState = {};
+            const store = mockStore(initialState);
 
-        const initialState = {};
-        const store = mockStore(initialState);
+            let routeParams = {appId: 1, tblId: 2, rptId: 3, recordId: 2};
+            let router = [];
+            let expectedRouter = [];
 
-        let routeParams = {appId:1, tblId:2, recordId: 4};
+            component = TestUtils.renderIntoDocument(
+                <Provider store={store}>
+                    <ConnectedRecordRoute params={routeParams} flux={flux} router={router}/>
+                </Provider>);
 
-        component = TestUtils.renderIntoDocument(
-            <Provider store={store}>
-                <ConnectedRecordRoute params={routeParams} flux={flux}/>
-            </Provider>);
+            expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
 
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+            let formBuilder = TestUtils.scryRenderedDOMComponentsWithClass(component, "formBuilderButton");
 
-        expect(flux.actions.selectTableId).toHaveBeenCalledWith(routeParams.tblId);
+            TestUtils.Simulate.click(formBuilder[0]);
+            expectedRouter.push('/qbase/builder/app/1/table/2/form');
 
-        // test Redux actions
-        expect(store.getActions()[0]).toEqual(loadingForm("view"));
+            expect(router).toEqual(expectedRouter);
+        });
 
-        let qbForm = TestUtils.scryRenderedComponentsWithType(component, QBForm);
-        expect(qbForm.length).toBe(1);
+        it('should render a component with a form type', () => {
+            const initialState = {forms: [{id: 'view'}]};
+            const store = mockStore(initialState);
 
-        let prevRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "prevRecord");
-        let nextRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "nextRecord");
-        let returnToReport = TestUtils.scryRenderedDOMComponentsWithClass(component, "backToReport");
+            let routeParams = {appId: 1, tblId: 2, rptId: 3, recordId: 2};
+            let router = [];
+            let expectedRouter = [];
 
-        // no report param so no nav links
-        expect(prevRecord.length).toBe(0);
-        expect(nextRecord.length).toBe(0);
-        expect(returnToReport.length).toBe(0);
-    });
+            component = TestUtils.renderIntoDocument(
+                <Provider store={store}>
+                    <ConnectedRecordRoute params={routeParams} flux={flux} router={router}/>
+                </Provider>);
 
-    it('test render of component with report param', () => {
+            expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
 
-        const initialState = {};
-        const store = mockStore(initialState);
+            let formBuilder = TestUtils.scryRenderedDOMComponentsWithClass(component, "formBuilderButton");
 
-        let routeParams = {appId:1, tblId:2, recordId: 3, rptId: 4};
+            TestUtils.Simulate.click(formBuilder[0]);
+            expectedRouter.push('/qbase/builder/app/1/table/2/form?formType=view');
 
-        component = TestUtils.renderIntoDocument(
-            <Provider store={store}>
-                <ConnectedRecordRoute params={routeParams} flux={flux}/>
-            </Provider>);
-
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
-
-        let prevRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "prevRecord");
-        let nextRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "nextRecord");
-        let returnToReport = TestUtils.scryRenderedDOMComponentsWithClass(component, "backToReport");
-
-        // no next/prev nav links since we don't have props for those records (a bookmarked link)
-        expect(prevRecord.length).toBe(0);
-        expect(nextRecord.length).toBe(0);
-
-        // but we can still nav back to the report
-        expect(returnToReport.length).toBe(1);
-    });
-
-    it('test correct state is pushed to history', () => {
-        const initialState = {};
-        const store = mockStore(initialState);
-
-        let routeParams = {appId:1, tblId:2, rptId:3, recordId: 2};
-        let reportData = {
-            appId:1,
-            tblId:2,
-            rptId:3,
-
-            previousRecordId:1,
-            currentRecordId:2,
-            nextRecordId:3,
-
-            data: {
-                records: [
-                    {"Record ID#": {id:1, value:1, display: "1"}},
-                    {"Record ID#": {id:2, value:2, display: "2"}},
-                    {"Record ID#": {id:3, value:3, display: "3"}}
-                ],
-                columns: [
-                    {
-                        id:1,
-                        field:"Record ID#",
-                        headerName:"Record ID#",
-                        fieldDef: {datatypeAttributes: {type:"NUMERIC"}}
-                    }
-                ]
-            }
-        };
-
-        let router = [];
-        let expectedRouter = [];
-
-        component = TestUtils.renderIntoDocument(
-            <Provider store={store}>
-                <ConnectedRecordRoute params={routeParams} reportData={reportData} flux={flux} router={router}/>
-            </Provider>);
-
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
-
-        let prevRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "prevRecord");
-        let nextRecord = TestUtils.scryRenderedDOMComponentsWithClass(component, "nextRecord");
-        let returnToReport = TestUtils.scryRenderedDOMComponentsWithClass(component, "backToReport");
-
-        // should have all 3 nav links
-        expect(prevRecord.length).toBe(1);
-        expect(nextRecord.length).toBe(1);
-        expect(returnToReport.length).toBe(1);
-
-        // previous record
-        TestUtils.Simulate.click(prevRecord[0]);
-        expect(flux.actions.showPreviousRecord).toHaveBeenCalled();
-        expectedRouter.push('/qbase/app/1/table/2/report/3/record/1');
-
-        // next record
-        TestUtils.Simulate.click(nextRecord[0]);
-        expect(flux.actions.showNextRecord).toHaveBeenCalled();
-        expectedRouter.push('/qbase/app/1/table/2/report/3/record/3');
-
-        // return to report
-        TestUtils.Simulate.click(returnToReport[0]);
-        expectedRouter.push('/qbase/app/1/table/2/report/3');
-
-        expect(router).toEqual(expectedRouter);
-    });
-
-    it('navigateToBuilder should render a component without form type or form id', () => {
-        const initialState = {};
-        const store = mockStore(initialState);
-
-        let routeParams = {appId:1, tblId:2, rptId:3, recordId: 2};
-        let router = [];
-        let expectedRouter = [];
-
-        component = TestUtils.renderIntoDocument(
-            <Provider store={store}>
-                <ConnectedRecordRoute params={routeParams} flux={flux} router={router}/>
-            </Provider>);
-
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
-
-        let formBuilder = TestUtils.scryRenderedDOMComponentsWithClass(component, "formBuilderButton");
-
-        TestUtils.Simulate.click(formBuilder[0]);
-        expectedRouter.push('/qbase/builder/app/1/table/2/form');
-
-        expect(router).toEqual(expectedRouter);
-    });
-
-    it('navigateToBuilder should render a component with a form type', () => {
-        const initialState = {forms:[{id:'view'}]};
-        const store = mockStore(initialState);
-
-        let routeParams = {appId:1, tblId:2, rptId:3, recordId: 2};
-        let router = [];
-        let expectedRouter = [];
-
-        component = TestUtils.renderIntoDocument(
-            <Provider store={store}>
-                <ConnectedRecordRoute params={routeParams} flux={flux} router={router}/>
-            </Provider>);
-
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
-
-        let formBuilder = TestUtils.scryRenderedDOMComponentsWithClass(component, "formBuilderButton");
-
-        TestUtils.Simulate.click(formBuilder[0]);
-        expectedRouter.push('/qbase/builder/app/1/table/2/form?formType=view');
-
-        expect(router).toEqual(expectedRouter);
+            expect(router).toEqual(expectedRouter);
+        });
     });
 });
