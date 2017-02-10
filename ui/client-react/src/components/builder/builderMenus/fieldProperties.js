752bab081ea507c959122b4a2d2ddab6b7a3b34c
Brandon Stookey <bstookey@quickbase.com>
2017-02-10 15:05:55 -0500

    Built out components

---
+++ ui/client-react/src/components/builder/builderMenus/fieldProperties.js
@@ -0,0 +1,16 @@
+import React, {PropTypes} from 'react';
+
+let FieldProperties = React.createClass({
+    propTypes: {
+        value: PropTypes.bool,
+    },
+    render() {
+        return (
+            <div className="fieldPropertiesContainer">
+
+            </div>
+        );
+    }
+});
+
+export default FieldProperties;
---
+++ ui/client-react/src/components/builder/builderMenus/toolPalette.js
@@ -0,0 +1,17 @@
+import React, {PropTypes} from 'react';
+
+let ToolPalette = React.createClass({
+    propTypes: {
+        value: PropTypes.bool,
+    },
+
+    render() {
+        return (
+            <div className="toolPaletteContainer">
+
+            </div>
+        )
+    }
+});
+
+export default ToolPalette;
