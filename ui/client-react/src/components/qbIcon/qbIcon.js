4140e6c57e761d60429854dd26f6991100369e40
nvanselow <nvanselow@quickbase.com>
2017-02-13 13:06:50 -0500

    MC193-Include new icon font for use with QbIcon

--- ui/client-react/src/components/qbIcon/qbIcon.js
+++ ui/client-react/src/components/qbIcon/qbIcon.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './qbIcon.scss';
 import './style.css';
+import './tableIcons.css';
+
+export const AVAILABLE_FONTS = {
+    DEFAULT: 'iconTableUISturdy',
+    UI_STURDY: 'iconTableUISturdy',
+    TABLE_STURDY: 'iconTableSturdy'
+};
 
 /**
  * # QuickBase Icon Font
@@ -10,30 +17,39 @@ import './style.css';
  *   <QBicon icon="accessibility" />
  * ```
  */
-const QBicon = React.createClass({
+const QbIcon = React.createClass({
     propTypes: {
         /**
          * See QuickBase.design for full list of icons.
          */
         icon: React.PropTypes.string.isRequired,
         className: React.PropTypes.string,
-        onClick: React.PropTypes.func
+        onClick: React.PropTypes.func,
+
+        /**
+         * Optionally set the font set to use for this icon
+         * Import the AVAILABLE_FONTS constant for easy access to which fonts are available
+         * `import QbIcon, {AVAILABLE_FONTS} from './qbIcon';
+         */
+        iconFont: React.PropTypes.oneOf(Object.keys(AVAILABLE_FONTS).map(key => AVAILABLE_FONTS[key])),
     },
     getDefaultProps() {
         return {
-            className: ""
+            className: '',
+            isTableIcon: false,
+            iconFont: AVAILABLE_FONTS.DEFAULT
         };
     },
     render: function() {
-        let className = this.props.className + ' qbIcon iconTableUISturdy-' + this.props.icon;
+        let {className, iconFont, icon} = this.props;
+        let qbIconClassName = `${className} qbIcon ${iconFont}-${icon}`;
 
         return (
-            <span className={className} onClick={this.props.onClick}>
+            <span className={qbIconClassName} onClick={this.props.onClick}>
                 {this.props.children}
             </span>
         );
     }
 });
 
-export default QBicon;
-
+export default QbIcon;
--- ui/client-react/test/components/qbIcon.unit.spec.js
+++ ui/client-react/test/components/qbIcon.unit.spec.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
-import QBicon  from '../../src/components/qbIcon/qbIcon';
+import {shallow} from 'enzyme';
+import jasmineEnzyme from 'jasmine-enzyme';
 
-describe('QBicon functions', () => {
-    'use strict';
+import QbIcon, {AVAILABLE_FONTS} from '../../src/components/qbIcon/qbIcon';
 
-    let component;
+let component;
 
+describe('QbIcon', () => {
+    beforeEach(() => {
+        jasmineEnzyme();
+    });
 
-    it('test render of component', () => {
-        component = TestUtils.renderIntoDocument(<QBicon icon="pencil" />);
-        expect(TestUtils.isCompositeComponent(component)).toBeTruthy();
+    it('renders an icon from the default font set', () => {
+        component = shallow(<QbIcon icon="pencil" />);
+
+        expect(component.find(`.${AVAILABLE_FONTS.DEFAULT}-pencil`)).toBePresent();
+    });
+
+    it('renders an icon from a different font set', () => {
+        component = shallow(<QbIcon iconFont={AVAILABLE_FONTS.TABLE_STURDY} icon="Dimensions"/>);
+
+        expect(component.find(`.${AVAILABLE_FONTS.TABLE_STURDY}-Dimensions`)).toBePresent();
     });
 });
