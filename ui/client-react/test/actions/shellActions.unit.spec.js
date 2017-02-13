3b65e986cff7d30982ca1e6511ba8fefa24942ee
Don Hatch <donald_hatch@intuit.com>
2017-02-13 16:38:34 -0500

    add missing rewire release on test finish

--- ui/client-react/test/actions/shellActions.unit.spec.js
+++ ui/client-react/test/actions/shellActions.unit.spec.js
@@ -33,6 +33,7 @@ describe('Shell actions', () => {
 
         const navState = true;
         expect(shellActions.toggleLeftNav(navState)).toEqual({type: types.TOGGLE_LEFT_NAV_EXPANDED, navState});
+        shellActions.__ResetDependency__('Breakpoints');
     });
 
     it('should create an action to show row actions menu', () => {
