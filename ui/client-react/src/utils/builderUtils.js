f7cd36c74a6054f44d714e9ae0288d837d206d7e
Brandon Stookey <bstookey@quickbase.com>
2017-02-21 18:29:03 -0500

    update comments

--- ui/client-react/src/utils/builderUtils.js
+++ ui/client-react/src/utils/builderUtils.js
@@ -1,7 +1,7 @@
 (function() {
     /**
      * As of right now we are using ontouchstart to detect if it is a touch device for form builder Drag and Drop. The styles for tablets and mobiles are different than
-     * the styles on a desktop. Desktop and tablet screens can potentially be the same dimensions, this is why media breakpoints in CSS are solely being relied on.
+     * the styles on a desktop. Desktop and tablet screens styles are also different, this is why media breakpoints in CSS are not solely being relied on.
      * */
     module.exports = {
         /**
