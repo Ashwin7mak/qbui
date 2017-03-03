f3501e83363e9ac9356a740956fd5aae61527413
nvanselow <nvanselow@quickbase.com>
2017-03-03 12:51:28 -0500

    Create first governance component

---
+++ ui/governance/src/components/main/main.js
@@ -0,0 +1,13 @@
+import React, {PropTypes, Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="governanceMain">
+                <h1>Welcome to governance!</h1>
+            </div>
+        );
+    }
+}
+
+export default Main;
