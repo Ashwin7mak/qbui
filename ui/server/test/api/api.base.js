29898620170c1854eb6a78ad2ff6be0b32711a9b
Ken LaBak <Ken_LaBak@intuit.com>
2017-03-06 15:35:12 -0500

    Removed executeEERequest from api.base.js, bumped screen resolution in sauce labs for Chrome and Edge

--- ui/server/test/api/api.base.js
+++ ui/server/test/api/api.base.js
@@ -354,46 +354,6 @@
                 return deferred.promise;
             },
 
-            executeEERequest              : function(stringPath, method, body, headers, params) {
-
-                //if there is a realm & we're not making a ticket request, use the realm subdomain request URL
-                var subdomain = '';
-                if (this.realm) {
-                    subdomain = this.realm.subdomain;
-                }
-                var opts = generateEERequestOpts(stringPath, method, subdomain);
-                if (body) {
-                    opts.body = jsonBigNum.stringify(body);
-                }
-                // if we have a GET request and have params to add (since GET requests don't use JSON body values)
-                // we have to add those to the end of the generated URL as ?param=value
-                if (params) {
-                    // remove the trailing slash and add the parameters
-                    opts.url = opts.url.substring(0, opts.url.length - 1) + params;
-                }
-                //Setup headers
-                if (headers) {
-                    opts.headers = headers;
-                } else {
-                    opts.headers = DEFAULT_HEADERS;
-                }
-                if (this.authTicket) {
-                    opts.headers[TICKET_HEADER_KEY] = this.authTicket;
-                }
-                var reqInfo = opts.url;
-                log.debug('About to execute the request: ' + jsonBigNum.stringify(opts));
-                //Make request and return promise
-                var deferred = promise.pending();
-                apiBase.executeRequestRetryable(opts, 3).then(function(resp) {
-                    log.debug('Response for reqInfo ' + reqInfo + ' got success response' + resp);
-                    deferred.resolve(resp);
-                }).catch(function(error) {
-                    log.debug('Response ERROR! for reqInfo ' + reqInfo + ' got error response' + error);
-                    deferred.reject(error);
-                });
-                return deferred.promise;
-            },
-
             executeRequestRetryable: function(opts, numRetries) {
                 var deferred = promise.pending();
                 var tries = numRetries;
