0a29b551987f0a3484703507736d861ef835f4a9
nvanselow <nvanselow@quickbase.com>
2017-03-09 14:00:13 -0500

    MC1005-Add comment regarding TopNav move to reuse library

--- ui/client-react/src/components/header/topNav.js
+++ ui/client-react/src/components/header/topNav.js
@@ -1,3 +1,7 @@
+/**
+ * The original file in this location has moved to the reuse library.
+ * What remains here is a stub so existing code does not have to change yet.
+ **/
 import TopNav from '../../../../reuse/client/src/components/reTopNav/reTopNav';
 
 export default TopNav;
