323f4fd6849b792b24dbf4a088e367902c6351bb
Don Hatch <donald_hatch@intuit.com>
2017-03-09 12:26:32 -0500

    remove recordPendingEditsAction and store

--- ui/client-react/src/scripts/fluxxor.js
+++ ui/client-react/src/scripts/fluxxor.js
@@ -1,11 +1,6 @@
 import ReactDOM from "react-dom";
 
 import Fluxxor from "fluxxor";
-import ReportDataStore from "../stores/reportDataStore";
-//import ReportDataSearchStore from "../stores/reportDataSearchStore";
-//import RecordPendingEditsStore from "../stores/recordPendingEditsStore";
-import reportDataActions from "../actions/reportDataActions";
-//import recordPendingEditsActions from "../actions/recordPendingEditsActions";
 import FieldsStore from "../stores/fieldsStore";
 import fieldsActions from "../actions/fieldsActions";
 import AppsStore from "../stores/appsStore";
@@ -16,30 +11,21 @@ import FacetMenuStore from "../stores/facetMenuStore";
 import facetMenuActions from "../actions/facetMenuActions";
 import PerfStore from "../stores/perfStore";
 import perfActions from "../actions/perfActions";
-import recordActions from "../actions/recordActions";
-import tableActions from '../actions/tableActions';
 
 export default function getFlux() {
     let stores = {
-        ReportDataStore: new ReportDataStore(),
         AppsStore: new AppsStore(),
         NavStore: new NavStore(),
         FacetMenuStore: new FacetMenuStore(),
-        //ReportDataSearchStore: new ReportDataSearchStore(),
-        //RecordPendingEditsStore: new RecordPendingEditsStore(),
         FieldsStore: new FieldsStore(),
         PerfStore: new PerfStore()
     };
     let flux = new Fluxxor.Flux(stores);
-    flux.addActions(reportDataActions);
-    //flux.addActions(recordPendingEditsActions);
     flux.addActions(appsActions);
     flux.addActions(navActions);
     flux.addActions(facetMenuActions);
     flux.addActions(fieldsActions);
-    flux.addActions(tableActions);
     flux.addActions(perfActions);
-    flux.addActions(recordActions);
 
     //to ensure you don't get cascading dispatch errors with Fluxxor
     // if you dispatch actions from within componentWillMount or componentDidMount
