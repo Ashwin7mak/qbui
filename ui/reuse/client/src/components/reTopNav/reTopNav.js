7bc8e02ec6670e8964a727b75c442a59e61ade33
nvanselow <nvanselow@quickbase.com>
2017-03-09 13:59:51 -0500

    MC1005-Refactor TopNav

--- ui/reuse/client/src/components/reTopNav/reTopNav.js
+++ ui/reuse/client/src/components/reTopNav/reTopNav.js
@@ -1,63 +1,53 @@
-import React from 'react';
+import React, {PropTypes, Component} from 'react';
 import {I18nMessage} from '../../../../../client-react/src/utils/i18nMessage';
-import Locale from '../../../../../client-react/src/locales/locales';
-import Fluxxor from 'fluxxor';
 import QBicon from '../../../../../client-react/src/components/qbIcon/qbIcon';
-let FluxMixin = Fluxxor.FluxMixin(React);
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Button from 'react-bootstrap/lib/Button';
 import Tooltip from 'react-bootstrap/lib/Tooltip';
 
-import './reTopNav.scssopNav.scss';
+import './reTopNav.scss';
 
-var ReTopNav = React.createClass({
-    mixins: [FluxMixin],
+class ReTopNav extends Component {
+    constructor(props) {
+        super(props);
 
-    propTypes: {
-        showOnSmall: React.PropTypes.bool,
-        title: React.PropTypes.node,
-        onNavClick: React.PropTypes.func,
-        globalActions: React.PropTypes.element
-    },
-    getInitialState() {
-        return {
-            searchText:""
-        };
-    },
-
-    searchChanged: function(ev) {
-        const text = ev.target.value;
-        this.setState({searchText: text});
-        //let flux = this.getFlux();
-        //flux.actions.searchFor(text);
-    },
-    searchCleared: function() {
-        this.setState({searchText: ""});
-    },
+        this.getTopTitle = this.getTopTitle.bind(this);
+    }
 
     getTopTitle() {
-        return this.props.title && (
-                <div className="topTitle">
-                    {this.props.title}
-                </div>);
-    },
-    render() {
+        let {title} = this.props;
 
-        const classes = "topNav" + (this.props.showOnSmall ? "" : " hideSmall");
+        if (title) {
+            return (
+                <div className="topTitle">
+                    {title}
+                </div>
+            );
+        }
+
+        return null;
+    }
+
+
+    render() {
+        let {showOnSmall, onNavClick, globalActions} = this.props;
+
+        const classes = "topNav" + (showOnSmall ? "" : " hideSmall");
 
         const unimplementedSearchTip = <Tooltip id="unimplemented.search.tt"><I18nMessage message="unimplemented.search"/></Tooltip>;
         const unimplementedFavoritesTip = <Tooltip id="unimplemented.favorites.tt"><I18nMessage message="unimplemented.favorites"/></Tooltip>;
+
         return (
             <div className={classes}>
                 <div className="top">
                     <div className="navGroup left">
                         <ButtonGroup className="navItem">
-                            <Button tabIndex="1"  className="iconLink toggleNavButton" onClick={this.props.onNavClick}>
+                            <Button tabIndex="1"  className="iconLink toggleNavButton" onClick={onNavClick}>
                                 <QBicon icon="hamburger" />
                             </Button>
 
-                        {this.getTopTitle()}
+                            {this.getTopTitle()}
                         </ButtonGroup>
                     </div>
 
@@ -65,25 +55,36 @@ var ReTopNav = React.createClass({
 
                         <ButtonGroup className="navItem">
 
-                            <OverlayTrigger placement="bottom" trigger={['hover', 'click']}
-                                            overlay={unimplementedSearchTip}>
-                                <Button tabIndex="2" className="disabled"><QBicon icon="search" /></Button>
+                            <OverlayTrigger placement="bottom" trigger={['hover', 'click']} overlay={unimplementedSearchTip}>
+                                <Button tabIndex="2" className="disabled">
+                                    <QBicon icon="search" />
+                                </Button>
                             </OverlayTrigger>
 
-                            <OverlayTrigger placement="bottom" trigger={['hover', 'click']}
-                                            overlay={unimplementedFavoritesTip}>
+                            <OverlayTrigger placement="bottom" trigger={['hover', 'click']} overlay={unimplementedFavoritesTip}>
                                 <Button tabIndex="3" className="disabled"><QBicon icon="star-full" /></Button>
                             </OverlayTrigger>
                         </ButtonGroup>
                     </div>
 
                     <div className="navGroup right">
-                        {this.props.globalActions}
+                        {globalActions}
                     </div>
                 </div>
             </div>
         );
     }
-});
+}
+
+ReTopNav.propTypes = {
+    showOnSmall: PropTypes.bool,
+    title: PropTypes.node,
+    onNavClick: PropTypes.func,
+    globalActions: PropTypes.element
+};
+
+ReTopNav.defaultProps = {
+    showOnSmall: false,
+};
 
 export default ReTopNav;
