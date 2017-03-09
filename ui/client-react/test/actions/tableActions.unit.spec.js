5c08ebb7a81c1ff4bb10236b43ccafba175dd9a7
Don Hatch <donald_hatch@intuit.com>
2017-03-09 15:36:02 -0500

    add unit tests against redux table actions

--- ui/client-react/test/actions/tableActions.unit.spec.js
+++ ui/client-react/test/actions/tableActions.unit.spec.js
@@ -1,67 +1,136 @@
-import Fluxxor from 'fluxxor';
-import tableActions, {__RewireAPI__ as tableActionsRewireAPI} from '../../src/actions/tableActions';
+import {loadTableHomePage, __RewireAPI__ as TableActionsRewireAPI} from '../../src/actions/tableActions';
 import * as actions from '../../src/constants/actions';
-import Constants from '../../../common/src/constants';
-import reportModel from '../../src/models/reportModel';
+import * as types from '../../src/actions/types';
+import * as query from '../../src/constants/query';
+import {PAGE} from '../../../common/src/constants';
+import ReportModel from '../../src/models/reportModel';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
 import Promise from 'bluebird';
 
-describe('Table Actions functions', () => {
-    'use strict';
+let appId = 'appId';
+let tblId = 'tblId';
+let rptId = null;
+let offset = PAGE.DEFAULT_OFFSET;
+let numRows = PAGE.DEFAULT_NUM_ROWS;
+let context = 'view';
 
-    let appId = 'appId';
-    let tblId = 'tblId';
-    let offset = Constants.PAGE.DEFAULT_OFFSET;
-    let numRows = Constants.PAGE.DEFAULT_NUM_ROWS;
+// we mock the Redux store when testing async action creators
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+class mockReportModel {
+    constructor() { }
+    get() {
+        return null;
+    }
+}
+
+describe('Table Actions success workflow functions', () => {
 
     let responseData = {
-        reportMetaData: {data: {id: null}},
-        reportData: {data:{filteredCount: 1}}
+        data: null
     };
-
-    let stores = {};
-    let flux = new Fluxxor.Flux(stores);
-    flux.addActions(tableActions);
-
     class mockTableService {
         constructor() { }
         getHomePage() {
-            return Promise.resolve({data: responseData});
+            return Promise.resolve(responseData);
         }
     }
 
     beforeEach(() => {
-        spyOn(flux.dispatchBinder, 'dispatch');
         spyOn(mockTableService.prototype, 'getHomePage').and.callThrough();
-        tableActionsRewireAPI.__Rewire__('TableService', mockTableService);
+        TableActionsRewireAPI.__Rewire__('TableService', mockTableService);
+        TableActionsRewireAPI.__Rewire__('ReportModel', mockReportModel);
     });
 
     afterEach(() => {
-        tableActionsRewireAPI.__ResetDependency__('TableService');
-        flux.dispatchBinder.dispatch.calls.reset();
+        TableActionsRewireAPI.__ResetDependency__('TableService');
+        TableActionsRewireAPI.__ResetDependency__('ReportModel');
     });
 
-    var tableActionsTests = [
-        {name:'test loadTableHomePage action', appId: appId, tblId: tblId, offset: offset, numRows: numRows}
-    ];
+    it('loads table home page', (done) => {
+        // the mock store makes the actions dispatched available via getActions()
+        // so we don't need to spy on the dispatcher etc.
+        const expectedActions = [
+            {id: context, type: types.LOAD_REPORT, content: {appId, tblId, rptId}},
+            {id: context, type: types.LOAD_REPORT_SUCCESS, content: null}
+        ];
 
-    tableActionsTests.forEach(function(test) {
-        it(test.name, function(done) {
-            flux.actions.loadTableHomePage(test.appId, test.tblId, test.offset, test.numRows).then(
-                () => {
-                    done();
-                    let model = reportModel.set(responseData.reportMetaData, responseData.reportData);
-                    expect(flux.dispatchBinder.dispatch.calls.count()).toEqual(2);
-                    expect(flux.dispatchBinder.dispatch.calls.argsFor(0)).toEqual([actions.LOAD_REPORT, {appId: 'appId', tblId: 'tblId', rptId:null, offset:test.offset, numRows:test.numRows}]);
-                    expect(flux.dispatchBinder.dispatch.calls.argsFor(1)).toEqual([actions.LOAD_REPORT_SUCCESS, model]);
-                    expect(model.rptId, Constants.SYNTHETIC_TABLE_REPORT.id);
-                },
-                () => {
-                    done();
-                    expect(false).toBe(true);
-                }
-            );
-        });
+        const store = mockStore({});
+        return store.dispatch(loadTableHomePage(context, appId, tblId, rptId, offset, numRows)).then(
+            () => {
+                expect(store.getActions()).toEqual(expectedActions);
+                done();
+            },
+            () => {
+                expect(false).toEqual(true);
+                done();
+            });
+    });
+});
+
+describe('Table Actions functions', () => {
+
+    let errorData = {
+        response: 'some error'
+    };
+    class mockTableService {
+        constructor() { }
+        getHomePage() {
+            return Promise.reject(errorData);
+        }
+    }
+
+    beforeEach(() => {
+        spyOn(mockTableService.prototype, 'getHomePage').and.callThrough();
+        TableActionsRewireAPI.__Rewire__('TableService', mockTableService);
+        TableActionsRewireAPI.__Rewire__('ReportModel', mockReportModel);
+    });
+
+    afterEach(() => {
+        TableActionsRewireAPI.__ResetDependency__('TableService');
+        TableActionsRewireAPI.__ResetDependency__('ReportModel');
+    });
+
+    it('loads table home page with invalid parameters', (done) => {
+        // the mock store makes the actions dispatched available via getActions()
+        // so we don't need to spy on the dispatcher etc.
+        const expectedActions = [
+            {id: context, type: types.LOAD_REPORT_FAILED, content: 500}
+        ];
+
+        const store = mockStore({});
+        return store.dispatch(loadTableHomePage(context)).then(
+            () => {
+                expect(true).toEqual(false);
+                done();
+            },
+            () => {
+                expect(store.getActions()).toEqual(expectedActions);
+                done();
+            });
+    });
+
+    it('loads table home page with invalid response', (done) => {
+        // the mock store makes the actions dispatched available via getActions()
+        // so we don't need to spy on the dispatcher etc.
+        const expectedActions = [
+            {id: context, type: types.LOAD_REPORT, content: {appId, tblId, rptId}},
+            {id: context, type: types.LOAD_REPORT_FAILED, content: errorData}
+        ];
+
+        const store = mockStore({});
+        return store.dispatch(loadTableHomePage(context, appId, tblId, rptId, offset, numRows)).then(
+            () => {
+                expect(true).toEqual(false);
+                done();
+            },
+            () => {
+                expect(store.getActions()).toEqual(expectedActions);
+                done();
+            });
     });
 
 });
