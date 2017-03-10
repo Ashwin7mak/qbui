8b2c8d159e88a44e57e5695c4767adf98815adc2
nvanselow <nvanselow@quickbase.com>
2017-03-09 20:17:09 -0500

    MC1005-Add stub in place of original GlobalActions file

---
+++ ui/client-react/src/components/actions/globalActions.js
@@ -0,0 +1,7 @@
+/**
+ * The original file in this location has moved to the reuse library.
+ * What remains here is a stub so existing code does not have to change yet.
+ **/
+import GlobalActions from '../../../../reuse/client/src/components/reGlobalActions/reGlobalActions';
+
+export default GlobalActions;
