69ea1ad09ed413d0ee2db88d52b7e87d2dfd2bf1
Sergio Haro <sergio.haro.jr@gmail.com>
2017-03-10 15:30:27 -0800

    adding partial unit test for grid
    mainly to show as an example. to be filled in once we have an actual
    spec

--- ui/governance/test/account/users/AccountUsersGrid.unit.spec.js
+++ ui/governance/test/account/users/AccountUsersGrid.unit.spec.js
@@ -1,15 +1,172 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount} from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
+import {AccountUsersGrid} from '../../../src/account/users/AccountUsersGrid';
+import moment from 'moment';
 
-let component;
-
-describe('example jasmine/enzyme setup', () => {
+describe('AccountUsersGrid', () => {
     beforeEach(() => {
         jasmineEnzyme();
     });
 
-    it('does something', () => {
-        expect(true).toEqual(true);
+    describe("Component", () => {
+
+        it("should show the correct set of headers", ()=> {
+            let component = mount(<AccountUsersGrid fetchAccountUsers={() => false} />);
+            let headers = component.find("th").map(node => node.text());
+            expect(headers).toEqual(["First Name" , "Last Name", "Email", "User Name", "Last Access", "Paid Seat?", "# Groups", "Group Manager?", "Can create apps?", "# App Managed", "Realm Approved?", "Denied?", "Deactivated?", "Inactive?"]);
+        });
+
+        it("should should call fetch on mount", ()=> {
+            let props = {
+                fetchAccountUsers: () => false
+            };
+
+            spyOn(props, 'fetchAccountUsers');
+            mount(<AccountUsersGrid {...props} />);
+            expect(props.fetchAccountUsers.calls.any()).toEqual(true);
+        });
+
+        describe("firstName", () => {
+            it("should render properly", () => {
+                let props = {
+                    users: [{firstName: "Test", uid:0}],
+                    fetchAccountUsers: () => false
+                };
+
+                let component = mount(<AccountUsersGrid {...props} />);
+                let cell = component.find("tbody tr td").at(0);
+                expect(cell.text()).toEqual(props.users[0].firstName);
+            });
+        });
+
+        describe("lastName", () => {
+            it("should render properly", () => {
+                let props = {
+                    users: [{lastName: "Test", uid:0}],
+                    fetchAccountUsers: () => false
+                };
+
+                let component = mount(<AccountUsersGrid {...props} />);
+                let cell = component.find("tbody tr td").at(1);
+                expect(cell.text()).toEqual(props.users[0].lastName);
+            });
+        });
+
+        describe("email", () => {
+            it("should render properly", () => {
+                let props = {
+                    users: [{email: "Test", uid:0}],
+                    fetchAccountUsers: () => false
+                };
+
+                let component = mount(<AccountUsersGrid {...props} />);
+                let cell = component.find("tbody tr td").at(2);
+                expect(cell.text()).toEqual(props.users[0].email);
+            });
+        });
+
+        describe("userName", () => {
+            it("should render properly", () => {
+                let props = {
+                    users: [{userName: "Test", uid:0}],
+                    fetchAccountUsers: () => false
+                };
+
+                let component = mount(<AccountUsersGrid {...props} />);
+                let cell = component.find("tbody tr td").at(3);
+                expect(cell.text()).toEqual(props.users[0].userName);
+            });
+        });
+
+        describe("lastAccess", () => {
+            it("should render null properly", () => {
+                let props = {
+                    users: [{lastAccess: "1900-01-01T00:00:00Z", uid:0}],
+                    fetchAccountUsers: () => false
+                };
+
+                let component = mount(<AccountUsersGrid {...props} />);
+                let cell = component.find("tbody tr td").at(4);
+                expect(cell.text()).toEqual("never");
+            });
+
+            it("should render properly", () => {
+                let props = {
+                    users: [{lastAccess: moment().subtract(60, 'minutes').toISOString(), uid:0}],
+                    fetchAccountUsers: () => false
+                };
+
+                let component = mount(<AccountUsersGrid {...props} />);
+                let cell = component.find("tbody tr td").at(4);
+                expect(cell.text()).toEqual("an hour ago");
+            });
+        });
+
+        describe("paid seat", () => {
+            it("should render no app access correctly", () => {
+                let props = {
+                    users: [{
+                        hasAppAccess: false,
+                        userBasicFlags: 0,
+                        realmDirectoryFlags: 0,
+                        uid:0
+                    }],
+                    fetchAccountUsers: () => false
+                };
+
+                let component = mount(<AccountUsersGrid {...props} />);
+                let cell = component.find("tbody tr td").at(5);
+                expect(cell.text()).toEqual("");
+            });
+
+            it("should render app access and not denied/deactivated correctly", () => {
+                let props = {
+                    users: [{
+                        hasAppAccess: true,
+                        userBasicFlags: 0,
+                        realmDirectoryFlags: 0,
+                        uid:0
+                    }],
+                    fetchAccountUsers: () => false
+                };
+
+                let component = mount(<AccountUsersGrid {...props} />);
+                let cell = component.find("tbody tr td").at(5);
+                expect(cell.text()).toEqual("Check");
+            });
+
+            it("should render app access and but denied correctly", () => {
+                let props = {
+                    users: [{
+                        hasAppAccess: true,
+                        userBasicFlags: 0,
+                        realmDirectoryFlags: 8,
+                        uid:0
+                    }],
+                    fetchAccountUsers: () => false
+                };
+
+                let component = mount(<AccountUsersGrid {...props} />);
+                let cell = component.find("tbody tr td").at(5);
+                expect(cell.text()).toEqual("");
+            });
+
+            it("should render app access and but deactivated correctly", () => {
+                let props = {
+                    users: [{
+                        hasAppAccess: true,
+                        userBasicFlags: 64,
+                        realmDirectoryFlags: 0,
+                        uid:0
+                    }],
+                    fetchAccountUsers: () => false
+                };
+
+                let component = mount(<AccountUsersGrid {...props} />);
+                let cell = component.find("tbody tr td").at(5);
+                expect(cell.text()).toEqual("");
+            });
+        });
     });
 });
