d21c00c03c47e14e7090355e0505bb4316298b53
Don Hatch <donald_hatch@intuit.com>
2017-03-12 17:31:02 -0400

    add unit tests against record reducers

--- ui/client-react/test/reducers/record.unit.spec.js
+++ ui/client-react/test/reducers/record.unit.spec.js
@@ -1,40 +1,258 @@
-import reducer, {__RewireAPI__ as ReducerRewireAPI} from '../../src/reducers/record';
+import reducer, {__RewireAPI__ as RecordReducerRewireAPI} from '../../src/reducers/record';
 import * as types from '../../src/actions/types';
+import {UNSAVED_RECORD_ID} from "../../src/constants/schema";
 
-describe('Saving form functions', () => {
-    it('returns correct state when saving form', () => {
-        expect(reducer(initialState, {type: types.SAVE_FORM, id: 'edit'})).toDeepEqual([{
-            id: 'edit',
-            saving: true,
-            errorStatus: null
-        }]);
-    });
+function event(id, type, content) {
+    return {
+        id: id,
+        type: type,
+        content: content || null
+    };
+}
 
-    it('returns correct state when save error occurs', () => {
-        let savingFormState = [{
-            id: 'edit',
-            saving: true,
-            errorStatus: null
-        }];
+let initialState = [];
+let appId = '1';
+let tblId = '2';
 
-        expect(reducer(savingFormState, {type: types.SAVE_FORM_FAILED, id: "edit", error: "oops"})).toDeepEqual([{
-            id: 'edit',
-            saving: false,
-            errorStatus: "oops"
-        }]);
-    });
+describe('Record reducer delete functions', () => {
 
-    it('returns correct state when save succeeds', () => {
-        let savingFormState = [{
-            id: 'edit',
-            saving: true,
-            errorStatus: null
-        }];
+    let testCases = [
+        {'name':'delete a single record start state', recIds: [1]},
+        {'name':'delete a group of records start state', recIds: [1, 2, 3]}
+    ];
+    testCases.forEach(testCase => {
+        let states;
+        it(testCase.name, (done) => {
+            //  start delete records state
+            let ev = event(testCase.recIds[0], types.DELETE_RECORDS, {appId, tblId, recIds:testCase.recIds});
+            states = reducer(initialState, ev);
 
-        expect(reducer(savingFormState, {type: types.SAVE_FORM_SUCCESS, id: "edit"})).toDeepEqual([{
-            id: 'edit',
-            saving: false,
-            errorStatus:null
-        }]);
+            expect(states.length).toEqual(testCase.recIds.length);
+            states.forEach(state => {
+                expect(state.pendEdits).toBeDefined();
+                expect(state.pendEdits.saving).toEqual(true);
+            });
+            done();
+        });
+
+        //  delete records error state..use existing store state to add errors
+        let errors = [
+            {msg: 'error message 1'},
+            {msg: 'error message 2'}
+        ];
+        let errorTestCases = [
+            {'name':'delete error state with no error block', errors:[], isValid:false},
+            {'name':'delete error state with no error block defined', isValid:false},
+            {'name':'delete error state with error block', errors:errors, isValid:true}
+        ];
+        errorTestCases.forEach(errorTestCase => {
+            it(errorTestCase.name, (done) => {
+                let ev = event(testCase.recIds[0], types.DELETE_RECORDS_ERROR, {
+                    appId,
+                    tblId,
+                    recIds: testCase.recIds,
+                    errors: errorTestCase.errors
+                });
+                const errorStates = reducer(states, ev);
+                if (errorTestCase.isValid) {
+                    expect(errorStates.length).toEqual(testCase.recIds.length);
+                    errorStates.forEach(state => {
+                        expect(state.pendEdits).toBeDefined();
+                        expect(state.pendEdits.saving).toEqual(true);
+                        expect(state.pendEdits.editErrors.errors.length).toEqual(errorTestCase.errors.length);
+                        expect(state.pendEdits.editErrors.ok).toEqual(false);
+                    });
+                } else {
+                    //  no change in state if invalid/missing errors list
+                    expect(errorStates).toEqual(states);
+                }
+                done();
+            });
+        });
+
+        ////  delete records completed state..use existing store state
+        it('delete records complete state', (done) => {
+            let ev = event(testCase.recIds[0], types.DELETE_RECORDS_COMPLETE, {appId, tblId, recIds: testCase.recIds});
+            const completeState = reducer(states, ev);
+
+            expect(completeState.length).toEqual(testCase.recIds.length);
+            completeState.forEach(state => {
+                expect(state.pendEdits).toBeDefined();
+                expect(state.pendEdits.saving).toEqual(false);
+                expect(state.pendEdits.editErrors.errors.length).toEqual(0);
+                expect(state.pendEdits.editErrors.ok).toEqual(true);
+            });
+            done();
+        });
     });
 });
+
+describe('Record reducer open record', () => {
+
+    let initialState = [];
+    let testCases = [
+        {testCase:'open record with navigateAfterSave false', recId:1, nextRecordId:10, previousRecordId:20, navigateAfterSave:false, nextOrPreviousEdit:'11'},
+        {testCase:'open record with navigateAfterSave true ', recId:1, nextRecordId:10, previousRecordId:20, navigateAfterSave:true, nextOrPreviousEdit:'11'},
+        {testCase:'open record with missing input', recId:1, nextRecordId:10, previousRecordId:20}
+    ];
+    testCases.forEach(testCase => {
+        it(testCase.name, (done) => {
+            const obj = {
+                recId: testCase.recId,
+                nextRecordId: testCase.nextRecordId,
+                previousRecordId: testCase.previousRecordId,
+                navigateAfterSave: testCase.navigateAfterSave,
+                nextOrPreviousEdit: testCase.nextOrPreviousEdit
+            };
+            const state = reducer(initialState, event(testCase.recId, types.OPEN_RECORD, obj));
+            const record = state[0];
+
+            expect(record.id).toEqual(testCase.recId);
+            expect(record.recId).toEqual(testCase.recId);
+            expect(record.nextRecordId).toEqual(testCase.nextRecordId);
+            expect(record.previousRecordId).toEqual(testCase.previousRecordId);
+            expect(record.navigateAfterSave).toEqual(testCase.navigateAfterSave || false);
+            expect(record.nextOrPreviousEdit).toEqual(testCase.nextOrPreviousEdit || '');
+            done();
+        });
+    });
+});
+
+describe('Record reducer save record events', () => {
+    let saveState = [];
+    let changes = {};
+    let recId = 3;
+
+    class mockRecordModel {
+        constructor() { }
+        setRecordChanges(a, b, c) { }
+        setRecordSaveSuccess(a, b, c) { }
+        setRecordSaveError(a, b, c, e) { }
+        setSaving(b) { }
+        get() { }
+        set(a) { }
+    }
+
+    beforeEach(() => {
+        spyOn(mockRecordModel.prototype, 'setRecordChanges').and.callThrough();
+        spyOn(mockRecordModel.prototype, 'setRecordSaveSuccess').and.callThrough();
+        spyOn(mockRecordModel.prototype, 'setRecordSaveError').and.callThrough();
+        spyOn(mockRecordModel.prototype, 'setSaving').and.callThrough();
+        spyOn(mockRecordModel.prototype, 'get').and.callThrough();
+        spyOn(mockRecordModel.prototype, 'set').and.callThrough();
+        RecordReducerRewireAPI.__Rewire__('RecordModel', mockRecordModel);
+    });
+
+    afterEach(() => {
+        mockRecordModel.prototype.get.calls.reset();
+        mockRecordModel.prototype.set.calls.reset();
+        mockRecordModel.prototype.setSaving.calls.reset();
+        mockRecordModel.prototype.setRecordSaveError.calls.reset();
+        mockRecordModel.prototype.setRecordSaveSuccess.calls.reset();
+        mockRecordModel.prototype.setRecordChanges.calls.reset();
+        RecordReducerRewireAPI.__ResetDependency__('RecordModel');
+    });
+
+    let testCases = [
+        {testCase: 'save new record', appId: appId, tblId: tblId, recId: recId, changes: changes, recordInStore: false},
+        {testCase: 'save existing record', appId: appId, tblId: tblId, recId: recId, changes: changes, recordInStore: true}
+    ];
+    testCases.forEach(testCase => {
+        it(testCase.name, (done) => {
+            const obj = {
+                appId: testCase.appId,
+                tblId: testCase.tblId,
+                recId: testCase.recId,
+                changes: testCase.changes
+            };
+
+            saveState = reducer(saveState, event(testCase.recId, types.SAVE_RECORD, obj));
+            const record = saveState[0];
+
+            expect(mockRecordModel.prototype.setRecordChanges).toHaveBeenCalled();
+            if (testCase.recordInStore) {
+                expect(mockRecordModel.prototype.get).not.toHaveBeenCalled();
+                expect(mockRecordModel.prototype.set).toHaveBeenCalled();
+            } else {
+                expect(mockRecordModel.prototype.get).toHaveBeenCalled();
+                expect(mockRecordModel.prototype.set).not.toHaveBeenCalled();
+            }
+            done();
+        });
+    });
+
+    let errors = [
+        {msg: 'error message 1'},
+        {msg: 'error message 2'}
+    ];
+    let errorTestCases = [
+        {'name': 'save record error - record not in store', appId:appId, tblId:tblId, recId:99, errors:errors, recordInStore:false},
+        {'name': 'save record error - record in store', appId:appId, tblId:tblId, recId:recId, errors:errors, recordInStore:true}
+    ];
+    errorTestCases.forEach(testCase => {
+        it(testCase.name, (done) => {
+            const obj = {
+                appId: testCase.appId,
+                tblId: testCase.tblId,
+                recId: testCase.recId,
+                errors: testCase.errors
+            };
+            saveState = reducer(saveState, event(testCase.recId, types.SAVE_RECORD_ERROR, obj));
+            if (testCase.recordInStore) {
+                expect(mockRecordModel.prototype.set).toHaveBeenCalled();
+                expect(mockRecordModel.prototype.setRecordSaveError).toHaveBeenCalled();
+            } else {
+                expect(mockRecordModel.prototype.set).not.toHaveBeenCalled();
+                expect(mockRecordModel.prototype.setRecordSaveError).not.toHaveBeenCalled();
+            }
+            done();
+        });
+    });
+
+    let successTestCases = [
+        {'name': 'save record success - record not in store', appId:appId, tblId:tblId, recId:99, recordInStore:false},
+        {'name': 'save record error - record in store', appId:appId, tblId:tblId, recId:recId,  recordInStore:true}
+    ];
+    successTestCases.forEach(testCase => {
+        it(testCase.name, (done) => {
+            const obj = {
+                appId: testCase.appId,
+                tblId: testCase.tblId,
+                recId: testCase.recId
+            };
+            saveState = reducer(saveState, event(testCase.recId, types.SAVE_RECORD_SUCCESS, obj));
+            if (testCase.recordInStore) {
+                expect(mockRecordModel.prototype.setSaving).toHaveBeenCalledWith(false);
+                expect(mockRecordModel.prototype.setRecordSaveSuccess).toHaveBeenCalled();
+            } else {
+                expect(mockRecordModel.prototype.setSaving).not.toHaveBeenCalled();
+                expect(mockRecordModel.prototype.setRecordSaveSuccess).not.toHaveBeenCalled();
+            }
+            done();
+        });
+    });
+
+    let completeTestCases = [
+        {'name': 'save record complete - record not in store', appId:appId, tblId:tblId, recId:99, recordInStore:false},
+        {'name': 'save record complete - record in store', appId:appId, tblId:tblId, recId:recId,  recordInStore:true}
+    ];
+    completeTestCases.forEach(testCase => {
+        it(testCase.name, (done) => {
+            const obj = {
+                appId: testCase.appId,
+                tblId: testCase.tblId,
+                recId: testCase.recId
+            };
+            saveState = reducer(saveState, event(testCase.recId, types.SAVE_RECORD_COMPLETE, obj));
+            if (testCase.recordInStore) {
+                expect(mockRecordModel.prototype.setSaving).toHaveBeenCalledWith(false, true);
+            } else {
+                expect(mockRecordModel.prototype.setSaving).not.toHaveBeenCalled();
+            }
+            done();
+        });
+    });
+});
+
+
+
