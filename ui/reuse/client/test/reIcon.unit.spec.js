e85ca87ab2d91e923ab711996477257b3e67d937
Michael Harris <mharris@quickbase.com>
2017-03-13 17:25:16 -0400

    update unit tests

---
+++ ui/reuse/client/test/reIcon.unit.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import jasmineEnzyme from 'jasmine-enzyme';
+import AVAILABLE_ICON_FONTS from '../src/components/reIcon/iconConstants';
+
+import ReIcon from '../src/components/reIcon/reIcon';
+
+let component;
+
+describe('ReIcon', () => {
+    beforeEach(() => {
+        jasmineEnzyme();
+    });
+
+    it('renders an icon from the default font set', () => {
+        component = shallow(<ReIcon icon="pencil" />);
+
+        expect(component.find(`.${AVAILABLE_ICON_FONTS.DEFAULT}-pencil`)).toBePresent();
+    });
+
+    it('renders an icon from a different font set', () => {
+        component = shallow(<ReIcon iconFont={AVAILABLE_ICON_FONTS.TABLE_STURDY} icon="Dimensions"/>);
+
+        expect(component.find(`.${AVAILABLE_ICON_FONTS.TABLE_STURDY}-Dimensions`)).toBePresent();
+    });
+});
