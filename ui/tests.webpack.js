a041d2a4474f1af779d21fafed5ab63f515a455b
Claire Martinez <cmartinez@quickbase.com>
2017-03-13 13:32:49 -0400

    enable tests

--- ui/tests.webpack.js
+++ ui/tests.webpack.js
@@ -6,7 +6,7 @@
  * See webpack docs for how it works
  * https://webpack.github.io/docs/context.html
  */
-var clientReactModules = require.context('./client-react/test/actions', true, /.unit.spec.js$/);
+var clientReactModules = require.context('./client-react/test', true, /.unit.spec.js$/);
 clientReactModules.keys().forEach(clientReactModules);
 
 //var compLibraryModules = require.context('./componentLibrary/test', true, /.unit.spec.js$/);
