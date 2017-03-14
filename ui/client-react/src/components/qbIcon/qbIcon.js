0c46ee95c455811fbaebbc8f91e5addc274f4bbf
Michael Harris <mharris@quickbase.com>
2017-03-13 22:18:41 -0400

    cleanup governance file

--- ui/client-react/src/components/qbIcon/qbIcon.js
+++ ui/client-react/src/components/qbIcon/qbIcon.js
@@ -2,8 +2,8 @@
  * The original file in this location has moved to the reuse library.
  * What remains here is a stub so existing code does not have to change yet.
  **/
-import ReIcon from '../../../../reuse/client/src/components/reIcon/reIcon.js';
 
-const QBicon = ReIcon;
+// import the default renaming it to what we want to export
+import QBicon from '../../../../reuse/client/src/components/reIcon/reIcon.js';
 
 export default QBicon;
--- ui/governance/src/account/users/AccountUsers.js
+++ ui/governance/src/account/users/AccountUsers.js
@@ -1,12 +1,11 @@
 import React, {PropTypes, Component} from 'react';
 import AccountUsersGrid from './AccountUserGrid';
-import ReIcon from '../../../../reuse/client/src/components/reIcon/reIcon';
 
 class AccountUsers extends Component {
     render() {
         return (
             <div className="governanceMain">
-                <h1>Welcome to governance! <ReIcon icon="speechbubble-outline"/></h1>
+                <h1>Welcome to governance!</h1>
                 <AccountUsersGrid />
             </div>
         );
