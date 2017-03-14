d1fc5cacdc8926df2f1c2536b9081bd884404d5b
nvanselow <nvanselow@quickbase.com>
2017-03-14 18:08:58 -0400

    MC1005-Use presentational component for appShell

--- ui/governance/src/app/appShell.js
+++ ui/governance/src/app/appShell.js
@@ -3,23 +3,18 @@ import ReAppShell from '../../../reuse/client/src/components/reAppShell/reAppShe
 import ReDefaultTopNavGlobalActions from '../../../reuse/client/src/components/reTopNav/reDefaultTopNavGlobalActions';
 import ReTopNav from '../../../reuse/client/src/components/reTopNav/reTopNav';
 
-class NavShell extends Component {
-    render() {
-        return (
-            <ReAppShell functionalAreaName="governance">
-                <ReTopNav title="Governance" globalActions={
-                    <ReDefaultTopNavGlobalActions
-                        position="top"
-                        startTabIndex={4}
-                        dropdownIcon="user"
-                        dropdownMsg="globalActions.user"
-                    />
-                }/>
-                {this.props.children}
-            </ReAppShell>
-        );
+const AppShell = ({children}) => (
+    <ReAppShell functionalAreaName="governance">
+        <ReTopNav title="Governance" globalActions={
+            <ReDefaultTopNavGlobalActions
+                position="top"
+                startTabIndex={4}
+                dropdownIcon="user"
+                dropdownMsg="globalActions.user"
+            />
+        }/>
+        {children}
+    </ReAppShell>
+);
 
-    }
-}
-
-export default NavShell;
+export default AppShell;
