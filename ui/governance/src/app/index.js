54b13865558fff7e10f315f2d272fb969154c6e8
nvanselow <nvanselow@quickbase.com>
2017-03-14 18:07:45 -0400

    MC1005-Move app shell

--- ui/governance/src/app/index.js
+++ ui/governance/src/app/index.js
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import 'react-fastclick';
 import {GOVERNANCE_BASE_ROUTE, GOVERNANCE_USERS_ROUTE} from './routes';
-import NavShell from '../navShell/navShell';
+import AppShell from './appShell';
 import AccountUsers from '../account/users/AccountUsers';
 
 import {Provider} from 'react-redux';
@@ -11,11 +11,10 @@ import createGovernanceStore from './store';
 
 const store = createGovernanceStore();
 
-
 render((
     <Provider store={store}>
         <Router history={browserHistory}>
-            <Route component={NavShell}>
+            <Route component={AppShell}>
                 <Route path={GOVERNANCE_USERS_ROUTE} component={AccountUsers} />
             </Route>
         </Router>
