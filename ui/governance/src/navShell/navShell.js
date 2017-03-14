f8bf062592ab845760548cb0b92f8af4c409b813
nvanselow <nvanselow@quickbase.com>
2017-03-14 17:29:44 -0400

    MC1005-Use base app shell to hook up notifications to governance

--- ui/governance/src/navShell/navShell.js
+++ ui/governance/src/navShell/navShell.js
@@ -1,23 +1,22 @@
 import React, {PropTypes, Component} from 'react';
+import ReAppShell from '../../../reuse/client/src/components/reAppShell/reAppShell';
 import ReDefaultTopNavGlobalActions from '../../../reuse/client/src/components/reTopNav/reDefaultTopNavGlobalActions';
-import TopNav from '../../../reuse/client/src/components/reTopNav/reTopNav';
-
+import ReTopNav from '../../../reuse/client/src/components/reTopNav/reTopNav';
 
 class NavShell extends Component {
     render() {
         return (
-            <div className="governanceShell">
-                <TopNav title="Governance" globalActions={
+            <ReAppShell functionalAreaName="governance">
+                <ReTopNav title="Governance" globalActions={
                     <ReDefaultTopNavGlobalActions
                         position="top"
                         startTabIndex={4}
                         dropdownIcon="user"
                         dropdownMsg="globalActions.user"
-                        app={null}
                     />
                 }/>
                 {this.props.children}
-            </div>
+            </ReAppShell>
         );
 
     }
