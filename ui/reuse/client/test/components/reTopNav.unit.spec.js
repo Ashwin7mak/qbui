e117a77055eb81c1dd750f35aee652d1349b1007
nvanselow <nvanselow@quickbase.com>
2017-03-14 22:51:25 -0400

    MC1005-Lint fixes

--- ui/reuse/client/test/components/reTopNav.unit.spec.js
+++ ui/reuse/client/test/components/reTopNav.unit.spec.js
@@ -13,26 +13,26 @@ describe('ReTopNav', () => {
     });
 
     it('has a left menu icon that can be clicked', () => {
-       const mockNavParent = {onNavClick() {}};
-       spyOn(mockNavParent, 'onNavClick');
+        const mockNavParent = {onNavClick() {}};
+        spyOn(mockNavParent, 'onNavClick');
 
-       component = shallow(<ReTopNav onNavClick={mockNavParent.onNavClick} />);
+        component = shallow(<ReTopNav onNavClick={mockNavParent.onNavClick} />);
 
-       let menuIcon = component.find('.toggleNavButton');
-       expect(menuIcon).toBePresent();
+        let menuIcon = component.find('.toggleNavButton');
+        expect(menuIcon).toBePresent();
 
-       menuIcon.simulate('click');
+        menuIcon.simulate('click');
 
-       expect(mockNavParent.onNavClick).toHaveBeenCalled();
+        expect(mockNavParent.onNavClick).toHaveBeenCalled();
     });
-    
+
     it('displays a title on small devices (title is hidden through css)', () => {
         const testTitle = 'test title';
         component = shallow(<ReTopNav title={testTitle} />);
-        
+
         expect(component.find('.topTitle')).toHaveText(testTitle);
     });
-    
+
     it('has default actions (favorite and search, disabled) in the center of the nav bar if none are provided', () => {
         // Using mount as the icons are nested inside of other components
         component = mount(<ReTopNav/>);
@@ -41,7 +41,7 @@ describe('ReTopNav', () => {
         expect(centerIcons.at(0)).toHaveProp('icon', 'search');
         expect(centerIcons.at(1)).toHaveProp('icon', 'star-full');
     });
-    
+
     it('can optionally display different elements in the center of the nav bar', () => {
         const testCenterElement = <div className="centerComponent">My center component</div>;
         // Using mount as the icons are nested inside of other components
@@ -50,7 +50,7 @@ describe('ReTopNav', () => {
         expect(component.find('.center').find('.centerComponent')).toBePresent();
         expect(component.find('.center').find(ReIcon)).not.toBePresent();
     });
-    
+
     it('displays actions passed in on the right side of the nav bar', () => {
         const testRightElement = <div className="rightComponent">My right component</div>;
         component = shallow(<ReTopNav globalActions={testRightElement} />);
