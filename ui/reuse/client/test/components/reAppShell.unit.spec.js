27d98c19d791918d1db6e318ce44c615af069a18
nvanselow <nvanselow@quickbase.com>
2017-03-14 20:32:58 -0400

    MC1005-Unit tests for reAppShell

---
+++ ui/reuse/client/test/components/reAppShell.unit.spec.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import jasmineEnzyme from 'jasmine-enzyme';
+
+import ReAppShell from '../../src/components/reAppShell/reAppShell';
+import {NotificationContainer} from 'react-notifications';
+
+let component;
+
+describe('ReAppShell', () => {
+    beforeEach(() => {
+        jasmineEnzyme();
+    });
+
+    it('has a containing div with a class name based on the functional area', () => {
+        component = shallow(<ReAppShell functionalAreaName="test"/>);
+
+        expect(component.find('.testAppShell')).toBePresent();
+    });
+
+    it('has a notification container so that notifications can be displayed', () => {
+        component = shallow(<ReAppShell functionalAreaName="test"/>);
+
+        expect(component.find(NotificationContainer)).toBePresent();
+    });
+
+    it('displays child elements', () => {
+        const testChildElement = <div className="childElement">Test Child Element</div>;
+
+        component = shallow(<ReAppShell functionalAreaName="test">{testChildElement}</ReAppShell>);
+
+        expect(component.find('.childElement')).toBePresent();
+    });
+});
