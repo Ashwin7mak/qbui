fae4db99b445b4c67e387caeab011e7ca4a4459e
nvanselow <nvanselow@quickbase.com>
2017-03-16 17:59:33 -0400

    MC1058-Minor cleanup of tooltip code

--- ui/reuse/client/src/components/tooltip/tooltip.js
+++ ui/reuse/client/src/components/tooltip/tooltip.js
@@ -1,40 +1,64 @@
 import React from 'react';
-import {I18nMessage} from '../../../../../client-react/src/utils/i18nMessage';
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import BsToolTip from 'react-bootstrap/lib/Tooltip';
 import _ from 'lodash';
 import './tooltip.scss';
 
+// CLIENT REACT IMPORT
+import {I18nMessage} from '../../../../../client-react/src/utils/i18nMessage';
+// CLIENT REACT IMPORT
+
 const Tooltip = React.createClass({
     propTypes: {
-        // optional name to use in classname
+        /**
+         * Added to the classes on the tooltip */
         tipId:  React.PropTypes.string,
-        //message to render use if string has already been localized
+
+        /**
+         * Message to render in the tooltip. Use this prop if string has already been localized. */
         plainMessage:  React.PropTypes.string,
-        //messageId to lookup in resource bundle for message to render pass in plainMessage
-        // or i18nMessageKey. i18nMessageKey will render if both are supplied
+
+        /**
+         * The i18nMessage key to lookup in the current language bundle.
+         * i18nMessageKey will be rendered instead of plainMessage if an i18nMessageKey if both props are passed in */
         i18nMessageKey:  React.PropTypes.string,
-        // override the default delay millisecond s for tooltip help
-        delayShow: React.PropTypes.number // options
+
+        /**
+         * Override the default delay in milliseconds for tooltip help.
+         * Recommend using default to maintain consistency between tooltips across the qbase ecosystem. */
+        delayShow: React.PropTypes.number,
+
+        /**
+         * Where the tooltip displays relative to the child elements */
+        location: React.PropTypes.oneOf(['top', 'bottom', 'left', 'right'])
     },
-    defaultDelayShow: 300,
+
+    getDefaultProps() {
+        return {
+            defaultDelayShow: 300,
+            location: 'top'
+        };
+    },
 
     toolTipIt(tooltipId, plainMessage, i18nMessageKey) {
         let label = plainMessage ? plainMessage : '';
+
         if (i18nMessageKey) {
             label = (<I18nMessage message={i18nMessageKey} {...this.props}/>);
         }
-        let tipClassName = tooltipId ? "qbtooltip " + tooltipId : " qbtooltip";
+
+        let tipClassName = tooltipId ? `qbtooltip  ${tooltipId}` : 'qbtooltip';
+
         return (
-            <BsToolTip className={tipClassName} id={_.uniqueId("qbtooltip_")} >{label}</BsToolTip>
+            <BsToolTip className={tipClassName} id={_.uniqueId('qbtooltip_')} >{label}</BsToolTip>
         );
     },
 
     render() {
         let itemTip = this.toolTipIt(this.props.tipId, this.props.plainMessage, this.props.i18nMessageKey);
-        let startDelay = this.props.delayShow ? this.props.delayShow : this.defaultDelayShow;
+
         return (
-            <OverlayTrigger rootClose={true} placement={this.props.location ? this.props.location : "top"} overlay={itemTip} delayShow={startDelay} {...this.props}>
+            <OverlayTrigger rootClose={true} placement={this.props.location} overlay={itemTip} delayShow={this.props.delayShow} {...this.props}>
                 <div className="tipChildWrapper">
                     {this.props.children}
                 </div>
@@ -44,4 +68,3 @@ const Tooltip = React.createClass({
 });
 
 export default Tooltip;
-
