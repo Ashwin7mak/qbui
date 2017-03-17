c53873452da45620331abcf272ad8c3408d17ec7
Michael Harris <mharris@quickbase.com>
2017-03-16 23:37:35 -0400

    replace QbTableIcon with a stub pointing to Icon in the reuse library

--- ui/client-react/src/components/qbTableIcon/qbTableIcon.js
+++ ui/client-react/src/components/qbTableIcon/qbTableIcon.js
@@ -1,25 +1,41 @@
-import React from 'react';
+/**
+ * This component should not be used anymore.
+ * Use the Icon component from the reuse library.
+ * This stub remains here so code doesn't have to change yet.
+ */
+
+import React, {PropTypes} from 'react';
+import Icon, {AVAILABLE_ICON_FONTS} from '../../../../reuse/client/src/components/icon/icon.js';
 import './style.css';
 
 /**
  * an icon using a new qb icon font (from Lisa)
  */
-const TableIcon = React.createClass({
-    propTypes: {
-        icon: React.PropTypes.string.isRequired,
-        classes: React.PropTypes.string
-    },
-    render: function() {
-        let className = 'qbIcon ' + this.props.icon;
-        if (this.props.classes) {
-            className += ' ' +  this.props.classes;
-        }
-        return (
-            <span className={className}>
-                {this.props.children}
-            </span>
-        );
+const TableIcon = ({classes, icon}) => {
+
+    // Map the parameters to the format expected by the embedded tag.
+    // The icon passed in is the full class name including font.
+    // So separate them into two parameters.
+    let realIcon = `${icon}`;
+    let iconFont = AVAILABLE_ICON_FONTS.TABLE_STURDY;
+
+    if (realIcon.indexOf(AVAILABLE_ICON_FONTS.UI_STURDY) === 0) {
+        iconFont = AVAILABLE_ICON_FONTS.UI_STURDY;
     }
-});
+    realIcon = realIcon.replace(iconFont+'-', '');
+
+    return (
+        <Icon className={`${classes}`} icon={realIcon} iconFont={iconFont} />
+    );
+};
+
+TableIcon.propTypes = {
+    icon: React.PropTypes.string.isRequired,
+    classes: React.PropTypes.string
+};
+
+TableIcon.defaultProps = {
+    classes: ''
+};
 
 export default TableIcon;
