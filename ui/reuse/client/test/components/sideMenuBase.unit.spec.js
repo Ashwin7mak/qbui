77f460f1d7298f54995318582f38f3e329854d8a
nvanselow <nvanselow@quickbase.com>
2017-03-19 20:56:54 -0400

    MC1160-Fix classes expected in sideMenuBase unit test

--- ui/reuse/client/test/components/sideMenuBase.unit.spec.js
+++ ui/reuse/client/test/components/sideMenuBase.unit.spec.js
@@ -19,31 +19,31 @@ describe('SideMenuBase', () => {
     it('can be moved the right side of the screen', () => {
         component = shallow(<SideMenuBase {...requiredProps} isOpen={true} isCollapsed={true} pullRight={true}/>);
 
-        expect(component.find('.reSideMenuContent.sideMenuPullRight'));
-        expect(component.find('.reSideMenuMain.sideMenuPullRight'));
+        expect(component.find('.sideMenuContent.sideMenuPullRight'));
+        expect(component.find('.sideMenuMain.sideMenuPullRight'));
     });
 
     it('can be collapsed (still visible but a smaller width)', () => {
         component = shallow(<SideMenuBase {...requiredProps} isOpen={true} isCollapsed={true}/>);
 
-        expect(component.find('.reSideMenuBase.reSideMenuDocked')).toBePresent();
-        expect(component.find('.reSideMenuBase.reSideMenuCollapsed')).toBePresent();
-        expect(component.find('.reSideMenuContent.reSideMenuCollapsed')).toBePresent();
-        expect(component.find('.reSideMenuMain.reSideMenuCollapsed')).toBePresent();
+        expect(component.find('.sideMenuBase.sideMenuDocked')).toBePresent();
+        expect(component.find('.sideMenuBase.sideMenuCollapsed')).toBePresent();
+        expect(component.find('.sideMenuContent.sideMenuCollapsed')).toBePresent();
+        expect(component.find('.sideMenuMain.sideMenuCollapsed')).toBePresent();
     });
 
     it('displays the sideMenuContent', () => {
         component = mount(<SideMenuBase {...requiredProps} isOpen={true} />);
 
-        expect(component.find('.reSideMenuContent .testSideMenuContent')).toBePresent();
-        expect(component.find('.reSideMenuContent .testMainContent')).not.toBePresent();
+        expect(component.find('.sideMenuContent .testSideMenuContent')).toBePresent();
+        expect(component.find('.sideMenuContent .testMainContent')).not.toBePresent();
     });
 
     it('displays child elements in the main body of the page', () => {
         component = mount(<SideMenuBase {...requiredProps}>{testMainContent}</SideMenuBase>);
 
-        expect(component.find('.reSideMenuMain .testMainContent')).toBePresent();
-        expect(component.find('.reSideMenuMain .testSideMenuContent')).not.toBePresent();
+        expect(component.find('.sideMenuMain .testMainContent')).toBePresent();
+        expect(component.find('.sideMenuMain .testSideMenuContent')).not.toBePresent();
     });
 
     describe('General, Medium, and Large Breakpoint', () => {
@@ -58,15 +58,15 @@ describe('SideMenuBase', () => {
         it('has a default state of docked', () => {
             component = mount(<SideMenuBase {...requiredProps} />);
 
-            expect(component.find('.reSideMenuBase.reSideMenuDocked')).toBePresent();
-            expect(component.find('.reSideMenuBase.reSideMenuCollapsed')).not.toBePresent();
+            expect(component.find('.sideMenuBase.sideMenuDocked')).toBePresent();
+            expect(component.find('.sideMenuBase.sideMenuCollapsed')).not.toBePresent();
         });
 
         it('can be forced to hide on larger breakpoints', () => {
             component = mount(<SideMenuBase {...requiredProps} isOpen={false} willDock={false} />);
 
-            expect(component.find('.reSideMenuBase.reSideMenuDocked')).not.toBePresent();
-            expect(component.find('.reSideMenuMain.reSideMenuOpen')).not.toBePresent();
+            expect(component.find('.sideMenuBase.sideMenuDocked')).not.toBePresent();
+            expect(component.find('.sideMenuMain.sideMenuOpen')).not.toBePresent();
         });
     });
 
@@ -82,14 +82,14 @@ describe('SideMenuBase', () => {
         it('is not docked on small screens', () => {
             component = mount(<SideMenuBase {...requiredProps} isOpen={true} />);
 
-            expect(component.find('.reSideMenuBase.reSideMenuDocked')).not.toBePresent();
-            expect(component.find('.reSideMenuMain.reSideMenuOpen')).toBePresent();
+            expect(component.find('.sideMenuBase.sideMenuDocked')).not.toBePresent();
+            expect(component.find('.sideMenuMain.sideMenuOpen')).toBePresent();
         });
 
         it('can be hidden on small screens', () => {
             component = mount(<SideMenuBase {...requiredProps} isOpen={false} />);
 
-            expect(component.find('.reSideMenuMain.reSideMenuOpen')).not.toBePresent();
+            expect(component.find('.sideMenuMain.sideMenuOpen')).not.toBePresent();
         });
     });
 });
