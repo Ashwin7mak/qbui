76ed252c7774823d89e567aea238d22ed08f8bb3
nvanselow <nvanselow@quickbase.com>
2017-03-21 18:44:55 -0400

    MC1061-Add stub file after moving stage to the reuse library

---
+++ ui/client-react/src/components/stage/stage.js
@@ -0,0 +1,7 @@
+/**
+ * The original file in this location has moved to the reuse library.
+ * What remains here is a stub so existing code does not have to change yet.
+ **/
+import stage from '../../../../reuse/client/src/components/stage/stage';
+
+export default stage;
