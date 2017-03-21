9a9013263548c57411551b90a47593ec948a86ce
Beyer <rbeyer@cmbl0753f3ad8.corp.quickbase.net>
2017-03-21 14:48:02 -0400

    fixed a whole bunch of styling issues for ericka and cara. fixed colors, sizes, padding, margins, added some new semantic colors. I'm a real engineer

--- ui/client-react/src/components/app/settings/categories/userManagement.js
+++ ui/client-react/src/components/app/settings/categories/userManagement.js
@@ -25,7 +25,7 @@ class UserManagement extends React.Component {
     constructor(...args) {
         super(...args);
         this.createUserColumns = this.createUserColumns.bind(this);
-        this.createUserRows = this.createUserRows.bind(this);
+        this.createUserRows = this.createUserRows.bind(this); 
     }
 
     createUserColumns(cellFormatter) {
@@ -54,7 +54,7 @@ class UserManagement extends React.Component {
             {
                 property: 'screenName',
                 header: {
-                    label: 'User Name'
+                    label: 'User name'
                 },
                 cell: {formatters: [cellFormatter]}
             }
