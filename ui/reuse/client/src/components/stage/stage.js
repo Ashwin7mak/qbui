dd16d4f18bb6f0ccd835c714b95256a99e2fe4df
nvanselow <nvanselow@quickbase.com>
2017-03-21 18:40:10 -0400

    MC1061-Refactor stage component and imports after moving

--- ui/reuse/client/src/components/stage/stage.js
+++ ui/reuse/client/src/components/stage/stage.js
@@ -1,31 +1,31 @@
-
-import React from 'react';
-import './stage.scss';
-import QBicon from '../qbIcon/qbIcon';
+import React, {PropTypes, Component} from 'react';
 import Collapse from 'react-bootstrap/lib/Collapse';
 import Well from 'react-bootstrap/lib/Well';
+import Icon from '../icon/icon';
 
-import Logger from '../../utils/logger';
-var logger = new Logger();
+import './stage.scss';
 
-var Stage = React.createClass({
+/**
+ * The stage is a header element that appears at the top of most pages. It has a headline (left) and page-wide actions (right).
+ * Optionally, it can have children that appear when the expand/collapse button is clicked.
+ * <Stage>
+ *     <p>Content that will appear when expand is clicked</p>
+ * </Stage>
+ */
+class Stage extends Component {
+    constructor(props) {
+        super(props);
 
-    propTypes: {
-        pageActions: React.PropTypes.element,
-        stageHeadline: React.PropTypes.node
-    },
+        this.state = {open: false};
 
-    getInitialState: function() {
-        return {open: false};
-    },
+        this.toggleStage = this.toggleStage.bind(this);
+    }
 
-    toggleStage: function() {
+    toggleStage() {
         this.setState({open: !this.state.open});
-        logger.debug('Click event: ' + this.state.stage + ' staging area');
-    },
+    }
 
     render() {
-
         return (
             <div className="layout-stage">
                 <div className="stageHeader">
@@ -36,13 +36,25 @@ var Stage = React.createClass({
                         {this.props.pageActions}
                     </div>
                 </div>
+                
                 <Collapse in={this.state.open}>
                     <Well>{this.props.children}</Well>
                 </Collapse>
-                <button className="toggleStage" onClick={this.toggleStage} ><QBicon icon={this.state.open ? "caret-up" : "caret-down"} /></button>
-            </div>);
-    }
 
-});
+                <button className="toggleStage" onClick={this.toggleStage}><Icon icon={this.state.open ? 'caret-up' : 'caret-down'} /></button>
+            </div>
+        );
+    }
+}
+
+Stage.propTypes = {
+    /**
+     * Page actions that appear on the right side of the page. It's easiest to pass in a IconActions component, but not required. */
+    pageActions: PropTypes.element,
+
+    /**
+     * The title/headline of the stage. It appears toward the upper-left of the stage. */
+    stageHeadline: PropTypes.node
+};
 
 export default Stage;
