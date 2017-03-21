0609d67a519b81575ec4655f5182e0e66b21d1c0
Brandon Stookey <bstookey@quickbase.com>
2017-03-21 14:37:59 -0400

    add unit tests for rekeyboardshortcuts

---
+++ ui/reuse/client/test/components/reKeyboardShortCuts.unit.spec.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import jasmineEnzyme from 'jasmine-enzyme';
+import ReKeyboardShortCuts from '../../src/components/reKeyboardShortcuts/reKeyboardShortcuts'
+
+const keyBindings = [{key: 'esc', callback: () => {}}, {key: 'mod+s', callback: () => {}}];
+const testId = "testId";
+
+let component;
+
+fdescribe('ReKeyboardShortCuts', () => {
+    beforeEach(() => {
+        jasmineEnzyme();
+    });
+
+    afterEach(() => {
+    });
+
+    it('calls addAllKeyBindings when component mounts ', () => {
+        component = shallow(<ReKeyboardShortCuts
+            shortcutBindings={keyBindings}
+            id={testId}
+        />);
+
+        let instance = component.instance();
+        spyOn(instance, 'addAllKeyBindings');
+        instance.componentWillMount();
+
+        expect(instance.addAllKeyBindings).toHaveBeenCalledWith(keyBindings);
+    });
+
+    it('calls removeAllKeyBindings when component unmounts ', () => {
+        component = shallow(<ReKeyboardShortCuts
+            shortcutBindings={keyBindings}
+            id={testId}
+        />);
+
+        let instance = component.instance();
+        spyOn(instance, 'removeAllKeyBindings');
+        instance.componentWillUnmount();
+
+        expect(instance.removeAllKeyBindings.calls.count()).toBe(1);
+    });
+});
+
+
+
