4a741870a8573261b3642bc6efe2acb6e9b2f679
Beyer <rbeyer@CMBL130e9e44d.local>
2017-03-25 12:05:34 -0400

    first pass at getting stuff to appear

--- ui/client-react/src/components/builder/builderMenus/fieldProperties.js
+++ ui/client-react/src/components/builder/builderMenus/fieldProperties.js
@@ -1,13 +1,50 @@
 import React, {PropTypes} from 'react';
+import TextFieldValueEditor from '../../fields/textFieldValueEditor';
+import CheckBoxFieldValueEditor from '../../fields/checkBoxFieldValueEditor';
 import './fieldProperties.scss';
 
 let FieldProperties = React.createClass({
     propTypes: {
     },
+
+    createPropertiesTitle() {
+        return (
+            <div className="fieldPropertiesTitle">Field properties</div>
+        );
+    },
+
+    createTextPropertyContainer(propertyTitle) {
+        return (
+            <div className="fieldPropertyContainer">
+                <div className="textPropertyTitle">{propertyTitle}</div>
+                <TextFieldValueEditor classes="textPropertyValue"/>
+            </div>
+        );
+    },
+
+    createCheckBoxPropertyContainer(propertyTitle) {
+        return (
+            <div className="checkboxPropertyContainer">
+                <CheckBoxFieldValueEditor classes="checkboxPropertyValue"/>
+                <div className="checkboxPropertyTitle">{propertyTitle}</div>
+            </div>
+        );
+    },
+
+    createNameProperty() {
+        return (this.createTextPropertyContainer("Name"));
+    },
+
+    createRequiredProperty() {
+        return (this.createCheckBoxPropertyContainer("Must be filled in"));
+    },
+
     render() {
         return (
             <div className="fieldPropertiesContainer">
-
+                {this.createPropertiesTitle()}
+                {this.createNameProperty()}
+                {this.createRequiredProperty()}
             </div>
         );
     }
