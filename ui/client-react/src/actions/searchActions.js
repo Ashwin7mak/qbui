13626b24c5d296e605e11dc39f45f7d69949847e
Don Hatch <donald_hatch@intuit.com>
2017-03-27 08:41:50 -0400

    add method doc comment to searchActions

--- ui/client-react/src/actions/searchActions.js
+++ ui/client-react/src/actions/searchActions.js
@@ -1,5 +1,11 @@
 import * as types from '../actions/types';
 
+/**
+ * Save in the store search input text entered by an end user.
+ *
+ * @param content
+ * @returns {{type, content: *}}
+ */
 export const searchInput = (content) => {
     return {
         type: types.SEARCH_INPUT,
@@ -7,6 +13,11 @@ export const searchInput = (content) => {
     };
 };
 
+/**
+ * Clear the search input text box
+ *
+ * @returns {{type, content: string}}
+ */
 export const clearSearchInput = () => {
     return {
         type: types.SEARCH_INPUT,
