1f59fabe7a40e7f505ac804b25733024bbf0a851
Brandon Stookey <bstookey@quickbase.com>
2017-03-28 11:01:50 -0400

    fix typo

--- ui/client-react/src/components/formBuilder/fieldEditingTools/fieldEditingTools.js
+++ ui/client-react/src/components/formBuilder/fieldEditingTools/fieldEditingTools.js
@@ -92,7 +92,7 @@ export class FieldEditingTools extends Component {
     componentDidMount() {
         /**
          * For keyboard, we need to reset the focus, to maintain proper tabbing order
-         * and wee need to keep the current form element in view, by scrolling it into view
+         * and we need to keep the current form element in view, by scrolling it into view
          * */
         if (this.props.previouslySelectedField && this.props.previouslySelectedField[0] && this.props.tabIndex !== '-1') {
             let previouslySelectedField = document.querySelectorAll('.fieldEditingTools');
