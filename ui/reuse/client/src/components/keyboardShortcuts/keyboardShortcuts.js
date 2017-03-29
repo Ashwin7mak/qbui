275980e6915a282fae664e6dff0b6e7c02d9b0d8
Brandon Stookey <bstookey@quickbase.com>
2017-03-29 17:36:21 -0400

    update keyboard component

--- ui/reuse/client/src/components/keyboardShortcuts/keyboardShortcuts.js
+++ ui/reuse/client/src/components/keyboardShortcuts/keyboardShortcuts.js
@@ -1,25 +1,21 @@
 import React, {PropTypes, Component} from 'react';
 import MouseTrap from 'mousetrap';
 
-/**
- * adds a bindGlobal method to Mousetrap that allows you to
- * bind specific keyboard shortcuts that will still work
- * inside a text input field
- *
- * usage:
- * Mousetrap.bindGlobal('ctrl+s', _saveChanges);
- */
-
 class KeyboardShortcuts extends Component {
     constructor(props) {
         super(props);
 
         this.addAllKeyBindings = this.addAllKeyBindings.bind(this);
+        this.addAllKeyBindingsPreventDefault = this.addAllKeyBindingsPreventDefault.bind(this);
         this.removeAllKeyBindings = this.removeAllKeyBindings.bind(this);
     }
 
     componentWillMount() {
-        this.addAllKeyBindings(this.props.shortcutBindings);
+        if (this.props.shortcutBindingsPreventDefault) {
+            this.addAllKeyBindingsPreventDefault(this.props.shortcutBindingsPreventDefault);
+        } else {
+            this.addAllKeyBindings(this.props.shortcutBindings);
+        }
     }
 
     componentWillUnmount() {
@@ -28,14 +24,26 @@ class KeyboardShortcuts extends Component {
 
     addAllKeyBindings(bindings = []) {
         bindings.forEach(binding => {
+            MouseTrap.bind(binding.key, () => binding.callback(binding.content));
+        });
+    }
+
+    addAllKeyBindingsPreventDefault(bindings = []) {
+        bindings.forEach(binding => {
             MouseTrap(document.body).bind(binding.key, () => binding.callback(binding.content));
         });
     }
 
     removeAllKeyBindings() {
-        this.props.shortcutBindings.forEach(binding => {
-            MouseTrap(document.body).unbind(binding.key);
-        });
+        if (this.props.shortcutBindingsPreventDefault) {
+            this.props.shortcutBindingsPreventDefault.forEach(binding => {
+                MouseTrap.unbind(binding.key);
+            });
+        } else {
+            this.props.shortcutBindings.forEach(binding => {
+                MouseTrap.unbind(binding.key);
+            });
+        }
     }
 
     render() {
@@ -64,8 +72,21 @@ KeyboardShortcuts.propTypes = {
         /** Content will be passed as the first argument to the callback. It is optional. */
         content: PropTypes.any,
     })),
-    /** This stops the browser default callbacks */
-    stopDefaultCallback: PropTypes.bool
+    /**
+     * An array of keyboard bindings that will be active while this component is mounted. It should be immutable. */
+    shortcutBindingsPreventDefault: PropTypes.arrayOf(PropTypes.shape({
+        /**
+         * The keyboard shortcut you want to activate the callback
+         * See available options at https://craig.is/killing/mice#api.bind */
+        key: PropTypes.string.isRequired,
+
+        /** The callback that will be activated when the key is pressed */
+        callback: PropTypes.func.isRequired,
+
+        /** Content will be passed as the first argument to the callback. It is optional. */
+        content: PropTypes.any,
+    }))
+
 };
 
 export default KeyboardShortcuts;
