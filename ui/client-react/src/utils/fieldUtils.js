570793ddb9d6e4ae89cb58e47b4a4af8260b8b65
Brandon Stookey <bstookey@quickbase.com>
2017-03-31 16:34:14 -0400

    making fields locale more generic

--- ui/client-react/src/utils/fieldUtils.js
+++ ui/client-react/src/utils/fieldUtils.js
@@ -94,7 +94,7 @@ class FieldUtils {
         if (element && element.useAlternateLabel) {
             return element.displayText || '';
         } else if (relatedField) {
-            return relatedField.name || Locale.getMessage(`builder.fields.${fieldType}`) || '';
+            return relatedField.name || Locale.getMessage(`fields.${fieldType}`) || '';
         } else {
             return '';
         }
