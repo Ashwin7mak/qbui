f8e95799c7a9b2ea8c4b5d47ebb9cd73ee60016d
Brandon Stookey <bstookey@quickbase.com>
2017-03-31 17:17:35 -0400

    create constants file for default field properties

---
+++ ui/client-react/src/constants/fieldsDefaultPropertiesConstants.js
@@ -0,0 +1,73 @@
+/**
+ * Created by bstookey on 3/31/17.
+ */
+
+(function() {
+    'use strict';
+    module.exports = Object.freeze({
+        DEFAULT_FIELD: {
+            "type": "SCALAR",
+            "datatypeAttributes": {
+                "type": "TEXT",
+                "htmlAllowed": false
+            },
+            "name": "Text",
+            "builtIn": false,
+            "dataIsCopyable": true,
+            "includeInQuickSearch": true,
+            "userEditableValue": true,
+            "required": false,
+            "unique": false,
+            "indexed": false,
+            "multiChoiceSourceAllowed": false
+        },
+        DEFAULT_TEXT_FIELD: {
+                   "type": "SCALAR",
+                    "datatypeAttributes": {
+                        "type": "TEXT",
+                        "htmlAllowed": false
+                    },
+                    "name": "Text",
+                    "builtIn": false,
+                    "dataIsCopyable": true,
+                    "includeInQuickSearch": true,
+                    "userEditableValue": true,
+                    "required": false,
+                    "unique": false,
+                    "indexed": false,
+                    "multiChoiceSourceAllowed": false
+        },
+        DEFAULT_NUMERIC_FIELD: {
+            "type": "SCALAR",
+            "datatypeAttributes": {
+                "type": "TEXT",
+                "htmlAllowed": false
+            },
+            "name": "Text",
+            "builtIn": false,
+            "dataIsCopyable": true,
+            "includeInQuickSearch": true,
+            "userEditableValue": true,
+            "required": false,
+            "unique": false,
+            "indexed": false,
+            "multiChoiceSourceAllowed": false
+        },
+        DEFAULT_CHECKBOX_FIELD: {
+            "type": "SCALAR",
+            "datatypeAttributes": {
+                "type": "TEXT",
+                "htmlAllowed": false
+            },
+            "name": "Text",
+            "builtIn": false,
+            "dataIsCopyable": true,
+            "includeInQuickSearch": true,
+            "userEditableValue": true,
+            "required": false,
+            "unique": false,
+            "indexed": false,
+            "multiChoiceSourceAllowed": false
+        }
+    })
+}());
