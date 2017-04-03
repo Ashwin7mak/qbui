55fc8106979a97cf7b7cb48378c964c1f14dae6f
Michael Harris <mharris@quickbase.com>
2017-04-03 13:36:28 -0400

    rename colors3 to colors

--- ui/componentLibrary/src/pages/colors.js
+++ ui/componentLibrary/src/pages/colors.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ColorSwatches from '../components/colorSwatches';
 
 
-export default function Colors3Page() {
+export default function ColorsPage() {
 
     let scssFileContents = require('!raw-loader!../../../reuse/client/src/assets/css/_qbColorPalette-QuickBase3.scss');
     return (
         <div>
-            <h2>Colors 3</h2>
+            <h2>Colors</h2>
 
             <ColorSwatches scssFileContents={scssFileContents}/>
         </div>
