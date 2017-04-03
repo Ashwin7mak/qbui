32686e6f0a41706e69b7750c2bb2e053cd3cc8e6
Don Hatch <donald_hatch@intuit.com>
2017-04-03 14:43:50 -0400

    fix formBuild unit tests

--- ui/client-react/test/components/formBuilder/formBuilder.unit.spec.js
+++ ui/client-react/test/components/formBuilder/formBuilder.unit.spec.js
@@ -2,9 +2,8 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
 
-
+import {__RewireAPI__ as FormBuilderRewireAPI} from '../../../src/components/formBuilder/formBuilder';
 import {FormBuilder} from '../../../src/components/formBuilder/formBuilder';
-import QbForm from '../../../src/components/QBForm/qbform';
 
 const mockFormData = {formMeta: {tabs: [{orderIndex: 0,
     sections: [{orderIndex: 1,
@@ -16,21 +15,32 @@ const mockFormData = {formMeta: {tabs: [{orderIndex: 0,
     }]
 }]}};
 
+const QbFormMock = React.createClass({
+    render: function() {
+        return <div>qbForm mock</div>;
+    }
+});
+
 let component;
 let instance;
 
 describe('FormBuilder (drag/drop container)', () => {
     beforeEach(() => {
+        FormBuilderRewireAPI.__Rewire__('QbForm', QbFormMock);
         jasmineEnzyme();
     });
 
+    afterEach(() => {
+        FormBuilderRewireAPI.__ResetDependency__('QbFormMock');
+    });
+
     it('wraps QbForm in a drag drop container', () => {
-        component = shallow(<FormBuilder formData={mockFormData} showCustomDragLayer={false} />);
+        component = shallow(<FormBuilder formId={'view'} formData={mockFormData} showCustomDragLayer={false} />);
         instance = component.instance();
 
         expect(component.find('.formBuilderContainer')).toBePresent();
 
-        let qbForm = component.find(QbForm);
+        let qbForm = component.find(QbFormMock);
         expect(qbForm).toBePresent();
         expect(qbForm).toHaveProp('edit', true);
         expect(qbForm).toHaveProp('editingForm', true);
