8820c46bf032294f7469af41f4bb370b1ce8b4e1
Ken LaBak <Ken_LaBak@intuit.com>
2017-04-03 15:36:32 -0400

    Adding Git UI Branch to the other config files

--- ui/wdio/config/wdioSauceFirefox.conf.js
+++ ui/wdio/config/wdioSauceFirefox.conf.js
@@ -6,8 +6,8 @@ var config = {
             browserName: 'firefox',
             version: '46.0',
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-            build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - OSX Firefox Browser',
-            tags            : [process.env.SAUCE_JOB_NAME + '_OSX_Firefox', 'try', 'OSX', 'Firefox', process.env.BUILD_NUMBER],
+            build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - OSX Firefox Browser - Git branch: ' + process.env.GIT_UIBRANCH,
+            tags            : [process.env.SAUCE_JOB_NAME + '_OSX_Firefox', 'try', 'OSX', 'Firefox', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
             screenResolution: '2048x1536',
             // Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
             idleTimeout: '180',
