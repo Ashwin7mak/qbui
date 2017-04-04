a32bb39f336049aa24a3f6061a7348af74d878a2
Brandon Stookey <bstookey@quickbase.com>
2017-04-04 09:21:53 -0400

    add locale test

--- ui/client-react/test/components/formBuilder/formBuilderCustomDragLayer.unit.spec.js
+++ ui/client-react/test/components/formBuilder/formBuilderCustomDragLayer.unit.spec.js
@@ -47,6 +47,13 @@ describe('FormBuilderCustomDragLayer', () => {
             expect(component.find(FieldToken)).not.toBePresent();
         });
 
+        it('calls getMessage when a valid field type is passed through and name is not available', () => {
+            let item = {relatedField: {datatypeAttributes: {type: consts.NUMERIC}}};
+            component = shallow(<FormBuilderCustomDragLayer item={item} isDragging={true} itemType={draggableTypes.FIELD}/>);
+
+            expect(mockLocale.getMessage).toHaveBeenCalled();
+        });
+
         let testCases = [
             {
                 description: 'renders a default FieldToken',
