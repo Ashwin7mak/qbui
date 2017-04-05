312347873af7aa308c8da0f1dfb549163e786d8b
jose.hidalgo <hidalgo860@gmail.com>
2017-04-05 15:18:09 -0400

    style

--- ui/governance/src/account/users/AccountUsers.js
+++ ui/governance/src/account/users/AccountUsers.js
@@ -73,8 +73,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 function checkDataFetchingError(error) {
     //if it's a redirect error, do not display the 'error' text and show spinner
-    if (error && error.data && error.data.statusCode === 401)
-    {
+    if (error && error.data && error.data.statusCode === 401) {
         return null;
     }
     return error;
