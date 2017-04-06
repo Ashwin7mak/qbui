7be3dfdf169b78710c8385548e928bcfc115413a
Brandon Stookey <bstookey@quickbase.com>
2017-04-06 18:06:30 -0400

    fix broken unit tests

--- ui/client-react/test/components/formBuilder/toolPalette.unit.spec.js
+++ ui/client-react/test/components/formBuilder/toolPalette.unit.spec.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import {mount} from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
-
+import {FieldTokenInMenu} from '../../../src/components/formBuilder/fieldToken/fieldTokenInMenu';
 import ToolPalette from '../../../src/components/builder/builderMenus/toolPalette';
-import NewFieldsMenu from '../../../src/components/formBuilder/menus/newFieldsMenu';
+import NewFieldsMenu, {__RewireAPI__ as NewFieldsMenuRewireAPI} from '../../../src/components/formBuilder/menus/newFieldsMenu';
 
 let component;
 
-describe('ToolPalette', () => {
+fdescribe('ToolPalette', () => {
     beforeEach(() => {
         jasmineEnzyme();
+        NewFieldsMenuRewireAPI.__Rewire__('FieldTokenInMenu', FieldTokenInMenu);
+    });
+
+    afterEach(() => {
+        NewFieldsMenuRewireAPI.__ResetDependency__('FieldTokenInMenu');
     });
 
     it('displays new fields', () => {
