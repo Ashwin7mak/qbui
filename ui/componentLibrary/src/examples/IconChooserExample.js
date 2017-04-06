09ec4375f757f99b696daa14b02b20ef97cd996f
Michael Harris <mharris@quickbase.com>
2017-04-06 17:55:42 -0400

    add icon chooser files to the component library

---
+++ ui/componentLibrary/src/examples/IconChooserExample.js
@@ -0,0 +1,26 @@
+// This is a basic example for the React playground
+// Please update to include other properties or states for your component
+
+let mock = {
+    onOpen: () => {},
+    onClose: () => {},
+    onSelect: () => {},
+    icons: ["Apple", "carfront", "cat", "Chicken", "heart", "ruler", "bell", "duck"]
+}
+
+const basicIconChooserExample = (
+    <div>
+        <dt>Default: </dt>
+        <dd>
+            <IconChooser selectedIcon="Apple"
+                         isOpen={true}
+                         onOpen={mock.onOpen}
+                         onClose={mock.onClose}
+                         font="iconTableSturdy"
+                         icons={mock.icons}
+                         onSelect={mock.onSelect} />
+        </dd>
+    </div>
+);
+
+ReactDOM.render(basicIconChooserExample, mountNode);
