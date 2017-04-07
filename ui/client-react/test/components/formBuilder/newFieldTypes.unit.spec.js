2ae416dca9b8eff3aba56ebb41bb31ea7d9055d7
Brandon Stookey <bstookey@quickbase.com>
2017-04-07 17:02:20 -0400

    fix broken unit test

--- ui/client-react/test/components/formBuilder/newFieldTypes.unit.spec.js
+++ ui/client-react/test/components/formBuilder/newFieldTypes.unit.spec.js
@@ -15,13 +15,12 @@ describe('NewFieldTypes', () => {
     afterEach(() => {
         NewFieldTypesRewireAPI.__ResetDependency__('Locale');
     });
-
     describe('createFieldTypeProps', () => {
         it('returns an object with the correct field type props', () => {
             expect(createFieldTypeProps(fieldFormats.TEXT_FORMAT)).toEqual({
                 key: `fieldType_${fieldFormats.TEXT_FORMAT}`,
                 type: fieldFormats.TEXT_FORMAT,
-                ...createScalarDefaultFieldsProperties()[fieldFormats.TEXT_FORMAT],
+                relatedField: {...createScalarDefaultFieldsProperties()[fieldFormats.TEXT_FORMAT]},
                 isNewField: true,
                 title: `fieldsDefaultLabels.${fieldFormats.TEXT_FORMAT}`,
                 tooltipText: `builder.formBuilder.tooltips.addNew${fieldFormats.TEXT_FORMAT}`
