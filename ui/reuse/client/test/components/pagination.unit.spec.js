11c569fa7a3544724d6baee08408ebc3e5ddcd0f
Arun Manohar <amanohar@quickbase.com>
2017-04-10 13:40:48 -0400

    Create a stub component in the original location and refactor test cases

--- ui/reuse/client/test/components/pagination.unit.spec.js
+++ ui/reuse/client/test/components/pagination.unit.spec.js
@@ -4,15 +4,13 @@ import jasmineEnzyme from 'jasmine-enzyme';
 
 const I18nMessageMock = ({message}) => <span className="mockI18nMessage">{message}</span>;
 
-
 import Pagination, {__RewireAPI__ as PaginationRewireAPI} from '../../src/components/pagination/pagination';
 
-let component;
 const mockParentFunctions = {
   onChange() {},
 };
 
-fdescribe('Pagination component', () => {
+describe('Pagination component', () => {
   beforeEach(() => {
       jasmineEnzyme();
       PaginationRewireAPI.__Rewire__('I18nMessage', ({message}) => <div className="mockMessage">{message}</div>);
@@ -22,27 +20,27 @@ fdescribe('Pagination component', () => {
     PaginationRewireAPI.__ResetDependency__('I18nMessage');
   });
 
-  fit('should render a `.reportNavigation` ', () => {
+  it('should render a `.reportNavigation` ', () => {
     const wrapper = mount(<Pagination pagingData={true}/>);
     expect(wrapper.find(".reportNavigation")).toBePresent();
   });
 
-  fit('should render a `.pageNumbers` ', () => {
+  it('should render a `.pageNumbers` ', () => {
     const wrapper = mount(<Pagination pagingData={true}/>);
     expect(wrapper.find(".pageNumbers")).toBePresent();
   });
 
-  fit('has a previous page button', () => {
+  it('has a previous page button', () => {
     const wrapper = shallow(<Pagination />);
     wrapper.instance().previousPageButton();
   });
 
-  fit('has a next page button', () => {
+  it('has a next page button', () => {
     const wrapper = shallow(<Pagination />);
     wrapper.instance().nextPageButton();
   });
 
-  fit('has a previous page button that can be clicked', () => {
+  it('has a previous page button that can be clicked', () => {
   spyOn(mockParentFunctions, 'onChange');
 
   const component = mount(<Pagination pagingData={true} current={3} total={1000} onChange={mockParentFunctions.onChange}/>);
@@ -53,7 +51,7 @@ fdescribe('Pagination component', () => {
   expect(mockParentFunctions.onChange).toHaveBeenCalledWith(2,20);
   })
 
-  fit('has a next page button that can be clicked', () => {
+  it('has a next page button that can be clicked', () => {
     spyOn(mockParentFunctions, 'onChange');
 
   const component = mount(<Pagination pagingData={true} total={1000} onChange={mockParentFunctions.onChange}/>);
@@ -64,8 +62,8 @@ fdescribe('Pagination component', () => {
   expect(mockParentFunctions.onChange).toHaveBeenCalledWith(2,20);
   })
 
-  fit('displays a message with page number', () => {
-    component = mount(<Pagination pagingData={true}/>);
+  it('displays a message with page number', () => {
+    const component = mount(<Pagination pagingData={true}/>);
     expect(component.find('.mockMessage')).toHaveText("report.reportNavigationBar");
   });
 })
