1dafdb1bb03c9c58606f80559191175c9eb5fbc4
Don Hatch <donald_hatch@intuit.com>
2017-04-13 16:40:09 -0400

    minor comment update to e2e test

--- ui/wdio/tests/forms/formEditValidation.e2e.spec.js
+++ ui/wdio/tests/forms/formEditValidation.e2e.spec.js
@@ -57,7 +57,6 @@
 
             //Step 2 - Enter invalid values to get the form to error state
             fieldTypes.forEach(function(fieldType) {
-                //TODO change the empty string to special characters once MB-1970 is fixed.
                 formsPO.enterInvalidFormValues(fieldType, '2345-7');
             });
 
