f09549df782fe027d3bd66017f6f5a2aac7dc7f0
Gen Edwards <gedwards@quickbase.com>
2017-04-13 08:46:07 -0400

    MC-733:update comments

--- ui/client-react/src/components/drawer/drawerContainer.js
+++ ui/client-react/src/components/drawer/drawerContainer.js
@@ -7,53 +7,52 @@ import {RecordRouteWithUniqueId} from '../../components/record/recordRoute';
 import './drawer.scss';
 
 /**
- *
+ * A parent component which orchestrates stacks of drawers.
  */
 class DrawerContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {addedDrawers: []};
     }
-    // TODO:
-    // - get all drawers from url or passed in via parent
-    // - render drawer's inner component, RecordWrapper
-    //
+
     getDrawers = () => {
-        // TODO: handle all the records in url since we'll support multiple
+        // TODO: handle all the records in url since we'll support multiple drawers
+        //       the array or tables/records should not be passed in as props, we should retrieve
+        //       or generate the list of records from the router
         const drawerTableIds = _.get(this, 'props.drawerTableIds');
         let drawerRecordIds = _.get(this, 'props.drawerRecordIds');
         drawerRecordIds = drawerRecordIds.concat(this.state.addedDrawers);
 
         if (drawerTableIds.length && drawerRecordIds.length) {
-            const appId = this.props.appId;
-            const map =  drawerRecordIds.map(drawerRecordId => {
+            return drawerRecordIds.map(drawerRecordId => {
                 return (
                     <Drawer visible={true} key={drawerRecordId} keyboardOnCancel={()=> {console.log('hey');}}>
-                        {/* TODO: render RecordWrapper*/}
                         <RecordRouteWithUniqueId {...this.props} isDrawerContext={true}/>
                     </Drawer>);
             });
-            return map;
         } else {
             return null;
         }
-    }
+    };
+
+    closeDrawers = () => {
+        // TODO: probably a router-type thing to close all the drawers
+    };
 
     // TODO: remove this function, this is temporary
     addDrawers = () => {
         const drawers = this.state.addedDrawers;
         drawers.push(_.uniqueId());
         this.setState({addedDrawers: drawers});
-    }
+    };
 
     // TODO: remove this function, this is temporary
     removeDrawers = () => {
         this.setState({addedDrawers: []});
-    }
+    };
 
-    // TODO: render a 'Close' button, probably a <Link> component and pass to child
-    //       OR pass a closeDrawers function to drawers, drawers pass close button as a prop to
-    //          RecordWrapper, RecordWrapper renders button. YES!
+    // TODO: pass a closeDrawers function to drawers, drawers pass close button as a prop to
+    //       RecordWrapper, RecordWrapper renders button. YES!
     render() {
         const classNames = ['drawerContainer', this.props.position];
         classNames.push(this.props.visible ? 'visible' : '');
@@ -70,18 +69,10 @@ class DrawerContainer extends React.Component {
 }
 
 DrawerContainer.propTypes = {
-    /** whether the DrawerContainer is be visible */
+    /** whether the DrawerContainer is visible */
     visible: PropTypes.bool.isRequired,
     /** main content of DrawerContainer */
     content: PropTypes.node,
-    /** position of the DrawerContainer, defaults to 'right' */
-    position: PropTypes.oneOf(['left', 'right', 'top', 'bottom'])
-
 };
-
-DrawerContainer.defaultProps = {
-    position: 'right'
-};
-
 
 export default DrawerContainer;
