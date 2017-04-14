66ca6dd4c28de53dfc37a044f7f82a54ac13c3d9
Gen Edwards <gedwards@quickbase.com>
2017-04-14 17:45:41 -0400

    MC-733:shenanigans

--- ui/client-react/src/components/drawer/drawerContainer.js
+++ ui/client-react/src/components/drawer/drawerContainer.js
@@ -8,11 +8,46 @@ import {RecordRouteWithUniqueId} from '../../components/record/recordRoute';
 import './drawer.scss';
 
 /**
- * A parent component which orchestrates stacks of drawers.
+ * A parent component which orchestrates sliding a drawer in and out of the parent container.
+ * There are still some things I'm not sure about until I get my hands on the actual router. But for all the odd cases
+ * I can think of, I have ideas on how to get around them.
+ *
+ * I think Route's children prop might come in handy https://reacttraining.com/react-router/web/api/Route/children-func
+ *
+ * Lifecycle of this component:
+ * 1> initial render                                                          <ReactCSSTransitionGroup>     <Drawer>
+ *    render an invisible <ReactCSSTransitionGroup> with no inner child,       |     display:none               null
+ *    ReactCSSTransitionGroup is invisible using display:none                  |
+ *                                                                             |
+ * 2> slide the drawer in                                                      |
+ *    when a route change dictates that we need to render a drawer,            |       visible                rendered
+ *    setState({visible: true}), this is currently done in                     |
+ *    componentWillReceiveProps. Once we have the router, we probably          |
+ *    will get rid of componentWillReceiveProps.                               |
+ *    The drawer will slide in. ReactCSSTransitionGroup is visible.            |
+ *                                                                             |
+ * 3> slide the drawer out                                                     |
+ *    drawer slides out > after drawer slides out, ReactCSSTransitionGroup     |      visible                 rendered
+ *    is still visible but the drawer is off screen                            |
+ *                                                                             |
+ * 4> ReactCSSTransitionGroup unmounts the drawer                              |
+ *    drawer component will call ComponentWillUnmount, which then calls        |      visible                unmounting
+ *    closeInvisiblePane                                                       |
+ *                                                                             |
+ * 5> closeInvisiblePane called on DrawerContainer                             |
+ *    this.state.visible is set to false, we can make ReactCSSTransitionGroup  |    display:none                null
+ *    invisible so you can click                                               |
+ *
+ * We do all this shenanigan because we need ReactCSSTransitionGroup visible at step (4), and invisible at (1) and (5).
+ * If we don't use this.state.visible and set to false in a callback at step (5), we never see the drawer slide out.
+ * Which is because display:none is not a transitionable property.
  */
 class DrawerContainer extends React.Component {
     constructor(props) {
         super(props);
+        /* visible is initially set to false. We set this to true when we slide in a child drawer for the first time.
+         *
+         */
         this.state = {
             visible: false
         };
@@ -32,16 +67,16 @@ class DrawerContainer extends React.Component {
     };
 
     getDrawer = () => {
-        // TODO: handle all the records in url since we'll support multiple drawers
-        //       the array or tables/records should not be passed in as props, we should retrieve
-        //       or generate the list of records from the router
+        // TODO: Once we integrate with the router, `getDrawer` will return:
+        //           <Route path={/*something*/} component={component}>
+
         if (this.props.hasDrawer) {
             const params = {
                 appId:'0duiiaaaaap',
                 tblId:'0duiiaaaaa2',
                 recordId:'3'
             };
-            return (
+            const component = (
                 <Drawer key={1} unmount={this.closeInvisiblePane}>
                     <RecordRouteWithUniqueId
                         {...this.props}
@@ -50,6 +85,7 @@ class DrawerContainer extends React.Component {
                         params={params}
                         />
                 </Drawer>);
+            return component;
         } else {
             return null;
         }
@@ -61,8 +97,11 @@ class DrawerContainer extends React.Component {
         const classNames = ['drawerContainer', this.props.position, this.props.className];
         classNames.push(this.props.visible ? 'visible' : '');
         classNames.push(this.props.hasDrawer ? 'visible' : '');
-        classNames.push(this.state.visible ? 'visible' : '');
-        
+        //classNames.push(this.state.visible ? 'visible' : '');
+
+        const drawer = this.getDrawer();
+        classNames.push(drawer ? 'visible' : '');
+
         return (
             <div className={classNames.join(' ')} onClick={this.closeDrawers}>
                 <ReactCSSTransitionGroup
@@ -73,7 +112,7 @@ class DrawerContainer extends React.Component {
                     transitionEnterTimeout={1200}
                     transitionLeaveTimeout={1200}
                     >
-                    {this.getDrawer()}
+                    {drawer}
                 </ReactCSSTransitionGroup>
 
                 {this.props.content}
