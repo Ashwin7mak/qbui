aa74b3ee84722d2bad15f7f21ace0a9e90401322
agade <agade@quickbase.com>
2017-04-17 14:40:50 -0400

    enabling tableCreateTest

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -566,7 +566,7 @@ module.exports = function(grunt) {
                     './wdio/tests/forms/formPermissionsViewerRole.e2e.spec.js',
                     // currently intermittently broken in CI need to fix in another PR
                     './wdio/tests/forms/formDragDrop.e2e.spec.js',
-                    './wdio/tests/tables/tableCreate.e2e.spec.js',
+                    //'./wdio/tests/tables/tableCreate.e2e.spec.js',
                     './wdio/tests/tables/tableEdit.e2e.spec.js',
                     './wdio/tests/reports/reportNavigation.e2e.spec.js',
                     './wdio/tests/reports/reportEditRecord.e2e.spec.js'
