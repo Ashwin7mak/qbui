fbbb6414c4bf16b3c88374207ce92519779c2dee
Brandon Stookey <bstookey@quickbase.com>
2017-04-17 10:27:39 -0400

    fix linting errors

--- ui/client-react/src/components/autoScroll/autoScroll.js
+++ ui/client-react/src/components/autoScroll/autoScroll.js
@@ -130,7 +130,6 @@ class AutoScroll extends Component {
             return containerBottom - this.props.pixelsFromBottomForLargeDevices;
         }
     }
-    
     updateScrolling(e) {
         this.stopScrolling();
 
