ab45ef9cae696cb85ed6ce2ba86a9dd363d93bb2
Ken LaBak <Ken_LaBak@intuit.com>
2017-04-17 10:16:58 -0400

    Updating path to grouping test. Updated sample file to match Gruntfile.

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -568,11 +568,12 @@ module.exports = function(grunt) {
                     './wdio/tests/forms/formDragDrop.e2e.spec.js',
                     './wdio/tests/tables/tableCreate.e2e.spec.js',
                     './wdio/tests/tables/tableEdit.e2e.spec.js',
-                    './wdio/tests/reports/reportNavigation.e2e.spec.js',
                     './wdio/tests/reports/reportEditRecord.e2e.spec.js',
-                    './wdio/tests/forms/grouping/reportGroupingViaColumnHeader.e2e.spec.js',
-                    './wdio/tests/reports/sorting/reportSortingViaColumnHeader.e2e.spec.js',
-                    './wdio/tests/reports/reportTable.e2e.spec.js'
+                    './wdio/tests/reports/reportNavigation.e2e.spec.js',
+                    './wdio/tests/reports/reportTable.e2e.spec.js',
+                    './wdio/tests/reports/grouping/reportGroupingViaColumnHeader.e2e.spec.js',
+                    './wdio/tests/reports/sorting/reportSortingViaColumnHeader.e2e.spec.js'
+
                 ],
                 suites: {
                     reports: [
