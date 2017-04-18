64df1bf84dbd47fb479a3ad5b32e119681d187b4
amanoharquickbase <amanohar@quickbase.com>
2017-04-18 14:40:26 -0400

    Add Pagination to component library

--- ui/componentLibrary/src/components/Metadata.js
+++ ui/componentLibrary/src/components/Metadata.js
@@ -43,6 +43,7 @@ import TopNavMetadata from 'component-metadata!../../../reuse/client/src/compone
 import StageMetadata from 'component-metadata!../../../reuse/client/src/components/stage/stage.js';
 import TooltipMetadata from 'component-metadata!../../../reuse/client/src/components/tooltip/tooltip.js';
 import StandardLeftNavMetadata from 'component-metadata!../../../reuse/client/src/components/sideNavs/standardLeftNav.js';
+import PaginationMetadata from 'component-metadata!../../../reuse/client/src/components/pagination/pagination.js';
 // END OF IMPORT STATEMENTS
 // above comment used for grunt task, please do not delete
 
@@ -88,7 +89,8 @@ var Metadata = () => {
         TopNavMetadata,
         StageMetadata,
         TooltipMetadata,
-        StandardLeftNavMetadata
+        StandardLeftNavMetadata,
+        PaginationMetadata
         // END OF METADATA MERGE
         // above comment used for grunt task, please do not delete
     };
