13b2f5c9b69e9ff06c5c4e2b52f9d29f2abe0ed1
amanoharquickbase <amanohar@quickbase.com>
2017-04-18 13:30:22 -0400

    Add comments for propTypes

--- ui/reuse/client/src/components/rowActions/rowActions.js
+++ ui/reuse/client/src/components/rowActions/rowActions.js
@@ -74,31 +74,77 @@ class RowActions extends Component {
 }
 
 RowActions.propTypes = {
+    /*
+    * Gets the row id - can either be a string or a number */
     rowId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    /*
+    * when true, the row can be edited, initially set to false */
     isEditing: PropTypes.bool,
+    /*
+    * Selects the corresponding row to be edited, can be enabled using checkbox */
     isSelected: PropTypes.bool,
+    /*
+    * Holds the corresponding rowId of the row that is currently being edited */
     editingRowId: PropTypes.number,
+    /*
+    * Checks whether the editable row is valid or not */
     isEditingRowValid: PropTypes.bool.isRequired,
+    /*
+    * Saves the values inside the current editing row */
     isEditingRowSaving: PropTypes.bool.isRequired,
+    /*
+    * Checks whether the inline edit is open or not */
     isInlineEditOpen: PropTypes.bool.isRequired,
+    /*
+    * Checks for errors in the editing row */
     editingRowErrors: PropTypes.array.isRequired,
+    /*
+    * Cancels editing of the row */
     onCancelEditingRow: PropTypes.func.isRequired,
+    /*
+    * Adds a new row onClick */
     onClickAddNewRow: PropTypes.func.isRequired,
+    /*
+    * Toggles between being selected and not being selected - selected means the current row can be edited */
     onClickToggleSelectedRow: PropTypes.func.isRequired,
+    /*
+    * Icon through which the row can be edited on click */
     onClickEditRowIcon: PropTypes.func,
+    /*
+    * Icon through which the row can be deleted on click */
     onClickDeleteRowIcon: PropTypes.func,
+    /*
+    * Icon through which the row can be saved on click */
     onClickSaveRow: PropTypes.func.isRequired,
+    /*
+    * a node which splits the logic between QbIconActions and IconActions - initially set to null */
     iconActionsNode: PropTypes.element
 };
 
 RowActions.defaultProps = {
+    /*
+    * Set the initial rowId to be 1 */
     rowId: 1,
+    /*
+    * Set to false so it doesn't make the row editable without being toggled to true */
     isEditing: false,
+    /*
+    * No editing row available until selected - so set to null */
     editingRowId: null,
+    /*
+    * Makes sure that the editing row is valid */
     isEditingRowValid: true,
+    /*
+    * Makes sure that the editing Row can be saved */
     isEditingRowSaving: true,
+    /*
+    * Checks whether inline edit is open - initially set to false and has to be toggled to enable it */
     isInlineEditOpen: false,
+    /*
+    * No errors present until editing starts - so set to an empty array */
     editingRowErrors: [],
+    /*
+    * Node set to null initially - so renders IconActions - if any value is passed, renders QbIconActions */
     iconActionsNode: null
 };
 
