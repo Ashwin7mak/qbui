6721286fa0886b1bc21e5175142a1fd1b3c06db7
Ken LaBak <Ken_LaBak@intuit.com>
2017-04-18 13:06:05 -0400

    Disabling form test

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -568,6 +568,7 @@ module.exports = function(grunt) {
                     './wdio/tests/forms/formAdd.e2e.spec.js',
                     './wdio/tests/forms/formEdit.e2e.spec.js',
                     './wdio/tests/forms/formDragDrop.e2e.spec.js',
+                    './wdio/tests/forms/formPermissionsParticipantRole.e2e.spec.js',
                     './wdio/tests/tables/tableCreate.e2e.spec.js',
                     './wdio/tests/tables/tableEdit.e2e.spec.js',
                     './wdio/tests/reports/reportEditRecord.e2e.spec.js',
