e7866187cbda7ed2524da1fe37b63f6b0a143142
lakshmi kamineni <skamineni@quickbase.com>
2017-04-19 10:02:45 -0400

    Enabled form add and form edit

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -565,10 +565,10 @@ module.exports = function(grunt) {
                     // permissions for viewer are not working correctly
                     './wdio/tests/forms/formPermissionsViewerRole.e2e.spec.js',
                     // currently intermittently broken in CI need to fix in another PR
-                    './wdio/tests/forms/formAdd.e2e.spec.js',
-                    './wdio/tests/forms/formEdit.e2e.spec.js',
+                    //'./wdio/tests/forms/formAdd.e2e.spec.js',
+                    //'./wdio/tests/forms/formEdit.e2e.spec.js',
                     './wdio/tests/forms/formDragDrop.e2e.spec.js',
-                    './wdio/tests/forms/formPermissionsParticipantRole.e2e.spec.js',
+                    //'./wdio/tests/forms/formPermissionsParticipantRole.e2e.spec.js',
                     './wdio/tests/tables/tableCreate.e2e.spec.js',
                     './wdio/tests/tables/tableEdit.e2e.spec.js',
                     './wdio/tests/reports/reportEditRecord.e2e.spec.js',
