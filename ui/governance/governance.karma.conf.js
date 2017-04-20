56faed22eaf68f4260f291d7a89f8837eab23903
Yogi <ynadkarni@quickbase.com>
2017-04-20 13:51:20 -0400

    MC-2077 Changed to use headless chrome as default

--- ui/governance/governance.karma.conf.js
+++ ui/governance/governance.karma.conf.js
@@ -126,7 +126,7 @@ module.exports = function(config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ["PhantomJS_Desktop"],
+        browsers: ["ChromeCanaryHeadless"],
         customLaunchers: {
             'ChromeCanaryHeadless': {
                 base: 'ChromeCanary',
--- ui/reuse/reuse.karma.conf.js
+++ ui/reuse/reuse.karma.conf.js
@@ -122,7 +122,7 @@ module.exports = function(config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ["PhantomJS_Desktop"],
+        browsers: ["ChromeCanaryHeadless"],
         customLaunchers: {
             'ChromeCanaryHeadless': {
                 base: 'ChromeCanary',
