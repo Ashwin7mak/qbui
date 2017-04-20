2772a88ca2788008c1de4b5bf84989b22d13e0c6
Yogi <ynadkarni@quickbase.com>
2017-04-20 14:04:56 -0400

    MC-2077 Reverting to PhantomJS

--- ui/reuse/reuse.karma.conf.js
+++ ui/reuse/reuse.karma.conf.js
@@ -122,7 +122,7 @@ module.exports = function(config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ["ChromeCanaryHeadless"],
+        browsers: ["PhantomJS_Desktop"],
         customLaunchers: {
             'ChromeCanaryHeadless': {
                 base: 'ChromeCanary',
