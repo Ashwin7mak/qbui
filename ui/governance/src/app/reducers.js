13ec65c69c67aa1738d7348def8066dffc3d7ba7
Sergio Haro <sergio.haro.jr@gmail.com>
2017-04-20 13:39:27 -0700

    RED-515 Support Sorting (#777)
    * updating date on legacy data
    
    * primary classes describing how to add a menu to classes
    
    * adding sort menu items
    
    * adding redux components for handling sort
    
    * adding sort transform to standard grid
    
    StandardGrid now takes more responsibility for rendering react tabular
    
    * updating the AccountUsers page to make user of the new standard grid
    
    * fixing import
    
    * adding full set of sort functions
    
    * fixing problem with removing sort
    
    * some sort styles
    
    * starting to test the grid
    
    * starting to test the grid
    
    * adding shallow standard grid test
    
    * testing grid columns
    
    * grid column testing
    
    * testing the sort for accountusers actions
    
    * testing account grid formatter
    
    * first pass in testing the updateAction

--- ui/governance/src/app/reducers.js
+++ ui/governance/src/app/reducers.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import AccountUsers from '../account/users/AccountUsersReducer';
 import RequestContext from '../common/requestContext/RequestContextReducer';
 import CommonNavReducer from '../../../reuse/client/src/components/sideNavs/commonNavReducer';
+import StandardGridReducer from '../common/grid/standardGridReducer';
 
 export default combineReducers({
     Nav: CommonNavReducer(),
+    Grids: StandardGridReducer,
     AccountUsers,
     RequestContext
 });
---
+++ ui/governance/src/common/grid/transforms/columnTransform.js
@@ -0,0 +1,27 @@
+/**
+ * Interface class to describe a transform to be performed
+ * on a set of React Tabular column definition
+ */
+class ColumnTransform {
+    /**
+     * Constructor
+     * @param grid - a reference to the StandardGrid
+     * @param props - additional properties for this class
+     */
+    constructor(grid, props) {
+        this.grid = grid;
+        this.props = props;
+    }
+
+    /**
+     * Should apply whatever transform is desired to the column
+     *
+     * @param column - the original array of columns
+     * @returns a new column definitions transformed as desired
+     */
+    apply(columns) {
+        return columns;
+    }
+}
+
+export default ColumnTransform;
