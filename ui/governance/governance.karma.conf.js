8a8a91e254557d98264b939b37934599c9252c2d
Yogi <ynadkarni@quickbase.com>
2017-04-20 09:47:42 -0400

    MC-2077 Setting default to phantomjs

--- ui/governance/governance.karma.conf.js
+++ ui/governance/governance.karma.conf.js
@@ -126,7 +126,7 @@ module.exports = function(config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ["ChromeCanaryHeadless"],
+        browsers: ["PhantomJS_Desktop"],
         customLaunchers: {
             'ChromeCanaryHeadless': {
                 base: 'ChromeCanary',
--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -131,7 +131,7 @@ module.exports = function(config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ["ChromeCanaryHeadless"],
+        browsers: ["PhantomJS_Desktop"],
         customLaunchers: {
             'ChromeCanaryHeadless': {
                 base: 'ChromeCanary',
