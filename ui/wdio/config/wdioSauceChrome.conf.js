920c6eaedc00609f7e69f16415d3bc886aaa926e
lakshmi kamineni <skamineni@quickbase.com>
2017-04-20 21:43:00 -0400

    Appium Test

--- ui/wdio/config/wdioSauceChrome.conf.js
+++ ui/wdio/config/wdioSauceChrome.conf.js
@@ -1,21 +1,36 @@
 var baseConf = require('./wdioSauce.conf');
 var config = {
     capabilities: [
+        //{
+        //    platform : 'OS X 10.11',
+        //    browserName     : 'chrome',
+        //    version: '57.0',
+        //    tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
+        //    build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - OSX Chrome Browser',
+        //    tags            : [process.env.SAUCE_JOB_NAME + '_OSX_Chrome', 'try', 'OSX', 'Chrome', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
+        //    screenResolution : '2048x1536',
+        //    // Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
+        //    idleTimeout: '180',
+        //    maxDuration: 10800,
+        //    breakpointSize: 'xlarge',
+        //    // These two values enable parallel testing which will run a spec file per instance
+        //    shardTestFiles: true,
+        //    maxInstances: 4
+        //}
         {
             platform : 'OS X 10.11',
-            browserName     : 'chrome',
-            version: '57.0',
+            browserName     : 'iphone',
+            version: '4.3',
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-            build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - OSX Chrome Browser',
-            tags            : [process.env.SAUCE_JOB_NAME + '_OSX_Chrome', 'try', 'OSX', 'Chrome', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
-            screenResolution : '2048x1536',
+            build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - OSX iPhone Browser',
+            tags            : [process.env.SAUCE_JOB_NAME + '_OSX_Chrome', 'try', 'OSX', 'iPhone', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
+            deviceOrientation : 'portrait',
             // Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
             idleTimeout: '180',
             maxDuration: 10800,
-            breakpointSize: 'xlarge',
             // These two values enable parallel testing which will run a spec file per instance
             shardTestFiles: true,
-            maxInstances: 4
+            maxInstances: 1
         }
     ]
 };
