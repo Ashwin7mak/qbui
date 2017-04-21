564bce16ee24e1c87bb856347fcf0ebf7a67392d
Brandon Stookey <bstookey@quickbase.com>
2017-04-21 13:58:54 -0400

    constants to maintain tabbing flow

---
+++ ui/client-react/src/components/formBuilder/tabindexConstants.js
@@ -0,0 +1,7 @@
+export const collapsibleMenuButtonTabIndex = 1;
+export const userMenuTabIndex = 2;
+export const toolPaletteChildrenTabIndex = 3;
+export const formChildrenTabIndex = 4;
+export const fieldPropsChildrenTabIndex = 5;
+export const cancelButtonTabIndex = 6;
+export const saveButtonTabIndex = 7;
