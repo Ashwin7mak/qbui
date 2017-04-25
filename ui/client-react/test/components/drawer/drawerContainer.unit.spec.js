9fa9f5c3925b71223e3022917ad1c59711515375
Gen Edwards <gedwards@quickbase.com>
2017-04-25 11:33:23 -0400

    MC-733:add tests to drawerContainer unit tests
    add test for non-root drawer cases

--- ui/client-react/test/components/drawer/drawerContainer.unit.spec.js
+++ ui/client-react/test/components/drawer/drawerContainer.unit.spec.js
@@ -27,6 +27,7 @@ describe('Drawer Container functions ', () => {
                 isExact: false,
             }
         };
+        const matchedUrl = "/qbase/app/app1/table/tbl1/report/rpt1/record/rcd1/sr_app_app1_table_tbl1_report_rpt2_record_rcd2";
 
         beforeEach(() => {
             spyOn(props, 'closeDrawer');
@@ -40,8 +41,26 @@ describe('Drawer Container functions ', () => {
             expect(transitionGroupContainer.length).toBe(1);
         });
 
+        it('renders a backdrop when route match occurs', () => {
+            let routeDiv = mount(
+                <MemoryRouter initialEntries={[matchedUrl]}>
+                    <DrawerContainer {...props}/>
+                </MemoryRouter>);
+            let backdrop = routeDiv.find('.closeHandleBackdrop');
+            expect(backdrop.length).toBe(1);
+        });
+
+        it('does not render a backdrop when path does not match', () => {
+            let routeDiv = mount(
+                <MemoryRouter>
+                    <DrawerContainer {...props}/>
+                </MemoryRouter>);
+            let backdrop = routeDiv.find('.closeHandleBackdrop');
+            expect(backdrop.length).toBe(0);
+        });
+
         it('test that container renders a drawer when route match occurs', () => {
-            let routeDiv = mount(<MemoryRouter initialEntries={["/qbase/app/app1/table/tbl1/report/rpt1/record/rcd1/sr_app_app1_table_tbl1_report_rpt2_record_rcd2"]}>
+            let routeDiv = mount(<MemoryRouter initialEntries={[matchedUrl]}>
                                         <DrawerContainer {...props}/>
                                     </MemoryRouter>);
             let drawerContainerWrapper = routeDiv.find('DrawerContainer');
@@ -61,7 +80,7 @@ describe('Drawer Container functions ', () => {
         });
 
         it('test that container render results in showDrawerContainer function being invoked', () => {
-            let routeDiv = mount(<MemoryRouter initialEntries={["/qbase/app/app1/table/tbl1/report/rpt1/record/rcd1/sr_app_app1_table_tbl1_report_rpt2_record_rcd2"]}>
+            let routeDiv = mount(<MemoryRouter initialEntries={[matchedUrl]}>
                 <DrawerContainer {...props}/>
             </MemoryRouter>);
             let drawerContainerWrapper = routeDiv.find('DrawerContainer');
@@ -70,5 +89,50 @@ describe('Drawer Container functions ', () => {
         });
     });
 
+    describe('Non-root drawer', () => {
+        let childProps = {
+            rootDrawer: false,
+            closeDrawer: () => {
+            },
+            match: {
+                url: "/qbase/app/app1/table/tbl1/report/rpt2/record/rcd2",
+            }
+        };
 
+        const matchedUrl = "/qbase/app/app1/table/tbl1/report/rpt2/record/rcd2/sr_app_app2_table_tbl2_report_rpt2_record_rcd2";
+        it('renders Route, DrawerContainer and TransitionGroup', () => {
+            let childRoute = mount(<MemoryRouter><DrawerContainer {...childProps}/></MemoryRouter>);
+            let childRouteContainer = childRoute.find('Route');
+            expect(childRouteContainer.length).toBe(1);
+            let childDrawerContainer = childRoute.find('.drawerContainer');
+            expect(childDrawerContainer.length).toBe(1);
+            let childTransitionGroupContainer = childRoute.find('ReactCSSTransitionGroup');
+            expect(childTransitionGroupContainer.length).toBe(1);
+        });
+
+        it('does not render backdrop', () => {
+            // only the root drawer should render a backdrop
+            let childRoute = mount(<MemoryRouter><DrawerContainer {...childProps}/></MemoryRouter>);
+            let backdrop = childRoute.find('.closeHandleBackdrop');
+            expect(backdrop.length).toBe(0);
+        });
+
+        it('test that drawer container is rendered when a match occurs', () => {
+            let childRoute = mount(
+                <MemoryRouter initialEntries={[matchedUrl]}>
+                    <DrawerContainer {...childProps}/>
+                </MemoryRouter>);
+            let drawer = childRoute.find('.drawer');
+            expect(drawer.length).toBe(1);
+        });
+
+        it('test that drawer container is not rendered when a match does not occur', () => {
+            let childRoute = mount(
+                <MemoryRouter>
+                    <DrawerContainer {...childProps}/>
+                </MemoryRouter>);
+            let drawer = childRoute.find('.drawer');
+            expect(drawer.length).toBe(0);
+        });
+    });
 });
