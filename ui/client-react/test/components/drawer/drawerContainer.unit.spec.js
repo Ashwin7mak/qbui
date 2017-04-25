3c5fe09bc45adf139fe8291324bffa7bb026cbab
Gen Edwards <gedwards@quickbase.com>
2017-04-25 10:15:37 -0400

    MC-733:wip:use mockRecordRoute for drawerContainer test

--- ui/client-react/test/components/drawer/drawerContainer.unit.spec.js
+++ ui/client-react/test/components/drawer/drawerContainer.unit.spec.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
-import {MemoryRouter} from 'react-router-dom';
-import DrawerContainer from '../../../src/components/drawer/drawerContainer';
+import {MemoryRouter, Route} from 'react-router-dom';
+import DrawerContainer, {
+    __RewireAPI__ as DrawerContainerRewireAPI
+} from '../../../src/components/drawer/drawerContainer';
 
 describe('Drawer Container functions ', () => {
     'use strict';
+
+    const mockRecordroute = (props) => <div className="mockRecordRoute"/>;
+
+    beforeEach(() => {
+        DrawerContainerRewireAPI.__Rewire__('RecordRouteWithUniqueId', mockRecordroute);
+    });
+
+    afterEach(() => {
+        DrawerContainerRewireAPI.__ResetDependency__('RecordRouteWithUniqueId');
+    });
 
     describe('Root drawer', () => {
         let props = {
@@ -67,14 +79,6 @@ describe('Drawer Container functions ', () => {
     });
 
     describe('Non-root drawer', () => {
-        let rootProps = {
-            rootDrawer: true,
-            closeDrawer: () => {
-            },
-            match: {
-                url: "/qbase/app/app1/table/tbl1/report/rpt1/record/rcd1",
-            }
-        };
         let childProps = {
             rootDrawer: false,
             closeDrawer: () => {
@@ -83,40 +87,45 @@ describe('Drawer Container functions ', () => {
                 url: "/qbase/app/app1/table/tbl1/report/rpt2/record/rcd2",
             }
         };
-
-        beforeEach(() => {
-            spyOn(rootProps, 'closeDrawer');
-            spyOn(childProps, 'closeDrawer');
-        });
-
-        afterEach(() => {
-            if (rootProps.closeDrawer()) {
-                rootProps.closeDrawer().calls.reset();
+        let matchedChildProps = {
+            rootDrawer: false,
+            closeDrawer: () => {
+            },
+            match: {
+                url: "/qbase/app/app1/table/tbl1/report/rpt2/record/rcd2",
             }
-            if (childProps.closeDrawer()) {
-                childProps.closeDrawer().calls.reset();
-            }
+        };
+
+        ///sr_app_app2_table_tbl2_report_rpt2_record_rcd2
+        //path: "/qbase/app/:appId/table/:tblId/(report)?/:rptId?/record/:recordId",
+
+        it('renders Route, DrawerContainer and TransitionGroup', () => {
+            let childRoute = mount(<MemoryRouter><DrawerContainer {...childProps}/></MemoryRouter>);
+            let childRouteContainer = childRoute.find('Route');
+            expect(childRouteContainer.length).toBe(1);
+            let childDrawerContainer = childRoute.find('DrawerContainer');
+            expect(childDrawerContainer.length).toBe(1);
+            let childTransitionGroupContainer = childRoute.find('ReactCSSTransitionGroup');
+            expect(childTransitionGroupContainer.length).toBe(1);
         });
 
-        it('test render of drawer container, drawer and transition group', () => {
-            // let parentRoute = mount(<DrawerContainer {...rootProps}/>);
-            // let parentRouteContainer = parentRoute.find('Route');
-            // expect(parentRouteContainer.length).toBe(1);
-            // let parentDrawerContainer = parentRouteContainer.find('DrawerContainer');
-            // expect(parentDrawerContainer.length).toBe(1);
-            // let parentTransitionGroupContainer = parentDrawerContainer.find('ReactCSSTransitionGroup');
-            // expect(parentTransitionGroupContainer.length).toBe(1);
-            //
-            // let childRoute = parentRoute.mount(<DrawerContainer {...childProps}/>);
-            // let childRouteContainer = childRoute.find('Route');
-            // expect(childRouteContainer.length).toBe(1);
-            // let childDrawerContainer = childRouteContainer.find('DrawerContainer');
-            // expect(childDrawerContainer.length).toBe(1);
-            // let childTransitionGroupContainer = childDrawerContainer.find('ReactCSSTransitionGroup');
-            // expect(childTransitionGroupContainer.length).toBe(1);
+        it('does not render backdrop', () => {
+            // only the root drawer should render a backdrop
+            let childRoute = mount(<MemoryRouter><DrawerContainer {...childProps}/></MemoryRouter>);
+            let backdrop = childRoute.find('.closeHandleBackdrop');
+            expect(backdrop.length).toBe(0);
         });
 
-        it('test that show drawer container function is invoked', () => {
+        it('test that drawer container is rendered when a match occurs', () => {
+            let childRoute = mount(
+                <MemoryRouter initialEntries={["/qbase/app/app1/table/tbl1/report/rpt2/record/rcd2/sr_app_app2_table_tbl2_report_rpt2_record_rcd2"]}>
+                    <DrawerContainer {...matchedChildProps}/>
+                </MemoryRouter>);
+            let drawer = childRoute.find('.drawer');
+            expect(drawer.length).toBe(1);
+        });
+
+        it('test that drawer container is not rendered when a match does not occur', () => {
             // let parentRoute = mount(<DrawerContainer {...rootProps}/>);
             // let childRoute = parentRoute.mount(<DrawerContainer {...childProps}/>);
             // let childRouteContainer = childRoute.find('Route');
@@ -141,6 +150,43 @@ describe('Drawer Container functions ', () => {
             // let childDrawerContainer = childRouteContainer.find('DrawerContainer');
             // childDrawerContainer.unmount();
             // expect(childProps.closeDrawer).toHaveBeenCalled();
+        });
+
+        xit('test render of drawer container, drawer and transition group', () => {
+            let routeDiv = mount(<MemoryRouter><DrawerContainer {...props}/></MemoryRouter>);
+            let drawerContainerWrapper = routeDiv.find('DrawerContainer');
+            expect(drawerContainerWrapper.length).toBe(1);
+            let transitionGroupContainer = drawerContainerWrapper.find('ReactCSSTransitionGroup');
+            expect(transitionGroupContainer.length).toBe(1);
+        });
+
+        it('test that container calls getDrawer function when route match occurs', () => {
+            // blah
+        });
+
+
+        it('test that container render results in showDrawerContainer function being invoked', () => {
+            // let routeDiv = mount(<MemoryRouter><DrawerContainer {...props}/></MemoryRouter>);
+            // let drawerContainerWrapper = routeDiv.find('DrawerContainer');
+            // expect(drawerContainerWrapper.length).toBe(1);
+            // spyOn(drawerContainerWrapper.node, 'getDrawer');
+            // expect(drawerContainerWrapper.node.getDrawer).toHaveBeenCalled();
+            // // drawerContainerWrapper.node.showDrawerContainer().calls.reset();
+        });
+
+        it('test that hideDrawerContainer function is invoked when contained drawer is unmounted', () => {
+            // let routeDiv = mount(<MemoryRouter><DrawerContainer {...props}/></MemoryRouter>);
+            // let drawerContainerWrapper = routeDiv.find('DrawerContainer');
+            // drawerContainerWrapper.unmount();
+            // expect(drawerContainerWrapper.hideDrawerContainer).toHaveBeenCalled();
+        });
+
+
+        it('test that closeDrawer function is invoked', () => {
+            // let routeDiv = mount(<MemoryRouter><DrawerContainer {...props}/></MemoryRouter>);
+            // let drawerContainerWrapper = routeDiv.find('DrawerContainer');
+            // drawerContainerWrapper.unmount();
+            // expect(props.closeDrawer).toHaveBeenCalled();
         });
 
     });
