9dba31875eacd2fcc6e104ab9582805c60f721b7
kunjan <kunjan@kshetri.com>
2017-04-25 09:49:40 -0400

    adding search box

--- ui/governance/src/account/users/AccountUsersToolBar/AccountUsersToolBar.js
+++ ui/governance/src/account/users/AccountUsersToolBar/AccountUsersToolBar.js
@@ -13,6 +13,14 @@ class AccountUsersToolBar extends React.Component {
     render() {
         return (
             <div>
+                <div className="filterSearchBoxContainer">
+                    <SearchBox className="filterSearchBox"
+                               searchBoxKey={"filterSearchBox_" + this.props.searchBoxKey}
+                               value={this.props.search.searchInput}
+                               onChange={this.props.onChange}
+                               onClearSearch={this.props.clearSearchString}
+                               placeholder={"Search Users"} />
+                </div>
                 <AccountUsersNavigation id={this.props.id} totalRecords={this.props.totalRecords}/>
             </div>
         );
