72c07e64898e3086f9c034c48348ce8c3fddb7c3
jondavidnelson <jnelson@quickbase.com>
2017-04-26 06:37:07 -0400

    MC-1765: removed obsolete FormDragDrop test from gruntfile

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -571,7 +571,6 @@ module.exports = function(grunt) {
                     './wdio/tests/forms/formPermissionsViewerRole.e2e.spec.js',
                     './wdio/tests/forms/formPermissionsParticipantRole.e2e.spec.js',
                     // currently intermittently broken in CI need to fix in another PR
-                    './wdio/tests/forms/formDragDrop.e2e.spec.js',
                     './wdio/tests/reports/reportEditRecord.e2e.spec.js',
                     './wdio/tests/reports/reportNavigation.e2e.spec.js',
                     './wdio/tests/reports/reportTable.e2e.spec.js',
