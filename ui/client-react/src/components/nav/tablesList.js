4d89746db84085c80b0e401e1cfeec51a55efd1e
Goel, Aditi <agoel@quickbase.com>
2017-04-28 11:20:27 -0400

    MC-1801: Alignment of New table button icon; safari doesn't render the icon picker on table creation panel properly; on settings page for small breakpoint left nav collapse hamburger doesn't work

--- ui/client-react/src/components/nav/tablesList.js
+++ ui/client-react/src/components/nav/tablesList.js
@@ -144,9 +144,9 @@ let TablesList = React.createClass({
 
         return (
             <li className="newTableItem link" key="newTable">
-                <div className="newTable" onClick={this.props.onCreateNewTable}>
-                    <QBicon icon="add-mini"/><I18nMessage message="tableCreation.newTablePageTitle"/>
-                </div>
+                <a className="newTable leftNavLink" onClick={this.props.onCreateNewTable}>
+                    <QBicon icon="add-mini"/><span className="leftNavLabel"><I18nMessage message="tableCreation.newTablePageTitle"/></span>
+                </a>
             </li>);
     },
 
