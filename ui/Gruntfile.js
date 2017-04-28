2cd0fabfe93dfdb8bdbc232337c5fb8aa6b0dafb
suryalakhan <slakhan@quickbase.com>
2017-04-28 16:36:47 -0400

    enabled usersTable.e2e.spec.js

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -614,7 +614,7 @@ module.exports = function(grunt) {
                 // Use the wdioSauce.conf.js file setting the options above
                 configFile: './wdio/config/' + wdioSauceConfig,
                 // Make sure there are no spaces between test suites here
-                suite: 'reports,forms,tables'
+                suite: 'reports,forms,tables,users'
             }
         },
 
