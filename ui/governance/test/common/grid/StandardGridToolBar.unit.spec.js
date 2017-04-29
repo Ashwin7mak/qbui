e29831843221098b9f9bc6c64c2612057804b547
kunjan <kunjan@kshetri.com>
2017-04-29 17:26:15 -0400

    standardgrid toolbar tests

--- ui/governance/test/common/grid/StandardGridToolBar.unit.spec.js
+++ ui/governance/test/common/grid/StandardGridToolBar.unit.spec.js
@@ -1,46 +1,42 @@
 import React from "react";
-import {mount, shallow} from "enzyme";
+import {mount} from "enzyme";
 import jasmineEnzyme from "jasmine-enzyme";
 import * as Actions from "../../../src/account/users/AccountUsersActions";
-import {StandardGridToolBar} from "../../../src/common/grid/toolbar/StandardGridToolbar";
-import {StandardGridNavigation} from "../../../src/common/grid/toolbar/StandardGridNavigation";
-import IconInputBox from "../../../../reuse/client/src/components/iconInputBox/iconInputBox";
+import StandardGridToolBar from "../../../src/common/grid/toolbar/StandardGridToolbar";
+import StandardGridNavigation from "../../../src/common/grid/toolbar/StandardGridNavigation";
 import {Provider} from "react-redux";
 import configureMockStore from "redux-mock-store";
 
 describe('StandardGridToolBar', () => {
 
+    const mockStore = configureMockStore();
     beforeEach(() => {
         jasmineEnzyme();
     });
 
     it('should render with navigation component and search component', () => {
 
-        // let component = mount(
-        //     <Provider store={store}>
-        //         <StandardGridToolBar {...baseProps} />
-        //     </Provider>);
-        //
-        // let StandardGridToolBarShallow = shallow(
-        //     <StandardGridToolBar
-        //         doUpdate={Actions.doUpdate}
-        //         id={"accountUsers"}
-        //         rowKey={"uid"}
-        //     />
-        // );
-        // expect(StandardGridToolBarShallow).toBeDefined();
-        // expect(StandardGridToolBarShallow.length).toBeTruthy();
+        let component = mount(
+            <Provider store={mockStore({Grids : 1})}>
+                <StandardGridToolBar
+                    doUpdate={Actions.doUpdate}
+                    id={"accountUsers"}
+                    rowKey={"uid"}
+                />
+            </Provider>);
 
-        // let StandardGridNavigationComponent = StandardGridToolBarShallow.find(StandardGridNavigation);
-        // expect(StandardGridNavigationComponent).toBeDefined();
-        // expect(StandardGridNavigationComponent.length).toBeTruthy();
-        // expect(StandardGridNavigationComponent.props().id).toEqual("accountUsers");
-        // let StandardGridSearchComponent = StandardGridToolBarShallow.find(IconInputBox);
-        // expect(StandardGridToolBarShallow.debug()).toEqual("sf");
-        // expect(StandardGridSearchComponent).toBeDefined();
-        // expect(StandardGridSearchComponent.length).toBeTruthy();
-        // expect(StandardGridSearchComponent.props().placeholder).toEqual("Search users");
-        // expect(StandardGridSearchComponent.props().onChange).toEqual(Actions.doUpdate);
+        expect(component).toBeDefined();
+        expect(component.length).toBeTruthy();
+
+        let StandardGridNavigationComponent = component .find(StandardGridNavigation);
+        expect(StandardGridNavigationComponent).toBeDefined();
+        expect(StandardGridNavigationComponent.length).toBeTruthy();
+        expect(StandardGridNavigationComponent.props().id).toEqual("accountUsers");
+
+        let StandardGridSearchComponent = component.find("input");
+        expect(StandardGridSearchComponent.props().placeholder).toEqual("Search users");
+        expect(StandardGridSearchComponent).toBeDefined();
+        expect(StandardGridSearchComponent.length).toBeTruthy();
 
     });
 });
