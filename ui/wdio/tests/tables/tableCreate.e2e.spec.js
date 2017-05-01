a9515a0ef61612784f066461e5c0f229cdc3c336
agade <agade@quickbase.com>
2017-05-01 12:21:35 -0400

    revert a change for tableCreate test: the test is failing locally, lets wait for fix (a couple people are working on this test)

--- ui/wdio/tests/tables/tableCreate.e2e.spec.js
+++ ui/wdio/tests/tables/tableCreate.e2e.spec.js
@@ -256,7 +256,7 @@
 
             //Add user to participant appRole
             browser.call(function() {
-                return e2eBase.recordBase.apiBase.assignUsersToAppRole(testApp.id, e2eConsts.PARTICIPANT_ROLEID, [userId]);
+                return e2eBase.recordBase.apiBase.assignUsersToAppRole(testApp.id, "11", [userId]);
             });
 
             //get the user authentication
