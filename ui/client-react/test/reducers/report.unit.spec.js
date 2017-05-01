d1c977eae0e5bda3a1b7c02b6b08cb71963febf0
jzybert <jzybert@quickbase.com>
2017-05-01 11:14:04 -0400

    MC-1716 CLOSE_FIELD_SELECTOR reducer tests

--- ui/client-react/test/reducers/report.unit.spec.js
+++ ui/client-react/test/reducers/report.unit.spec.js
@@ -1096,7 +1096,104 @@ describe('Report reducer functions', () => {
     });
 
     describe('Report reducer CLOSE_FIELD_SELECTOR test correct state', () => {
+        let contextId = "CLOSE_FIELD_SELECTOR";
+        let initialStateWithoutPlaceholder = [
+            {
+                id: contextId,
+                data:
+                    {
+                        columns: [
+                            {
+                                fieldDef: {
+                                    id: 6
+                                },
+                                isHidden: false,
+                                isPlaceholder: false,
+                                id: 6
+                            },
+                            {
+                                fieldDef: {
+                                    id: 7
+                                },
+                                isHidden: false,
+                                isPlaceholder: false,
+                                id: 7
+                            }
+                        ],
+                        fids: [6, 7],
+                        metaData: {
+                            fids: [6, 7]
+                        }
+                    }
+            }
+        ];
+        let initialStateWithPlaceholder = [
+            {
+                id: contextId,
+                data:
+                    {
+                        columns: [
+                            {
+                                fieldDef: {
+                                    id: 6
+                                },
+                                isHidden: false,
+                                isPlaceholder: false,
+                                id: 6
+                            },
+                            {
+                                fieldDef: {
+                                    id: 7
+                                },
+                                isHidden: false,
+                                isPlaceholder: true,
+                                id: 7
+                            }
+                        ],
+                        fids: [6, 7],
+                        metaData: {
+                            fids: [6, 7]
+                        }
+                    }
+            }
+        ];
+        let testCases = [
+            {
+                description: 'when placeholder does not exist in the columns the state remains the same',
+                initialState: initialStateWithoutPlaceholder,
+                content : {},
+                expects : (testState) => {
+                    expect(Array.isArray(testState)).toEqual(true);
+                    expect(testState[0].data.columns.length).toEqual(2);
+                    expect(testState[0].data.columns[0].id).toEqual(6);
+                    expect(testState[0].data.columns[1].id).toEqual(7);
+                }
+            },
+            {
+                description: 'when placeholder does exist in the columns that column is removed',
+                initialState: initialStateWithPlaceholder,
+                content : {},
+                expects : (testState) => {
+                    expect(Array.isArray(testState)).toEqual(true);
+                    expect(testState[0].data.columns.length).toEqual(1);
+                    expect(testState[0].data.columns[0].id).toEqual(6);
+                }
+            }
+        ];
 
+        testCases.forEach(testCase => {
+            it(testCase.description, () => {
+                let testState = testCase.initialState;
+                actionObj.type = types.CLOSE_FIELD_SELECTOR;
+                actionObj.id = contextId;
+                if (testCase.content) {
+                    actionObj.content = testCase.content;
+                }
+                testState = reducer(testState, actionObj);
+
+                testCase.expects(testState);
+            });
+        });
     });
 
     describe('Report reducer INVALID_ACTION test correct state', () => {
