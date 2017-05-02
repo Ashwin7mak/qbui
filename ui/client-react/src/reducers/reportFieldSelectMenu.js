ba41372d215f2f50c6d339c049426c1dd4223c44
jzybert <jzybert@quickbase.com>
2017-05-02 15:18:36 -0400

    MC-1716 code standards

--- ui/client-react/src/reducers/reportFieldSelectMenu.js
+++ ui/client-react/src/reducers/reportFieldSelectMenu.js
@@ -9,34 +9,34 @@ const reportFieldSelectMenu = (
     }, action) => {
 
     switch (action.type) {
-        case types.REFRESH_FIELD_SELECT_MENU: {
-            let fields = action.content.response.data;
-            let fids = fields.map(field => {
-                return field.id;
-            });
-            let columns = ReportModelHelper.getReportColumns(fields, fids);
-            return {
-                ...state,
-                availableColumns: columns
-            };
-        }
-        case types.OPEN_FIELD_SELECT_MENU: {
-            return {
-                ...state,
-                isCollapsed: false,
-                addBeforeColumn: action.content.addBeforeColumn
-            };
-        }
-        case types.CLOSE_FIELD_SELECT_MENU: {
-            return {
-                ...state,
-                isCollapsed: true,
-                addBeforeColumn: action.content.addBeforeColumn
-            };
-        }
-        default:
-            // return existing state by default in redux
-            return state;
+    case types.REFRESH_FIELD_SELECT_MENU: {
+        let fields = action.content.response.data;
+        let fids = fields.map(field => {
+            return field.id;
+        });
+        let columns = ReportModelHelper.getReportColumns(fields, fids);
+        return {
+            ...state,
+            availableColumns: columns
+        };
+    }
+    case types.OPEN_FIELD_SELECT_MENU: {
+        return {
+            ...state,
+            isCollapsed: false,
+            addBeforeColumn: action.content.addBeforeColumn
+        };
+    }
+    case types.CLOSE_FIELD_SELECT_MENU: {
+        return {
+            ...state,
+            isCollapsed: true,
+            addBeforeColumn: action.content.addBeforeColumn
+        };
+    }
+    default:
+        // return existing state by default in redux
+        return state;
     }
 };
 
