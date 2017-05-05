559a10bf9738822c128705f4d7916e8495903694
lakshmi kamineni <skamineni@quickbase.com>
2017-05-05 11:49:19 -0400

    reverted

--- ui/wdio/config/wdioSauceSafari.conf.js
+++ ui/wdio/config/wdioSauceSafari.conf.js
@@ -17,20 +17,35 @@ var config = {
         //    shardTestFiles: true,
         //    maxInstances: 5
         //}
+        //{
+        //    maxInstances: 1,
+        //    //For IPAD capabilities
+        //    appiumVersion: '1.6.4',
+        //    platformName: 'Android',
+        //    platformVersion: '5.0',
+        //    browserName: '',
+        //    deviceName: 'Android Emulator',
+        //    deviceType: 'tablet',
+        //    deviceOrientation: 'landscape',
+        //    automationName: 'Selendroid',
+        //    tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
+        //    build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - Android Chrome Browser',
+        //    tags            : [process.env.SAUCE_JOB_NAME + '_Android_Safari', 'try', 'Android', 'Chrome', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
+        //}
         {
             maxInstances: 1,
             //For IPAD capabilities
+            platform: 'OS X 10.11',
+            deviceName: 'iPad 2',
             appiumVersion: '1.6.4',
-            platformName: 'Android',
-            platformVersion: '5.0',
-            browserName: '',
-            deviceName: 'Android Emulator',
-            deviceType: 'tablet',
+            platformName: 'iOS',
+            platformVersion: '9.2',
+            browserName: 'safari',
             deviceOrientation: 'landscape',
-            automationName: 'Selendroid',
+            automationName: 'Appium',
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-            build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - Android Chrome Browser',
-            tags            : [process.env.SAUCE_JOB_NAME + '_Android_Safari', 'try', 'Android', 'Chrome', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
+            build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - iOS Safari Browser',
+            tags            : [process.env.SAUCE_JOB_NAME + '_iOS_Safari', 'try', 'iOS', 'Safari', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
         }
     ]
 };
