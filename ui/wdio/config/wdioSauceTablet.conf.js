ce1b7c6a03be9e4fbe3a60d55915d22b30162582
lakshmi kamineni <skamineni@quickbase.com>
2017-05-05 21:59:52 -0400

    Android

--- ui/wdio/config/wdioSauceTablet.conf.js
+++ ui/wdio/config/wdioSauceTablet.conf.js
@@ -3,7 +3,7 @@ var config = {
     capabilities: [
         {
             maxInstances: 5,
-            //For tablet capabilities
+            //For Android
             browserName: 'Chrome',
             appiumVersion: '1.6.4',
             platformName: 'Android',
@@ -14,23 +14,23 @@ var config = {
             automationName: 'Appium',
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
             build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - Android Chrome Browser',
-            tags            : [process.env.SAUCE_JOB_NAME + '_Android_Safari', 'try', 'Android', 'Chrome', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
+            tags            : [process.env.SAUCE_JOB_NAME + '_Android_Chrome', 'try', 'Android', 'Chrome', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
         },
-        //{
-        //    maxInstances: 5,
-        //    //For IPAD capabilities
-        //    appiumVersion: '1.6.4',
-        //    deviceName: 'iPad Air 2',
-        //    deviceOrientation: 'landscape',
-        //    platformVersion: '9.3',
-        //    platformName: 'iOS',
-        //    networkConnectionEnabled: 'true',
-        //    browserName: 'safari',
-        //    automationName: 'Appium',
-        //    tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-        //    build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - iOS Safari Browser',
-        //    tags            : [process.env.SAUCE_JOB_NAME + '_iOS_Safari', 'try', 'iOS', 'Safari', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
-        //}
+        {
+            maxInstances: 5,
+            //For iOS
+            appiumVersion: '1.6.4',
+            deviceName: 'iPad Air 2',
+            deviceOrientation: 'landscape',
+            platformVersion: '9.3',
+            platformName: 'iOS',
+            networkConnectionEnabled: 'true',
+            browserName: 'safari',
+            automationName: 'Appium',
+            tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
+            build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - iOS Safari Browser',
+            tags            : [process.env.SAUCE_JOB_NAME + '_iOS_Safari', 'try', 'iOS', 'Safari', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
+        }
     ]
 };
 var exportConfig = Object.assign(baseConf.config, config);
