b0b5083ad504dd93f7fe910d587d365cd52bf517
lakshmi kamineni <skamineni@quickbase.com>
2017-05-05 21:26:29 -0400

    Android

--- ui/wdio/config/wdioSauceTablet.conf.js
+++ ui/wdio/config/wdioSauceTablet.conf.js
@@ -4,13 +4,14 @@ var config = {
         {
             maxInstances: 5,
             //For tablet capabilities
-            browserName: 'android',
+            browserName: 'Chrome',
             appiumVersion: '1.6.4',
             platformName: 'Android',
-            deviceName: 'Amazon Kindle Fire HD 8.9 GoogleAPI Emulator',
+            deviceName: 'Android GoogleAPI Emulator',
+            devideType: 'tablet',
             deviceOrientation: 'potrait',
-            platformVersion: '4.4',
-            automationName: 'Selendroid',
+            platformVersion: '6.0',
+            automationName: 'Appium',
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
             build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - Android Chrome Browser',
             tags            : [process.env.SAUCE_JOB_NAME + '_Android_Safari', 'try', 'Android', 'Chrome', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
