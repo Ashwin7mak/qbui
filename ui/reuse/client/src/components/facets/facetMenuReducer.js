980b04abeb7c103d65798d72ef20d56bd4c2a10a
Arun Manohar <amanohar@quickbase.com>
2017-05-09 16:22:22 -0400

    Mc 1101 Reusable filter menu (#922)
    * Add facetProps
    
    * Move facetsAspect from client-react to reuse
    
    * Create a stub component FacetsAspect.js in client-react
    
    * Create a stub component in client-react
    
    * Refactor FacetsAspects
    
    * Move facetsSelection to reuse from client-react
    
    * Create a stub component inside reuse for facetSelections.js
    
    * Refactor facetSelections.js
    
    * remove white spaces in facetSelections.js
    
    * MC-1101 move SCSS file
    
    * MC-1101 move more facets components to the reuse library
    
    * MC-1101 Update imports
    
    * Create FacetsMenu stub and move FilterSearchBox
    
    * MC-1101 stub FilterSearchBox
    
    * Move facetMenuActions from client-react to reuse
    
    * Create a stub facetMenuActions inside client-react
    
    * Refactor facetMenuActions to import actions from client-react
    
    * MC-1101 move unit tests to reuse library
    
    * MC-1101 update tests, move FilterSearchBox test
    
    * MC-1101 remove unneeded stubs and move remaining tests to reuse library
    
    * Add actions and reducers for Facets
    
    * Refactor reducer
    
    * Refactor component to include redux store
    
    * testing redux
    
    * making headway on redux, kind of working state
    
    * working redux, changing show boolean
    
    * Working to get facetsexpanded
    
    * Fix payload
    
    * Fix report toolbar tests
    
    * MC-1101 add facet menu component to reuse library website
    
    * Make facets render step by step
    
    * Test cases for actions and reducers
    
    * Test cases for actions and reducers are in
    
    * Get rid of commented lines
    
    * Add default props from getDefaultProps
    
    * Create contextTypes
    
    * lint fixes
    
    * Add LOAD_REPORT type
    
    * Remove flux from test cases
    
    * Test cases
    
    * Fix typo
    
    * Test cases
    
    * Refactor test cases
    
    * Refactor test cases
    
    * Test cases
    
    * Add LOAD_REPORT reducer
    
    * Fix alignments
    
    * Remove Facets Menu from component library for now
    
    * Remove unused imports of actions and reducers
    
    * Fix tests
    
    * Fixing tests
    
    * Trying to fix popover shown on rerender
    
    * Fix
    
    * working reducer
    
    * Remove LOAD_REPORT
    
    * Checkout report.spec.js from master
    
    * Revert "Refactor facetSelections.js"
    
    This reverts commit a7d31098d4c5d59cd3d62f1e76df8cfc1929fa4d.
    
    * fix broken unit tests
    
    * Generic Facets Menu Component
    
    * Add imports to generic facets
    
    * Create generic facets
    
    * Reusable component revision 1
    
    * MC-1101 Fix factes menu rendering
    
    * Fix rendering of facets v4
    
    * Fix iteration v5
    
    * Fix iteration v6
    
    * Remove console.logs
    
    * Lint Fixes
    
    * Refactor and remove unused files
    
    * Cleanup
    
    * Cleanup
    
    * Code cleanup and comments
    
    * Code cleanup
    
    * Fix reducer tests
    
    * Cleanup and writing test cases
    
    * Cleanup and fix imports
    
    * Comments for payload
    
    * Fix typo's
    
    * Change .map to .forEach as data is pushed to a new array
    
    * Lint fixes
    
    * MC-1101 Fix lint errors
    
    * MC-1101 Documentation and cleanup of genericFacetMenu components
    
    * MC-1101 Fix unit tests for client-react facets menu
    
    * MC-1101 Revert component library additions and move tests to their own folder
    
    * MC-1101 Fix unit tests for facet menus. Add additional unit test for genericFacetMenu
    
    * MC-1101 Re-enable reportToolbar tests
    
    * MC-1101 Additional tests for reusable facet menu and facet item
    
    * MC-1101 Add misc tests to get coverage up
    
    * Lint fixes
    
    * MC-1101 Fix lint errors
    
    * MC-1101 Remove Component library files (to be added later)
    
    * Typo fix
    
    * Fat arrow for scoping and some notes

---
+++ ui/reuse/client/src/components/facets/facetMenuReducer.js
@@ -0,0 +1,45 @@
+import * as types from './facetMenuTypes';
+import _ from 'lodash';
+
+let payload;
+
+let initialState = {
+    show: false,
+    expandedFacetFields : [],
+    moreRevealedFacetFields :[]
+};
+
+const facetMenuReducer = (state = initialState, action) => {
+
+    let newState = _.cloneDeep(state);
+    switch (action.type) {
+
+    case types.SHOW_FACET_MENU: {
+        newState.show = true;
+        return newState;
+    }
+
+    case types.HIDE_FACET_MENU: {
+        newState.show = false;
+        return newState;
+    }
+
+    case types.SET_FACETS_EXPANDED: {
+        newState.expandedFacetFields = action.payload.expanded;
+        return newState;
+    }
+
+    case types.SET_FACETS_MORE_REVEALED: {
+        newState.moreRevealedFacetFields = action.payload.moreRevealed;
+        return newState;
+    }
+
+    default:
+        return state;
+    }
+};
+
+export const getExpandedFacetFields = state => state.facets.expandedFacetFields;
+export const getMoreRevealedFacetFields = state => state.facets.moreRevealedFacetFields;
+
+export default facetMenuReducer;
