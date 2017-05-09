1dc601ea74414eef35fe81905e5a47b76c86b410
lakshmi kamineni <skamineni@quickbase.com>
2017-05-09 08:50:46 -0400

    excluded some files for iOS

--- ui/wdio/config/wdioSauceTablet.conf.js
+++ ui/wdio/config/wdioSauceTablet.conf.js
@@ -30,6 +30,13 @@ var config = {
             tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
             build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - iOS Safari Browser',
             tags            : [process.env.SAUCE_JOB_NAME + '_iOS_Safari', 'try', 'iOS', 'Safari', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
+            exclude: [
+                //TODO there are bugs associated with these in jira
+                './wdio/tests/reports/reportDeleteRecord.e2e.spec.js',
+                './wdio/tests/reports/reportEditRecord.e2e.spec.js',
+                './wdio/tests/reports/grouping/reportGroupingViaColumnHeader.e2e.spec.js',
+                './wdio/tests/reports/sorting/reportSortingViaColumnHeader.e2e.spec.js',
+            ]
         }
     ]
 };
