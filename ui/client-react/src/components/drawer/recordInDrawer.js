00122ff8c763fb0e857e65a9212f3dde59119ad1
Gen Edwards <gedwards@quickbase.com>
2017-05-15 13:42:39 -0400

    MC-988:stateless functional record/reportInDrawer

--- ui/client-react/src/components/drawer/recordInDrawer.js
+++ ui/client-react/src/components/drawer/recordInDrawer.js
@@ -1,35 +1,18 @@
 import React from 'react';
-import _ from 'lodash';
 
 import DrawerContainer from './drawerContainer';
 import {RecordRouteWithUniqueId} from '../record/recordRoute';
 
-// const RecordInDrawer = (props) => (
-//     <DrawerContainer {...props}>
-//         <RecordRouteWithUniqueId
-//             {...props}
-//             isDrawerContext={true}
-//             hasDrawer={true}
-//         />
-//     </DrawerContainer>);
-
-class RecordInDrawer extends React.Component {
-    componentWillUpdate(nextProps, nextState) {
-        console.log(_.differenceWith(this.props, nextProps, _.isEqual));
-    }
-    componentWillReceiveProps(nextProps) {
-        console.log(_.differenceWith(this.props, nextProps, _.isEqual));
-    }
-    render() {
-        return (
-            <DrawerContainer {...this.props}>
-                <RecordRouteWithUniqueId
-                    {...this.props}
-                    isDrawerContext={true}
-                    hasDrawer={true}
-                />
-            </DrawerContainer>);
-    }
-}
+/**
+ * Renders a RecordRoute component inside a drawer. See DrawerContainer.
+ */
+const RecordInDrawer = (props) => (
+    <DrawerContainer {...props}>
+        <RecordRouteWithUniqueId
+            {...props}
+            isDrawerContext={true}
+            hasDrawer={true}
+        />
+    </DrawerContainer>);
 
 export default RecordInDrawer;
--- ui/client-react/src/components/drawer/reportInDrawer.js
+++ ui/client-react/src/components/drawer/reportInDrawer.js
@@ -1,30 +1,22 @@
 import React from 'react';
-import _ from 'lodash';
 
 import DrawerContainer from './drawerContainer';
 import {ReportRouteWithUniqueId} from '../report/reportRoute';
 
-class ReportInDrawer extends React.Component {
-    componentWillUpdate(nextProps, nextState) {
-        console.log(_.differenceWith(this.props, nextProps, _.isEqual));
-    }
-    componentWillReceiveProps(nextProps) {
-        console.log(_.differenceWith(this.props, nextProps, _.isEqual));
-    }
-    render() {
-        return (
-            <DrawerContainer
-                direction="bottom"
-                renderBackdrop={false}
-                rootDrawer={!this.props.isDrawerContext}
-                pathToAdd="/sr_report_app_:appId([A-Za-z0-9]+)_table_:tblId([A-Za-z0-9]+)_report_:rptId([A-Za-z0-9]+)_dtFid_:detailKeyFid([A-Za-z0-9]+)_dtVal_:detailKeyValue([A-Z-a-z0-9]+)"
-            >
-                <ReportRouteWithUniqueId
-                    isDrawerContext={true}
-                    hasDrawer={true}
-                />
-            </DrawerContainer>);
-    }
-}
+/**
+ * Renders a ReportRoute component inside a drawer. See DrawerContainer.
+ */
+const ReportInDrawer = (props) => (
+    <DrawerContainer
+        direction="bottom"
+        renderBackdrop={false}
+        rootDrawer={!props.isDrawerContext}
+        pathToAdd="/sr_report_app_:appId([A-Za-z0-9]+)_table_:tblId([A-Za-z0-9]+)_report_:rptId([A-Za-z0-9]+)_dtFid_:detailKeyFid([A-Za-z0-9]+)_dtVal_:detailKeyValue([A-Z-a-z0-9]+)"
+    >
+        <ReportRouteWithUniqueId
+            isDrawerContext={true}
+            hasDrawer={true}
+        />
+    </DrawerContainer>);
 
 export default ReportInDrawer;
