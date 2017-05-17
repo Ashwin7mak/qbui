631793aa9e1a7141e7395c8074c16eaaa64246e7
Perikal <cperikal@cmbl130e9e44d.corp.quickbase.net>
2017-05-17 11:44:19 -0400

    leftNav mobile test

--- ui/wdio/tests/global/globalLeftNav.e2e.spec.js
+++ ui/wdio/tests/global/globalLeftNav.e2e.spec.js
@@ -178,7 +178,7 @@
 
         });
 
-        //TODO: MC - 2799 need to be fixed for the below test to pass, Mouse hover on app icon in apps page is not displayin
+        //TODO: MC - 2799 need to be fixed for the below test to pass, Mouse hover on app icon in apps page is not displaying the app name when we have collapsed leftNav
 
         xit('Verify the mouse hover function on apps page collapsed leftNav', function() {
 
