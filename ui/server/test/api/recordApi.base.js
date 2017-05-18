574188a027e916d1d4fc1dcf5a638bb4db6c9d41
Bohra <kbohra@cmbl0712a6ca0.corp.quickbase.net>
2017-05-18 18:07:03 -0400

    MC-1811 Fix the failing integration test. Removed unused unit test.

--- ui/server/test/api/recordApi.base.js
+++ ui/server/test/api/recordApi.base.js
@@ -30,13 +30,13 @@
         }
 
         var recordBase = {
-            apiBase           : apiBase,
+            apiBase: apiBase,
             //delegate to apiBase to initialize
-            initialize        : function() {
+            initialize: function() {
                 init = apiBase.initialize();
             },
             //set the baseUrl we want to use to reach out for testing
-            setBaseUrl        : function(baseUrlConfig) {
+            setBaseUrl: function(baseUrlConfig) {
                 apiBase.setBaseUrl(baseUrlConfig);
             },
             //Helper method to create an app, can be used by multiple test cases
@@ -46,11 +46,11 @@
                     return apiBase.executeRequest(apiBase.resolveAppsEndpoint(), consts.POST, appToCreate).then(function(appResponse) {
                         let createdApp = JSON.parse(appResponse.body);
                         log.debug('App create response: ' + JSON.stringify(createdApp));
-                        var initTablePropsPromises = [];
-                        createdApp.tables.forEach(function(table, index) {
-                            initTablePropsPromises.push(self.initTableProperties(createdApp.id, table.id, table.name));
-                        });
-                        if (skipProperties !== undefined && skipProperties === "true") {
+                        if (!skipProperties) {
+                            var initTablePropsPromises = [];
+                            createdApp.tables.forEach(function(table, index) {
+                                initTablePropsPromises.push(self.initTableProperties(createdApp.id, table.id, table.name));
+                            });
                             // Set the tableProperties for each table
                             return promise.all(initTablePropsPromises).then(function(results) {
                                 // if all promises successful return the createApp response or code will error to catch block below
