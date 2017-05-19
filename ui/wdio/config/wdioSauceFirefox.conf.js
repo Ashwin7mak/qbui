17cd29bd9a7619fce2090c19f7793cf161fe90c9
lakshmi kamineni <skamineni@quickbase.com>
2017-05-19 10:10:39 -0400

    Covering both xlarge and large BP

--- ui/wdio/config/wdioSauceFirefox.conf.js
+++ ui/wdio/config/wdioSauceFirefox.conf.js
@@ -13,7 +13,7 @@ var config = {
             // Timeout in seconds for Sauce Labs to wait for another command (bumped this for sleeps in tests)
             idleTimeout: '180',
             maxDuration: 10800,
-            breakpointSize: 'xlarge',
+            breakpointSize: 'large',
             // These two values enable parallel testing which will run a spec file per instance
             shardTestFiles: true,
             maxInstances: 5
