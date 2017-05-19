e9cb8cf22a2ddf2c9eebd35f396ef94064bf59ca
jondavidnelson <jnelson@quickbase.com>
2017-05-19 09:13:06 -0400

    added waitForLeftNavLoaded to each beforeAll prior to loading report

--- ui/wdio/tests/forms/formBuilder/pairs-keyboard.e2e.spec.js
+++ ui/wdio/tests/forms/formBuilder/pairs-keyboard.e2e.spec.js
@@ -40,6 +40,8 @@
             } catch (err) {
                 browser.logger.info(err.toString());
             }
+            // wait for left nav to load completely (else settings button won't be rendered)
+            reportContentPO.waitForLeftNavLoaded();
             // open first table
             e2ePageBase.loadReportByIdInBrowser(realmName, testApp.id, testApp.tables[e2eConsts.TABLE1].id, 1);
             // wait for left nav to load completely (else settings button won't be rendered)
