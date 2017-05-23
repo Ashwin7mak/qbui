c58168e43800b2f65e79f70de89bf4a653249730
Brandon Stookey <bstookey@quickbase.com>
2017-05-23 18:42:21 -0400

    rewired existing menu in toolPalette

--- ui/client-react/test/components/formBuilder/toolPalette.unit.spec.js
+++ ui/client-react/test/components/formBuilder/toolPalette.unit.spec.js
@@ -4,6 +4,7 @@ import jasmineEnzyme from 'jasmine-enzyme';
 import {FieldTokenInMenu} from '../../../src/components/formBuilder/fieldToken/fieldTokenInMenu';
 import ToolPalette from '../../../src/components/builder/builderMenus/toolPalette';
 import NewFieldsMenu, {__RewireAPI__ as NewFieldsMenuRewireAPI} from '../../../src/components/formBuilder/menus/newFieldsMenu';
+import {ExistingFieldsMenu} from '../../../src/components/formBuilder/menus/existingFieldsMenu';
 
 let component;
 
@@ -11,10 +12,12 @@ describe('ToolPalette', () => {
     beforeEach(() => {
         jasmineEnzyme();
         NewFieldsMenuRewireAPI.__Rewire__('FieldTokenInMenu', FieldTokenInMenu);
+        NewFieldsMenuRewireAPI.__Rewire__('ExistingFieldsMenu', ExistingFieldsMenu);
     });
 
     afterEach(() => {
         NewFieldsMenuRewireAPI.__ResetDependency__('FieldTokenInMenu');
+        NewFieldsMenuRewireAPI.__ResetDependency__('ExistingFieldsMenu');
     });
 
     it('displays new fields', () => {
