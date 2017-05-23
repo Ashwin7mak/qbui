a7dc17ebe390ed462c5a31f2e5f0267a4ca42e63
lakshmi kamineni <skamineni@quickbase.com>
2017-05-22 22:10:29 -0400

    enabled Android

--- ui/wdio/config/wdioSauceTablet.conf.js
+++ ui/wdio/config/wdioSauceTablet.conf.js
@@ -1,30 +1,30 @@
 var baseConf = require('./wdioSauce.conf');
 var config = {
     capabilities: [
-        //{
-        //    maxInstances: 5,
-        //    //For Android
-        //    browserName: 'Chrome',
-        //    appiumVersion: '1.6.4',
-        //    platformName: 'Android',
-        //    platformVersion: '6.0',
-        //    deviceName: 'Android GoogleAPI Emulator',
-        //    deviceType: 'tablet',
-        //    deviceOrientation: 'landscape',
-        //    automationName: 'Appium',
-        //    tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
-        //    build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - Android Chrome Browser',
-        //    tags            : [process.env.SAUCE_JOB_NAME + '_Android_Chrome', 'try', 'Android', 'Chrome', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
-        //    exclude: [
-        //        //TODO inLineedit there is no functionality for touch devices atleast for now
-        //        './wdio/tests/reports/reportEditRecord.e2e.spec.js',
-        //        //TODO forms tests fail due to MC-1820
-        //        './wdio/tests/forms/formAdd.e2e.spec.js',
-        //        './wdio/tests/forms/formAddValidation.e2e.spec.js',
-        //        './wdio/tests/forms/formEdit.e2e.spec.js',
-        //        './wdio/tests/forms/formEditValidation.e2e.spec.js'
-        //    ]
-        //},
+        {
+            maxInstances: 5,
+            //For Android
+            browserName: 'Chrome',
+            appiumVersion: '1.6.4',
+            platformName: 'Android',
+            platformVersion: '6.0',
+            deviceName: 'Android GoogleAPI Emulator',
+            deviceType: 'tablet',
+            deviceOrientation: 'landscape',
+            automationName: 'Appium',
+            tunnelIdentifier: process.env.ENV_TUNNEL_NAME,
+            build           : 'WebdriverIO Jenkins Try Build #' + process.env.BUILD_NUMBER + ' - Git branch: ' + process.env.GIT_UIBRANCH + ' - Android Chrome Browser',
+            tags            : [process.env.SAUCE_JOB_NAME + '_Android_Chrome', 'try', 'Android', 'Chrome', process.env.BUILD_NUMBER, process.env.GIT_UIBRANCH],
+            exclude: [
+                //TODO inLineedit there is no functionality for touch devices atleast for now
+                './wdio/tests/reports/reportEditRecord.e2e.spec.js',
+                //TODO forms tests fail due to MC-1820
+                './wdio/tests/forms/formAdd.e2e.spec.js',
+                './wdio/tests/forms/formAddValidation.e2e.spec.js',
+                './wdio/tests/forms/formEdit.e2e.spec.js',
+                './wdio/tests/forms/formEditValidation.e2e.spec.js'
+            ]
+        },
         {
             maxInstances: 5,
             //For iOS
