1b56d443f857fa8873069f71f02980dd8ee9881f
Don Hatch <donald_hatch@intuit.com>
2017-05-23 08:41:29 -0400

    add unit tests against redux app actions

--- ui/client-react/src/actions/appActions.js
+++ ui/client-react/src/actions/appActions.js
@@ -4,8 +4,6 @@ import * as types from '../actions/types';
 import Logger from '../utils/logger';
 import LogLevel from '../utils/logLevels';
 
-let logger = new Logger();
-
 /**
  * Construct apps store redux store payload
  *
@@ -25,6 +23,7 @@ export const loadApps = () => {
     // (this is permitted when we're using redux-thunk middleware which invokes the store dispatch)
     return (dispatch) => {
         return new Promise((resolve, reject) => {
+            let logger = new Logger();
             logger.debug('AppActions loadApps');
             dispatch(event(types.LOAD_APPS));
 
@@ -49,6 +48,7 @@ export const loadApp = (appId) => {
     // (this is permitted when we're using redux-thunk middleware which invokes the store dispatch)
     return (dispatch) => {
         return new Promise((resolve, reject) => {
+            let logger = new Logger();
             logger.debug('AppActions loadApp');
             dispatch(event(types.LOAD_APP, appId));
 
@@ -63,30 +63,20 @@ export const loadApp = (appId) => {
                     dispatch(event(types.LOAD_APP_ERROR, error));
                     reject();
                 }
-            ).catch(ex => {
-                // TODO - remove catch block and update onPossiblyUnhandledRejection bluebird handler
-                logger.logException(ex);
-                reject();
-            });
+            );
         });
     };
 };
 
 export const clearSelectedApp = () => {
-    return (dispatch) => {
-        dispatch(event(types.CLEAR_APP));
-    };
+    return event(types.CLEAR_SELECTED_APP);
 };
 
-export const selectAppTable = (appId, tableId) => {
-    return (dispatch) => {
-        dispatch(event(types.SELECT_APP_TABLE, {appId, tableId}));
-    };
+export const selectAppTable = (appId, tblId) => {
+    return event(types.SELECT_APP_TABLE, {appId, tblId});
 };
 
 export const clearSelectedAppTable = () => {
-    return (dispatch) => {
-        dispatch(event(types.CLEAR_APP_TABLE));
-    };
+    return event(types.CLEAR_SELECTED_APP_TABLE);
 };
 
---
+++ ui/client-react/test/actions/appActions.unit.spec.js
@@ -0,0 +1,189 @@
+import {__RewireAPI__ as AppActionsRewireAPI} from '../../src/actions/appActions';
+import * as AppActions from '../../src/actions/appActions';
+import * as types from '../../src/actions/types';
+
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import Promise from 'bluebird';
+
+let appId = 'appId';
+let tblId = 'tblId';
+
+// we mock the Redux store when testing async action creators
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+class mockLogger {
+    constructor() {}
+    logException() {}
+    debug() {}
+    warn() {}
+    error() {}
+    parseAndLogError() {}
+}
+
+function event(type, content) {
+    return {
+        type: type,
+        content: content || null
+    };
+}
+
+describe('App Actions success workflow functions', () => {
+
+    let responseData = {
+        data: {id: '1'}
+    };
+    class mockAppService {
+        constructor() { }
+        getApps() {
+            return Promise.resolve(responseData);
+        }
+        getAppComponents() {
+            return Promise.resolve(responseData);
+        }
+    }
+
+    beforeEach(() => {
+        spyOn(mockAppService.prototype, 'getApps').and.callThrough();
+        spyOn(mockAppService.prototype, 'getAppComponents').and.callThrough();
+        AppActionsRewireAPI.__Rewire__('AppService', mockAppService);
+        AppActionsRewireAPI.__Rewire__('Logger', mockLogger);
+    });
+
+    afterEach(() => {
+        AppActionsRewireAPI.__ResetDependency__('AppService');
+        AppActionsRewireAPI.__ResetDependency__('Logger');
+    });
+
+    it('test load apps', (done) => {
+        // the mock store makes the actions dispatched available via getActions()
+        // so we don't need to spy on the dispatcher etc.
+        const expectedActions = [
+            event(types.LOAD_APPS),
+            event(types.LOAD_APPS_SUCCESS, responseData.data)
+        ];
+
+        const store = mockStore({});
+        return store.dispatch(AppActions.loadApps()).then(
+            () => {
+                expect(mockAppService.prototype.getApps).toHaveBeenCalled();
+                expect(store.getActions()).toEqual(expectedActions);
+                done();
+            },
+            () => {
+                expect(false).toEqual(true);
+                done();
+            });
+    });
+
+    it('test load app', (done) => {
+        // the mock store makes the actions dispatched available via getActions()
+        // so we don't need to spy on the dispatcher etc.
+        const expectedActions = [
+            event(types.LOAD_APP, appId),
+            event(types.LOAD_APP_SUCCESS, responseData.data)
+        ];
+
+        const store = mockStore({});
+        return store.dispatch(AppActions.loadApp(appId)).then(
+            () => {
+                expect(mockAppService.prototype.getAppComponents).toHaveBeenCalled();
+                expect(store.getActions()).toEqual(expectedActions);
+                done();
+            },
+            () => {
+                expect(false).toEqual(true);
+                done();
+            });
+    });
+
+    it('create clear selected app event', () => {
+        expect(AppActions.clearSelectedApp()).toEqual(event(types.CLEAR_SELECTED_APP));
+    });
+
+    it('create set selected app table event', () => {
+        expect(AppActions.selectAppTable(appId, tblId)).toEqual(event(types.SELECT_APP_TABLE, {appId, tblId}));
+    });
+
+    it('create clear selected app table event', () => {
+        expect(AppActions.clearSelectedAppTable()).toEqual(event(types.CLEAR_SELECTED_APP_TABLE));
+    });
+});
+
+describe('App Actions error workflow functions', () => {
+
+    let errorData = {
+        response: 'some error'
+    };
+    class mockAppService {
+        constructor() { }
+        getApps() {
+            return Promise.reject(errorData);
+        }
+        getAppComponents() {
+            return Promise.reject(errorData);
+        }
+    }
+
+    beforeEach(() => {
+        spyOn(mockAppService.prototype, 'getApps').and.callThrough();
+        spyOn(mockAppService.prototype, 'getAppComponents').and.callThrough();
+        spyOn(mockLogger.prototype, 'parseAndLogError').and.callThrough();
+        spyOn(mockLogger.prototype, 'logException').and.callThrough();
+        AppActionsRewireAPI.__Rewire__('AppService', mockAppService);
+        AppActionsRewireAPI.__Rewire__('Logger', mockLogger);
+    });
+
+    afterEach(() => {
+        AppActionsRewireAPI.__ResetDependency__('AppService');
+        AppActionsRewireAPI.__ResetDependency__('Logger');
+    });
+
+    it('loads apps with invalid response', (done) => {
+        // the mock store makes the actions dispatched available via getActions()
+        // so we don't need to spy on the dispatcher etc.
+        const expectedActions = [
+            event(types.LOAD_APPS),
+            event(types.LOAD_APPS_ERROR, errorData)
+        ];
+
+        const store = mockStore({});
+        return store.dispatch(AppActions.loadApps()).then(
+            () => {
+                expect(true).toEqual(false);
+                done();
+            },
+            () => {
+                expect(mockAppService.prototype.getApps).toHaveBeenCalled();
+                expect(mockLogger.prototype.parseAndLogError).toHaveBeenCalled();
+                expect(store.getActions()).toEqual(expectedActions);
+                done();
+            });
+    });
+
+    it('loads app with invalid response', (done) => {
+        // the mock store makes the actions dispatched available via getActions()
+        // so we don't need to spy on the dispatcher etc.
+        const expectedActions = [
+            event(types.LOAD_APP, appId),
+            event(types.LOAD_APP_ERROR, errorData)
+        ];
+
+        const store = mockStore({});
+        return store.dispatch(AppActions.loadApp(appId)).then(
+            () => {
+                expect(true).toEqual(false);
+                done();
+            },
+            () => {
+                expect(mockAppService.prototype.getAppComponents).toHaveBeenCalled();
+                expect(mockLogger.prototype.parseAndLogError).toHaveBeenCalled();
+                expect(store.getActions()).toEqual(expectedActions);
+                done();
+            });
+    });
+    
+});
+
