3e4cde0b11989b9a17230743bc9170c68e977db6
vnampally16 <vnampally@quickbase.com>
2017-05-24 11:52:35 -0400

    enter reportbuilder

---
+++ ui/wdio/pages/reportBuilder.po.js
@@ -0,0 +1,55 @@
+'use strict';
+let topNavPO = requirePO('topNav');
+let reportContentPO = requirePO('reportContent');
+let oneSecond = 1000; // millis
+let fiveSeconds = 5 * oneSecond;
+
+class reportBuilderPage {
+
+    // get cancelBtn() {
+    //     // CANCEL (form) button in footer bar
+    //     return browser.element('.mainTrowserFooterButton');
+    // }
+    //
+    // get saveBtn() {
+    //     // SAVE (form) button in footer bar
+    //     return browser.element('.mainTrowserFooterButton');
+    // }
+    // get saveOrCancelFooter() {
+    //     // footer bar (container for SAVE & CANCEL buttons)
+    //     return browser.element('.saveOrCancelFooter');
+    // }
+    //
+    cancel() {
+        // Clicks on CANCEL in the form builder and waits for the next page to render
+        this.cancelBtn.click();
+        // do we have a method to wait for spinner?
+        browser.pause(fiveSeconds);
+        return this;
+    }
+    get reportBuilderContainer() { 
+        // the whole form builder page (all 3 panels) 
+        return browser.element('.reportBuilderContainer'); 
+         }
+
+    open() {
+        // Invokes the form builder from the VIEW RECORD page
+        reportContentPO.settingsIcon.waitForVisible();
+        browser.pause(fiveSeconds);
+        reportContentPO.settingsIcon.click();
+        topNavPO.modifyThisForm.waitForExist(fiveSeconds);
+        topNavPO.modifyThisForm.click();
+        this.reportBuilderContainer.waitForVisible();
+        browser.pause(fiveSeconds);
+        return this;
+    }
+    // save() {
+    //     // Clicks on the SAVE button in the form builder and waits for the next page to appear
+    //     this.saveBtn.click();
+    //     // wait for spinner?
+    //     browser.pause(fiveSeconds);
+    //     return this;
+    // }
+
+}
+module.exports = new reportBuilderPage();
---
+++ ui/wdio/tests/reports/reportBuilder.e2e.spec.js
@@ -0,0 +1,51 @@
+(function() {
+    'use strict';
+
+    let newStackAuthPO = requirePO('newStackAuth');
+    let e2ePageBase = requirePO('e2ePageBase');
+    let reportContentPO = requirePO('reportContent');
+    let reportBuilderPO = requirePO('reportBuilder');
+    let topNavPO = requirePO('topNav');
+
+    let realmName;
+    let realmId;
+    let testApp;
+
+    describe('Report Builder Tests: ', function() {
+        beforeAll(function() {
+            /**
+             * Setup method. Creates test app then authenticates into the new stack
+             */
+            browser.logger.info('beforeAll spec function - Generating test data and logging in');
+            // Need to return here. beforeAll is completely async, need to return the Promise chain in any before or after functions!
+            // No need to call done() anymore
+            return e2eBase.basicAppSetup().then(function(createdApp) {
+                // Set your global objects to use in the test functions
+                testApp = createdApp;
+                realmName = e2eBase.recordBase.apiBase.realm.subdomain;
+                realmId = e2eBase.recordBase.apiBase.realm.id;
+            }).then(function() {
+                // Auth into the new stack
+                return newStackAuthPO.realmLogin(realmName, realmId);
+            }).catch(function(error) {
+                // Global catch that will grab any errors from chain above
+                // Will appropriately fail the beforeAll method so other tests won't run
+                browser.logger.error('Error in beforeAll function:' + JSON.stringify(error));
+                return Promise.reject('Error in beforeAll function:' + JSON.stringify(error));
+            });
+        });
+
+        beforeEach(function() {
+            // open first table
+            e2ePageBase.loadReportByIdInBrowser(realmName, testApp.id, testApp.tables[e2eConsts.TABLE1].id, 1);
+            // invoke form builder
+            return reportBuilderPO.open();
+        });
+
+
+        it('verify CANCEL', function() {
+            reportBuilderPO.cancel().open();
+        });
+    });
+
+}());
