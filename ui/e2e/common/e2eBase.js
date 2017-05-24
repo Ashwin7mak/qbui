8a659a22e814942bbb3591f63c9396594053f190
Oleksandr Prykhodko <oprykhodko@quickbase.com>
2017-05-24 16:09:12 -0400

    Merge remote-tracking branch 'remotes/origin/master' into MC-2656_app_automationSettings_e2eTest
    # Conflicts:
    #	ui/wdio/common/e2eBase.js

--- ui/e2e/common/e2eBase.js
+++ ui/e2e/common/e2eBase.js
@@ -74,6 +74,12 @@
                 var requestAppsPageEndPoint = e2eBase.recordBase.apiBase.generateFullRequest(realmName, '/qbase/apps/');
                 return requestAppsPageEndPoint;
             },
+
+            getAppPageEndpoint: function(realmName, appId) {
+                let appPageEndpoint = e2eBase.recordBase.apiBase.generateFullRequest(realmName, '/qbase/app/' + appId);
+                return appPageEndpoint;
+            },
+
             // Helper method to get the proper URL for loading the table home page containing a list of tables for a realm for an app
             getRequestTableEndpoint: function(realmName, appId, tableId) {
                 var requestTableEndPoint = e2eBase.recordBase.apiBase.generateFullRequest(realmName, '/qbase/app/' + appId + '/table/' + tableId);
