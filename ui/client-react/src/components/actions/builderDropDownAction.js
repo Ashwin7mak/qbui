7d9e4270850a5774beb421c066e5579982b233c2
cchen7196 <cchen@quickbase.com>
2017-05-30 16:21:58 -0400

    MC-3108 Remove reportBuilder from tableHomePageRoute

--- ui/client-react/src/components/actions/builderDropDownAction.js
+++ ui/client-react/src/components/actions/builderDropDownAction.js
@@ -40,7 +40,7 @@ let BuilderDropDownAction = React.createClass({
         let isAppView = !!this.props.selectedApp; // !! converts to boolean
         let isTableView = (isAppView && this.props.selectedTable);
         let isFormView = (isTableView && this.props.recId);
-        let isReportView = (isTableView && !this.props.recId && this.props.rptId);
+        let isReportView = (isTableView && !this.props.recId && this.props.rptId > 0);
         let hasContextView = isFormView || isReportView;
         let classes = "dropdownToggle globalActionLink" + (hasContextView ? " hasContextView" : "");
 
