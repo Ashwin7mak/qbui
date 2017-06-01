6437e8d81c4167bbf886abafa8adec95c1103a25
Mynuddin Syed <msyed@quickbase.com>
2017-06-01 16:42:24 -0400

    MC-2987

--- ui/client-react/test/actions/automationActions.unit.spec.js
+++ ui/client-react/test/actions/automationActions.unit.spec.js
@@ -70,6 +70,10 @@ describe('Test AutomationActions function success workflow', () => {
             () => {
                 expect(store.getActions()).toEqual(expectedActions);
                 done();
+            },
+            () => {
+                expect(false).toBe(true);
+                done();
             });
     });
     it('verify testAutomation action', (done) => {
@@ -182,10 +186,6 @@ describe('Test AutomationActions function failure workflow', () => {
         const store = mockAutomationStore({});
 
         return store.dispatch(automationActions.testAutomation(automationName, appId)).then(
-            () => {
-                expect(false).toBe(true);
-                done();
-            },
             () => {
                 expect(store.getActions()).toEqual(expectedActions);
                 done();
