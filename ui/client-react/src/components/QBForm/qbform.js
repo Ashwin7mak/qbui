ea4db634d299afd6805ac0459dd4c431c754dc0d
jzybert <jzybert@quickbase.com>
2017-06-02 09:52:57 -0400

    qbform spacing fix

--- ui/client-react/src/components/QBForm/qbform.js
+++ ui/client-react/src/components/QBForm/qbform.js
@@ -376,10 +376,10 @@ export const QBForm = React.createClass({
         /* if the form prop calls for element to be required update fieldDef accordingly
          * This isn't functionality that currently exists in newstack. Its causing issues with updating field properties
          * in form builder. Once we had support for forms to have required fields,etc we will need to address this
-         if (relatedField) {
-             relatedField.required = relatedField.required || FormFieldElement.required;
-         }
-         */
+        if (relatedField) {
+            relatedField.required = relatedField.required || FormFieldElement.required;
+        }
+        */
 
         let CurrentFieldElement = (this.props.editingForm ? DragDropFieldElement : FieldElement);
 
