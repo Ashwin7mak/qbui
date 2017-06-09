f8330e46053f275277099fd0a10f62c6da5bcbde
ajdcolcord <ajdcolcord@gmail.com>
2017-06-09 16:08:38 -0400

    Adding tests for facets in standardGridToolBar

--- ui/governance/test/common/grid/StandardGridToolBar.unit.spec.js
+++ ui/governance/test/common/grid/StandardGridToolBar.unit.spec.js
@@ -5,6 +5,7 @@ import * as Actions from "../../../src/account/users/AccountUsersActions";
 import StandardGridToolBar from "../../../src/common/grid/toolbar/StandardGridToolbar";
 import StandardGridNavigation from "../../../src/common/grid/toolbar/StandardGridNavigation";
 import StandardGridItemsCount from "../../../../reuse/client/src/components/itemsCount/StandardGridItemsCount";
+import StandardGridFacetsMenu from "../../../src/common/grid/toolbar/StandardGridFacetsMenu";
 import GenericFilterSearchBox from "../../../../reuse/client/src/components/facets/genericFilterSearchBox";
 import {Provider} from "react-redux";
 import configureMockStore from "redux-mock-store";
@@ -16,7 +17,7 @@ describe('StandardGridToolBar', () => {
         jasmineEnzyme();
     });
 
-    it('should render with navigation, search and itemsCount component', () => {
+    it('should render with navigation, search, filter, and itemsCount component', () => {
 
         let mockSearchTerm = "test search";
 
@@ -30,6 +31,7 @@ describe('StandardGridToolBar', () => {
                     itemTypePlural= "users"
                     itemTypeSingular="user"
                     itemsPerPage={500}
+                    shouldFacet={true}
                 />
             </Provider>);
 
@@ -48,6 +50,8 @@ describe('StandardGridToolBar', () => {
         expect(StandardGridSearchComponent).toHaveProp('searchTerm', mockSearchTerm);
 
         expect(component.find(StandardGridItemsCount)).toBePresent();
+        expect(component.find(StandardGridFacetsMenu)).toBePresent();
+
     });
 
     it('should not render with navigation but render with search and itemsCount component', () => {
@@ -80,5 +84,43 @@ describe('StandardGridToolBar', () => {
         expect(StandardGridSearchComponent).toHaveProp('searchTerm', mockSearchTerm);
 
         expect(component.find(StandardGridItemsCount)).toBePresent();
+        expect(component.find(StandardGridFacetsMenu)).toBePresent();
+
+    });
+
+    it('should not render filter, but still render with navigation, search, and itemsCount component', () => {
+
+        let mockSearchTerm = "test search";
+
+        let component = mount(
+            <Provider store={mockStore({Grids : {accountUsers: {pagination: {totalItems: 1500}, searchTerm: mockSearchTerm}}})}>
+                <StandardGridToolBar
+                    doUpdate={Actions.doUpdate}
+                    doFacet={false}
+                    id={"accountUsers"}
+                    rowKey={"uid"}
+                    itemTypePlural= "users"
+                    itemTypeSingular="user"
+                    itemsPerPage={500}
+                    shouldFacet={false}
+                />
+            </Provider>);
+
+        expect(component).toBeDefined();
+        expect(component.length).toBeTruthy();
+
+        let StandardGridNavigationComponent = component.find(StandardGridNavigation);
+        expect(StandardGridNavigationComponent).toBeDefined();
+        expect(StandardGridNavigationComponent.length).toBeTruthy();
+        expect(StandardGridNavigationComponent.props().id).toEqual("accountUsers");
+
+        let StandardGridSearchComponent = component.find(GenericFilterSearchBox);
+        expect(StandardGridSearchComponent).toBeDefined();
+        expect(StandardGridSearchComponent.length).toBeTruthy();
+        expect(StandardGridSearchComponent.props().placeholder).toEqual("Search users");
+        expect(StandardGridSearchComponent).toHaveProp('searchTerm', mockSearchTerm);
+
+        expect(component.find(StandardGridItemsCount)).toBePresent();
+        expect(component.find(StandardGridFacetsMenu)).not.toBePresent();
     });
 });
