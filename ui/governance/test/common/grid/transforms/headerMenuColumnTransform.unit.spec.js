b9030c59cc407f350dc2a24895e21cde921d4fce
amanoharquickbase <amanohar@quickbase.com>
2017-06-08 22:28:50 -0400

    Add tests for headerMenuColumnTransform

---
+++ ui/governance/test/common/grid/transforms/headerMenuColumnTransform.unit.spec.js
@@ -0,0 +1,9 @@
+import headerMenuColumnTransform from "../../../../src/common/grid/transforms/headerMenuColumnTransform";
+import jasmineEnzyme from "jasmine-enzyme";
+
+describe('HeaderMenuColumnTransform', () => {
+    beforeEach(() => {
+        jasmineEnzyme();
+        let testHeaderMenuColumnTransform = new headerMenuColumnTransform();
+    });
+})
