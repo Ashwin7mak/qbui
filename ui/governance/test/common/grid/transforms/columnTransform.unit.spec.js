528bb50b261ed8a052f0c4641ab54b4c8355104b
amanoharquickbase <amanohar@quickbase.com>
2017-06-09 09:55:33 -0400

    Make test changes

--- ui/governance/test/common/grid/transforms/columnTransform.unit.spec.js
+++ ui/governance/test/common/grid/transforms/columnTransform.unit.spec.js
@@ -6,10 +6,27 @@ describe('columns', () => {
         jasmineEnzyme();
     });
 
+    let mockColumns = [{
+        "property":"firstName",
+        "header":{
+            "label":"First Name"
+        },
+        "fieldDef":{
+            "id":1,
+            "datatypeAttributes":{
+                "type":"TEXT"
+            }
+        }
+    }];
+
     it('returns columns to the instance of the ColumnTransform', () => {
 
         let testColumnTransform = new ColumnTransform();
         spyOn(testColumnTransform, 'apply');
-        expect(testColumnTransform.apply).toBeDefined();
+        // expect(testColumnTransform.apply).toBeDefined();
+        let result = testColumnTransform.apply(mockColumns);
+        console.log(result);
+        expect(testColumnTransform.apply).toHaveBeenCalled();
+        expect(result).toEqual(mockColumns);
     });
 });
