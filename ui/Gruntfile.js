b1d45510f66c6d7021caad59d1750a46371a05da
Goel, Aditi <agoel@quickbase.com>
2017-06-12 17:52:52 -0400

    Wrongly names test to exclude.

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -598,7 +598,7 @@ module.exports = function(grunt) {
                     './wdio/tests/automations/settingsMenu.e2e.spec.js',
                     './wdio/tests/automations/automationsList.e2e.spec.js',
                     //failing in CI
-                    './wdio/tests/relationships/createNegativeRelationshipTests.e2e.spec.js'
+                    './wdio/tests/relationships/createRelationshipNegativeTests.e2e.spec.js'
                 ],
                 suites: {
                     // Please alphabetize these by folder level
