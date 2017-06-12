4a8190e2dca010c3885cd70a4591f8ea8b5759c1
Brandon Stookey <bstookey@quickbase.com>
2017-06-12 13:49:27 -0500

    mc3248 update localize message per andrew request

--- ui/client-react/src/locales/bundles/apps-en_us.js
+++ ui/client-react/src/locales/bundles/apps-en_us.js
@@ -615,7 +615,7 @@ export default {
             name: "Name",
             required: "Must be filled in",
             multiChoice: "Choices",
-            unique: "Must have unique values",
+            unique: "Must be unique",
             linkToRecord: "Link to a record in the table",
             connectedTo: "Connected on {fieldName} field"
         },
