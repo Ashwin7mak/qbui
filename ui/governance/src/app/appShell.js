5570955551ce4ac8a6f174a912a1e28a316cdeda
amanoharquickbase <amanohar@quickbase.com>
2017-06-13 11:01:27 -0400

    Initial changes for evergage

--- ui/governance/src/app/appShell.js
+++ ui/governance/src/app/appShell.js
@@ -7,11 +7,15 @@ import TopNav from "../../../reuse/client/src/components/topNav/topNav";
 import LeftNav from "../common/leftNav/GovernanceLeftNav";
 import {Switch} from "react-router-dom";
 import RouteWithSubRoutes from "../../../client-react/src/scripts/RouteWithSubRoutes";
+import Analytics from "../../../reuse/client/src/components/analytics/analytics";
+import Config from '../../../client-react/src/config/app.config';
+import AccountUsersStage from "../../src/account/users/AccountUsersStage";
+
 
 import "./governanceAppShell.scss";
-
 export const GovernanceAppShell = (props) => (
     <AppShell functionalAreaName="governance">
+        <Analytics dataset={Config.evergageDataset} isAdmin={props.isAdmin} userId={props.currentUserId}/>
         <LeftNav
             isNavCollapsed={props.isNavCollapsed}
             isNavOpen={props.isNavOpen}
@@ -36,15 +40,28 @@ export const GovernanceAppShell = (props) => (
     </AppShell>
 );
 
+var myChild = React.renderComponent(AccountUsersStage);
+console.log(myChild.getTotalPaidUsers);
+
+
 GovernanceAppShell.propTypes = {
     isNavOpen: PropTypes.bool,
     isNavCollapsed: PropTypes.bool,
     toggleNav: PropTypes.func,
 };
 
-const mapStateToProps = (state) => ({
-    isNavOpen: state.Nav.isNavOpen,
-    isNavCollapsed: state.Nav.isNavCollapsed
-});
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+        isNavOpen: state.Nav.isNavOpen,
+        isNavCollapsed: state.Nav.isNavCollapsed,
+        accountId: state.RequestContext.account.id,
+        currentUserId: state.RequestContext.currentUser.id,
+        isAdmin: state.RequestContext.currentUser.isAccountAdmin,
+        isRealmAdmin: state.RequestContext.currentUser.isRealmAdmin,
+        subdomainName: state.RequestContext.account.name,
+
+    };
+};
 
 export default connect(mapStateToProps, {toggleNav})(GovernanceAppShell);
