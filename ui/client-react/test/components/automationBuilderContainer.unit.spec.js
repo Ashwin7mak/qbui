03390ab9f08dfb13a18e98b0b721f8cdaf39383d
Sam Schifman <sschifman@quickbase.com>
2017-06-13 11:01:04 -0400

    MC-2990: more unit tests for automationBuilderContainer

--- ui/client-react/test/components/automationBuilderContainer.unit.spec.js
+++ ui/client-react/test/components/automationBuilderContainer.unit.spec.js
@@ -3,6 +3,7 @@ import {shallow, mount} from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
 import {AutomationBuilderContainer, __RewireAPI__ as AutomationBuilderContainerRewireAPI}  from '../../src/components/automation/builder/AutomationBuilderContainer';
 import {MemoryRouter, Link} from 'react-router-dom';
+import NavigationUtils from '../../src/utils/navigationUtils';
 import FieldValueEditor from "../../src/components/fields/fieldValueEditor";
 import MultiLineTextFieldValueEditor from "../../src/components/fields/multiLineTextFieldValueEditor";
 import EmailFieldValueEditor from "../../src/components/fields/emailFieldValueEditor";
@@ -19,6 +20,10 @@ const sampleAuto1 = {id: 'auto1', name: 'Auto 1', active: true, type: "EMAIL", i
 const props = {
     app: sampleApp,
     loadAutomation: (context, appId) => {},
+    saveAutomation: (appId, automationId, automation) => {},
+    changeAutomationEmailTo: (value) => {},
+    changeAutomationEmailSubject: (value) => {},
+    changeAutomationEmailBody: (value) => {},
     automation: undefined
 };
 
@@ -91,6 +96,58 @@ describe('AutomationBuilderContainer', () => {
             expect(component.find(EmailFieldValueEditor)).toHaveValue("test@test.com");
             expect(component.find(FieldValueEditor)).toHaveValue("Test subject");
             expect(component.find(MultiLineTextFieldValueEditor)).toHaveValue("Test body");
+        });
+    });
+
+    describe('AutomationBuilderContainer editing automation', () => {
+        beforeEach(() => {
+            jasmineEnzyme();
+            spyOn(propsWithAuto, 'changeAutomationEmailTo').and.callThrough();
+            spyOn(propsWithAuto, 'changeAutomationEmailSubject').and.callThrough();
+            spyOn(propsWithAuto, 'changeAutomationEmailBody').and.callThrough();
+
+            component = mount(<AutomationBuilderContainer {...propsWithAuto}/>);
+        });
+
+        afterEach(() => {
+        });
+
+        it('test edit to', () => {
+            let field = component.find(EmailFieldValueEditor);
+            field.simulate('change', {target: {value: 'test2@test2.com'}});
+
+            expect(propsWithAuto.changeAutomationEmailTo).toHaveBeenCalled();
+        });
+
+        // TODO: I can't figure out how to test changing the text on subject and body.
+    });
+
+    describe('AutomationBuilderContainer save and cancel buttons with automation', () => {
+        beforeEach(() => {
+            jasmineEnzyme();
+            spyOn(propsWithAuto, 'saveAutomation').and.callThrough();
+            spyOn(NavigationUtils, 'goBackToPreviousLocation');
+
+            component = mount(<AutomationBuilderContainer {...propsWithAuto}/>);
+        });
+
+        afterEach(() => {
+        });
+
+        it('test save button click', () => {
+            let saveButton = component.find('.mainTrowserFooterButton');
+            saveButton.simulate('click');
+
+            expect(propsWithAuto.saveAutomation).toHaveBeenCalled();
+            expect(NavigationUtils.goBackToPreviousLocation).toHaveBeenCalled();
+        });
+
+        it('test cancel button click', () => {
+            let saveButton = component.find('.alternativeTrowserFooterButton');
+            saveButton.simulate('click');
+
+            expect(propsWithAuto.saveAutomation).not.toHaveBeenCalled();
+            expect(NavigationUtils.goBackToPreviousLocation).toHaveBeenCalled();
         });
     });
 });
