7e260f7fb39620949a7aa75e017aac2ed13b4354
sruthi <schalivendri@quickbase.com>
2017-06-14 13:09:38 -0400

    Removing the extra prop

--- ui/client-react/src/components/formBuilder/menus/existingFieldsMenu.js
+++ ui/client-react/src/components/formBuilder/menus/existingFieldsMenu.js
@@ -32,7 +32,6 @@ export class ExistingFieldsMenu extends Component {
                 isFilterable={true}
                 hideTitle={true}
                 emptyMessage={this.noExistingFields()}
-                app={this.props.app}
             />
         );
     }
