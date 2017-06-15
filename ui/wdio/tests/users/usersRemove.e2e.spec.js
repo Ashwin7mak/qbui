f1e0adb383165df143ec27d7cf822fe070450264
Molloy, Peter <pmolloy@quickbase.com>
2017-06-15 11:21:55 -0400

    adding tests back

--- ui/wdio/tests/users/usersRemove.e2e.spec.js
+++ ui/wdio/tests/users/usersRemove.e2e.spec.js
@@ -59,10 +59,9 @@
             //wait until notification container goes away
             browser.pause(e2eConsts.shortWaitTimeMs);
 
-            //TODO: MC-3410 - Need a better way to verify user was removed
             // Check that the user was removed
-            // ReportTableActionsPO.selectAllRecordsCheckbox();
-            // expect(ReportTableActionsPO.getReportRecordsSelectedCount()).toBe("5");
+            ReportTableActionsPO.selectAllRecordsCheckbox();
+            expect(ReportTableActionsPO.getReportRecordsSelectedCount()).toBe("5");
         });
 
         /**
@@ -79,10 +78,9 @@
             //wait until notification container goes away
             browser.pause(e2eConsts.shortWaitTimeMs);
 
-            //TODO: MC-3410 - Need a better way to verify user was removed
             // Check for the user not removed
-            // ReportTableActionsPO.selectAllRecordsCheckbox();
-            // expect(ReportTableActionsPO.getReportRecordsSelectedCount()).toBe("5");
+            ReportTableActionsPO.selectAllRecordsCheckbox();
+            expect(ReportTableActionsPO.getReportRecordsSelectedCount()).toBe("5");
         });
     });
 }());
