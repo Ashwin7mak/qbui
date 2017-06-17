eca91e6152120657e4c37550cf8bcc767ef72331
Brandon Stookey <bstookey@quickbase.com>
2017-06-17 13:00:25 -0500

    mc2870 fix unit test

--- ui/client-react/src/reducers/appBuilder.js
+++ ui/client-react/src/reducers/appBuilder.js
@@ -1,6 +1,6 @@
 import * as types from '../actions/types';
 
-const appBuilder = (state, action) => {
+const appBuilder = (state = {}, action) => {
     switch (action.type) {
     case types.SHOW_APP_CREATION_DIALOG:
         return {
