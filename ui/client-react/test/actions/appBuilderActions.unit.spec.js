d8316b37313bc1e05ab1517d1371c64d7280cb47
Brandon Stookey <bstookey@quickbase.com>
2017-06-17 11:53:51 -0500

    mc2870 remove event function from test

--- ui/client-react/test/actions/appBuilderActions.unit.spec.js
+++ ui/client-react/test/actions/appBuilderActions.unit.spec.js
@@ -1,16 +1,6 @@
 import * as AppBuilderActions from '../../src/actions/appBuilderActions';
 import * as types from '../../src/actions/types';
 
-let appId = 'appId';
-let tblId = 'tblId';
-
-function event(type, content) {
-    return {
-        type: type,
-        content: content || null
-    };
-}
-
 describe('App Actions success workflow functions', () => {
     it('create showAppCreationDialog event', () => {
         expect(AppBuilderActions.showAppCreationDialog()).toEqual(event(types.SHOW_APP_CREATION_DIALOG));
