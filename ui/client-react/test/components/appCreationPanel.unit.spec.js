1462476b72bf027895387bc5382e7970ee42129c
Brandon Stookey <bstookey@quickbase.com>
2017-06-19 14:54:22 -0500

    mc 3282 add unit tests for appCreationPanel

--- ui/client-react/test/components/appCreationPanel.unit.spec.js
+++ ui/client-react/test/components/appCreationPanel.unit.spec.js
@@ -6,92 +6,15 @@ import {Simulate} from 'react-addons-test-utils';
 
 let component;
 
-let tableInfo = {
-    name: {value: 'Customers'},
-    tableNoun: {value: 'customer'},
-    description: {value: ''},
-    tableIcon: {value: 'projects'}
-};
-
-let appTables = [
-    'table1',
-    'table2'
-];
-
-const mockParentFunctions = {
-    openIconChooser() {},
-    closeIconChooser() {},
-    setTableProperty() {},
-    setEditingProperty() {},
-};
-
 describe('TableCreationPanel', () => {
     beforeEach(() => {
         jasmineEnzyme();
     });
 
     it('renders a TableCreationPanel', () => {
-        component = mount(<TableCreationPanel tableInfo={tableInfo}
-                                              openIconChooser={mockParentFunctions.openIconChooser}
-                                              closeIconChooser={mockParentFunctions.closeIconChooser}
-                                              setTableProperty={mockParentFunctions.setTableProperty}
-                                              setEditingProperty={mockParentFunctions.setEditingProperty}
-                                              focusOn="name"
-                                              validate={true}
-                                              appTables={appTables} />);
+        component = mount(<AppCreationPanel />);
 
-        expect(component.find('.tableInfo.dialogCreationPanelInfo')).toBePresent();
-    });
-
-    it('calls icon chooser callbacks when necessary', () => {
-        spyOn(mockParentFunctions, 'openIconChooser');
-        spyOn(mockParentFunctions, 'closeIconChooser');
-        component = mount(<TableCreationPanel tableInfo={tableInfo}
-                                              openIconChooser={mockParentFunctions.openIconChooser}
-                                              closeIconChooser={mockParentFunctions.closeIconChooser}
-                                              setTableProperty={mockParentFunctions.setTableProperty}
-                                              setEditingProperty={mockParentFunctions.setEditingProperty}
-                                              focusOn="name"
-                                              validate={true}
-                                              appTables={appTables} />);
-
-        expect(component.find('.tableInfo.dialogCreationPanelInfo')).toBePresent();
-
-        let iconChooser = component.find(".iconChooser .showAllToggle");
-        expect(iconChooser).toBePresent();
-
-        Simulate.click(iconChooser.get(0));
-        expect(mockParentFunctions.openIconChooser).toHaveBeenCalled();
-
-    });
-
-    it('calls editing callbacks', () => {
-        spyOn(mockParentFunctions, 'setTableProperty');
-        spyOn(mockParentFunctions, 'setEditingProperty');
-
-        component = mount(<TableCreationPanel tableInfo={tableInfo}
-                                              openIconChooser={mockParentFunctions.openIconChooser}
-                                              closeIconChooser={mockParentFunctions.closeIconChooser}
-                                              setTableProperty={mockParentFunctions.setTableProperty}
-                                              setEditingProperty={mockParentFunctions.setEditingProperty}
-                                              focusOn="name"
-                                              validate={true}
-                                              appTables={appTables} />);
-
-        expect(component.find('.tableInfo.dialogCreationPanelInfo')).toBePresent();
-
-        let inputFields = component.find(".dialogFieldInput input");
-        expect(inputFields).toBePresent();
-
-        let input = inputFields.get(0);
-
-        Simulate.focus(input);
-        Simulate.change(input);
-        expect(mockParentFunctions.setEditingProperty).toHaveBeenCalled();
-
-        input.value = 'newText';
-        Simulate.change(input);
-        Simulate.keyDown(input, {key: "Tab", keyCode: 9, which: 9}); // tab out
-        expect(mockParentFunctions.setTableProperty).toHaveBeenCalled();
+        expect(component.find('.appCreationPanel.dialogCreationPanelInfo')).toBePresent();
+        expect(component.find('.appCreationPanel.dialogField').length).toEqual(2);
     });
 });
