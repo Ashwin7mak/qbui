995d5fddfe6289c70065a8d5fb38599b011f1914
Don Hatch <donald_hatch@intuit.com>
2017-06-20 11:18:25 -0400

    add create app actions and events

--- ui/client-react/src/actions/appBuilderActions.js
+++ ui/client-react/src/actions/appBuilderActions.js
@@ -1,5 +1,62 @@
 import * as types from '../actions/types';
 
+/**
+ * Create new app event.   Used to inform that a new
+ * app is getting created on the servers.
+ */
+export const createNewApp = () => ({
+    type: types.CREATE_APP
+});
+
+/**
+ * A new app was successfully saved
+ *
+ * @param app
+ */
+export const createAppSuccess = (app) => ({
+    type: types.CREATE_APP_SUCCESS,
+    app: app
+});
+
+/**
+ * An error was thrown when creating an app
+ */
+export const createAppFailed = () => ({
+    type: types.CREATE_APP_FAILED
+});
+
+/**
+ * create the app on the server
+ *
+ * @param app object
+ * @returns {function(*=)}
+ */
+export const createApp = (app) => {
+
+    return (dispatch) => {
+
+        const logger = new Logger();
+
+        return new Promise((resolve, reject) => {
+
+            dispatch(createNewApp());
+
+            const appService = new AppService();
+
+            appService.createApp(app).then(
+                response => {
+                    dispatch(createAppSuccess(response));
+                    resolve(response);
+                }
+            ).catch(error => {
+                dispatch(createAppFailed());
+                logger.logException(error, 'appBulderActions:createNewApp');
+                reject(error);
+            });
+        });
+    };
+};
+
 export const showAppCreationDialog = () => {
     return types.SHOW_APP_CREATION_DIALOG;
 };
--- ui/client-react/src/reducers/appBuilder.js
+++ ui/client-react/src/reducers/appBuilder.js
@@ -1,7 +1,31 @@
 import * as types from '../actions/types';
 
-const appBuilder = (state = {}, action) => {
+const appBuilder = (
+    //  default states
+    state = {
+        savingApp: false,
+        dialogOpen: false
+    },
+    action) => {
+    // reducer - no mutations!
     switch (action.type) {
+    case types.CREATE_APP:
+        return {
+            ...state,
+            savingApp: true
+        };
+    case types.CREATE_APP_SUCCESS:
+        return {
+            //  TODO: what if any data to be added to state for the new app
+            ...state,
+            savingApp: false
+        };
+    case types.CREATE_APP_FAILED:
+        return {
+            //  TODO: should the error condition to be added to state or is the promise reject enough
+            ...state,
+            savingApp: false
+        };
     case types.SHOW_APP_CREATION_DIALOG:
         return {
             ...state,
