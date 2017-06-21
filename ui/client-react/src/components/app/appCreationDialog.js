5a5006502f320e2b9d24d36f77dfc8075f1fcf45
Brandon Stookey <bstookey@quickbase.com>
2017-06-21 10:25:08 -0500

    mc3468 close add new app window onfinish

--- ui/client-react/src/components/app/appCreationDialog.js
+++ ui/client-react/src/components/app/appCreationDialog.js
@@ -24,6 +24,7 @@ export class AppCreationDialog extends React.Component {
     onFinished = () => {
         if (this.props.createApp && this.props.newApp) {
             this.props.createApp(this.props.newApp);
+            this.props.hideAppCreationDialog();
         }
     };
 
