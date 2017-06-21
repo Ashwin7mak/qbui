2b7cdfe76adc24d682fa9f72693c2735577d3bd5
Brandon Stookey <bstookey@quickbase.com>
2017-06-21 18:14:31 -0500

    mc3468 add space

--- ui/client-react/test/models/appModel.unit.spec.js
+++ ui/client-react/test/models/appModel.unit.spec.js
@@ -34,7 +34,7 @@ describe('APP Model', () => {
 
     it('Initialize app model object with no data', () => {
         const emptyApp = {};
-        let appModel = new AppModel({app:emptyApp});
+        let appModel = new AppModel({app: emptyApp});
 
         expect(appModel.getApp()).toEqual(emptyApp);
         expect(appModel.getAppId()).toEqual(emptyApp.id);
