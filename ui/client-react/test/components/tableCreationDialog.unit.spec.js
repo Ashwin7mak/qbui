4f9aa997f038a1ae0833caf990a266f2eb793bec
Brandon Stookey <bstookey@quickbase.com>
2017-06-23 18:10:32 -0500

    mc3592 first pass at updating unit tests for tablCreationDialog

--- ui/client-react/test/components/tableCreationDialog.unit.spec.js
+++ ui/client-react/test/components/tableCreationDialog.unit.spec.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import jasmineEnzyme from 'jasmine-enzyme';
+import {shallow} from 'enzyme';
 import TestUtils, {Simulate} from 'react-addons-test-utils';
 
-import {TableCreationDialog} from '../../src/components/table/tableCreationDialog';
+import {TableCreationDialog, __RewireAPI__ as TableCreationDialogRewireAPI} from '../../src/components/table/tableCreationDialog';
 
 let component;
+let instance;
 let domComponent;
+let tableInfo = {name: {}, description: {}, tableIcon: {}, tableNoun: {}};
+
+const mockPromiseAll = {
+    all: () => ({then: callback => callback()})
+};
+
+const mockNotificationManager = {
+    error() {}
+};
+
+const AppHistoryMock = {
+    history: {push(_location) {}},
+};
 
 const mockParentFunctions = {
     hideDialog() {},
     setEditingProperty() {},
     setTableProperty() {},
     createTable: () => {return {then: (fn) => fn({data:'tableId'})};},
-    tableCreated() {}
+    createTableFailed: () => {return {then: (fn, fnFailed) => fnFailed()};},
+    tableCreated() {},
+    hideTableCreationDialog() {},
+    updateFormRedirectRoute() {},
+    onTableCreated() {}
 };
 
 let app = {
+    id: 'mockAppId',
     tables: [
         {name: 'table1'},
         {name: 'table2'}
@@ -65,6 +85,15 @@ function buildMockParentComponent(options) {
 describe('TableCreationDialog', () => {
     beforeEach(() => {
         jasmineEnzyme();
+        TableCreationDialogRewireAPI.__Rewire__('Promise', mockPromiseAll);
+        TableCreationDialogRewireAPI.__Rewire__('NotificationManager', mockNotificationManager);
+        TableCreationDialogRewireAPI.__Rewire__('NotificationManager', mockNotificationManager);
+        TableCreationDialogRewireAPI.__Rewire__('AppHistory', AppHistoryMock);
+
+        spyOn(mockParentFunctions, 'createTable').and.callThrough();
+        spyOn(mockParentFunctions, 'createTableFailed').and.callThrough();
+        spyOn(AppHistoryMock.history, 'push');
+        spyOn(mockNotificationManager, 'error');
     });
 
     afterEach(() => {
@@ -73,6 +102,8 @@ describe('TableCreationDialog', () => {
         if (modalInDom) {
             modalInDom.parentNode.removeChild(modalInDom);
         }
+        TableCreationDialogRewireAPI.__ResetDependency__('Promise');
+        TableCreationDialogRewireAPI.__ResetDependency__('NotificationMan');
     });
 
     it('renders a TableCreationDialog', () => {
@@ -90,5 +121,38 @@ describe('TableCreationDialog', () => {
 
         let cancelButton = domComponent.querySelector('.cancelButton');
         Simulate.click(cancelButton);
+    });
+
+    it('will invoke createTable action when onFinished is called', () => {
+        component = shallow(<TableCreationDialog createTable={mockParentFunctions.createTable}
+                                                 tableCreation={{iconChooserOpen: {}}}
+                                                 tableInfo={tableInfo}
+                                                 app={app}
+                                                 onTableCreated={mockParentFunctions.onTableCreated}
+                                                 updateFormRedirectRoute={mockParentFunctions.updateFormRedirectRoute}
+                                                 hideTableCreationDialog={mockParentFunctions.hideTableCreationDialog} />);
+
+        instance = component.instance();
+        instance.onFinished();
+
+        expect(mockParentFunctions.createTable).toHaveBeenCalledWith(app, tableInfo);
+        expect(AppHistoryMock.history.push).toHaveBeenCalledWith(jasmine.any(String));
+    });
+
+    it('will invoke NotificationManger if createTable fails', () => {
+        component = shallow(<TableCreationDialog createTable={mockParentFunctions.createTableFailed}
+                                                 tableCreation={{iconChooserOpen: {}}}
+                                                 tableInfo={tableInfo}
+                                                 app={app}
+                                                 onTableCreated={mockParentFunctions.onTableCreated}
+                                                 updateFormRedirectRoute={mockParentFunctions.updateFormRedirectRoute}
+                                                 hideTableCreationDialog={mockParentFunctions.hideTableCreationDialog} />);
+
+        instance = component.instance();
+        instance.onFinished();
+
+        expect(mockParentFunctions.createTable).not.toHaveBeenCalled();
+        expect(AppHistoryMock.history.push).not.toHaveBeenCalled();
+        expect(mockNotificationManager.error).toHaveBeenCalled();
     });
 });
