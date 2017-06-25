ed0b27092992d28de2ee04b95210a7298e538c4c
lakshmi kamineni <skamineni@quickbase.com>
2017-06-25 14:52:47 -0400

    made to select time from timeFelds faster

--- ui/wdio/pages/formsPage.po.js
+++ ui/wdio/pages/formsPage.po.js
@@ -438,7 +438,7 @@
                         browser.pause(e2eConsts.shortWaitTimeMs);
                         timeFields.value[i].waitForVisible();
                         timeFields.value[i].click();
-                        if (browserName === 'chrome' || browserName === 'MicrosoftEdge') {
+                        if (browserName === 'chrome') {
                             browser.keys([sTime, 'Enter']);
                         } else {
                             this.selectFromList(sTime);
