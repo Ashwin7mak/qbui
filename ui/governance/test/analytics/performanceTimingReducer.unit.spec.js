057ecc3e9e8209bd3255013ff6edcd9132a7580f
amanoharquickbase <amanohar@quickbase.com>
2017-06-26 16:09:33 -0400

    Add tests

--- ui/governance/test/analytics/performanceTimingReducer.unit.spec.js
+++ ui/governance/test/analytics/performanceTimingReducer.unit.spec.js
@@ -2,6 +2,9 @@ import performanceTimingReducer, {getPageLoadTime, getGridLoadTime} from "../../
 import * as types from "../../src/app/actionTypes";
 import _ from "lodash";
 
+let mockPageLoadTime = 1.50;
+let mockGridStartTime = 0.40;
+
 describe('Performance Timing Reducer', () => {
     beforeEach(() => {
         jasmine.addMatchers({
@@ -15,24 +18,42 @@ describe('Performance Timing Reducer', () => {
         });
     });
 
-
     let initialState = {
         pageLoadTime: 0,
         gridStartTime: 0
     };
 
-    const mockPageLoadTime = 1.50;
-    const mockGridLoadTime = 0.80;
-
-    fit('returns correct initial state', () => {
+    it('returns correct initial state', () => {
         expect(performanceTimingReducer(undefined, {})).toEqual(initialState);
     });
 
-    fit('shows the page load time', () => {
-        expect(getPageLoadTime(jasmine.any(Number))).toEqual(mockPageLoadTime);
+    it('gets the page load time', () => {
+
+        let pageLoadTimeAction = {
+            type: types.GET_PAGE_LOAD_TIME,
+            payload: mockPageLoadTime
+        };
+
+        expect(performanceTimingReducer(initialState, pageLoadTimeAction)).toEqual({...initialState, pageLoadTime: mockPageLoadTime});
     });
 
-    fit('shows the page load time', () => {
-        expect(getGridLoadTime(jasmine.any(Number))).toEqual(_.subtract(mockPageLoadTime, mockGridLoadTime));
+    it('gets the grid start time', () => {
+
+        let gridStartTimeAction = {
+            type: types.GET_GRID_START_TIME,
+            payload: mockGridStartTime
+        };
+
+        expect(performanceTimingReducer(initialState, gridStartTimeAction)).toEqual({...initialState, gridStartTime: mockGridStartTime});
+    });
+
+    it('gets the page load time from state', () => {
+
+        expect(getPageLoadTime({performanceTiming: {pageLoadTime: mockPageLoadTime}})).toEqual(mockPageLoadTime);
+    });
+
+    it('gets the grid load time from state', () => {
+
+        expect(getGridLoadTime({performanceTiming: {pageLoadTime: mockPageLoadTime, gridStartTime: mockGridStartTime}})).toEqual(_.subtract(mockPageLoadTime, mockGridStartTime));
     });
 });
