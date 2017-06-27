c8e833da143475ca56cc3d45a2d8b664c0548590
sruthi <schalivendri@quickbase.com>
2017-06-27 10:35:49 -0400

    Adding tabIndex

--- ui/reuse/client/src/components/sideNavs/emptyStateForLeftNav.js
+++ ui/reuse/client/src/components/sideNavs/emptyStateForLeftNav.js
@@ -6,7 +6,7 @@ import './emptyStateForLeftNav.scss';
 const EmptyStateForLeftNav = ({handleOnClick, emptyMessage, icon, iconMessage, className}) => (
     <div className = {`${className} emptyState`}>
         <p>{Locale.getMessage(emptyMessage)}</p>
-        <div className="createNewIcon" onClick={handleOnClick} role="button">
+        <div className="createNewIcon" onClick={handleOnClick} role="button" tabIndex="0">
                 <Icon className={`${className} addNewIcon`} icon={icon}/>
             <li className="iconMessage">{Locale.getMessage(iconMessage)}</li>
             </div>
