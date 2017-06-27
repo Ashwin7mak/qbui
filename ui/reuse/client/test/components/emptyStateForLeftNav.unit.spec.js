a93fde7fb801aec7a23427e2747b4542a9754609
sruthi <schalivendri@quickbase.com>
2017-06-27 11:37:22 -0400

    Code Clean up

--- ui/reuse/client/test/components/emptyStateForLeftNav.unit.spec.js
+++ ui/reuse/client/test/components/emptyStateForLeftNav.unit.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow,mount} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
 import EmptyStateForLeftNav, {__RewireAPI__ as EmptyStateForLeftNavRewireAPI} from '../../src/components/sideNavs/emptyStateForLeftNav';
 
