ae63e67bebe89e883c1009e2b8a7692e6b89de02
Arun Manohar <amanohar@quickbase.com>
2017-06-27 14:37:59 -0400

    Remove uncompleted test

--- ui/governance/test/common/grid/transforms/headerMenuColumnTransform.unit.spec.js
+++
@@ -1,9 +0,0 @@
-import headerMenuColumnTransform from "../../../../src/common/grid/transforms/headerMenuColumnTransform";
-import jasmineEnzyme from "jasmine-enzyme";
-
-describe('HeaderMenuColumnTransform', () => {
-    beforeEach(() => {
-        jasmineEnzyme();
-        let testHeaderMenuColumnTransform = new headerMenuColumnTransform();
-    });
-});
