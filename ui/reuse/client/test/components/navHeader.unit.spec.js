0b8a11281ce76f78e6528e64ec2aabb55f76d520
nvanselow <nick@nickvanselow.com>
2017-06-26 22:59:40 -0400

    MC-3597 Fix unit tests after renaming component

--- ui/reuse/client/test/components/navHeader.unit.spec.js
+++ ui/reuse/client/test/components/navHeader.unit.spec.js
@@ -37,19 +37,19 @@ describe('NavHeader', () => {
         component = shallow(<NavHeader />);
         component = component.find(MotionMock).dive(); // Render the motion component so we can access the rendered subcomponents
 
-        expect(component.find('.menuHeaderTitle')).toHaveText('');
-        expect(component.find('.menuHeaderIcon')).not.toBePresent();
-        expect(component.find('.menuHeaderToggle')).not.toBePresent();
-        expect(component.find('.navNavHeader')).toHaveProp('style', {height: 40});
+        expect(component.find('.navHeaderTitle')).toHaveText('');
+        expect(component.find('.navHeaderIcon')).not.toBePresent();
+        expect(component.find('.navHeaderToggle')).not.toBePresent();
+        expect(component.find('.navHeader')).toHaveProp('style', {height: 40});
     });
 
     it('shows a title', () => {
         component = shallow(<NavHeader title={testTitle} />);
         component = component.find(MotionMock).dive();
 
-        expect(component.find('.menuHeaderTitle')).toHaveText(testTitle);
-        expect(component.find('.menuHeaderHidden')).not.toBePresent();
-        expect(component.find('.menuHeaderCollapsed')).not.toBePresent();
+        expect(component.find('.navHeaderTitle')).toHaveText(testTitle);
+        expect(component.find('.navHeaderHidden')).not.toBePresent();
+        expect(component.find('.navHeaderCollapsed')).not.toBePresent();
     });
 
     it('shows the content of a long title in a tooltip', () => {
@@ -60,7 +60,7 @@ describe('NavHeader', () => {
         // The truncation in the visible title happens in CSS
         const tooltip = component.find(Tooltip);
         expect(tooltip).toBePresent();
-        expect(tooltip.find('.menuHeaderTitle')).toHaveText(reallyLongTitle);
+        expect(tooltip.find('.navHeaderTitle')).toHaveText(reallyLongTitle);
     });
 
     it('shows the content of a long title in a tooltip on the small breakpoint', () => {
@@ -74,14 +74,14 @@ describe('NavHeader', () => {
         // The truncation in the visible title happens in CSS
         const tooltip = component.find(Tooltip);
         expect(tooltip).toBePresent();
-        expect(tooltip.find('.menuHeaderTitle')).toHaveText(reallyLongTitle);
+        expect(tooltip.find('.navHeaderTitle')).toHaveText(reallyLongTitle);
     });
 
     it('shows an icon', () => {
         component = shallow(<NavHeader title={testTitle} icon={testIcon} />);
         component = component.find(MotionMock).dive();
 
-        expect(component.find('.menuHeaderIcon').find(Icon)).toHaveProp('icon', testIcon);
+        expect(component.find('.navHeaderIcon').find(Icon)).toHaveProp('icon', testIcon);
     });
 
     it('shows icon from a different icon font', () => {
@@ -89,14 +89,14 @@ describe('NavHeader', () => {
         component = shallow(<NavHeader title={testTitle} icon={testIcon} iconFont={alternateFont} />);
         component = component.find(MotionMock).dive();
 
-        expect(component.find('.menuHeaderIcon').find(Icon)).toHaveProp('iconFont', alternateFont);
+        expect(component.find('.navHeaderIcon').find(Icon)).toHaveProp('iconFont', alternateFont);
     });
 
     it('hides the content of the menu header', () => {
         component = shallow(<NavHeader title={testTitle} icon={testIcon} isVisible={false} />);
         component = component.find(MotionMock).dive();
 
-        expect(component.find('.menuHeaderHidden')).toBePresent();
+        expect(component.find('.navHeaderHidden')).toBePresent();
     });
 
     describe('optionally displays a toggle (in down position by default)', () => {
@@ -104,7 +104,7 @@ describe('NavHeader', () => {
             component = shallow(<NavHeader title={testTitle} isToggleVisible={true} />);
             component = component.find(MotionMock).dive();
 
-            expect(component.find('.menuHeaderToggle')).toBePresent();
+            expect(component.find('.navHeaderToggle')).toBePresent();
             expect(component.find('.menuToggleDown')).toBePresent();
         });
 
@@ -112,7 +112,7 @@ describe('NavHeader', () => {
             component = shallow(<NavHeader title={testTitle} isToggleVisible={true} isToggleDown={false} />);
             component = component.find(MotionMock).dive();
 
-            expect(component.find('.menuHeaderToggle')).toBePresent();
+            expect(component.find('.navHeaderToggle')).toBePresent();
             expect(component.find('.menuToggleDown')).not.toBePresent();
         });
 
@@ -120,7 +120,7 @@ describe('NavHeader', () => {
             component = shallow(<NavHeader title={testTitle} isToggleVisible={true} isCollapsed={true} />);
             component = component.find(MotionMock).dive();
 
-            expect(component.find('.menuHeaderToggle')).not.toBePresent();
+            expect(component.find('.navHeaderToggle')).not.toBePresent();
         });
     });
 
@@ -129,7 +129,7 @@ describe('NavHeader', () => {
         component = shallow(<NavHeader title={testTitle} onClickHeader={onClickHeader} />);
         component = component.find(MotionMock).dive();
 
-        component.find('.menuHeaderButton').simulate('click');
+        component.find('.navHeaderButton').simulate('click');
 
         expect(onClickHeader).toHaveBeenCalled();
     });
@@ -140,16 +140,16 @@ describe('NavHeader', () => {
 
 
         // Title is hidden via CSS
-        expect(component.find('.navNavHeader')).toHaveProp('style', {height: 40});
+        expect(component.find('.navHeader')).toHaveProp('style', {height: 40});
     });
 
     it('displays as a large header', () => {
         component = shallow(<NavHeader title={testTitle} isSmall={false} />);
         component = component.find(MotionMock).dive();
 
-        console.log(component.find('.navNavHeader').props().style);
-        expect(component.find('.menuHeader')).toHaveProp('style', {height: 90});
-        expect(component.find('.menuHeaderSmall')).not.toBePresent();
-        expect(component.find('.menuHeaderCollapsed')).not.toBePresent();
+        console.log(component.find('.navHeader').props().style);
+        expect(component.find('.navHeader')).toHaveProp('style', {height: 90});
+        expect(component.find('.navHeaderSmall')).not.toBePresent();
+        expect(component.find('.navHeaderCollapsed')).not.toBePresent();
     });
 });
