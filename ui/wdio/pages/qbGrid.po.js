53db35f368b48f1a55a3c093f1a9d588d16ba661
Wei Li <wli@quickbase.com>
2017-06-28 17:29:00 -0400

    Made changes as: 1. added comments for the class 2. added return statements for functions 3. updated function names to follow the same naming convention: set, get, click 4. removed unfinished/tested functions

--- ui/wdio/pages/qbGrid.po.js
+++ ui/wdio/pages/qbGrid.po.js
@@ -1,82 +1,60 @@
 'use strict';
 
+/**
+ * This file uses the Page Object pattern to qbGrid based on governance page for test
+ */
+
 class qbGrid {
 
-    get filterSearchingBox() {
-        // filter searching box
+    get searchBox() {
+        // Search textbox
         browser.element('.searchInput').waitForVisible();
         return browser.element('.searchInput');
     }
 
     get gridHeader() {
-        // Grid column header
-        // var elem  = "browser.element('.qbHeader .qbHeaderCell:nth-child(" + col + "')";
+        // Grid column header row, returns all columns
         return $$('.qbHeader .qbHeaderCell');
     }
 
     get gridHeaderSortIcon() {
-        // Grid column sort icon
-        // var elem  = "browser.element('.qbHeader .qbHeaderCell:nth-child(" + col + ") .qbIcon.iconUISturdy-caret-filled-down')";
+        // Grid column sort icon of each columns
         return $$('.qbIcon.iconUISturdy-caret-filled-down');
     }
 
     get gridRow() {
-        // Grid rows
+        // Grid current displayed rows
         return $$('.qbTbody .qbRow');
     }
 
     /**
-     * Enter searching string in the filter seaching box
-     * @param searchingString
+     * Enter searching string in the seaching textbox
+     * @param str - string to be searched
      */
-    enterSearchingString(str) {
+    setSearchString(str) {
         try {
-            this.filterSearchingBox.setValue(str);
+            this.searchBox.setValue(str);
+            return this;
         } catch (err) {
             browser.logger.error('Error in enterSearchingString function:' + err);
         }
     }
 
     /**
-     * Return text of the given cell
-     * @param row - row number of the cell
-     * @param col - column number of the cell
+     * Get total column counts
      */
-    // get gridCell(row, col) {
-    //     var elem  = "browser.element('.qbTbody .qbRow:nth-child(" + row + "1) .qbCell:nth-child(" + col + ")')";
-    //     elem.waitForVisible();
-    //     return elem.getText();
-    // }
-
-    //* get column length
-    totalColumnCounts() {
+    getTotalColumnCounts() {
         // total column counts
         return this.gridHeader.length;
     }
 
-    //* get current view row counts
-    currentViewRowCounts() {
+    /**
+     * Get current view row counts
+     */
+    getCurrentViewRowCounts() {
         // total column counts
         return this.gridRow.length;
     }
-
-    /**
-     * Validate column headers
-     * @param expHeaders: expected headers
-     */
-    validateColumnHeaders(expHeaders) {
-        let i = 0;
-        let elem= "";
-        try {
-            for (i = 0; i < this.gridHeader.length(); i++) {
-                elem  = "browser.element('.qbHeader .qbHeaderCell:nth-child(" + i + "')";
-                elem.getText();
-            }
-        } catch (err) {
-            browser.logger.error('Error in enterSearchingString function:' + err);
-        }
-    }
-
 
 }
 module.exports = new qbGrid();
