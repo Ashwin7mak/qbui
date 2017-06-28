c38da93d1eb8920a7c09f3a91427a160d87c7bb3
Wei Li <wli@quickbase.com>
2017-06-28 13:25:41 -0400

    Created legacyLogin.po.js file.

---
+++ ui/wdio/pages/legacyLogin.po.js
@@ -0,0 +1,70 @@
+'use strict';
+
+class legacyLogin {
+
+    get fieldUsername() {
+        // EmailAddress or User name text field
+        browser.element('.UserInput.WithPadding[name="loginid"]').waitForVisible();
+        return browser.element('.UserInput.WithPadding[name="loginid"]');
+    }
+
+    get fieldPassword() {
+        // Password text field
+        return browser.element('.UserInput.WithPadding[name="password"]');
+    }
+
+    get buttonSignIn() {
+        // Sign in button
+        return browser.element('.SubmitButton');
+    }
+
+    /**
+     * Login user
+     * @param username
+     * @param password
+     */
+    singInUser(username, password) {
+        // Login user
+        this.enterUsername(username);
+        this.enterPassword(password);
+        this.clickSignInButton();
+    }
+
+    /**
+     * Enter username/email address
+     * @param username
+     */
+    enterUsername(username) {
+        try {
+            this.fieldUsername.setValue(username);
+        } catch (err) {
+            browser.logger.error('Error in enterUsername function:' + err);
+        }
+    }
+
+    /**
+     * Enter enter password
+     * @param password
+     */
+    enterPassword(password) {
+        try {
+            this.fieldPassword.setValue(password);
+        } catch (err) {
+            browser.logger.error('Error in enterPassword function:' + err);
+        }
+    }
+
+    /**
+     * Click Sign in button
+     */
+    clickSignInButton() {
+        try {
+            this.buttonSignIn.click();
+        } catch (err) {
+            browser.logger.error('Error in clickSignInButton function:' + err);
+        }
+    }
+
+}
+module.exports = new legacyLogin();
+// export default legacyLogin();
