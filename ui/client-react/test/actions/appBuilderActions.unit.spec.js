bc8e43edad2d364da7180c86ace5ce353f1e7d7e
Brandon Stookey <bstookey@quickbase.com>
2017-06-29 10:05:24 -0500

    mc3590 update unit tests descripition

--- ui/client-react/test/actions/appBuilderActions.unit.spec.js
+++ ui/client-react/test/actions/appBuilderActions.unit.spec.js
@@ -114,11 +114,11 @@ describe('App Actions event functions', () => {
         expect(AppBuilderActions.setAppProperty('mockProperty', 'mockValue')).toEqual({type: types.SET_APP_PROPERTY, property: 'mockProperty', value: 'mockValue'});
     });
 
-    it('create setAppProperty event', () => {
+    it('create openIconChooserForApp event', () => {
         expect(AppBuilderActions.openIconChooserForApp()).toEqual({type: types.OPEN_ICON_CHOOSER_FOR_APP});
     });
 
-    it('create setAppProperty event', () => {
+    it('create closeIconChooserForApp event', () => {
         expect(AppBuilderActions.closeIconChooserForApp()).toEqual({type: types.CLOSE_ICON_CHOOSER_FOR_APP});
     });
 });
