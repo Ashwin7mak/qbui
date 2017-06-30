f245eb7167dd9e82ad2fc6476999afe1354c763b
agade <agade@quickbase.com>
2017-06-30 17:31:46 -0400

    - E2E test for feature switches - Helper methods to go to the FS page

--- ui/wdio/common/e2eBase.js
+++ ui/wdio/common/e2eBase.js
@@ -75,10 +75,10 @@
                     return promise.reject(error);
                 });
             },
-            // Helper method to get the proper URL for loading the dashboard page containing a list of apps and tables for a realm
-            getRequestAppsPageEndpoint: function(realmName) {
-                var requestAppsPageEndPoint = e2eBase.recordBase.apiBase.generateFullRequest(realmName, '/qbase/apps/');
-                return requestAppsPageEndPoint;
+            // Helper method to get the proper URL for loading FS page
+            getRequestFSPageEndpoint: function(realmName) {
+                let requestFSPageEndPoint = e2eBase.recordBase.apiBase.generateFullRequest(realmName, '/qbase/admin/featureSwitches');
+                return requestFSPageEndPoint;
             },
             // Helper method to get the proper URL for loading the app in an realm
             getRequestAppPageEndpoint: function(realmName, appId) {
---
+++ ui/wdio/tests/tables/configationbasedFeatureSwitches.e2e.spec.js
@@ -0,0 +1,70 @@
+/**
+ * E2E tests for configuration based feature switches
+ */
+
+(function() {
+    'use strict';
+
+    //Load the page Objects
+    let newStackAuthPO = requirePO('newStackAuth');
+    let e2ePageBase = requirePO('e2ePageBase');
+
+    describe('Configuration based feature switches tests: ', function() {
+        let realmName;
+        let realmId;
+        let testApp;
+
+        /**
+         * Setup method. Creates test app then authenticates into the new stack
+         */
+        beforeAll(function() {
+            browser.logger.info('beforeAll spec function - Generating test data and logging in');
+            // Need to return here. beforeAll is completely async, need to return the Promise chain in any before or after functions!
+            // No need to call done() anymore
+            return e2eBase.basicAppSetup(null, 5).then(function(createdApp) {
+                // Set your global objects to use in the test functions
+                testApp = createdApp;
+                realmName = e2eBase.recordBase.apiBase.realm.subdomain;
+                realmId = e2eBase.recordBase.apiBase.realm.id;
+            }).then(function() {
+                // Auth into the new stack
+                return newStackAuthPO.realmLogin(realmName, realmId);
+            }).catch(function(error) {
+                // Global catch that will grab any errors from chain above
+                // Will appropriately fail the beforeAll method so other tests won't run
+                browser.logger.error('Error in beforeAll function:' + JSON.stringify(error));
+                return Promise.reject('Error in beforeAll function:' + JSON.stringify(error));
+            });
+        });
+
+        /**
+         * Before All it block load the app.
+         */
+        beforeEach(function() {
+            // go to the feature switches page
+            return e2ePageBase.loadFSPageInBrowser(realmName);
+        });
+
+        //Verify feature switches heading is visible
+        it('Verify feature switches heading is visible', function() {
+
+            browser.waitForVisible('.mainContent');
+            let element = browser.element('h1');
+            //make sure FS heading is visible
+            expect(element.isVisible()).toBe(true);
+            //verify the heading text
+            expect(element.getText()).toEqual('Feature Switches');
+        });
+
+        //Verify feature switches content is not visible
+        it('Verify feature switches content is not visible', function() {
+
+            browser.waitForVisible('.mainContent');
+            let element = browser.element('.featureSwitchesLoader .featureSwitches');
+            //make sure FS heading is visible
+            expect(element.isVisible()).toBe(false);
+        });
+
+    });
+
+}());
