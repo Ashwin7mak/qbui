5dd96109c1d073948381990ff554c2d5299fdae0
Brandon Stookey <bstookey@quickbase.com>
2017-07-05 11:32:01 -0500

    mc3596 update unit tests

--- ui/client-react/test/components/navItem.unit.spec.js
+++ ui/client-react/test/components/navItem.unit.spec.js
@@ -2,15 +2,22 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
 import NavItem, {__RewireAPI__ as NavItemRewireAPI} from '../../src/components/nav/navItem';
-import {AVAILABLE_ICON_FONTS} from '../../../reuse/client/src/components/icon/icon.js';
+import Icon, {AVAILABLE_ICON_FONTS} from '../../../reuse/client/src/components/icon/icon.js';
+import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import {Link} from 'react-router-dom';
 
 let component;
 let instance;
+let secondaryIcon = 'mockSecondaryIcon';
+let mockEvent = {};
+let mockItem = {
+    name: 'mockName'
+};
+
+const I18nMessageMock = () => <div>test</div>;
 
 let mockFuncs = {
-    getApp() {},
-    showAppCreationDialog() {}
+    onClick() {}
 };
 
 const LinkMock = React.createClass({
@@ -23,10 +30,13 @@ describe('NavItem', () => {
     beforeEach(() => {
         jasmineEnzyme();
         NavItemRewireAPI.__Rewire__('Link', LinkMock);
+        NavItemRewireAPI.__Rewire__('I18nMessage', I18nMessageMock);
+        spyOn(mockFuncs, 'onClick');
     });
 
     afterEach(() => {
         NavItemRewireAPI.__ResetDependency__('Link');
+        NavItemRewireAPI.__ResetDependency__('I18nMessage');
     });
 
     it('will have defaultProps', () => {
@@ -37,5 +47,64 @@ describe('NavItem', () => {
         expect(component).toHaveProp('showToolTip', false);
         expect(component).toHaveProp('icon', 'favicon');
         expect(component).toHaveProp('iconFont', AVAILABLE_ICON_FONTS.DEFAULT);
+    });
+
+    it('will render a heading if isHeading is true with a secondaryIcon', () => {
+        component = mount(<NavItem item={{}}
+                                   isHeading={true}
+                                   secondaryIcon={secondaryIcon} />);
+
+        let li = component.find('li');
+        let icon = component.find(Icon);
+        expect(li).toHaveClassName("heading withSecondary");
+        expect(icon.length).toBe(2);
+
+    });
+
+    it('will render a heading if isHeading is true WITHOUT a secondaryIcon', () => {
+        component = mount(<NavItem item={{}}
+                                   isHeading={true}
+                                   secondaryIcon={false} />);
+
+        let li = component.find('li');
+        let icon = component.find(Icon);
+
+        expect(li).toHaveClassName("heading");
+        expect(icon.length).toBe(1);
+
+    });
+
+    xit('will invoke onClick if heading is clicked on', () => {
+        instance = component.instance();
+        spyOn(instance, 'onHeadingClick');
+
+        component = mount(<NavItem item={{}}
+                                   isHeading={true}
+                                   secondaryIcon={false}
+                                   onClick={mockFuncs.onClick} />);
+
+        let li = component.find('li');
+        let icon = component.find(Icon);
+        icon.simulate('click');
+
+        expect(li.length).toBe(1);
+        expect(instance.onHeadingClick).toHaveBeenCalled();
+        expect(mockFuncs.onClick).toHaveBeenCalledWith(mockEvent);
+    });
+
+    it('will render OverLayTrigger if showToolTip is true and isHeading is false', () => {
+        component = mount(<NavItem isHeading={false}
+                                   showToolTip={true}
+                                   item={mockItem} />);
+
+        expect(component.find(OverlayTrigger)).toBePresent();
+    });
+
+    it('will not render OverLayTrigger if showToolTip is true and isHeading is false', () => {
+        component = shallow(<NavItem isHeading={false}
+                                     showToolTip={false}
+                                     item={mockItem} />);
+
+        expect(component.find(OverlayTrigger)).not.toBePresent();
     });
 });
