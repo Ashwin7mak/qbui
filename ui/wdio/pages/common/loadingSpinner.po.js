191a67e4d65b88e8f22f84149ad2506b4a04c245
lakshmi kamineni <skamineni@quickbase.com>
2017-07-07 13:57:20 -0400

    increased the timeout

--- ui/wdio/pages/common/loadingSpinner.po.js
+++ ui/wdio/pages/common/loadingSpinner.po.js
@@ -13,38 +13,5 @@
                 //Need this to stabilize DOM
                 return browser.pause(e2eConsts.mediumWaitTimeMs);
             }},
-
-        /**
-         * Method for spinner to dissaper in leftNav
-         */
-        waitUntilLeftNavSpinnerGoesAway: {
-            value: function() {
-                //wait until loading screen disappear
-                browser.waitForVisible('.leftNav .loader .spinner', e2eConsts.longWaitTimeMs, true);
-                //Need this to wait for leftNav labels to load
-                return browser.pause(e2eConsts.shortWaitTimeMs);
-            }},
-
-        /**
-         * Method for spinner to dissaper in report content
-         */
-        waitUntilReportLoadingSpinnerGoesAway: {
-            value: function() {
-                //wait until report loading screen disappear
-                browser.waitForVisible('.reportContent .loader .spinner', e2eConsts.longWaitTimeMs, true);
-                //Need this to stabilize DOM
-                return browser.pause(e2eConsts.shortWaitTimeMs);
-            }},
-
-        /**
-         * Method for spinner to dissaper in record content
-         */
-        waitUntilRecordLoadingSpinnerGoesAway: {
-            value: function() {
-                //wait until report loading screen disappear
-                browser.waitForVisible('.recordContainer .loader .spinner', e2eConsts.longWaitTimeMs, true);
-                //Need this to stabilize DOM
-                return browser.pause(e2eConsts.shortWaitTimeMs);
-            }},
     });
 }());
