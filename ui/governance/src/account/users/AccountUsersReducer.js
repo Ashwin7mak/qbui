125f0bc2800dbb003146e4fbda41676cabb5eb0b
Austin Colcord <ajdcolcord@gmail.com>
2017-07-10 16:08:48 -0400

    Installing Reselect.js, using reselect createSelector to cache AccountUsersReducer values

--- ui/governance/src/account/users/AccountUsersReducer.js
+++ ui/governance/src/account/users/AccountUsersReducer.js
@@ -1,8 +1,9 @@
 import * as types from "../../app/actionTypes";
 import GetStatus from "../../common/reducer/RequestStatusReducer";
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
 import _ from "lodash";
 import * as RealmUserAccountFlagConstants from "../../common/constants/RealmUserAccountFlagConstants.js";
+import { createSelector } from 'reselect';
 
 const users = (state = [], action) => {
     switch (action.type) {
@@ -20,29 +21,61 @@ const AccountUsers = combineReducers({
     status: GetStatus
 });
 
+/**
+ * Selector to return the users contained in the account
+ * @param state
+ */
+const usersSelector = state => state.AccountUsers.users;
+
+/**
+ * Selector to return the isFetching status (intended to be used for the initial API call for users)
+ * @param state
+ * @returns {boolean} - True if the users are being fetched, False if finished
+ */
 export const isFetching = (state) => {
     return state.AccountUsers.status.isFetching;
 };
 
 /**
+ * Calculates the number of paid users in the account.
  * Paid users are any users that have access to the app and are not internal Quick Base users
- * @returns {*}
+ * @returns {integer}
  */
-export const getTotalPaidUsers = state => {
-    return _.sumBy(state.AccountUsers.users, user =>  (
-        user.hasAppAccess && !RealmUserAccountFlagConstants.HasAnySystemPermissions(user) && !RealmUserAccountFlagConstants.IsDenied(user) && !RealmUserAccountFlagConstants.IsDeactivated(user) ? 1 : 0));
-};
+export const getTotalPaidUsers = createSelector(
+    usersSelector,
+    users =>
+        _.sumBy(users, user => (
+            user.hasAppAccess
+            && !RealmUserAccountFlagConstants.HasAnySystemPermissions(user)
+            && !RealmUserAccountFlagConstants.IsDenied(user)
+            && !RealmUserAccountFlagConstants.IsDeactivated(user) ? 1 : 0))
+);
 
-export const getTotalDeniedUsers = state => {
-    return _.sumBy(state.AccountUsers.users, user =>  (RealmUserAccountFlagConstants.IsDenied(user) ? 1 : 0));
-};
+/**
+ * Calculates the number of denied users in the account
+ * @returns {integer}
+ */
+export const getTotalDeniedUsers = createSelector(
+    usersSelector,
+    users => _.sumBy(users, user => RealmUserAccountFlagConstants.IsDenied(user) ? 1 : 0)
+);
 
-export const getTotalDeactivatedUsers = state => {
-    return _.sumBy(state.AccountUsers.users, user =>  (RealmUserAccountFlagConstants.IsDeactivated(user) ? 1 : 0));
-};
+/**
+ * Calculates the number of deactivated users in the account
+ * @returns {number}
+ */
+export const getTotalDeactivatedUsers = createSelector(
+    usersSelector,
+    users => _.sumBy(users, user => RealmUserAccountFlagConstants.IsDeactivated(user) ? 1 : 0)
+);
 
-export const getTotalRealmUsers = state => {
-    return _.sumBy(state.AccountUsers.users, user =>  (user.realmDirectoryFlags !== 0 ? 1 : 0));
-};
+/**
+ * Calculates the number of realm users in the account
+ * @returns {number}
+ */
+export const getTotalRealmUsers = createSelector(
+    usersSelector,
+    users => _.sumBy(users, user => user.realmDirectoryFlags !== 0 ? 1 : 0)
+);
 
 export default AccountUsers;
