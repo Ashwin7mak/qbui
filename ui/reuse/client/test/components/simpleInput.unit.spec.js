14c72090cda257ab1f1092eb3eedcb0c9dac02da
nvanselow <nick@nickvanselow.com>
2017-07-11 14:55:34 -0400

    MC-3841 Minor code cleanup

--- ui/reuse/client/test/components/simpleInput.unit.spec.js
+++ ui/reuse/client/test/components/simpleInput.unit.spec.js
@@ -22,31 +22,31 @@ describe('SimpleInput', () => {
     it('will render a SimpleInput component', () => {
         component = shallow(<SimpleInput />);
 
-        let simpleInput = component.find(".simpleInput");
+        const simpleInput = component.find(".simpleInput");
 
         expect(simpleInput).toBePresent();
     });
 
     it('will render a default className for simpleInput', () => {
-        component = shallow(<SimpleInput className="mockSimpleInputClassName"/>);
+        component = shallow(<SimpleInput />);
 
-        let simpleInput = component.find(".simpleInput");
+        const simpleInput = component.find(".simpleInput");
 
         expect(simpleInput).toBePresent();
     });
 
     it('will render a passed in className for simpleInput', () => {
-        component = shallow(<SimpleInput className="mockSimpleInputClassName"/>);
+        component = shallow(<SimpleInput className="mockSimpleInputClassName" />);
 
-        let simpleInput = component.find(".simpleInput .mockSimpleInputClassName");
+        const simpleInput = component.find(".simpleInput .mockSimpleInputClassName");
 
         expect(simpleInput).toBePresent();
     });
 
     it('will render an isInvalid className if there is a validationErrorMessage', () => {
-        component = shallow(<SimpleInput validationErrorMessage="mockValidationError"/>);
+        component = shallow(<SimpleInput validationErrorMessage="mockValidationError" />);
 
-        let isInvalid = component.find(".simpleInput .isInvalid");
+        const isInvalid = component.find(".simpleInput .isInvalid");
 
         expect(isInvalid).toBePresent();
     });
@@ -54,24 +54,24 @@ describe('SimpleInput', () => {
     it('will NOT render an isInvalid className if there is a validationErrorMessage', () => {
         component = shallow(<SimpleInput />);
 
-        let isInvalid = component.find(".simpleInput .isInvalid");
+        const isInvalid = component.find(".simpleInput .isInvalid");
 
         expect(isInvalid).not.toBePresent();
     });
 
     describe('input', () => {
         it('will render a default className for input', () => {
-            component = shallow(<SimpleInput className="mockClassName"/>);
+            component = shallow(<SimpleInput />);
 
-            let input = component.find(".input");
+            const input = component.find(".input");
 
             expect(input).toBePresent();
         });
 
         it('will render a passed in className for input', () => {
-            component = shallow(<SimpleInput className="mockClassName"/>);
+            component = shallow(<SimpleInput className="mockClassName" />);
 
-            let input = component.find(".input .mockClassNameInput");
+            const input = component.find(".input .mockClassNameInput");
 
             expect(input).toBePresent();
         });
@@ -80,16 +80,16 @@ describe('SimpleInput', () => {
             component = mount(<SimpleInput placeholder="mockPlaceHolder"
                                            value="mockValue"/>);
 
-            let input = component.find(".input");
+            const input = component.find(".input");
 
             expect(input).toHaveValue('mockValue');
             expect(input).toHaveProp('placeholder', 'mockPlaceHolder');
         });
 
-        it('will invoke prop on change and will not mask any characters', () => {
-            component = shallow(<SimpleInput onChange={mockPropFuncs.onChange}/>);
+        it('will invoke prop onChange and will not mask any characters', () => {
+            component = shallow(<SimpleInput onChange={mockPropFuncs.onChange} />);
 
-            let input = component.find('.input');
+            const input = component.find('.input');
             input.simulate('change', {target: {value: 'Mock Value'}});
 
             expect(mockPropFuncs.onChange).toHaveBeenCalledWith('Mock Value');
@@ -97,12 +97,12 @@ describe('SimpleInput', () => {
 
         it('will invoke prop on change and will mask characters and only accept an uppercase character', () => {
             component = shallow(<SimpleInput onChange={mockPropFuncs.onChange}
-                                             mask={/^[A-Z]*$/}/>);
+                                             mask={/^[A-Z]*$/} />);
 
             instance = component.instance();
             spyOn(instance, 'onChange').and.callThrough();
 
-            let input = component.find('.input');
+            const input = component.find('.input');
 
             input.simulate('change', {target: {value: 'M'}});
             expect(mockPropFuncs.onChange).toHaveBeenCalledWith('M');
@@ -110,12 +110,12 @@ describe('SimpleInput', () => {
 
         it('will invoke prop on change and will mask characters and will not accept a lowercase character', () => {
             component = shallow(<SimpleInput onChange={mockPropFuncs.onChange}
-                                             mask={/^[A-Z]*$/}/>);
+                                             mask={/^[A-Z]*$/} />);
 
             instance = component.instance();
             spyOn(instance, 'onChange').and.callThrough();
 
-            let input = component.find('.input');
+            const input = component.find('.input');
 
             input.simulate('change', {target: {value: 'm'}});
             expect(mockPropFuncs.onChange).not.toHaveBeenCalled();
@@ -124,7 +124,7 @@ describe('SimpleInput', () => {
         it('will invoke passed in prop onBlur', () => {
             component = shallow(<SimpleInput onBlur={mockPropFuncs.onBlur}/>);
 
-            let input = component.find('.input');
+            const input = component.find('.input');
             input.simulate('blur');
 
             expect(mockPropFuncs.onBlur).toHaveBeenCalled();
@@ -133,7 +133,7 @@ describe('SimpleInput', () => {
         it('will set a maxLength on the input box', () => {
             component = mount(<SimpleInput maxLength={2} />);
 
-            let input = component.find('.input');
+            const input = component.find('.input');
 
             expect(input).toHaveProp('maxLength', 2);
             expect(input.nodes[0].maxLength).toEqual(2);
@@ -142,35 +142,35 @@ describe('SimpleInput', () => {
 
     describe('label', () => {
         it('will render a default className for label', () => {
-            component = shallow(<SimpleInput className="mockClassName"/>);
+            component = shallow(<SimpleInput />);
 
-            let label = component.find(".label");
+            const label = component.find(".label");
 
             expect(label).toBePresent();
         });
 
         it('will render a passed in className for label', () => {
-            component = shallow(<SimpleInput className="mockClassName"/>);
+            component = shallow(<SimpleInput className="mockClassName" />);
 
-            let label = component.find(".label .mockClassNameLabel");
+            const label = component.find(".label .mockClassNameLabel");
 
             expect(label).toBePresent();
         });
 
         it('will render a required * with label if it is required', () => {
             component = shallow(<SimpleInput isRequired={true}
-                                             label="Mock Label"/>);
+                                             label="Mock Label" />);
 
-            let label = component.find(".label");
+            const label = component.find(".label");
 
             expect(label).toHaveText('* Mock Label');
         });
 
         it('will NOT render a required * with label if it is NOT required', () => {
             component = shallow(<SimpleInput isRequired={false}
-                                             label="Mock Label"/>);
+                                             label="Mock Label" />);
 
-            let label = component.find(".label");
+            const label = component.find(".label");
 
             expect(label).toHaveText('Mock Label');
         });
